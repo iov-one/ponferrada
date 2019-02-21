0b25d35663bfff7158fac71aa4fb99d08334fd0e
Albert Andrejev <albert.andrejev@gmail.com>
2019-02-21 13:28:03 +0200

    Common theme variables

---
+++ packages/medulas-react-components/src/theme/variables.d.ts
@@ -0,0 +1,17 @@
+declare module "~/theme/variables" {
+  export const primary: string;
+  export const secondary: string;
+  export const error: string;
+  export const temporaryError: string;
+  export const fontColor: string;
+  export const background: string;
+  export const backgroundPrimary: string;
+  export const buttonLargeFontSize: string;
+  export const extraLightFont: number;
+  export const lightFont: number;
+  export const regularFont: number;
+  export const semiBoldFont: number;
+  export const smallFontSize: string;
+  export const mediumFontSize: string;
+  export const xxlFontSize: string;
+}
