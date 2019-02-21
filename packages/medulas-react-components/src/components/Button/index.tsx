6d006e5d2557ec141cf909a16365b32f8a5f10f7
Albert Andrejev <albert.andrejev@gmail.com>
2019-02-21 18:18:59 +0200

    Muli fonts

--- packages/medulas-react-components/src/components/Button/index.tsx
+++ packages/medulas-react-components/src/components/Button/index.tsx
@@ -1,7 +1,15 @@
-import React from 'react';
-import MuiButton from "@material-ui/core/Button";
+import React from "react";
+import { makeStyles } from "@material-ui/styles";
+import MuiButton, { ButtonProps } from "@material-ui/core/Button";
 
+const useStyles = makeStyles({
+  root: {
+    fontFamily: "'Muli', sans-serif",
+  },
+});
 
-export default function Button() {
-  return <MuiButton>Hook</MuiButton>;
+export default function Button({ children }: ButtonProps) {
+  //const classes = useStyles();
+
+  return <MuiButton>{children}</MuiButton>;
 }
--- packages/medulas-react-components/src/utils/storybook/index.tsx
+++ packages/medulas-react-components/src/utils/storybook/index.tsx
@@ -1,16 +1,8 @@
+import "../bootstrap";
 import { ThemeProvider, makeStyles } from "@material-ui/styles";
 import * as React from "react";
 import theme from "../../theme/mui";
 import { globalStyles } from "../../theme/globalStyles";
-
-const storyBookStyles = makeStyles({
-  '@global': {
-    "body>div:nth-child(3)": {
-      display: "flex",
-      minHeight: "100%",
-    }
-  }
-});
 
 interface Props {
   readonly children: React.ReactNode;
@@ -18,11 +10,6 @@ interface Props {
 
 export const Storybook = ({ children }: Props) => {
   globalStyles();
-  storyBookStyles();
 
-  return (
-    <ThemeProvider theme={theme}>
-      {children}
-    </ThemeProvider>
-  );
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
 };
