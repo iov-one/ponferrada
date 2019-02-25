8d1564a555561f390f3a63e46beb887fe4b058b3
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-02-26 00:37:26 +0100

    Adding prettier and eslint to all packages

--- packages/medulas-react-components/src/components/Button/index.stories.tsx
+++ packages/medulas-react-components/src/components/Button/index.stories.tsx
@@ -1,19 +1,11 @@
-import { action } from '@storybook/addon-actions'
-import { storiesOf } from '@storybook/react'
-import React from 'react'
-import Button from './index'
+import { action } from "@storybook/addon-actions";
+import { storiesOf } from "@storybook/react";
+import React from "react";
+import Button from "./index";
 
+storiesOf("Button", module)
+  .add("with text", () => <Button onClick={action("clicked")}>Buttoon</Button>)
 
-storiesOf('Button', module)
+  .add("with some emoji", () => <Button onClick={action("clicked")}>😀 😎 👍 💯</Button>)
 
-  .add('with text', () => (
-      <Button onClick={action('clicked')}>Buttoon</Button>
-  ))
-
-  .add('with some emoji', () => (
-      <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>
-  ))
-
-  .add('with a theme provider', () => (
-    <Button onClick={action('clicked')}>Button</Button>
-  ))
+  .add("with a theme provider", () => <Button onClick={action("clicked")}>Button</Button>);
