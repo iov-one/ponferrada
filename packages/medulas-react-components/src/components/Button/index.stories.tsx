283b2370bd7c23a1492b0269a1bd6abcecdeffa2
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-02-26 10:41:10 +0100

    Adding lint script to packages' projects

--- packages/medulas-react-components/src/components/Button/index.stories.tsx
+++ packages/medulas-react-components/src/components/Button/index.stories.tsx
@@ -6,6 +6,12 @@ import Button from "./index";
 storiesOf("Button", module)
   .add("with text", () => <Button onClick={action("clicked")}>Buttoon</Button>)
 
-  .add("with some emoji", () => <Button onClick={action("clicked")}>😀 😎 👍 💯</Button>)
+  .add("with some emoji", () => (
+    <Button onClick={action("clicked")}>
+      <span role="img" aria-label="Express mood">
+        😀 😎 👍 💯
+      </span>
+    </Button>
+  ))
 
   .add("with a theme provider", () => <Button onClick={action("clicked")}>Button</Button>);
