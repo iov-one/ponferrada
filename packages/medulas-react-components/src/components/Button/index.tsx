a9a2a693dc0a77a197d332294ceea950e8071de8
Albert Andrejev <albert.andrejev@gmail.com>
2019-02-26 12:07:02 +0200

    Button label font face

--- packages/medulas-react-components/src/components/Button/index.tsx
+++ packages/medulas-react-components/src/components/Button/index.tsx
@@ -3,13 +3,13 @@ import { makeStyles } from "@material-ui/styles";
 import MuiButton, { ButtonProps } from "@material-ui/core/Button";
 
 const useStyles = makeStyles({
-  root: {
+  label: {
     fontFamily: "'Muli', sans-serif",
   },
 });
 
 export default function Button({ children }: ButtonProps) {
-  //const classes = useStyles();
+  const classes = useStyles();
 
-  return <MuiButton>{children}</MuiButton>;
+  return <MuiButton classes={classes}>{children}</MuiButton>;
 }
