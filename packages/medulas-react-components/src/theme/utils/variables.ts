49edc1fe8848f6aa145669797a4f9ddad0584d09
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-02-27 17:39:12 +0100

    Removing bootstrap.ts

---
+++ packages/medulas-react-components/src/theme/utils/variables.ts
@@ -0,0 +1,9 @@
+export const error = "#F05956";
+export const background = "#ffffff";
+export const backgroundPrimary = "#f5f7f9";
+export const buttonLargeFontSize = "12px";
+export const smallFontSize = "14px";
+export const mediumFontSize = "16px";
+export const xxlFontSize = "32px";
+export const extraLightFont = 200;
+export const lightFont = 300;
