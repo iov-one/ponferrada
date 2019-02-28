68e3b137219905ed87613b646b30585a72331c83
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-02-28 21:56:16 +0100

    Adding types and messages

---
+++ packages/sanes-chrome-extension/extension/utils/messages.ts
@@ -0,0 +1,5 @@
+// WTC means "web to content"
+export const WTC_MSG_HELLO = "wtc_HELLO";
+
+// CTB means "content to backgrouns"
+export const CTB_MSG_HELLO = "ctb_HELLO";
---
+++ packages/sanes-chrome-extension/extension/utils/types.ts
@@ -0,0 +1,4 @@
+export interface MsgToBackground {
+  readonly msg: string;
+  readonly data: string | undefined;
+}
