51a1ab88b21901a871abf25e9ff3a8d234dc2b0d
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-03-02 12:59:38 +0100

    Updating oza-react-scripts to 2.1.5 target version

--- packages/oza-react-scripts/fixtures/kitchensink/src/features/webpack/SvgComponent.js
+++ packages/oza-react-scripts/fixtures/kitchensink/src/features/webpack/SvgComponent.js
@@ -8,10 +8,4 @@
 import React from 'react';
 import { ReactComponent as Logo } from './assets/logo.svg';
 
-export default () => {
-  return <Logo id="feature-svg-component" />;
-};
-
-export const SvgComponentWithRef = React.forwardRef((props, ref) => (
-  <Logo id="feature-svg-component-with-ref" ref={ref} />
-));
+export default () => <Logo id="feature-svg-component" />;
--- packages/oza-react-scripts/fixtures/kitchensink/src/features/webpack/SvgComponent.test.js
+++ packages/oza-react-scripts/fixtures/kitchensink/src/features/webpack/SvgComponent.test.js
@@ -7,21 +7,12 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SvgComponent, { SvgComponentWithRef } from './SvgComponent';
+import SvgComponent from './SvgComponent';
 
 describe('svg component', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<SvgComponent />, div);
     expect(div.textContent).toBe('logo.svg');
-  });
-
-  it('svg root element equals the passed ref', () => {
-    const div = document.createElement('div');
-    const someRef = React.createRef();
-    ReactDOM.render(<SvgComponentWithRef ref={someRef} />, div);
-    const svgElement = div.getElementsByTagName('svg');
-    expect(svgElement).toHaveLength(1);
-    expect(svgElement[0]).toBe(someRef.current);
   });
 });
