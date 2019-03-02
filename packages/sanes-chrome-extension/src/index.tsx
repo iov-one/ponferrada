c13ce332f2d112c7352e84958665c8cd7b30e99c
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-03-02 15:55:32 +0100

    Enabling redux HMR properly

--- packages/sanes-chrome-extension/src/index.tsx
+++ packages/sanes-chrome-extension/src/index.tsx
@@ -1,32 +1,33 @@
+import { ConnectedRouter } from 'connected-react-router';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { Store } from 'webext-redux';
-import App from './routes/home/container/App';
+import Route from './routes';
+import { history } from './store/reducers';
+import { HOME_ROUTE } from './routes/paths';
 
 const store = new Store();
 
-console.log(store);
-
 const rootEl = document.getElementById('root');
-console.log('rootEl' + rootEl);
 
-try {
-  const moe = store.ready().then(() => {
-    console.log('Vamos que nos vamos');
-  });
-  console.log(moe);
-} catch (err) {
-  console.log('Hola');
-  console.log(err);
-}
+store.ready().then(() => {
+  history.push(HOME_ROUTE);
 
-console.log('Done!!!');
-ReactDOM.render(<App />, rootEl);
-console.log('Done 2!!!');
+  const Root = (): JSX.Element => (
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <Route />
+      </ConnectedRouter>
+    </Provider>
+  );
 
-if (module.hot) {
-  module.hot.accept('./routes', () => {
-    const NextApp = require('./routes').default;
-    ReactDOM.render(<NextApp />, rootEl);
-  });
-}
+  ReactDOM.render(<Root />, rootEl);
+
+  if (module.hot) {
+    module.hot.accept('./routes', () => {
+      const NextApp = require('./routes').default;
+      ReactDOM.render(<NextApp />, rootEl);
+    });
+  }
+});
