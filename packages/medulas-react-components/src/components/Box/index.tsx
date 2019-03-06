2d115d429595189eb9c5119e4b4ef27c85dca745
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-03-06 17:54:56 +0100

    Adding nasty casting while proper Box types are generated

--- packages/medulas-react-components/src/components/Box/index.tsx
+++ packages/medulas-react-components/src/components/Box/index.tsx
@@ -1,12 +1,16 @@
-import { unstable_Box as Box } from '@material-ui/core/Box';
+import * as React from 'react';
+import { unstable_Box as MuiBox, BoxProps } from '@material-ui/core/Box';
 
-interface Props {
+interface Props extends BoxProps {
   readonly children: React.ReactNode;
-  //readonly container: Boolean;
 }
 
-export const Grid = ({ children }: Props): JSX.Element => {
-  return <Box display="flex">{children}</Box>;
+// TODO fix once the proper BoxProps have been updated
+// See: https://github.com/mui-org/material-ui/blob/next/packages/material-ui/src/Box/Box.d.ts
+const IovBox = MuiBox as any; // eslint-disable-line @typescript-eslint/no-explicit-any
+
+export const Box = ({ children }: Props): JSX.Element => {
+  return <IovBox display="flex">{children}</IovBox>;
 };
 
-export default Grid;
+export default Box;
