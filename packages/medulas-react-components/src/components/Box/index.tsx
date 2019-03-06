d2864b2b5a8fb636d7951da9f9815bf5eead6f1e
Albert Andrejev <albert.andrejev@gmail.com>
2019-03-06 17:42:20 +0200

    Grid using Box

--- packages/medulas-react-components/src/components/Box/index.tsx
+++ packages/medulas-react-components/src/components/Box/index.tsx
@@ -1,3 +1,12 @@
 import { unstable_Box as Box } from '@material-ui/core/Box';
 
-export default Box;
+interface Props {
+  readonly children: React.ReactNode;
+  //readonly container: Boolean;
+}
+
+export const Grid = ({ children }: Props): JSX.Element => {
+  return <Box display="flex">{children}</Box>;
+};
+
+export default Grid;
