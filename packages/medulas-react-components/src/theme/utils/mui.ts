30d58ca7b9038498ec672491b5cbeb2411a07556
Albert Andrejev <albert.andrejev@gmail.com>
2019-03-07 10:40:40 +0200

    Remove unused variables

--- packages/medulas-react-components/src/theme/utils/mui.ts
+++ packages/medulas-react-components/src/theme/utils/mui.ts
@@ -1,6 +1,6 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 import { ThemeOptions } from '@material-ui/core/styles/createMuiTheme';
-import { lightFont, white, secondaryColor, semiBoldFont } from './variables';
+import { lightFont, white, secondaryColor } from './variables';
 
 const themeObject: ThemeOptions = {
   palette: {
--- packages/medulas-react-components/src/theme/utils/variables.ts
+++ packages/medulas-react-components/src/theme/utils/variables.ts
@@ -1,4 +1,3 @@
 export const white = '#ffffff';
 export const secondaryColor = '#6F749A';
 export const lightFont = 300;
-export const semiBoldFont = 600;
