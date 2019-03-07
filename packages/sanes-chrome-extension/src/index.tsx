5a8c6fe83012f082f520f745773c6999c47d4f6a
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-03-07 12:38:26 +0100

    Using MedulasThemeProvider in chrome extension

--- packages/sanes-chrome-extension/src/index.tsx
+++ packages/sanes-chrome-extension/src/index.tsx
@@ -1,25 +1,28 @@
 import 'medulas-react-components/lib/utils/bootstrap';
 import { ConnectedRouter } from 'connected-react-router';
+import MedulasThemeProvider from 'medulas-react-components/lib/theme/MedulasThemeProvider';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { Store } from 'webext-redux';
 import Route from './routes';
 import { history } from './store/reducers';
-import { HOME_ROUTE } from './routes/paths';
+import { WELCOME_ROUTE } from './routes/paths';
 
 const store = new Store();
 
 const rootEl = document.getElementById('root');
 
 store.ready().then(() => {
-  history.push(HOME_ROUTE);
+  history.push(WELCOME_ROUTE);
 
   const Root = (): JSX.Element => (
     <Provider store={store}>
-      <ConnectedRouter history={history}>
-        <Route />
-      </ConnectedRouter>
+      <MedulasThemeProvider injectGlobalStyle injectFonts>
+        <ConnectedRouter history={history}>
+          <Route />
+        </ConnectedRouter>
+      </MedulasThemeProvider>
     </Provider>
   );
 
