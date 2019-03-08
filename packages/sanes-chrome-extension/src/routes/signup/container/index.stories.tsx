5ae78113560835c9796fb3e0c5673031f5ef716a
LucasIsasmendi <lucas.isasmendi@gmail.com>
2019-03-08 14:05:54 +0100

    Use form with hooks

--- packages/sanes-chrome-extension/src/routes/signup/container/index.stories.tsx
+++ packages/sanes-chrome-extension/src/routes/signup/container/index.stories.tsx
@@ -3,7 +3,7 @@ import { Storybook } from 'medulas-react-components/lib/utils/storybook';
 import React from 'react';
 import Layout from './index';
 
-storiesOf('Routes/Signup', module).add('Signup page', () => (
+storiesOf('/Routes Signup', module).add('Signup page', () => (
   <Storybook>
     <Layout />
   </Storybook>
