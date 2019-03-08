a807a643cb81808eef109f97ff5de570c6f1973b
Albert Andrejev <albert.andrejev@gmail.com>
2019-03-08 10:40:22 +0200

    Welcome screen layout

--- packages/sanes-chrome-extension/src/routes/welcome/components/index.tsx
+++ packages/sanes-chrome-extension/src/routes/welcome/components/index.tsx
@@ -1,15 +1,39 @@
 import { Button } from 'medulas-react-components/lib/components/Button';
 import { Typography } from 'medulas-react-components/lib/components/Typography';
 import { Block } from 'medulas-react-components/lib/components/Block';
+import { Image } from 'medulas-react-components/lib/components/Image';
+import iovLogo from '../assets/iov-logo.png';
 import * as React from 'react';
 
 const Layout = (): JSX.Element => (
-  <Block padding={4}>
-    <Typography color="primary" inline>
+  <Block paddingRight={4} paddingLeft={4} paddingTop={4}>
+    <Typography color="primary" variant="h4" inline>
       Welcome
     </Typography>
-    &nbsp;<Typography inline>to your IOV manager</Typography>
-    <Button type="contained">Hii Albert</Button>
+    <Typography variant="h4" inline>
+      {' '}
+      to your IOV manager
+    </Typography>
+    <Block padding={2} marginTop={4} marginBottom={1}>
+      <Typography variant="body1" inline>
+        This plugin lets you manage all your accounts in one place.
+      </Typography>
+      <Block marginTop={2} />
+      <Button type="contained" fullWidth>
+        Log in
+      </Button>
+      <Block marginTop={2} />
+      <Button type="contained" fullWidth>
+        New account
+      </Button>
+      <Block marginTop={2} />
+      <Button type="contained" fullWidth>
+        Import account
+      </Button>
+    </Block>
+    <Block textAlign="center" marginBottom={1}>
+      <Image src={iovLogo} alt="IOV logo" />
+    </Block>
   </Block>
 );
 
