f5d24f8e1f2d1495b5a00e19c254a4f9c8a84369
LucasIsasmendi <lucas.isasmendi@gmail.com>
2019-03-08 14:05:54 +0100

    Use form with hooks

--- packages/sanes-chrome-extension/src/routes/signup/container/index.stories.tsx
+++ packages/sanes-chrome-extension/src/routes/signup/container/index.stories.tsx
@@ -3,7 +3,7 @@ import { Storybook } from 'medulas-react-components/lib/utils/storybook';
 import React from 'react';
 import Signup from './index';
 
-storiesOf('Routes/Signup', module).add('Signup page', () => (
+storiesOf('/Routes Signup', module).add('Signup page', () => (
   <Storybook>
     <Signup />
   </Storybook>
--- packages/sanes-chrome-extension/src/routes/signup/container/index.tsx
+++ packages/sanes-chrome-extension/src/routes/signup/container/index.tsx
@@ -1,5 +1,33 @@
 import * as React from 'react';
+import { useForm, useField } from 'react-final-form-hooks';
+import { Button } from 'medulas-react-components/lib/components/Button';
 
-const Signup = (): JSX.Element => <div>This is the signup route</div>;
+const onSubmit = async (values: object) => {
+  window.alert(JSON.stringify(values));
+};
 
+const Signup = (): JSX.Element => {
+  const { form, handleSubmit, pristine, submitting } = useForm({
+    onSubmit,
+  });
+
+  const uniqueIdentifier = useField('uniqueIdentifier', form);
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Unique Identifier</label>
+          <input {...uniqueIdentifier.input} placeholder="Unique Identifier" />
+          {uniqueIdentifier.meta.touched && uniqueIdentifier.meta.error && (
+            <span>{uniqueIdentifier.meta.error}</span>
+          )}
+        </div>
+        <Button type="submit" disabled={pristine || submitting}>
+          Submit
+        </Button>
+      </form>
+    </div>
+  );
+};
 export default Signup;
