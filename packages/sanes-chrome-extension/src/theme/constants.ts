6afca8302d4591f91f11ee9bc2934ec5e14da9a5
Albert Andrejev <albert.andrejev@gmail.com>
2019-03-11 10:24:17 +0200

    Extension's global styles refactoring.

---
+++ packages/sanes-chrome-extension/src/theme/constants.ts
@@ -0,0 +1,2 @@
+export const EXTENSION_WIDTH = 350;
+export const EXTENSION_HEIGHT = 500;
--- packages/sanes-chrome-extension/src/theme/globalStyles.ts
+++ packages/sanes-chrome-extension/src/theme/globalStyles.ts
@@ -1,11 +1,12 @@
 import { makeStyles } from '@material-ui/styles';
 import theme from 'medulas-react-components/lib/theme/utils/mui';
+import { EXTENSION_WIDTH, EXTENSION_HEIGHT } from './constants';
 
 export const globalStyles = makeStyles({
   '@global': {
     'html, body': {
-      height: '500px',
-      width: '350px',
+      width: `${EXTENSION_WIDTH}px`,
+      height: `${EXTENSION_HEIGHT}px`,
       '-ms-overflow-style': '-ms-autohiding-scrollbar',
     },
     body: {
