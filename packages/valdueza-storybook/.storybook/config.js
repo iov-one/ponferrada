33fc469006c345c790006f43fcd7a894ae3e7e6e
Albert Andrejev <albert.andrejev@gmail.com>
2019-03-11 22:05:01 +0200

    Implement JEST for storybook

--- packages/valdueza-storybook/.storybook/config.js
+++ packages/valdueza-storybook/.storybook/config.js
@@ -1,6 +1,8 @@
 import { configure } from '@storybook/react';
+import requireContext from 'require-context.macro';
 
-const req = require.context('../../../packages', true, /\.stories\.((js|ts)x?)$/);
+
+const req = requireContext('../../../packages', true, /\.stories\.((js|ts)x?)$/);
 
 function loadStories() {
   req.keys().forEach((filename) => req(filename));
