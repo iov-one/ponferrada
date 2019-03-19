99b6e9bcbb4645787ff089764f25573d6ad2ad18
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-03-19 12:44:56 +0100

    Fix validate function returning undefined

--- packages/medulas-react-components/src/components/forms/TextFieldForm/index.tsx
+++ packages/medulas-react-components/src/components/forms/TextFieldForm/index.tsx
@@ -1,18 +1,22 @@
 import * as React from 'react';
 import MuiTextField, { TextFieldProps } from '@material-ui/core/TextField';
 
-import { FormApi, FieldSubscription } from 'final-form';
+import { FormApi, FieldSubscription, FieldState } from 'final-form';
+import { NonFunctionProperties } from 'react-final-form-hooks';
 
 interface InnerProps {
   name: string;
   form: FormApi;
+  meta: NonFunctionProperties<FieldState>;
   subscription?: FieldSubscription;
 }
 
 type Props = InnerProps & TextFieldProps;
 
-const TextFieldForm = ({ ...restProps }: Props): JSX.Element => {
-  return <MuiTextField {...restProps} />;
+const TextFieldForm = ({ meta, ...restProps }: Props): JSX.Element => {
+  const error = meta.error && (meta.touched || !meta.pristine);
+
+  return <MuiTextField error={error} {...restProps} />;
 };
 
 export default TextFieldForm;
