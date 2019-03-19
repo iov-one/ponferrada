568eab427e17c57fb9aba4a0d36738861da70456
Albert Andrejev <albert.andrejev@gmail.com>
2019-03-19 21:46:05 +0200

    Stories path update

--- packages/sanes-chrome-extension/src/routes/welcome/container/index.stories.tsx
+++ packages/sanes-chrome-extension/src/routes/welcome/container/index.stories.tsx
@@ -3,7 +3,7 @@ import { Storybook } from 'medulas-react-components/lib/utils/storybook';
 import React from 'react';
 import Layout from './index';
 
-storiesOf('/Routes Welcome', module).add('Welcome page', () => (
+storiesOf('Routes/Welcome', module).add('Welcome page', () => (
   <Storybook>
     <Layout />
   </Storybook>
