1ac0c33303598c80e7fd7fca4811811c89cf4a3d
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-03-19 12:44:56 +0100

    Fix validate function returning undefined

--- packages/medulas-react-components/src/components/forms/Form/index.stories.tsx
+++ packages/medulas-react-components/src/components/forms/Form/index.stories.tsx
@@ -1,31 +1,61 @@
 import { Button } from '@material-ui/core';
 import { storiesOf } from '@storybook/react';
 import * as React from 'react';
-import Form, { useForm, useField } from './index';
 import TextFieldForm from '../TextFieldForm';
+import { Storybook } from '../../../utils/storybook';
+import Form, { useForm, useField } from './index';
 
 const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms)); // eslint-disable-line
 
 const onSubmit = async (values: object): Promise<void> => {
   // eslint-disable-line
-  await sleep(300);
-  window.alert(JSON.stringify(values));
+  console.log('Simulate before');
+  await sleep(7000);
+  console.log(values);
+  console.log('Simulate after');
 };
+
+const FIELD_NAME = 'uniqueIdentifier';
 
 // eslint-disable-next-line
 const validate = (values: any): object => {
-  let errors = {
-    uniqueIdentifier: '',
-  };
+  let errors = {};
   if (!values.uniqueIdentifier) {
-    errors.uniqueIdentifier = 'Required';
+    errors[FIELD_NAME] = 'Required';
   } else if (values.uniqueIdentifier.length <= 4) {
-    errors.uniqueIdentifier = 'Must be at least 4 chars';
+    errors[FIELD_NAME] = 'Must be at least 4 chars';
   }
   return errors;
 };
 
-const FIELD_NAME = 'test';
+const FormStory = (): JSX.Element => {
+  const { form, handleSubmit, values, pristine, submitting } = useForm({
+    onSubmit,
+    validate,
+  });
+
+  const { input, meta } = useField(FIELD_NAME, form);
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <TextFieldForm
+        label="Unique Identifier"
+        placeholder="Unique Identifier"
+        margin="normal"
+        variant="standard"
+        value={input.value}
+        onChange={input.onChange}
+        form={form}
+        name={input.name}
+        meta={meta}
+      />
+      <Button type="submit" disabled={pristine || submitting}>
+        Submit
+      </Button>
+      <pre>{JSON.stringify(values, undefined, 2)}</pre>
+    </Form>
+  );
+};
 
 storiesOf('Components /forms', module).add('Form', () => (
   <Storybook>
