897321c0ec7dee8f9c42aba88f440e6dbe80e252
LucasIsasmendi <lucas.isasmendi@gmail.com>
2019-03-20 13:31:25 +0100

    Add profile to main reducer

--- packages/sanes-chrome-extension/src/store/reducers/index.ts
+++ packages/sanes-chrome-extension/src/store/reducers/index.ts
@@ -3,6 +3,7 @@ import { connectRouter } from 'connected-react-router';
 import { History } from 'history';
 import { combineReducers } from 'redux';
 import { StateType } from 'typesafe-actions';
+import { ProfileActions, profileReducer } from '../profile/reducer';
 
 export const history = createBrowserHistory();
 
@@ -10,8 +11,10 @@ export const history = createBrowserHistory();
 const createRootReducer = (history: History) =>
   combineReducers({
     router: connectRouter(history),
+    profile: profileReducer,
   });
 
 export const reducer = createRootReducer(history);
 
+export type RootActions = ProfileActions;
 export type RootState = StateType<ReturnType<typeof createRootReducer>>;
