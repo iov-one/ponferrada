563e4d9dd7635b22afefc6a7308e678a5f81359b
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-03-20 17:12:23 +0100

    Adding e2e welcome test

---
+++ packages/sanes-chrome-extension/src/routes/welcome/container/index.e2e.spec.tsx
@@ -0,0 +1,46 @@
+import puppeteer, { Browser, Page } from 'puppeteer';
+import { EXTENSION_HEIGHT, EXTENSION_WIDTH } from '../../../theme/constants';
+import { WELCOME_ROUTE } from '../../paths';
+
+describe('DOM > Welcome route', () => {
+  let browser: Browser;
+
+  beforeEach(async () => {
+    const CRX_PATH = require('path').join(__dirname, '../../../../build');
+    browser = await puppeteer.launch({
+      headless: false,
+      devtools: true,
+      args: [
+        `--disable-extensions-except=${CRX_PATH}`,
+        `--load-extension=${CRX_PATH}`,
+      ],
+      defaultViewport: {
+        width: EXTENSION_WIDTH,
+        height: EXTENSION_HEIGHT,
+      },
+    });
+  });
+
+  afterEach(async () => {
+    await browser.close();
+  });
+
+  it('loads correctly', async () => {
+    const page: Page = await browser.newPage();
+    await page.goto(
+      'chrome-extension://lcfnnoggdagjbeoefdfgfpliobcablom/index.html'
+    );
+
+    const inner = await page.evaluate((id: string) => {
+      const element = document.getElementById(id);
+
+      if (!element) {
+        return undefined;
+      }
+
+      return element.id;
+    }, WELCOME_ROUTE);
+
+    expect(inner).toBe(WELCOME_ROUTE);
+  }, 45000);
+});
