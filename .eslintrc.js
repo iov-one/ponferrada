e69b5d337ef11d97cd146016b183e158ef1d737e
Albert Andrejev <albert.andrejev@gmail.com>
2019-03-21 10:59:42 +0200

    eslint configuration update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,22 @@
 module.exports = {
+  //https://codequs.com/p/rk7e07UBV/using-eslint-and-prettier-in-a-typescript-project
   parser: '@typescript-eslint/parser',
   extends: [
-      'plugin:@typescript-eslint/recommended',
-      'react-app',
-      'plugin:prettier/recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
-  plugins: ['@typescript-eslint', 'react'],
-  rules: {
-    "indent": "off",
-    "@typescript-eslint/indent": ["error", 2]
+  parserOptions: {
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true, // Allows for the parsing of JSX
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
+    },
   },
 };
