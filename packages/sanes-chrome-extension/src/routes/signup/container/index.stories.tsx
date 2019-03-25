a561a83f32129419ac3a7d32ec61f1a4721ce850
Albert Andrejev <albert.andrejev@gmail.com>
2019-03-25 18:17:59 +0200

    Signup sotries update

--- packages/sanes-chrome-extension/src/routes/signup/container/index.stories.tsx
+++ packages/sanes-chrome-extension/src/routes/signup/container/index.stories.tsx
@@ -1,10 +1,17 @@
 import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
 import { Storybook } from 'medulas-react-components/lib/utils/storybook';
 import React from 'react';
-import Layout from './index';
+import Layout from '../components';
 
-storiesOf('Routes/Signup', module).add('Signup page', () => (
-  <Storybook>
-    <Layout />
-  </Storybook>
-));
+storiesOf('Routes/Signup', module)
+  .add('New account page', () => (
+    <Storybook>
+      <Layout step="first" setStep={action('setStep')} />
+    </Storybook>
+  ))
+  .add('Recovery Phrase page', () => (
+    <Storybook>
+      <Layout step="second" setStep={action('setStep')} />
+    </Storybook>
+  ));
