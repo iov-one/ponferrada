03514397f5fd100c54d4726b2d5c8e77ac0035f2
Albert Andrejev <albert.andrejev@gmail.com>
2019-03-25 11:44:16 +0200

    Block component props update

--- packages/medulas-react-components/src/components/Block/index.tsx
+++ packages/medulas-react-components/src/components/Block/index.tsx
@@ -5,6 +5,60 @@ import { SizingBreakpoint } from '../Grid';
 // TODO: Remove those props after BoxProps will be properly implemented.
 interface Props extends BoxProps {
   readonly children?: React.ReactNode;
+  readonly display?: string;
+  //Flexbox Props
+  readonly flexDirection?:
+    | 'row'
+    | 'row-reverse'
+    | 'column'
+    | 'column-reverse'
+    | 'initial'
+    | 'inherit';
+  readonly flexWrap?:
+    | 'nowrap'
+    | 'wrap'
+    | 'wrap-reverse'
+    | 'initial'
+    | 'inherit';
+  readonly justifyContent?:
+    | 'flex-start'
+    | 'flex-end'
+    | 'center'
+    | 'space-between'
+    | 'space-around'
+    | 'initial'
+    | 'inherit';
+  readonly alignItems?:
+    | 'stretch'
+    | 'center'
+    | 'flex-start'
+    | 'flex-end'
+    | 'baseline'
+    | 'initial'
+    | 'inherit';
+  readonly alignContent?:
+    | 'stretch'
+    | 'center'
+    | 'flex-start'
+    | 'flex-end'
+    | 'space-between'
+    | 'space-around'
+    | 'initial'
+    | 'inherit';
+  readonly order?: number;
+  readonly flexGrow?: number;
+  readonly flexBasis?: string | number;
+  readonly flexShrink?: number;
+  readonly flex?: string;
+  readonly alignSelf?:
+    | 'auto'
+    | 'stretch'
+    | 'center'
+    | 'flex-start'
+    | 'flex-end'
+    | 'baseline'
+    | 'initial'
+    | 'inherit';
   //Sizing Props
   readonly width?: string | number | SizingBreakpoint;
   readonly minWidth?: string | number | SizingBreakpoint;
@@ -30,8 +84,16 @@ interface Props extends BoxProps {
 // See: https://github.com/mui-org/material-ui/blob/next/packages/material-ui/src/Box/Box.d.ts
 const IovBlock = Box as any; // eslint-disable-line @typescript-eslint/no-explicit-any
 
-const Block = ({ children, ...restProps }: Props): JSX.Element => {
-  return <IovBlock {...restProps}>{children}</IovBlock>;
+const Block = ({
+  children,
+  display = 'block',
+  ...restProps
+}: Props): JSX.Element => {
+  return (
+    <IovBlock display={display} {...restProps}>
+      {children}
+    </IovBlock>
+  );
 };
 
 export default Block;
