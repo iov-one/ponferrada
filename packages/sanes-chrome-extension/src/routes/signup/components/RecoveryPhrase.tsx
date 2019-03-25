ef3fe051736b5882538442ba7c4b2d3ef34ba3a0
Albert Andrejev <albert.andrejev@gmail.com>
2019-03-25 18:17:25 +0200

    recovery phrase view

--- packages/sanes-chrome-extension/src/routes/signup/components/RecoveryPhrase.tsx
+++ packages/sanes-chrome-extension/src/routes/signup/components/RecoveryPhrase.tsx
@@ -16,11 +16,25 @@ const NewAccount = ({ setStep }: StepProps): JSX.Element => {
       paddingTop={2}
     >
       <Typography color="primary" variant="h4" inline>
-        New
+        Recovery
       </Typography>
       <Typography variant="h4" inline>
-        {' Account'}
+        {' Phrase'}
       </Typography>
+      <Block padding={2} marginTop={3} marginBottom={6}>
+        <Block
+          padding={2}
+          border={1}
+          borderColor="grey.300"
+          borderRadius={5}
+          bgcolor="grey.300"
+        >
+          <Typography variant="body1" inline>
+            organ wheat manage mirror wish truly tool trumpet since equip flight
+            bracket
+          </Typography>
+        </Block>
+      </Block>
       <Block display="flex" justifyContent="flex-end">
         <Button type="submit" onClick={() => setStep('first')}>
           Create
