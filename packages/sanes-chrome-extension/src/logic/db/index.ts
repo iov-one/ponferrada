942409bfa17fab182d50f2e788420279be6178cc
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-03-26 08:35:43 +0100

    Reverting to memdown

--- packages/sanes-chrome-extension/src/logic/db/index.ts
+++ packages/sanes-chrome-extension/src/logic/db/index.ts
@@ -1,6 +1,6 @@
 import { AbstractLevelDOWN } from 'abstract-leveldown';
 import levelup, { LevelUp } from 'levelup';
-import leveldown, { LevelDown } from 'leveldown';
+import MemDownConstructor from 'memdown';
 
 export type DB<K, V> = LevelUp<AbstractLevelDOWN<K, V>>;
 export type StringDB = DB<string, string>;
@@ -8,24 +8,27 @@ export type StringDB = DB<string, string>;
 // This was reporting jest as a browser....
 // const isBrowser = () => typeof window !== "undefined" && typeof window.document !== "undefined";
 const isNode = (): boolean =>
-  typeof process === 'object' && !(process as NodeJS.process).browser;
+  typeof process === 'object' && !(process as any).browser;
 const isBrowser = (): boolean => !isNode();
 
-export function createMemDb(name: string): LevelUp<LevelDown> {
-  return levelup(leveldown('memdb/' + name));
+export function createMemDb(): StringDB {
+  return levelup(MemDownConstructor<string, string>());
 }
 
 export function createBrowserDb(name: string): StringDB {
-  const encode = require('encoding-down');
-  const leveljs = require('level-js');
+  import encode = require('encoding-down');
+  import leveljs = require('level-js');
   return levelup(encode(leveljs(name)));
 }
 
 // placeholder to be read from configuration later
-export const createDb = (name: string): DB<K, V> =>
-  isBrowser() ? createBrowserDb(name) : createMemDb(name);
+export const createDb = (name: string): StringDB =>
+  isBrowser() ? createBrowserDb(name) : createMemDb();
 
-const isNotFoundError = (err: any): boolean => err && err.notFound;
+interface TypeError {
+  readonly notFound: boolean;
+}
+const isNotFoundError = (err: TypeError): boolean => err && err.notFound;
 
 export async function hasDbKey(db: StringDB, key: string): Promise<boolean> {
   try {
