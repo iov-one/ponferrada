03e2e86fbf38689534eccb925e70e4ca39d6b616
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-03-27 13:31:21 +0100

    Adding Account entity

--- packages/sanes-chrome-extension/src/logic/account/index.ts
+++ packages/sanes-chrome-extension/src/logic/account/index.ts
@@ -1,69 +1,19 @@
-import { PublicIdentity } from '@iov/bcp';
+import { CodecType } from '../../utils/config';
 
-export class BlockchainAccount {
+class Account {
   private _derivation: number;
-  private _address: string;
-  private _publicIdentity: PublicIdentity;
+  private _bcAddresses: Map<CodecType, string> = new Map([]); // [["eth","0xajkfgshdjlg"]
 
-  public constructor(
-    derivation: number,
-    address: string,
-    publicIdentity: PublicIdentity
-  ) {
+  public constructor(derivation: number) {
     this._derivation = derivation;
-    this._address = address;
-    this._publicIdentity = publicIdentity;
+  }
+
+  public addBlockchainAddress(codecType: string, bcAddress: string): void {
+    this._bcAddresses.set(codecType, bcAddress);
   }
 
   public get derivation(): number {
     return this._derivation;
-  }
-
-  public get address(): string {
-    return this._address;
-  }
-
-  public get publicIdentity(): PublicIdentity {
-    return this._publicIdentity;
-  }
-}
-
-class Account {
-  private _bnsIdentity: PublicIdentity | undefined;
-  private _bovIdentity: PublicIdentity | undefined;
-  private _lskIdentity: PublicIdentity | undefined;
-  private _ethIdentity: PublicIdentity | undefined;
-
-  public get bnsIdentity(): PublicIdentity | undefined {
-    return this._bnsIdentity;
-  }
-
-  public set bnsIdentity(bnsIdentity) {
-    this._bnsIdentity = bnsIdentity;
-  }
-
-  public get bovIdentity(): PublicIdentity | undefined {
-    return this._bovIdentity;
-  }
-
-  public set bovIdentity(bovIdentity) {
-    this._bovIdentity = bovIdentity;
-  }
-
-  public get lskIdentity(): PublicIdentity | undefined {
-    return this._lskIdentity;
-  }
-
-  public set lskIdentity(lskIdentity) {
-    this._lskIdentity = lskIdentity;
-  }
-
-  public get ethIdentity(): PublicIdentity | undefined {
-    return this._ethIdentity;
-  }
-
-  public set ethIdentity(ethIdentity) {
-    this._ethIdentity = ethIdentity;
   }
 }
 
