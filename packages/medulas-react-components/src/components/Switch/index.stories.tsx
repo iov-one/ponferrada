c4a5b7796ec9a71fbf4143b7e03e4f2bbb7e5e4a
Albert Andrejev <albert.andrejev@gmail.com>
2019-03-27 13:10:14 +0200

    Story update

--- packages/medulas-react-components/src/components/Switch/index.stories.tsx
+++ packages/medulas-react-components/src/components/Switch/index.stories.tsx
@@ -1,10 +1,16 @@
 import { storiesOf } from '@storybook/react';
 import React from 'react';
 import Switch from './index';
+import Block from '../Block';
 import { Storybook } from '../../utils/storybook';
 
 storiesOf('Components', module).add('Switch', () => (
   <Storybook>
-    <Switch color="primary" />
+    <Block>
+      <Switch color="primary" label="With label" />
+    </Block>
+    <Block>
+      <Switch color="primary" />
+    </Block>
   </Storybook>
 ));
