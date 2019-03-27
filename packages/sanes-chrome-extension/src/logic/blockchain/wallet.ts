eea9e24a29ec812f4ecb051d444fda5e0b8c19fe
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-03-27 16:56:47 +0100

    Added wallet utilities in logic folder

---
+++ packages/sanes-chrome-extension/src/logic/blockchain/wallet.ts
@@ -0,0 +1,38 @@
+import { WalletId } from '@iov/core';
+import { Codec } from './connection';
+import { ValueAndUpdates } from '@iov/stream';
+import { WalletInfo, HdPaths } from '@iov/keycontrol';
+import { Slip10RawIndex } from '@iov/crypto';
+
+export function walletFrom(
+  codec: Codec,
+  availableWallets: ValueAndUpdates<ReadonlyArray<WalletInfo>>
+): WalletId {
+  const wallets = availableWallets.value.map(i => i.id);
+  const [edWallet, secWallet] = wallets;
+  switch (codec) {
+    case Codec.Bns:
+    case Codec.Lisk:
+      return edWallet;
+    case Codec.Ethereum:
+      return secWallet;
+    default:
+      throw new Error(`unsupported codec: ${codec}`);
+  }
+}
+
+export function pathFrom(
+  codec: Codec,
+  derivation: number
+): ReadonlyArray<Slip10RawIndex> {
+  switch (codec) {
+    case Codec.Bns: // BNS and BOV
+      return HdPaths.iov(derivation);
+    case Codec.Lisk:
+      return HdPaths.bip44Like(134, derivation);
+    case Codec.Ethereum:
+      return HdPaths.ethereum(derivation);
+    default:
+      throw new Error(`unsupported codec: ${codec}`);
+  }
+}
