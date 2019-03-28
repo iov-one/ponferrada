af2fbfd9d53d06f3f25e3674cb1cabf9e734a999
Albert Andrejev <albert.andrejev@gmail.com>
2019-03-28 22:42:00 +0200

    Tooltip rPR remarks fix

--- packages/medulas-react-components/src/components/Tooltip/index.tsx
+++ packages/medulas-react-components/src/components/Tooltip/index.tsx
@@ -2,13 +2,19 @@ import { Popper, makeStyles } from '@material-ui/core';
 import Paper from '@material-ui/core/Paper';
 import * as React from 'react';
 import Image from '../Image';
-import infoNormal from './assets/info_normal.svg';
+import infoNormal from '../../theme/assets/info_normal.svg';
 import theme from '../../theme/utils/mui';
+
+const DEFAULT_HEIGHT = 16;
 
 const useStyles = makeStyles(theme => ({
   paper: {
     padding: theme.spacing(2),
-    boxShadow: `0 ${theme.spacing(1)} ${theme.spacing(2)} 0 #e3e4e7`,
+    boxShadow: `0 ${theme.spacing(1)}px ${theme.spacing(2)}px 0 #e3e4e7`,
+  },
+  container: {
+    height: `${DEFAULT_HEIGHT}px`,
+    display: 'inline',
   },
 }));
 
@@ -17,12 +23,11 @@ interface Props {
   readonly maxWidth?: number;
 }
 
-const DEFAULT_HEIGHT = 16;
-let tooltipRef = React.createRef<HTMLDivElement>();
+//let tooltipRef = React.createRef<HTMLDivElement>();
 
 const Tooltip = ({ children, maxWidth = 200 }: Props): JSX.Element => {
   const [open, setOpen] = React.useState<boolean>(false);
-  const toggle = (): void => setOpen(!open);
+  const toggle = (): void => setOpen(open => !open);
 
   const classes = useStyles();
 
@@ -37,14 +42,11 @@ const Tooltip = ({ children, maxWidth = 200 }: Props): JSX.Element => {
     },
   };
 
-  const divStyle = {
-    height: `${DEFAULT_HEIGHT}px`,
-    display: 'inline',
-  };
+  const tooltipRef = React.useRef(null);
 
   return (
     <React.Fragment>
-      <div style={divStyle} ref={tooltipRef} onClick={toggle}>
+      <div className={classes.container} ref={tooltipRef} onClick={toggle}>
         <Image
           src={infoNormal}
           alt="Info"
