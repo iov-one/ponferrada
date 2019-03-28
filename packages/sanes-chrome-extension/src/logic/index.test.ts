20e10b7384ea296e3ff1c6ae637bdf139b1a5ba1
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-03-28 13:46:19 +0100

    Fix test usage

--- packages/sanes-chrome-extension/src/logic/index.test.ts
+++ packages/sanes-chrome-extension/src/logic/index.test.ts
@@ -1,12 +1,18 @@
 import { getPersona } from './index';
+import Persona from './persona';
 
 describe('logic', () => {
   it('should get a Persona', async () => {
     const password = 'test-password';
     const accountName = 'test-account';
     const persona: Persona = await getPersona(password, accountName);
-    const accounts = persona.accounts().get(accountName);
+    const accounts = persona.accounts;
     expect(accounts.has(accountName)).toEqual(true);
-    expect(accounts.blockchainAddresses().size).toEqual(4);
+    const account = accounts.get(accountName);
+    if (!account) {
+      throw new Error('Account should not be undefined');
+    }
+
+    expect(account.blockchainAddresses.size).toEqual(4);
   });
 });
