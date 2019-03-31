c83f9d658244283fd65ccb42eb4c5cf944a737fe
Albert Andrejev <albert.andrejev@gmail.com>
2019-03-31 18:02:25 +0300

    PageLayout component

---
+++ packages/medulas-react-components/src/components/PageLayout/index.stories.tsx
@@ -0,0 +1,13 @@
+import { storiesOf } from '@storybook/react';
+import React from 'react';
+import Typography from '../Typography';
+import PageLayout from './index';
+import { Storybook } from '../../utils/storybook';
+
+storiesOf('Components', module).add('Layout', () => (
+  <Storybook>
+    <PageLayout primaryTitle="Title" title="storybook">
+      <Typography variant="h6">Layout content</Typography>
+    </PageLayout>
+  </Storybook>
+));
