1b5223d2da13696e423ba9ecd07216ec19350402
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-04-01 10:55:09 +0200

    Refactor Tooltip Storybook

--- packages/medulas-react-components/src/components/Tooltip/index.stories.tsx
+++ packages/medulas-react-components/src/components/Tooltip/index.stories.tsx
@@ -2,20 +2,27 @@ import { storiesOf } from '@storybook/react';
 import React from 'react';
 import Tooltip from './index';
 import Block from '../Block';
+import Typography from '../Typography';
 import { Storybook } from '../../utils/storybook';
 
-storiesOf('Components', module).add('Tooltip', () => (
-  <Storybook>
-    <Block>
-      <Tooltip>Some tooltip text</Tooltip>
-    </Block>
-    <br />
-    <br />
-    <br />
-    <br />
-    <Block>
-      Loooooooooooooooooooooooooooooooooong text
-      <Tooltip>Tooltip for long text</Tooltip>
-    </Block>
-  </Storybook>
-));
+storiesOf('Components', module).add('Tooltip', () => {
+  const tooltipStyle: React.CSSProperties = { marginRight: '4px' };
+
+  return (
+    <Storybook>
+      <Block marginBottom={4}>
+        <Tooltip>
+          <Typography>Some tooltip text</Typography>
+        </Tooltip>
+      </Block>
+      <Block>
+        <Typography inline style={tooltipStyle}>
+          Loooooooooooooooooooooooooooooooooong text
+        </Typography>
+        <Tooltip>
+          <Typography>Tooltip for long text</Typography>
+        </Tooltip>
+      </Block>
+    </Storybook>
+  );
+});
