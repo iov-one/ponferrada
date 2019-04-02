e1244728ed2605222c433f0dc54511c82ee84a36
Albert Andrejev <albert.andrejev@gmail.com>
2019-04-02 17:42:35 +0300

    Move PageLayout IOV icon to bottom

--- packages/medulas-react-components/src/components/PageLayout/index.tsx
+++ packages/medulas-react-components/src/components/PageLayout/index.tsx
@@ -18,19 +18,36 @@ const PageLayout = ({
   primaryTitle,
 }: Props): JSX.Element => {
   return (
-    <Block id={id} paddingRight={2} paddingLeft={2} paddingTop={2}>
-      <Typography color="primary" variant="h4" inline>
-        {primaryTitle}
-      </Typography>
-      <Typography variant="h4" inline>
-        {' '}
-        {title}
-      </Typography>
-      <Block padding={2} marginTop={3} marginBottom={1}>
+    <Block
+      display="flex"
+      flexDirection="column"
+      id={id}
+      paddingRight={4}
+      paddingLeft={4}
+      paddingTop={2}
+      height="100%"
+    >
+      <Block>
+        <Typography color="primary" variant="h4" inline>
+          {primaryTitle}
+        </Typography>
+        <Typography variant="h4" inline>
+          {' '}
+          {title}
+        </Typography>
+      </Block>
+      <Block marginTop={3} marginBottom={1}>
         {children}
       </Block>
-      <Block textAlign="center" marginBottom={1}>
-        <Image src={iovLogo} alt="IOV logo" />
+      <Block
+        display="flex"
+        flexDirection="column"
+        flexGrow={1}
+        alignItems="center"
+        justifyContent="flex-end"
+        marginBottom={2}
+      >
+        <Image src={iovLogo} alt="IOV logo" width={84} height={39} />
       </Block>
     </Block>
   );
