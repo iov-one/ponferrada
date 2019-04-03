80e9403f90efa83752dde9f4c4657a27eef99b2e
Albert Andrejev <albert.andrejev@gmail.com>
2019-04-03 11:28:53 +0300

    Test More Options link redirect

--- packages/sanes-chrome-extension/src/routes/login/index.dom.spec.ts
+++ packages/sanes-chrome-extension/src/routes/login/index.dom.spec.ts
@@ -3,6 +3,8 @@ import { RootState } from '../../store/reducers';
 import { aNewStore } from '../../store';
 import TestUtils from 'react-dom/test-utils';
 import { travelToLogin } from './test/travelToLogin';
+import { whenOnNavigatedToRoute } from '../../utils/test/navigation';
+import { WELCOME_ROUTE } from '../paths';
 
 describe('DOM > Feature > Login', () => {
   let store: Store<RootState>;
@@ -22,5 +24,16 @@ describe('DOM > Feature > Login', () => {
     expect(inputs.length).toBe(2);
 
     TestUtils.findRenderedDOMComponentWithTag(LoginDom, 'button');
+
+    //Bottom links
+    const links = TestUtils.scryRenderedDOMComponentsWithTag(LoginDom, 'h6');
+
+    expect(links.length).toBe(2);
+    const moreOptionsLink = links[1];
+    TestUtils.act(() => {
+      TestUtils.Simulate.click(moreOptionsLink);
+    });
+
+    await whenOnNavigatedToRoute(store, WELCOME_ROUTE);
   }, 55000);
 });
