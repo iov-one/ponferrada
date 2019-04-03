11752d1ca95fc84ac619394d5123af58d8f76c2d
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-04-03 12:04:41 +0200

    Adding Unit type

--- packages/medulas-react-components/src/components/Block/index.tsx
+++ packages/medulas-react-components/src/components/Block/index.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import Box, { BoxProps } from '@material-ui/core/Box';
 import { SizingBreakpoint } from '../Grid';
 
+export type Unit = 0 | 1 | 2 | 3 | 4;
+
 // TODO: Remove those props after BoxProps will be properly implemented.
 interface Props extends BoxProps {
   readonly children?: React.ReactNode;
