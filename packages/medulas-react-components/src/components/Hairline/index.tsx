f550df64564e01ba3b9d329dd81a9e8a71882b13
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-04-03 10:32:43 +0200

    Adding Hairline component

---
+++ packages/medulas-react-components/src/components/Hairline/index.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import Block, { Unit } from '../Block';
+
+interface Props {
+  readonly space?: Unit;
+  readonly color?: string;
+}
+
+const Hairline = ({ space = 0, color = 'grey.300' }: Props): JSX.Element => (
+  <Block
+    width="100%"
+    height="1px"
+    marginTop={space}
+    marginBottom={space}
+    bgcolor={color}
+  />
+);
+
+export default Hairline;
