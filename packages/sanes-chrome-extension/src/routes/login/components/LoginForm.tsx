ff3270328b85eec71cf5760f19e7feb3b5ed5e81
Albert Andrejev <albert.andrejev@gmail.com>
2019-04-04 13:45:56 +0300

    Adding LoginForm compoenent to Login view

---
+++ packages/sanes-chrome-extension/src/routes/login/components/LoginForm.tsx
@@ -0,0 +1,59 @@
+import * as React from 'react';
+import Button from 'medulas-react-components/lib/components/Button';
+import Block from 'medulas-react-components/lib/components/Block';
+import Form, {
+  useForm,
+} from 'medulas-react-components/lib/components/forms/Form';
+import TextFieldForm from 'medulas-react-components/lib/components/forms/TextFieldForm';
+
+export const ACCOUNT_NAME_FIELD = 'accountNameField';
+export const PASSWORD_FIELD = 'passwordInputField';
+
+interface Props {
+  readonly onLogin: (values: object) => Promise<void>;
+  readonly validate: (values: object) => object;
+}
+
+const LoginForm = ({ onLogin, validate }: Props): JSX.Element => {
+  const { form, handleSubmit, pristine, submitting, invalid } = useForm({
+    onSubmit: onLogin,
+    validate,
+  });
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Block marginBottom={1}>
+        <TextFieldForm
+          placeholder="Enter your unique identifier"
+          form={form}
+          required
+          fullWidth
+          name={ACCOUNT_NAME_FIELD}
+        />
+      </Block>
+      <Block marginBottom={2}>
+        <TextFieldForm
+          placeholder="Password"
+          type="password"
+          form={form}
+          required
+          fullWidth
+          name={PASSWORD_FIELD}
+        />
+      </Block>
+      <Block display="flex" justifyContent="center">
+        <Block width={120}>
+          <Button
+            fullWidth
+            type="submit"
+            disabled={invalid || pristine || submitting}
+          >
+            Continue
+          </Button>
+        </Block>
+      </Block>
+    </Form>
+  );
+};
+
+export default LoginForm;
