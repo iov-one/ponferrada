013fbc439f556ee17ca7ec8141440de019b1cff0
Albert Andrejev <albert.andrejev@gmail.com>
2019-04-04 13:46:15 +0300

    Adding LoginControls component to Login view

---
+++ packages/sanes-chrome-extension/src/routes/login/components/LoginControls.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import Block from 'medulas-react-components/lib/components/Block';
+import Typography from 'medulas-react-components/lib/components/Typography';
+import Link from 'medulas-react-components/lib/components/Link';
+import { WELCOME_ROUTE } from '../../paths';
+
+const LoginControls = (): JSX.Element => {
+  return (
+    <Block marginTop={4} textAlign="center">
+      <Block marginBottom={1}>
+        <Link to={WELCOME_ROUTE}>
+          <Typography variant="subtitle2" color="primary" link inline>
+            Restore account
+          </Typography>
+        </Link>
+      </Block>
+      <Block>
+        <Link to={WELCOME_ROUTE}>
+          <Typography variant="subtitle2" color="primary" link inline>
+            More options
+          </Typography>
+        </Link>
+      </Block>
+    </Block>
+  );
+};
+
+export default LoginControls;
