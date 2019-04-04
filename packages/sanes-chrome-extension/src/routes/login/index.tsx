eb099bbc8a7533c8009a7e2b7b8646cd2eba0b96
Albert Andrejev <albert.andrejev@gmail.com>
2019-04-04 13:48:47 +0300

    Login view component refactoring

--- packages/sanes-chrome-extension/src/routes/login/index.tsx
+++ packages/sanes-chrome-extension/src/routes/login/index.tsx
@@ -1,6 +1,41 @@
 import * as React from 'react';
-import Layout from './components';
+import PageLayout from 'medulas-react-components/lib/components/PageLayout';
+import {
+  FormValues,
+  ValidationError,
+} from 'medulas-react-components/lib/components/forms/Form';
+import LoginControls from './components/LoginControls';
+import { LOGIN_ROUTE } from '../paths';
+import {
+  ACCOUNT_NAME_FIELD,
+  PASSWORD_FIELD,
+} from '../signup/components/NewAccountForm';
+import LoginForm from './components/LoginForm';
 
-const Login = (): JSX.Element => <Layout />;
+const validate = (values: object): object => {
+  const formValues = values as FormValues;
+  let errors: ValidationError = {};
+  if (!formValues[ACCOUNT_NAME_FIELD]) {
+    errors[ACCOUNT_NAME_FIELD] = 'Required';
+  }
+  if (!formValues[PASSWORD_FIELD]) {
+    errors[PASSWORD_FIELD] = 'Required';
+  }
+
+  return errors;
+};
+
+const onLogin = async (_: object): Promise<void> => {
+  //const formValues = values as FormValues;
+};
+
+const Login = (): JSX.Element => {
+  return (
+    <PageLayout id={LOGIN_ROUTE} primaryTitle="Log" title="In">
+      <LoginForm onLogin={onLogin} validate={validate} />
+      <LoginControls />
+    </PageLayout>
+  );
+};
 
 export default Login;
