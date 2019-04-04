ce8cd8cb665df21ab555a2726340952172b06a45
Albert Andrejev <albert.andrejev@gmail.com>
2019-04-04 13:45:17 +0300

    Storybook route support

--- packages/medulas-react-components/src/utils/storybook/index.tsx
+++ packages/medulas-react-components/src/utils/storybook/index.tsx
@@ -1,10 +1,31 @@
 import ThemeProvider from '../../theme/MedulasThemeProvider';
+import { Provider } from 'react-redux';
+import { createMemoryHistory, History } from 'history';
+import { ConnectedRouter, connectRouter } from 'connected-react-router';
+import { createStore, combineReducers } from 'redux';
 import * as React from 'react';
+
+const history = createMemoryHistory();
+
+const createRootReducer = (history: History) =>
+  combineReducers({
+    router: connectRouter(history),
+  });
+
+const store = createStore(createRootReducer(history));
 
 interface Props {
   readonly children: React.ReactNode;
 }
 
 export const Storybook = ({ children }: Props): JSX.Element => {
-  return <ThemeProvider injectFonts>{children}</ThemeProvider>;
+  return (
+    <Provider store={store}>
+      <ThemeProvider injectFonts>
+        <ConnectedRouter history={history}>
+          <React.Fragment>{children}</React.Fragment>
+        </ConnectedRouter>
+      </ThemeProvider>
+    </Provider>
+  );
 };
