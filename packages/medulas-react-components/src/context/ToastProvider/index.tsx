627cbf20be239763ceef6b0d7642111a0e21b921
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-04-08 16:03:01 +0200

    Expose only show and close toast methods to Context API

--- packages/medulas-react-components/src/context/ToastProvider/index.tsx
+++ packages/medulas-react-components/src/context/ToastProvider/index.tsx
@@ -1,42 +1,15 @@
 import * as React from 'react';
-import { Toast } from './Toast';
-
-export enum ToastVariant {
-  SUCCESS = 'success',
-  WARNING = 'warning',
-  ERROR = 'error',
-  INFO = 'info',
-}
-
-const SharedToast = (): JSX.Element => (
-  <ToastConsumer>
-    {({ open, message, onClose, variant }: ToastContextInterface) => {
-      return (
-        <Toast
-          open={open}
-          onClose={onClose}
-          message={message}
-          variant={variant}
-        />
-      );
-    }}
-  </ToastConsumer>
-);
+import { Toast, ToastVariant } from './Toast';
+import { singleton } from '../../utils/singleton';
 
 export interface ToastContextInterface {
-  readonly showToast: (message: string, variant: ToastVariant) => void;
-  readonly onClose: () => void;
-  readonly open: boolean;
-  readonly message: string;
-  readonly variant: ToastVariant;
+  readonly show: (message: string, variant: ToastVariant) => void;
+  readonly close: () => void;
 }
 
 export const ToastContext = React.createContext<ToastContextInterface>({
-  showToast: (message: string, variant: ToastVariant) => ({ message, variant }),
-  onClose: () => null,
-  open: false,
-  message: '',
-  variant: ToastVariant.INFO,
+  show: (): void => {},
+  close: (): void => {},
 });
 
 interface Props {
@@ -45,15 +18,13 @@ interface Props {
 
 interface ToastState {
   readonly open: boolean;
-  readonly message: string;
-  readonly variant: ToastVariant;
+  readonly message?: string;
+  readonly variant?: ToastVariant;
 }
 
 export const ToastProvider = ({ children }: Props): JSX.Element => {
   const [toast, setToast] = React.useState<ToastState>({
     open: false,
-    message: '',
-    variant: ToastVariant.INFO,
   });
 
   const showToast = (message: string, variant: ToastVariant): void =>
