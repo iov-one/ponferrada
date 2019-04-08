25316423a2a5c2bf071478dfcb463931c4f45133
Albert Andrejev <albert.andrejev@gmail.com>
2019-04-08 12:48:37 +0300

    Update snackbar content width

--- packages/medulas-react-components/src/theme/utils/mui.ts
+++ packages/medulas-react-components/src/theme/utils/mui.ts
@@ -148,7 +148,7 @@ const themeObject: ThemeOptions = {
           borderRadius: 2,
           boxShadow: '0 0 6px 0 #f3f4fb',
         },
-        width: 460,
+        width: 350,
         backgroundColor: white,
         padding: `${theme.spacing(2)}px ${theme.spacing(3)}px`,
         flexWrap: 'nowrap',
