6e99d17e7604f41003d8549f0a8fb503531bf9bc
Simon Warta <simon@kullo.net>
2019-04-09 18:56:37 +0200

    Test Persona.generateNextAccount/.generateAccount

--- packages/sanes-chrome-extension/src/logic/persona/index.test.ts
+++ packages/sanes-chrome-extension/src/logic/persona/index.test.ts
@@ -1,7 +1,31 @@
+import { Algorithm, ChainId } from '@iov/bcp';
+import { bnsCodec } from '@iov/bns';
+import { HdPaths } from '@iov/core';
+import { ethereumCodec } from '@iov/ethereum';
+
 import { Persona } from '.';
 import { createUserProfile } from '../user';
+import { EnhancedChainSpec } from '../blockchain/chainsConfig';
 
 describe('Persona', () => {
+  const chain1: EnhancedChainSpec = {
+    algorithm: Algorithm.Ed25519,
+    bootstrapNodes: [],
+    chainId: 'test-chain-1' as ChainId,
+    codec: bnsCodec,
+    codecType: 'bns',
+    derivePath: x => HdPaths.iov(x),
+  };
+
+  const chain2: EnhancedChainSpec = {
+    algorithm: Algorithm.Secp256k1,
+    bootstrapNodes: [],
+    chainId: 'test-chain-2' as ChainId,
+    codec: ethereumCodec,
+    codecType: 'eth',
+    derivePath: x => HdPaths.ethereum(x),
+  };
+
   it('can be created', async () => {
     const userProfile = await createUserProfile();
     const persona = new Persona(userProfile, []);
@@ -23,6 +47,72 @@ describe('Persona', () => {
       const userProfile = await createUserProfile();
       const persona = new Persona(userProfile, []);
       expect(await persona.accounts()).toEqual([]);
+    });
+  });
+
+  describe('generateAccount', () => {
+    it('does not change accounts for empty chains list', async () => {
+      const userProfile = await createUserProfile();
+      const persona = new Persona(userProfile, []);
+      await persona.generateAccount(0);
+      expect(await persona.accounts()).toEqual([]);
+    });
+
+    it('generates one account with one identity when chains list has one element', async () => {
+      const userProfile = await createUserProfile();
+      const persona = new Persona(userProfile, [chain1]);
+      await persona.generateAccount(0);
+      const accounts = await persona.accounts();
+      expect(accounts.length).toEqual(1);
+      expect(accounts[0].publicIdentities.size).toEqual(1);
+      expect(
+        Array.from(accounts[0].publicIdentities).map(ident => ident[0])
+      ).toEqual([chain1.chainId]);
+    });
+
+    it('generates one account with two identities when chains list has two elements', async () => {
+      const userProfile = await createUserProfile();
+      const persona = new Persona(userProfile, [chain1, chain2]);
+      await persona.generateAccount(0);
+      const accounts = await persona.accounts();
+      expect(accounts.length).toEqual(1);
+      expect(accounts[0].publicIdentities.size).toEqual(2);
+      expect(
+        Array.from(accounts[0].publicIdentities).map(ident => ident[0])
+      ).toEqual([chain1.chainId, chain2.chainId]);
+    });
+  });
+
+  describe('generateNextAccount', () => {
+    it('does not change accounts for empty chains list', async () => {
+      const userProfile = await createUserProfile();
+      const persona = new Persona(userProfile, []);
+      await persona.generateNextAccount();
+      expect(await persona.accounts()).toEqual([]);
+    });
+
+    it('generates one account with one identity when chains list has one element', async () => {
+      const userProfile = await createUserProfile();
+      const persona = new Persona(userProfile, [chain1]);
+      await persona.generateNextAccount();
+      const accounts = await persona.accounts();
+      expect(accounts.length).toEqual(1);
+      expect(accounts[0].publicIdentities.size).toEqual(1);
+      expect(
+        Array.from(accounts[0].publicIdentities).map(ident => ident[0])
+      ).toEqual([chain1.chainId]);
+    });
+
+    it('generates one account with two identities when chains list has two elements', async () => {
+      const userProfile = await createUserProfile();
+      const persona = new Persona(userProfile, [chain1, chain2]);
+      await persona.generateNextAccount();
+      const accounts = await persona.accounts();
+      expect(accounts.length).toEqual(1);
+      expect(accounts[0].publicIdentities.size).toEqual(2);
+      expect(
+        Array.from(accounts[0].publicIdentities).map(ident => ident[0])
+      ).toEqual([chain1.chainId, chain2.chainId]);
     });
   });
 });
