bb5cd8cf4c220886e7a2046ab643fde905abc309
Albert Andrejev <albert.andrejev@gmail.com>
2019-04-09 09:47:37 +0300

    Set recovery phrase route path

--- packages/sanes-chrome-extension/src/routes/index.tsx
+++ packages/sanes-chrome-extension/src/routes/index.tsx
@@ -3,13 +3,20 @@ import { Route, Switch } from 'react-router-dom';
 import Welcome from './welcome';
 import Signup from './signup';
 import Login from './login';
-import { WELCOME_ROUTE, SIGNUP_ROUTE, LOGIN_ROUTE } from './paths';
+import RecoveryPhrase from './reccovery-phrase';
+import {
+  WELCOME_ROUTE,
+  SIGNUP_ROUTE,
+  LOGIN_ROUTE,
+  RECOVERY_PHRASE_ROUTE,
+} from './paths';
 
 export const MainRouter = (): JSX.Element => (
   <Switch>
     <Route exact path={WELCOME_ROUTE} component={Welcome} />
     <Route exact path={SIGNUP_ROUTE} component={Signup} />
     <Route exact path={LOGIN_ROUTE} component={Login} />
+    <Route exact path={RECOVERY_PHRASE_ROUTE} component={RecoveryPhrase} />
   </Switch>
 );
 
