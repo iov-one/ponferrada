28a6cb487e1dedf12d5af66414a691743f9ca148
Albert Andrejev <albert.andrejev@gmail.com>
2019-04-09 17:49:54 +0300

    Fixing linter warnings

--- packages/sanes-chrome-extension/src/routes/recovery-phrase/components/PdfDownload.tsx
+++ packages/sanes-chrome-extension/src/routes/recovery-phrase/components/PdfDownload.tsx
@@ -9,9 +9,9 @@ export interface Props {
 const PdfDownload = ({ mnemonic }: Props): JSX.Element => {
   const generatePdf = () => {
     const doc: jsPDF = new jsPDF();
-    const mnemonicArr: Array<string> = mnemonic.split(' ');
+    const mnemonicArr: string[] = mnemonic.split(' ');
 
-    const headerMsg: Array<string> = [
+    const headerMsg: string[] = [
       'Your Recovery Phrase are 12 random words that are set in a particular order',
       'that acts as a tool to recover or back up your wallet on any platform.',
       '',
