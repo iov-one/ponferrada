eba2f2be5f3b1863849a2938f8cb52b9b2fb4b71
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-04-10 11:25:04 +0200

    Adding HMR to web development

--- packages/bierzo-wallet/src/index.tsx
+++ packages/bierzo-wallet/src/index.tsx
@@ -3,4 +3,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootEl = document.getElementById('root');
+
+const render = (Component: React.ComponentType): void => {
+  ReactDOM.render(<Component />, rootEl);
+};
+
+render(App);
+
+if (module.hot) {
+  module.hot.accept(
+    './App',
+    (): void => {
+      const NextApp = require('./App').default;
+      render(NextApp);
+    }
+  );
+}
