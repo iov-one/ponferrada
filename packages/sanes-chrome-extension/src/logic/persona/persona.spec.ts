ee000c700e74f5c5f6c7dd0026956155f44f656e
Simon Warta <simon@kullo.net>
2019-04-11 23:57:54 +0200

    Run getBalances in tests

--- packages/sanes-chrome-extension/src/logic/persona/persona.spec.ts
+++ packages/sanes-chrome-extension/src/logic/persona/persona.spec.ts
@@ -2,9 +2,23 @@ import { Persona } from './persona';
 import { describeWithChains } from '../../utils/test/testExecutor';
 
 describeWithChains('Persona', () => {
-  it('can be created', async () => {
-    const persona = await Persona.create();
-    expect(persona).toBeTruthy();
-    persona.destroy();
+  describe('create', () => {
+    it('can be created', async () => {
+      const persona = await Persona.create();
+      expect(persona).toBeTruthy();
+      persona.destroy();
+    });
+  });
+
+  describe('getBalances', () => {
+    it('can get balances of first account', async () => {
+      const persona = await Persona.create();
+
+      const balances = await persona.getBalances(0);
+      // no address exists on chain, so balance exists
+      expect(balances.length).toEqual(0);
+
+      persona.destroy();
+    });
   });
 });
