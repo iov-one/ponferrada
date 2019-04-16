5a6b8a545a42df4378bb943deaeca7d84572de34
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-04-16 13:25:53 +0200

    Add PersonaProvider exposing accounts and mnemonic to react

---
+++ packages/sanes-chrome-extension/src/context/PersonaProvider.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+
+type Accounts = ReadonlyArray<string>;
+
+export interface PersonaContextInterface {
+  readonly accountNames: Accounts;
+  readonly mnemonic: string;
+  readonly update: (accountNames: Accounts, mnemonic: string) => void;
+}
+
+export const PersonaContext = React.createContext<PersonaContextInterface>({
+  accountNames: [],
+  mnemonic: '',
+  update: (): void => {},
+});
+
+interface Props {
+  readonly children: React.ReactNode;
+}
+
+export const PersonaProvider = ({ children }: Props): JSX.Element => {
+  const [accountNames, setAccountNames] = React.useState<Accounts>([]);
+  const [mnemonic, setMnemonic] = React.useState<string>('');
+  const loadPersonaInReact = (accountNames: Accounts, mnemonic: string): void => {
+    setAccountNames(accountNames);
+    setMnemonic(mnemonic);
+  };
+
+  const personaContextValue = {
+    accountNames,
+    mnemonic,
+    update: loadPersonaInReact,
+  };
+
+  return <PersonaContext.Provider value={personaContextValue}>{children}</PersonaContext.Provider>;
+};
