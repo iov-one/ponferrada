66bf639870e4f75e493e21ce501360f545b2e2ab
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-04-17 19:13:56 +0200

    Added List component

---
+++ packages/medulas-react-components/src/components/List/index.tsx
@@ -0,0 +1,6 @@
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+
+export { List, ListItem, ListItemText, ListItemIcon };
