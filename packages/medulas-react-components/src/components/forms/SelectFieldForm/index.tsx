a7121410607d2ea0190dc56691453959272f3d62
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-04-17 15:37:04 +0200

    Update font size in select field

--- packages/medulas-react-components/src/components/forms/SelectFieldForm/index.tsx
+++ packages/medulas-react-components/src/components/forms/SelectFieldForm/index.tsx
@@ -29,8 +29,8 @@ const useStyles = makeStyles((theme: Theme) => ({
     padding: `0 ${theme.spacing(1)}px`,
     cursor: 'pointer',
   },
-  root: (props: RootStyleProps) => ({
-    fontSize: theme.typography.fontSize,
+  root: props => ({
+    fontSize: theme.typography.subtitle2.fontSize,
     height: '32px',
     maxWidth: props.maxWidth,
   }),
