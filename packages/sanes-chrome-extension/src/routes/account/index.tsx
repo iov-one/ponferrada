0569149a1dbe5bbce78fc99551b259fd0e697b01
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-04-17 14:03:20 +0200

    Account view title

--- packages/sanes-chrome-extension/src/routes/account/index.tsx
+++ packages/sanes-chrome-extension/src/routes/account/index.tsx
@@ -4,13 +4,14 @@ import Block from 'medulas-react-components/lib/components/Block';
 import Hairline from 'medulas-react-components/lib/components/Hairline';
 import Image from 'medulas-react-components/lib/components/Image';
 import iovLogo from '../../assets/iov-logo.png';
-import { SIGNUP_ROUTE } from '../paths';
+import { ACCOUNT_STATUS_ROUTE } from '../paths';
 import Account from './components/Account';
 import SelectField, { Item } from 'medulas-react-components/lib/components/forms/SelectFieldForm';
 import { useForm } from 'react-final-form-hooks';
 import Form from 'medulas-react-components/lib/components/forms/Form';
 import { PersonaContext } from '../../context/PersonaProvider';
 import { AccountInfo } from '../../logic/persona/accountManager';
+import PageLayout from 'medulas-react-components/lib/components/PageLayout';
 
 const CREATE_NEW_ONE = 'Create a new one';
 
@@ -52,15 +53,11 @@ const AccountView = (): JSX.Element => {
   };
 
   return (
-    <Block id={`${SIGNUP_ROUTE}_first`} paddingRight={2} paddingLeft={2} paddingTop={2}>
-      <Typography color="primary" variant="h4" inline>
-        New
-      </Typography>
-      <Typography variant="h4" inline>
-        {' Account'}
-      </Typography>
-      <Hairline space={1} />
+    <PageLayout id={ACCOUNT_STATUS_ROUTE} primaryTitle="Account" title="Status">
       <Form onSubmit={handleSubmit}>
+        <Block marginBottom={1}>
+          <Typography variant="subtitle2">Available accounts</Typography>
+        </Block>
         <SelectField
           items={accounts}
           initial={CREATE_NEW_ONE}
@@ -69,8 +66,7 @@ const AccountView = (): JSX.Element => {
           onChangeCallback={onChange}
         />
       </Form>
-
-      <Hairline space={1} />
+      <Hairline space={2} />
       <Block padding={2} marginTop={3} marginBottom={1}>
         <Account blockchainAccounts={['foo', 'bar', 'baz']} />
       </Block>
@@ -78,7 +74,7 @@ const AccountView = (): JSX.Element => {
       <Block textAlign="center" marginBottom={1}>
         <Image src={iovLogo} alt="IOV logo" />
       </Block>
-    </Block>
+    </PageLayout>
   );
 };
 
--- packages/sanes-chrome-extension/src/routes/paths.ts
+++ packages/sanes-chrome-extension/src/routes/paths.ts
@@ -3,3 +3,4 @@ export const SIGNUP_ROUTE = '/signup';
 export const LOGIN_ROUTE = '/login';
 export const RECOVERY_PHRASE_ROUTE = '/recovery-phrase';
 export const RESTORE_ACCOUNT = '/restore-account';
+export const ACCOUNT_STATUS_ROUTE = '/account';
