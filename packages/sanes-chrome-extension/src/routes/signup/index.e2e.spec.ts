d6aadba24ddf081d24b3fe2b5442ff5680b2a078
Albert Andrejev <albert.andrejev@gmail.com>
2019-04-22 23:07:23 +0300

    Combining all testing method together

---
+++ packages/sanes-chrome-extension/src/routes/signup/index.e2e.spec.ts
@@ -0,0 +1,44 @@
+import puppeteer, { Browser, Page } from 'puppeteer';
+import { EXTENSION_HEIGHT, EXTENSION_WIDTH } from '../../theme/constants';
+import { travelToSignupNewAccountStep } from './test/travelToSignup';
+import { submitAccountFormE2E, handlePassPhrase2E } from './test/fillSignupForm';
+import { randomString } from '../../utils/test/random';
+import { withChainsDescribe } from '../../utils/test/testExecutor';
+
+withChainsDescribe(
+  'DOM > Signup route',
+  (): void => {
+    let browser: Browser;
+
+    beforeEach(async (): Promise<void> => {
+      const CRX_PATH = require('path').join(__dirname, '../../../build');
+      browser = await puppeteer.launch({
+        headless: false,
+        devtools: true,
+        //slowMo: 200,
+        args: [`--disable-extensions-except=${CRX_PATH}`, `--load-extension=${CRX_PATH}`],
+        defaultViewport: {
+          width: EXTENSION_WIDTH,
+          height: EXTENSION_HEIGHT,
+        },
+      });
+    }, 45000);
+
+    afterEach(
+      async (): Promise<void> => {
+        //await browser.close();
+      }
+    );
+
+    it('loads correctly', async (): Promise<void> => {
+      const page: Page = await browser.newPage();
+      await page.goto('chrome-extension://dafekhlcpidfaopcimocbcpciholgkkb/index.html', {
+        waitUntil: 'networkidle2',
+      });
+
+      await travelToSignupNewAccountStep(page);
+      await submitAccountFormE2E(page, randomString(10), randomString(10));
+      await handlePassPhrase2E(page);
+    }, 45000);
+  }
+);
