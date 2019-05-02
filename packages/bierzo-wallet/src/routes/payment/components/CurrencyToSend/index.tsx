1f8efc067e49235647a6e95acd403906a41b172e
abefernan <abel.fernandez.nandin@gmail.com>
2019-05-02 12:53:58 +0200

    Adds validation for CurrencyToSend component

--- packages/bierzo-wallet/src/routes/payment/components/CurrencyToSend/index.tsx
+++ packages/bierzo-wallet/src/routes/payment/components/CurrencyToSend/index.tsx
@@ -1,6 +1,6 @@
 import { faUser } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Theme } from '@material-ui/core';
+import { MenuItem, Theme } from '@material-ui/core';
 import Avatar from '@material-ui/core/Avatar';
 import Paper from '@material-ui/core/Paper';
 import Select from '@material-ui/core/Select';
@@ -8,7 +8,7 @@ import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/styles';
 import Block from 'medulas-react-components/lib/components/Block';
 import Typography from 'medulas-react-components/lib/components/Typography';
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
 
 const useStyles = makeStyles((theme: Theme) => ({
   avatar: {
@@ -20,11 +20,69 @@ const useStyles = makeStyles((theme: Theme) => ({
   },
 }));
 
-export const CurrencyToSend = (): JSX.Element => {
+interface Currencies {
+  [key: string]: number;
+}
+
+//NOTE hardcoded placeholder balances for each currency
+const currencies: Currencies = {
+  alt: 15,
+  iov: 24,
+};
+
+// TODO renders each MenuItem from the currencies array, but does not work because of Typescript type mismatch
+const renderedCurrencies = Object.keys(currencies).forEach(currency => {
+  return <MenuItem value={currency}>{currency.toUpperCase()}</MenuItem>;
+});
+
+export const CurrencyToSend = () => {
   const classes = useStyles();
 
   const avatarClasses = {
     root: classes.avatar,
+  };
+
+  const [validity, setValidity] = useState('');
+  //NOTE hardcoded placeholder 'iov'
+  const [currency, setCurrency] = useState('iov');
+  const [balance, setBalance] = useState(currencies[currency]);
+
+  const textFieldHandleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = Number(event.target.value);
+
+    setValidity('');
+
+    if (isNaN(value)) {
+      setValidity('Must be a number');
+    } else {
+      if (value > balance) {
+        setValidity(`Should be lower or equal than ${balance}`);
+      }
+
+      //TODO find out max limit
+      if (value > 9999999) {
+        setValidity('Should be lower than 9999999');
+      }
+
+      if (value < 1e-9) {
+        setValidity('Should be greater or equal than 1e-9');
+      }
+    }
+  };
+
+  const textFieldHandleBlur = (event: ChangeEvent<HTMLInputElement>) => {
+    if (!event.target.value) {
+      setValidity('Required');
+    }
+  };
+
+  const selectHandleChange = (event: ChangeEvent<{ name?: string | undefined; value: unknown }>) => {
+    const value = event.target.value;
+
+    if (typeof value === 'string') {
+      setCurrency(value);
+      setBalance(currencies[value]);
+    }
   };
 
   return (
@@ -38,16 +96,26 @@ export const CurrencyToSend = (): JSX.Element => {
         </Typography>
         <Block display="flex" marginTop={5} marginBottom={1}>
           <Block marginRight={1}>
-            <TextField placeholder="0,00" />
+            <TextField
+              placeholder="0,00"
+              fullWidth
+              onBlur={textFieldHandleBlur}
+              onChange={textFieldHandleChange}
+            />
           </Block>
-          <Select />
+          {/*TODO properly style Select component*/}
+          <Select value={currency} onChange={selectHandleChange} autoWidth>
+            {/* renderedCurrencies */}
+            <MenuItem value="alt">ALT</MenuItem>
+            <MenuItem value="iov">IOV</MenuItem>
+          </Select>
         </Block>
         <Typography color="error" variant="subtitle2">
-          Validity label
+          {validity}
         </Typography>
         <Block marginTop={1}>
           <Typography color="textSecondary" variant="subtitle2">
-            balance:
+            balance: {balance} {currency.toUpperCase()}
           </Typography>
         </Block>
       </Block>
