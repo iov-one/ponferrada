fcb9ee7c1f326c1b48375cd976c625319b9e0c03
Simon Warta <simon@kullo.net>
2019-05-02 14:13:21 +0200

    Remove docker pull to avoid network request for existing images

--- packages/sanes-chrome-extension/test/scripts/faucet/bcpd_start.sh
+++ packages/sanes-chrome-extension/test/scripts/faucet/bcpd_start.sh
@@ -8,13 +8,11 @@ FAUCET_VERSION="v0.5.2"
 TMP_DIR=$(mktemp -d "${TMPDIR:-/tmp}/faucet_start_bov.XXXXXXXXX")
 LOGFILE="$TMP_DIR/faucet_bov.log"
 
-docker pull "alpine"
 DOCKER_HOST_IP=$(docker run --rm alpine ip route | awk 'NR==1 {print $3}')
 
 BLOCKCHAIN_URL="ws://$DOCKER_HOST_IP:23457"
 echo "Connecting to $BLOCKCHAIN_URL"
 
-docker pull "iov1/iov-faucet:${FAUCET_VERSION}"
 docker run --read-only \
   --name "bcpd_faucet" \
   --env "FAUCET_CONCURRENCY=3" \
