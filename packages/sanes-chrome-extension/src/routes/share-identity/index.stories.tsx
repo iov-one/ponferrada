009aceb2945001ecae2ccc7b9c72903940489998
Albert Andrejev <albert.andrejev@gmail.com>
2019-05-03 22:23:40 +0300

    Updating storybook and snapshot

--- packages/sanes-chrome-extension/src/routes/share-identity/index.stories.tsx
+++ packages/sanes-chrome-extension/src/routes/share-identity/index.stories.tsx
@@ -1,13 +1,24 @@
 import { storiesOf } from '@storybook/react';
 import { Storybook } from 'medulas-react-components/lib/utils/storybook';
 import React from 'react';
-import Layout from './index';
+import ShowRequest from './components/ShowRequest';
+import AcceptRequest from './components/AcceptRequest';
+import { action } from '@storybook/addon-actions';
 
-storiesOf('Extension', module).add(
-  'Share Identity page',
-  (): JSX.Element => (
-    <Storybook>
-      <Layout />
-    </Storybook>
+storiesOf('Routes/Share Identity', module)
+  .add(
+    'Show Request page',
+    (): JSX.Element => (
+      <Storybook>
+        <ShowRequest showAcceptView={action('showAcceptView')} />
+      </Storybook>
+    )
   )
-);
+  .add(
+    'Accept Request page',
+    (): JSX.Element => (
+      <Storybook>
+        <AcceptRequest onBack={action('onBack')} onAcceptRequest={action('onAcceptRequest')} />
+      </Storybook>
+    )
+  );
