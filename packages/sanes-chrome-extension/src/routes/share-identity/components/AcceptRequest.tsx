1910df8137cf668c8f0c141a24fbd84ba296687b
Albert Andrejev <albert.andrejev@gmail.com>
2019-05-03 22:23:19 +0300

    Adding accept request view

---
+++ packages/sanes-chrome-extension/src/routes/share-identity/components/AcceptRequest.tsx
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import Button from 'medulas-react-components/lib/components/Button';
+import Typography from 'medulas-react-components/lib/components/Typography';
+import Block from 'medulas-react-components/lib/components/Block';
+import PageLayout from 'medulas-react-components/lib/components/PageLayout';
+import Back from 'medulas-react-components/lib/components/Button/Back';
+
+interface Props {
+  readonly onAcceptRequest: () => void;
+  readonly onBack: () => void;
+}
+
+const Layout = ({ onBack, onAcceptRequest }: Props): JSX.Element => (
+  <PageLayout primaryTitle="Share" title="Identity">
+    <Block textAlign="center">
+      <Typography variant="body1">The following site:</Typography>
+      <Typography variant="body1" color="primary">
+        http://finex.com
+      </Typography>
+      <Typography variant="body1" inline color="primary">
+        will be able to see
+      </Typography>
+      <Typography variant="body1" inline>
+        {' '}
+        your identity on
+      </Typography>
+      <Typography variant="body1" inline color="primary">
+        {' '}
+        BTC
+      </Typography>
+    </Block>
+    <Block marginTop={10} />
+    <Button variant="contained" fullWidth onClick={onAcceptRequest}>
+      Confirm
+    </Button>
+    <Block marginTop={2} />
+    <Back fullWidth onClick={onBack}>
+      Back
+    </Back>
+  </PageLayout>
+);
+
+export default Layout;
