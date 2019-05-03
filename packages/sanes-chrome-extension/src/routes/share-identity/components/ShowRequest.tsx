f551d5c927a4ec2cd17d68a85da19bf82c3701be
Albert Andrejev <albert.andrejev@gmail.com>
2019-05-03 22:22:49 +0300

    Show request view refactoring

--- packages/sanes-chrome-extension/src/routes/share-identity/components/ShowRequest.tsx
+++ packages/sanes-chrome-extension/src/routes/share-identity/components/ShowRequest.tsx
@@ -4,22 +4,26 @@ import Typography from 'medulas-react-components/lib/components/Typography';
 import Block from 'medulas-react-components/lib/components/Block';
 import PageLayout from 'medulas-react-components/lib/components/PageLayout';
 
-const Layout = (): JSX.Element => (
-  <PageLayout primaryTitle="Share" title="identity">
+interface Props {
+  readonly showAcceptView: () => void;
+}
+
+const Layout = ({ showAcceptView }: Props): JSX.Element => (
+  <PageLayout primaryTitle="Share" title="Identity">
     <Block textAlign="center">
       <Typography variant="body1">The following site:</Typography>
       <Typography variant="body1" color="primary">
         http://finex.com
       </Typography>
       <Typography variant="body1" inline>
-        wants to see you identity on
+        wants to see your identity on
       </Typography>{' '}
       <Typography variant="body1" inline color="primary">
         BTC
       </Typography>
     </Block>
     <Block marginTop={10} />
-    <Button variant="contained" fullWidth>
+    <Button variant="contained" fullWidth onClick={showAcceptView}>
       Accept
     </Button>
     <Block marginTop={2} />
