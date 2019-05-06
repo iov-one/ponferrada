ad2a8a205ad6704d8554a87b8e8d396c65dbfee7
abefernan <abel.fernandez.nandin@gmail.com>
2019-05-06 11:06:25 +0200

    Adds DOM test that checks if the "Next" button in /welcome redirects to /payment

--- packages/bierzo-wallet/src/store/reducers/index.ts
+++ packages/bierzo-wallet/src/store/reducers/index.ts
@@ -1,6 +1,7 @@
 import { connectRouter, RouterState } from 'connected-react-router';
 import { createBrowserHistory, History } from 'history';
 import { combineReducers, Reducer } from 'redux';
+import { StateType } from 'typesafe-actions';
 
 export const history = createBrowserHistory();
 
@@ -9,4 +10,5 @@ const createRootReducer = (history: History): Reducer<{ router: RouterState }> =
     router: connectRouter(history),
   });
 
+export type RootState = StateType<ReturnType<typeof createRootReducer>>;
 export default createRootReducer(history);
---
+++ packages/bierzo-wallet/src/utils/test/dom.tsx
@@ -0,0 +1,23 @@
+import { ConnectedRouter } from 'connected-react-router';
+import MedulasThemeProvider from 'medulas-react-components/lib/theme/MedulasThemeProvider';
+import * as React from 'react';
+import TestUtils from 'react-dom/test-utils';
+import { Provider } from 'react-redux';
+import { Store } from 'redux';
+import Route from '../../routes';
+import { history } from '../../store/reducers';
+
+export const createDom = (store: Store): React.Component =>
+  TestUtils.renderIntoDocument(
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <MedulasThemeProvider>
+          <Route />
+        </MedulasThemeProvider>
+      </ConnectedRouter>
+    </Provider>
+  ) as any; // eslint-disable-line @typescript-eslint/no-explicit-any
+
+export const expectRoute = (store: Store, route: string): void => {
+  expect(store.getState().router.location.pathname).toBe(route);
+};
