4ada2dd6d7df30987fc529d0287d2a4a2ec52de9
Simon Warta <simon@kullo.net>
2019-05-06 17:14:58 +0200

    Use callback style onMessage listener

--- packages/sanes-chrome-extension/src/extension/backgroundscript.ts
+++ packages/sanes-chrome-extension/src/extension/backgroundscript.ts
@@ -3,57 +3,65 @@ import { wrapStore } from 'webext-redux';
 import { JsonRpcErrorResponse } from '@iov/jsonrpc';
 
 import { MessageToBackground, MessageToBackgroundAction } from './messages';
-import { WELCOME_ROUTE } from '../routes/paths';
 import { makeStore } from '../store';
-import { history } from '../store/reducers';
 import { PersonaManager, UseOnlyJsonRpcSigningServer } from '../logic/persona';
 
 wrapStore(makeStore());
 
 let signingServer: UseOnlyJsonRpcSigningServer | undefined;
 
-// https://developer.chrome.com/extensions/messaging#simple
-chrome.runtime.onMessage.addListener(
-  async (message: MessageToBackground, sender: chrome.runtime.MessageSender, sendResponse): Promise<void> => {
-    console.log(message, sender);
-    switch (message.action) {
-      case MessageToBackgroundAction.ContentToBackground:
-        if (sender.tab) {
-          console.log('Received message from: ' + sender.tab.id);
-        }
-        sendResponse({ msg: 'Hello mate' });
-        history.push(WELCOME_ROUTE);
-        // do an operation in the extension
-        // replying back to content script
-        break;
-      case MessageToBackgroundAction.CreatePersona:
-        if (sender.id !== chrome.runtime.id) {
-          sendResponse('Sender is not allowed to perform this action');
-          return;
-        }
-        let response;
-        try {
-          const persona = await PersonaManager.create(message.data);
+async function handleMessageToBackground(
+  message: MessageToBackground,
+  sender: chrome.runtime.MessageSender
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+): Promise<any> {
+  console.log(message, sender);
+  switch (message.action) {
+    case MessageToBackgroundAction.ContentToBackground:
+      if (sender.tab) {
+        console.log('Received message from: ' + sender.tab.id);
+      }
+      return { msg: 'Hello mate' };
+    case MessageToBackgroundAction.CreatePersona:
+      if (sender.id !== chrome.runtime.id) {
+        return 'Sender is not allowed to perform this action';
+      }
+      let response;
+      try {
+        const persona = await PersonaManager.create(message.data);
 
-          signingServer = persona.startSigningServer();
-          console.log('Signing server ready to handle requests');
+        signingServer = persona.startSigningServer();
+        console.log('Signing server ready to handle requests');
 
-          response = {
-            mnemonic: persona.mnemonic,
-            txs: await persona.getTxs(),
-            accounts: await persona.getAccounts(),
-          };
-        } catch (error) {
-          console.error('Error when creating persona:', error);
-          response = error;
-        }
-        sendResponse(response);
-        break;
-      default:
-        sendResponse('Unknown action');
-    }
+        response = {
+          mnemonic: persona.mnemonic,
+          txs: await persona.getTxs(),
+          accounts: await persona.getAccounts(),
+        };
+      } catch (error) {
+        console.error('Error when creating persona:', error);
+        response = error;
+      }
+      return response;
+    default:
+      return 'Unknown action';
   }
-);
+}
+
+// https://developer.chrome.com/extensions/messaging#simple
+chrome.runtime.onMessage.addListener((message: MessageToBackground, sender, sendResponse) => {
+  handleMessageToBackground(message, sender)
+    .then(sendResponse)
+    .catch(console.error);
+
+  // return true to keep the sendResponse reference alive, see
+  // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/runtime/onMessage#Parameters
+  // However, the Promise return type seems to not work.
+  //
+  // > If you want to asynchronously use sendResponse, add return true; to the onMessage event handler.
+  // https://developer.chrome.com/extensions/messaging#simple
+  return true;
+});
 
 chrome.runtime.onMessageExternal.addListener((request, sender, sendResponse) => {
   if (!signingServer) {
