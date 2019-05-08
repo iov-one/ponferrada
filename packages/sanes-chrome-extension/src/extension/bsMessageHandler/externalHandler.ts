b457459dd8170a8eafd597de6533c437430064d7
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-05-08 17:33:24 +0200

    Removing unuseful comments

--- packages/sanes-chrome-extension/src/extension/bsMessageHandler/externalHandler.ts
+++ packages/sanes-chrome-extension/src/extension/bsMessageHandler/externalHandler.ts
@@ -3,18 +3,6 @@ import { UseOnlyJsonRpcSigningServer } from '../../logic/persona';
 
 type SigningServer = UseOnlyJsonRpcSigningServer | undefined;
 
-/*
-function handleResponseToExtension(request: JsonRpcRequest, sendResponse: SendResponse): void {
-  chrome.browserAction.setIcon({ path: 'assets/icons/request128.png' });
-  chrome.browserAction.setBadgeBackgroundColor({ color: [0, 0, 0, 0] });
-  chrome.browserAction.setBadgeText({ text: 'REQ' });
-
-  // TODO dispath info to redux for getting the info in the extension. Include the
-  // sendResponse ref for handling answer
-  sendResponse(`I have handled the ${request.method} request changing the icon`);
-}
-*/
-
 function generateErrorResponse(id: number | null): JsonRpcErrorResponse {
   return {
     jsonrpc: '2.0',
