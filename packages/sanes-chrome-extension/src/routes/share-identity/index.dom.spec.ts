a7ba3dec91be1c09217b83c1ab7056f164e69e98
Albert Andrejev <albert.andrejev@gmail.com>
2019-05-09 12:15:14 +0300

    Update Share identity test

--- packages/sanes-chrome-extension/src/routes/share-identity/index.dom.spec.ts
+++ packages/sanes-chrome-extension/src/routes/share-identity/index.dom.spec.ts
@@ -1,14 +1,15 @@
+import TestUtils from 'react-dom/test-utils';
 import { Store } from 'redux';
 import { RootState } from '../../store/reducers';
 import { aNewStore } from '../../store';
 import { travelToShareIdentity } from './test/travelToShareIdentity';
 import {
-  clickOnAcceptButton,
   clickOnBackButton,
   clickOnRejectButton,
-  confirmSelectionButton,
   checkPermanentRejection,
+  confirmRejectButton,
 } from './test/operateShareIdentity';
+import { sleep } from '../../utils/timer';
 
 describe('DOM > Feature > Share Identity', (): void => {
   let store: Store<RootState>;
@@ -19,21 +20,23 @@ describe('DOM > Feature > Share Identity', (): void => {
     identityDOM = await travelToShareIdentity(store);
   });
 
-  it('should accept incoming request and come back', async (): Promise<void> => {
-    await clickOnAcceptButton(identityDOM);
-    await confirmSelectionButton(identityDOM);
-    //TODO: Check here that share request rejection has been reject successfuly
+  it('should accept incoming request and redirect to account status view', async (): Promise<void> => {
+    const inputs = TestUtils.scryRenderedDOMComponentsWithTag(identityDOM, 'button');
 
-    /**
-     * Remove this code if not required in case if there is another redirection
-     * in confirmAcceptButton method. And apply this method in separate test method.
-     */
-    await clickOnBackButton(identityDOM);
+    expect(inputs.length).toBe(2);
+
+    const acceptButton = inputs[0];
+
+    TestUtils.act(() => {
+      TestUtils.Simulate.click(acceptButton);
+    });
+
+    //TODO: Check here that share request has been accepted successfuly
   }, 55000);
 
   it('should reject incoming request and come back', async (): Promise<void> => {
     await clickOnRejectButton(identityDOM);
-    await confirmSelectionButton(identityDOM);
+    await confirmRejectButton(identityDOM);
     //TODO: Check here that share request rejection has been reject successfuly
 
     /**
@@ -46,7 +49,8 @@ describe('DOM > Feature > Share Identity', (): void => {
   it('should reject incoming request permanently and come back', async (): Promise<void> => {
     await clickOnRejectButton(identityDOM);
     await checkPermanentRejection(identityDOM);
-    await confirmSelectionButton(identityDOM);
+    await confirmRejectButton(identityDOM);
+    await sleep(2000);
     //TODO: Check here that share request rejection has been reject successfuly and permanent
     //rejection flag has been set
   }, 55000);
--- packages/sanes-chrome-extension/src/routes/share-identity/test/operateShareIdentity.ts
+++ packages/sanes-chrome-extension/src/routes/share-identity/test/operateShareIdentity.ts
@@ -1,24 +1,7 @@
 import TestUtils from 'react-dom/test-utils';
 import { findRenderedDOMComponentWithId } from '../../../utils/test/reactElemFinder';
-import { SHARE_IDENTITY_ACCEPT } from '../components/AcceptRequest';
 import { SHARE_IDENTITY_SHOW } from '../components/ShowRequest';
 import { SHARE_IDENTITY_REJECT } from '../components/RejectRequest';
-
-export const clickOnAcceptButton = async (ShareIdentityDom: React.Component): Promise<void> => {
-  const inputs = TestUtils.scryRenderedDOMComponentsWithTag(ShareIdentityDom, 'button');
-
-  expect(inputs.length).toBe(2);
-
-  const acceptButton = inputs[0];
-
-  TestUtils.act(
-    (): void => {
-      TestUtils.Simulate.click(acceptButton);
-    }
-  );
-
-  await findRenderedDOMComponentWithId(ShareIdentityDom, SHARE_IDENTITY_ACCEPT);
-};
 
 export const clickOnRejectButton = async (ShareIdentityDom: React.Component): Promise<void> => {
   const inputs = TestUtils.scryRenderedDOMComponentsWithTag(ShareIdentityDom, 'button');
@@ -27,27 +10,23 @@ export const clickOnRejectButton = async (ShareIdentityDom: React.Component): Pr
 
   const rejectButton = inputs[1];
 
-  TestUtils.act(
-    (): void => {
-      TestUtils.Simulate.click(rejectButton);
-    }
-  );
+  TestUtils.act(() => {
+    TestUtils.Simulate.click(rejectButton);
+  });
 
   await findRenderedDOMComponentWithId(ShareIdentityDom, SHARE_IDENTITY_REJECT);
 };
 
-export const confirmSelectionButton = async (ShareIdentityDom: React.Component): Promise<void> => {
+export const confirmRejectButton = async (ShareIdentityDom: React.Component): Promise<void> => {
   const inputs = TestUtils.scryRenderedDOMComponentsWithTag(ShareIdentityDom, 'button');
 
   expect(inputs.length).toBe(2);
 
-  const confirmButton = inputs[0];
+  const rejectButton = inputs[0];
 
-  TestUtils.act(
-    (): void => {
-      TestUtils.Simulate.click(confirmButton);
-    }
-  );
+  TestUtils.act(() => {
+    TestUtils.Simulate.click(rejectButton);
+  });
 };
 
 export const checkPermanentRejection = async (ShareIdentityDom: React.Component): Promise<void> => {
@@ -55,13 +34,11 @@ export const checkPermanentRejection = async (ShareIdentityDom: React.Component)
   expect(inputs.length).toBe(1);
 
   const doNotShowAgainCheckbox = inputs[0];
-  TestUtils.act(
-    (): void => {
-      TestUtils.Simulate.change(doNotShowAgainCheckbox, {
-        target: { checked: true } as any, //eslint-disable-line @typescript-eslint/no-explicit-any
-      });
-    }
-  );
+  TestUtils.act(() => {
+    TestUtils.Simulate.change(doNotShowAgainCheckbox, {
+      target: { checked: true } as any, //eslint-disable-line @typescript-eslint/no-explicit-any
+    });
+  });
 };
 
 export const clickOnBackButton = async (ShareIdentityDom: React.Component): Promise<void> => {
@@ -71,11 +48,9 @@ export const clickOnBackButton = async (ShareIdentityDom: React.Component): Prom
 
   const backButton = inputs[1];
 
-  TestUtils.act(
-    (): void => {
-      TestUtils.Simulate.click(backButton);
-    }
-  );
+  TestUtils.act(() => {
+    TestUtils.Simulate.click(backButton);
+  });
 
   await findRenderedDOMComponentWithId(ShareIdentityDom, SHARE_IDENTITY_SHOW);
 };
