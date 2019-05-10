9ec671107bdc72b9f653597f2fb13869c545df44
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-05-10 17:10:58 +0200

    Fetch persona info when loading account status view

--- packages/sanes-chrome-extension/src/routes/account/index.tsx
+++ packages/sanes-chrome-extension/src/routes/account/index.tsx
@@ -5,19 +5,21 @@ import PageLayout from 'medulas-react-components/lib/components/PageLayout';
 import { ToastContext } from 'medulas-react-components/lib/context/ToastProvider';
 import { ToastVariant } from 'medulas-react-components/lib/context/ToastProvider/Toast';
 import Typography from 'medulas-react-components/lib/components/Typography';
-import { ACCOUNT_STATUS_ROUTE } from '../paths';
+import { ACCOUNT_STATUS_ROUTE, WELCOME_ROUTE } from '../paths';
 import SelectField, { Item } from 'medulas-react-components/lib/components/forms/SelectFieldForm';
 import { useForm } from 'react-final-form-hooks';
 import Form from 'medulas-react-components/lib/components/forms/Form';
 import { PersonaContext } from '../../context/PersonaProvider';
 import { AccountInfo } from '../../logic/persona/accountManager';
 import ListTxs from './components/ListTxs';
+import { history } from '../../store/reducers';
+import { sendGetPersonaMessage } from '../../extension/messages';
 
 const CREATE_NEW_ONE = 'Create a new one';
 
 const AccountView = (): JSX.Element => {
   const [accounts, setAccounts] = React.useState<Item[]>([]);
-  const persona = React.useContext(PersonaContext);
+  const personaProvider = React.useContext(PersonaContext);
   const { show } = React.useContext(ToastContext);
   const { form, handleSubmit } = useForm({
     onSubmit: () => {},
@@ -25,7 +27,7 @@ const AccountView = (): JSX.Element => {
 
   React.useEffect(() => {
     async function fetchMyAccounts(): Promise<void> {
-      const accounts = persona.accountNames;
+      const accounts = personaProvider.accountNames;
       let actualItems: Item[] = [];
       actualItems.push({ name: CREATE_NEW_ONE });
       accounts.forEach((acc: AccountInfo) => actualItems.push({ name: acc.name }));
@@ -33,7 +35,27 @@ const AccountView = (): JSX.Element => {
     }
 
     fetchMyAccounts();
-  }, [persona.accountNames]);
+  }, [personaProvider.accountNames]);
+
+  React.useEffect(() => {
+    async function fetchPersonaData(): Promise<void> {
+      const isExtensionContext = typeof chrome !== 'undefined';
+      if (!isExtensionContext) {
+        return;
+      }
+
+      const response = await sendGetPersonaMessage();
+      if (response) {
+        personaProvider.update(response.accounts, response.mnemonic, response.txs);
+      } else {
+        history.push(WELCOME_ROUTE);
+      }
+    }
+
+    fetchPersonaData();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const onChange = (item: Item): void => {
     if (item.name === CREATE_NEW_ONE) {
@@ -58,7 +80,7 @@ const AccountView = (): JSX.Element => {
       </Form>
       <Hairline space={2} />
       <Block marginBottom={4}>
-        <ListTxs title="Transations" txs={persona.txs} />
+        <ListTxs title="Transations" txs={personaProvider.txs} />
       </Block>
       <Block marginBottom={1}>
         <ListTxs title="Pending Transactions" txs={[]} />
