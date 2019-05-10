643cd63ec0d267fd11540525dfc4417709cfd955
Albert Andrejev <albert.andrejev@gmail.com>
2019-05-10 11:06:14 +0300

    Set default viewport to responsive

--- packages/bierzo-wallet/src/routes/welcome/index.stories.tsx
+++ packages/bierzo-wallet/src/routes/welcome/index.stories.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 import DecoratedStorybook, { WALLET_ROOT } from '../../utils/storybook';
 import Welcome from './index';
 
-storiesOf(WALLET_ROOT, module).add(
-  'Welcome page',
-  (): JSX.Element => (
-    <DecoratedStorybook>
-      <Welcome />
-    </DecoratedStorybook>
-  )
-);
+storiesOf(WALLET_ROOT, module)
+  .addParameters({ viewport: { defaultViewport: 'responsive' } })
+  .add(
+    'Welcome page',
+    (): JSX.Element => (
+      <DecoratedStorybook>
+        <Welcome />
+      </DecoratedStorybook>
+    )
+  );
