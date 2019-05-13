79e7f3eaaf27062bcc108f84ddb1d3483fffa067
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-05-13 12:59:11 +0200

    Removing unused import

--- packages/medulas-react-components/src/components/Drawer/index.tsx
+++ packages/medulas-react-components/src/components/Drawer/index.tsx
@@ -4,7 +4,6 @@ import { makeStyles, Theme } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
