11c593fda8d58b3cc58e35b4e0f6563ef57ff2c1
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-05-14 12:10:31 +0200

    Added RequestHandler logic for managing enqueued requests

--- packages/sanes-chrome-extension/src/extension/bsExternalHandler/requestHandler.ts
+++ packages/sanes-chrome-extension/src/extension/bsExternalHandler/requestHandler.ts
@@ -22,3 +22,38 @@ export function parseRpcCall(data: JsonRpcRequest): RpcCall {
   //eslint-disable-next-line
   return (data as any) as RpcCall;
 }
+
+interface Request {
+  readonly req: RpcCall;
+  readonly accept: () => void;
+  readonly reject: (permanently: boolean) => void;
+}
+
+export class RequestHandler {
+  private static instance: Request[];
+
+  public static create(): void {
+    RequestHandler.instance = [];
+  }
+
+  public static get(): Request {
+    if (!RequestHandler.instance) {
+      throw new Error('There are no requests stored');
+    }
+
+    const req = RequestHandler.instance.shift();
+    if (!req) {
+      throw new Error('Shifted element is undefined');
+    }
+
+    return req;
+  }
+
+  public static add(req: Request): number {
+    if (!RequestHandler.instance) {
+      throw new Error('Request handler instance has not been initilised');
+    }
+
+    return RequestHandler.instance.unshift(req);
+  }
+}
