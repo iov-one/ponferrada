a9377f57c61f67a6458b0b4cd299c40ce06465fc
Simon Warta <simon@kullo.net>
2019-05-14 10:16:58 +0200

    Reduce boilerplate in test DOM > Feature > Welcome

--- packages/sanes-chrome-extension/src/routes/welcome/index.dom.spec.ts
+++ packages/sanes-chrome-extension/src/routes/welcome/index.dom.spec.ts
@@ -7,52 +7,34 @@ import { aNewStore } from '../../store';
 import { whenOnNavigatedToRoute } from '../../utils/test/navigation';
 import { sleep } from '../../utils/timer';
 
-describe('DOM > Feature > Welcome', (): void => {
+describe('DOM > Feature > Welcome', () => {
   let store: Store<RootState>;
   let dom: React.Component;
 
-  beforeEach(
-    async (): Promise<void> => {
-      store = aNewStore();
-      dom = createDom(store);
-      TestUtils.act(
-        (): void => {
-          history.push(WELCOME_ROUTE);
-        },
-      );
-      await sleep(500);
-    },
-  );
+  beforeEach(async () => {
+    store = aNewStore();
+    dom = createDom(store);
+    TestUtils.act(() => history.push(WELCOME_ROUTE));
+    await sleep(500);
+  });
 
-  it(`should create Welcome layout view`, async (): Promise<void> => {
+  it('should create Welcome layout view', async () => {
     await whenOnNavigatedToRoute(store, WELCOME_ROUTE);
   }, 55000);
 
-  it(`should create three buttons and redirect to appropriate route when clicking on it`, async (): Promise<
-    void
-  > => {
+  it('should create three buttons and redirect to appropriate route when clicking on it', async () => {
     const buttons = TestUtils.scryRenderedDOMComponentsWithTag(dom, 'button');
 
     expect(buttons.length).toBe(3);
 
     //Click on login button
     const loginButton = buttons[0];
-    TestUtils.act(
-      (): void => {
-        TestUtils.Simulate.click(loginButton);
-      },
-    );
-
+    TestUtils.act(() => TestUtils.Simulate.click(loginButton));
     await whenOnNavigatedToRoute(store, LOGIN_ROUTE);
 
     //Click on signup button
     const signUpButton = buttons[1];
-    TestUtils.act(
-      (): void => {
-        TestUtils.Simulate.click(signUpButton);
-      },
-    );
-
+    TestUtils.act(() => TestUtils.Simulate.click(signUpButton));
     await whenOnNavigatedToRoute(store, SIGNUP_ROUTE);
   }, 55000);
 });
