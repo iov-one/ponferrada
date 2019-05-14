c232a8044a85ad2a77467c4806cc533d53d00b9c
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-05-14 13:56:38 +0200

    Add block localStorage logic in SenderWhitelist

--- packages/sanes-chrome-extension/src/extension/bsExternalHandler/senderWhitelist.ts
+++ packages/sanes-chrome-extension/src/extension/bsExternalHandler/senderWhitelist.ts
@@ -4,18 +4,15 @@ export function isNewSender(sender: string): boolean {
 
 const ALLOWED_URLS_KEY = 'auk';
 const BLOCKED_URLS_KEY = 'buk';
-const SEPARATOR = '|';
+const SEPARATOR = ',';
 
 export class SenderWhitelist {
   private static blocked: ReadonlyArray<string> | undefined = undefined;
   private static allowed: ReadonlyArray<string> | undefined = undefined;
 
   public static load(): void {
-    const allowed = localStorage.getItem(ALLOWED_URLS_KEY);
-    const blocked = localStorage.getItem(BLOCKED_URLS_KEY);
-
-    SenderWhitelist.allowed = allowed ? allowed.split(SEPARATOR) : [];
-    SenderWhitelist.blocked = blocked ? blocked.split(SEPARATOR) : [];
+    SenderWhitelist.allowed = SenderWhitelist.loadAllowed();
+    SenderWhitelist.blocked = SenderWhitelist.loadBlocked();
   }
 
   public static isBlocked(sender: string): boolean {
@@ -44,5 +41,28 @@ export class SenderWhitelist {
 
   public static loaded(): boolean {
     return SenderWhitelist.blocked !== undefined && SenderWhitelist.allowed !== undefined;
+  }
+
+  public static block(sender: string): void {
+    const blockedElements = SenderWhitelist.blocked || [];
+    const elem = blockedElements.find(url => url === sender);
+    if (elem) {
+      return;
+    }
+
+    const elements = [...blockedElements, sender];
+    localStorage.setItem(BLOCKED_URLS_KEY, elements.toString());
+
+    SenderWhitelist.load();
+  }
+
+  private static loadBlocked(): ReadonlyArray<string> {
+    const blocked = localStorage.getItem(BLOCKED_URLS_KEY);
+    return blocked ? blocked.split(SEPARATOR) : [];
+  }
+
+  private static loadAllowed(): ReadonlyArray<string> {
+    const allowed = localStorage.getItem(ALLOWED_URLS_KEY);
+    return allowed ? allowed.split(SEPARATOR) : [];
   }
 }
