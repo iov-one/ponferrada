4e68e86ec7c61763203a130f98758abf30f27fa8
Simon Warta <simon@kullo.net>
2019-05-14 09:45:42 +0200

    Pass sender into handleExternalMessage

--- packages/sanes-chrome-extension/src/extension/bsMessageHandler/externalHandler.ts
+++ packages/sanes-chrome-extension/src/extension/bsMessageHandler/externalHandler.ts
@@ -18,7 +18,12 @@ export async function handleExternalMessage(
   signingServer: SigningServer,
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   request: any,
+  sender: chrome.runtime.MessageSender,
 ): Promise<JsonRpcResponse> {
+  if (!sender.url) {
+    throw new Error('Got external message without sender URL');
+  }
+
   if (!signingServer) {
     const responseId = typeof request.id === 'number' ? request.id : null;
     return generateErrorResponse(responseId);
