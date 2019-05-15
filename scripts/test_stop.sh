b89a7b643b5de37e348fd762b2d3938ad6ca0f02
Simon Warta <simon@kullo.net>
2019-05-15 16:15:49 +0200

    Run scripts via shebang

--- scripts/test_stop.sh
+++ scripts/test_stop.sh
@@ -10,26 +10,26 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo
 echo ">>> Stopping bns chain and faucet..."
 echo
-bash "${SCRIPT_DIR}"/faucet/bnsd_stop.sh
-bash "${SCRIPT_DIR}"/bnsd/stop.sh
+"${SCRIPT_DIR}"/faucet/bnsd_stop.sh
+"${SCRIPT_DIR}"/bnsd/stop.sh
 
 echo
 echo ">>> Stopping bcpd (demo) chain and faucet..."
 echo
-bash "${SCRIPT_DIR}"/faucet/bcpd_stop.sh
-bash "${SCRIPT_DIR}"/bcpd/stop.sh
+"${SCRIPT_DIR}"/faucet/bcpd_stop.sh
+"${SCRIPT_DIR}"/bcpd/stop.sh
 
 echo
 echo ">>> Stopping lisk chain and faucet..."
 echo
-bash "${SCRIPT_DIR}"/faucet/lisk_stop.sh
-bash "${SCRIPT_DIR}"/lisk/stop.sh
+"${SCRIPT_DIR}"/faucet/lisk_stop.sh
+"${SCRIPT_DIR}"/lisk/stop.sh
 
 echo
 echo ">>> Stopping ethereum chain, scraper and faucet..."
 echo
-bash "${SCRIPT_DIR}"/faucet/ethereum_stop.sh
-bash "${SCRIPT_DIR}"/ethereum/scraper_stop.sh
-bash "${SCRIPT_DIR}"/ethereum/stop.sh
+"${SCRIPT_DIR}"/faucet/ethereum_stop.sh
+"${SCRIPT_DIR}"/ethereum/scraper_stop.sh
+"${SCRIPT_DIR}"/ethereum/stop.sh
 
 echo "Done!"
