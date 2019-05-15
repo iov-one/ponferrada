42f3a650f6dff32f81c9065dade03ef984cd00bc
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-05-15 10:23:17 +0200

    Create badgeUpdater class

---
+++ packages/sanes-chrome-extension/src/extension/background/internalHandler/badgeUpdater.ts
@@ -0,0 +1,17 @@
+/*global chrome*/
+import { RequestHandler } from '../externalHandler/requestHandler';
+
+export function updateExtensionBadge(): void {
+  const isExtensionContext = typeof chrome !== 'undefined'; // needed for tests
+  if (!isExtensionContext) {
+    return;
+  }
+
+  const queueSize = RequestHandler.requests().length;
+  const badgeText = queueSize === 0 ? '' : `${queueSize}`;
+  const iconPath = queueSize === 0 ? 'assets/icons/icon128.png' : 'assets/icons/request128.png';
+
+  chrome.browserAction.setIcon({ path: iconPath });
+  chrome.browserAction.setBadgeBackgroundColor({ color: [0, 0, 0, 0] });
+  chrome.browserAction.setBadgeText({ text: badgeText });
+}
