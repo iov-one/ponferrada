e909d5c560e68cec02105629d2d91c193b366c53
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-05-15 13:18:50 +0200

    Exposing extension badge updater function into a separate file

---
+++ packages/sanes-chrome-extension/src/extension/background/createPersona/requestExtensionBadge.ts
@@ -0,0 +1,17 @@
+import { RequestHandler } from './requestHandler';
+
+/*global chrome*/
+export function updateExtensionBadge(): void {
+  const isExtensionContext = typeof chrome !== 'undefined'; // needed for tests
+  if (!isExtensionContext) {
+    return;
+  }
+
+  const queueSize = RequestHandler.requests().length;
+  const badgeText = queueSize === 0 ? '' : `${queueSize}`;
+  const iconPath = queueSize === 0 ? 'assets/icons/icon128.png' : 'assets/icons/request128.png';
+
+  chrome.browserAction.setIcon({ path: iconPath });
+  chrome.browserAction.setBadgeBackgroundColor({ color: [0, 0, 0, 0] });
+  chrome.browserAction.setBadgeText({ text: badgeText });
+}
