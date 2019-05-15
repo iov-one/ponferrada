6c19254a7490dd5db245acc431f2298e057ac3ea
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-05-15 13:18:23 +0200

    Refactor request callback

--- packages/sanes-chrome-extension/src/extension/background/createPersona/requestCallback.ts
+++ packages/sanes-chrome-extension/src/extension/background/createPersona/requestCallback.ts
@@ -1,5 +1,5 @@
-/*global chrome*/
 import { PublicIdentity, UnsignedTransaction } from '@iov/bcp';
+import { updateExtensionBadge } from './requestExtensionBadge';
 import { RequestHandler } from './requestHandler';
 import { SenderWhitelist } from './requestSenderWhitelist';
 
@@ -16,84 +16,54 @@ function isRequestMeta(data: unknown): data is RequestMeta {
   return typeof (data as RequestMeta).senderUrl === 'string';
 }
 
-function updateExtensionBadge(): void {
-  const isExtensionContext = typeof chrome !== 'undefined'; // needed for tests
-  if (!isExtensionContext) {
-    return;
+type CallbackResponse = ReadonlyArray<PublicIdentity> | boolean;
+
+async function requestCallback(
+  reason: string,
+  meta: RequestMeta,
+  acceptResponse: CallbackResponse,
+  rejectResponse: CallbackResponse,
+): Promise<CallbackResponse> {
+  if (!isRequestMeta(meta)) {
+    throw new Error('Unexpected type of data in meta field');
   }
+  const { senderUrl } = meta;
 
-  const queueSize = RequestHandler.requests().length;
-  const badgeText = queueSize === 0 ? '' : `${queueSize}`;
-  const iconPath = queueSize === 0 ? 'assets/icons/icon128.png' : 'assets/icons/request128.png';
+  return new Promise(resolve => {
+    const accept = (): void => {
+      RequestHandler.solved();
+      updateExtensionBadge();
 
-  chrome.browserAction.setIcon({ path: iconPath });
-  chrome.browserAction.setBadgeBackgroundColor({ color: [0, 0, 0, 0] });
-  chrome.browserAction.setBadgeText({ text: badgeText });
+      resolve(acceptResponse);
+    };
+
+    const reject = (permanent: boolean): void => {
+      if (permanent) {
+        SenderWhitelist.block(senderUrl);
+      }
+      RequestHandler.solved();
+      updateExtensionBadge();
+
+      resolve(rejectResponse);
+    };
+
+    RequestHandler.add({ reason, sender: senderUrl, accept, reject });
+    updateExtensionBadge();
+  });
 }
 
 export async function getIdentitiesCallback(
   reason: string,
   matchingIdentities: ReadonlyArray<PublicIdentity>,
   meta: RequestMeta,
-): Promise<ReadonlyArray<PublicIdentity>> {
-  if (!isRequestMeta(meta)) {
-    throw new Error('Unexpected type of data in meta field');
-  }
-  const { senderUrl } = meta;
-
-  return new Promise(resolve => {
-    const accept = (): void => {
-      RequestHandler.solved();
-      updateExtensionBadge();
-
-      resolve(matchingIdentities);
-    };
-
-    const reject = (permanent: boolean): void => {
-      if (permanent) {
-        SenderWhitelist.block(senderUrl);
-      }
-      RequestHandler.solved();
-      updateExtensionBadge();
-
-      resolve([]);
-    };
-
-    RequestHandler.add({ reason, sender: meta.senderUrl, accept, reject });
-    updateExtensionBadge();
-  });
+): Promise<CallbackResponse> {
+  return requestCallback(reason, meta, matchingIdentities, []);
 }
 
 export async function signAndPostCallback(
   reason: string,
   _transaction: UnsignedTransaction,
   meta: RequestMeta,
-): Promise<boolean> {
-  if (!isRequestMeta(meta)) {
-    throw new Error('Unexpected type of data in meta field');
-  }
-
-  const { senderUrl } = meta;
-
-  return new Promise(resolve => {
-    const accept = (): void => {
-      RequestHandler.solved();
-      updateExtensionBadge();
-
-      resolve(true);
-    };
-
-    const reject = (permanent: boolean): void => {
-      if (permanent) {
-        SenderWhitelist.block(senderUrl);
-      }
-      RequestHandler.solved();
-      updateExtensionBadge();
-
-      resolve(false);
-    };
-
-    RequestHandler.add({ reason, sender: meta.senderUrl, accept, reject });
-    updateExtensionBadge();
-  });
+): Promise<CallbackResponse> {
+  return requestCallback(reason, meta, true, false);
 }
