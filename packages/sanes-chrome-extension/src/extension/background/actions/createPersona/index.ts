4346f41e0b407ce0667320a02ada4338b5c4d97e
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-05-16 15:02:28 +0200

    Removal of PersonaManager static entity

--- packages/sanes-chrome-extension/src/extension/background/actions/createPersona/index.ts
+++ packages/sanes-chrome-extension/src/extension/background/actions/createPersona/index.ts
@@ -1,4 +1,4 @@
-import { PersonaManager, UseOnlyJsonRpcSigningServer } from '../../../../logic/persona';
+import { Persona, UseOnlyJsonRpcSigningServer } from '../../../../logic/persona';
 import { CreatePersonaResponse } from '../../messages';
 import { transactionsUpdater } from './requestAppUpdater';
 import { getIdentitiesCallback, signAndPostCallback } from './requestCallback';
@@ -7,14 +7,21 @@ import { SenderWhitelist } from './requestSenderWhitelist';
 
 export type SigningServer = UseOnlyJsonRpcSigningServer | undefined;
 let signingServer: SigningServer;
+let persona: Persona;
 
 export function getSigningServer(): SigningServer {
   return signingServer;
 }
 
+export function getCreatedPersona(): Persona {
+  if (!persona) {
+    throw new Error('Persona instance does not exist');
+  }
+  return persona;
+}
+
 export async function createPersona(mnemonic?: string): Promise<CreatePersonaResponse> {
-  // TODO refactor when #175
-  const persona = await PersonaManager.create(mnemonic);
+  persona = await Persona.create(mnemonic);
   signingServer = persona.startSigningServer(getIdentitiesCallback, signAndPostCallback, transactionsUpdater);
   console.log('Signing server ready to handle requests');
 
