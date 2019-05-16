c316c5a92524c0b80bace62bcf37f03b15aaafeb
Albert Andrejev <albert.andrejev@gmail.com>
2019-05-16 10:32:49 +0300

    Add DOM test

---
+++ packages/sanes-chrome-extension/src/routes/tx-request/index.dom.spec.ts
@@ -0,0 +1,57 @@
+import TestUtils from 'react-dom/test-utils';
+import { Store } from 'redux';
+import { RootState } from '../../store/reducers';
+import { aNewStore } from '../../store';
+import { travelToTXRequest } from './test/travelToTXRequest';
+import {
+  clickOnBackButton,
+  clickOnRejectButton,
+  checkPermanentRejection,
+  confirmRejectButton,
+} from './test/operateTXRequest';
+import { sleep } from '../../utils/timer';
+
+describe('DOM > Feature > Transaction Request', (): void => {
+  let store: Store<RootState>;
+  let identityDOM: React.Component;
+
+  beforeEach(async () => {
+    store = aNewStore();
+    identityDOM = await travelToTXRequest(store);
+  });
+
+  it('should accept incoming request and redirect to account status view', async (): Promise<void> => {
+    const inputs = TestUtils.scryRenderedDOMComponentsWithTag(identityDOM, 'button');
+
+    expect(inputs.length).toBe(2);
+
+    const acceptButton = inputs[0];
+
+    TestUtils.act(() => {
+      TestUtils.Simulate.click(acceptButton);
+    });
+
+    //TODO: Check here that share request has been accepted successfuly
+  }, 55000);
+
+  it('should reject incoming request and come back', async (): Promise<void> => {
+    await clickOnRejectButton(identityDOM);
+    await confirmRejectButton(identityDOM);
+    //TODO: Check here that share request rejection has been reject successfuly
+
+    /**
+     * Remove this code if not required in case if there is another redirection
+     * in confirmAcceptButton method. And apply this method in separate test method.
+     */
+    await clickOnBackButton(identityDOM);
+  }, 55000);
+
+  it('should reject incoming request permanently and come back', async (): Promise<void> => {
+    await clickOnRejectButton(identityDOM);
+    await checkPermanentRejection(identityDOM);
+    await confirmRejectButton(identityDOM);
+    await sleep(2000);
+    //TODO: Check here that share request rejection has been reject successfuly and permanent
+    //rejection flag has been set
+  }, 55000);
+});
