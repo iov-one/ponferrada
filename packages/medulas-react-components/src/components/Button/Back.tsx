9753374335811adf2f614ac577c40d0c530522f0
Albert Andrejev <albert.andrejev@gmail.com>
2019-05-16 12:03:15 +0300

    Adding aria-label to back button

--- packages/medulas-react-components/src/components/Button/Back.tsx
+++ packages/medulas-react-components/src/components/Button/Back.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import MuiButton, { ButtonProps } from '@material-ui/core/Button';
 
 const Back = ({ children, ...restProps }: ButtonProps): JSX.Element => (
-  <MuiButton {...restProps} variant="text">
+  <MuiButton {...restProps} variant="text" aria-label="Go back">
     {children}
   </MuiButton>
 );
