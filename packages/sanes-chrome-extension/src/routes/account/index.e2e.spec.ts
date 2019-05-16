99c84d91e99c70e2c77139c746dda5f878544ffd
Simon Warta <simon@kullo.net>
2019-05-16 11:05:07 +0200

    Create e2e test for account status

---
+++ packages/sanes-chrome-extension/src/routes/account/index.e2e.spec.ts
@@ -0,0 +1,42 @@
+import { Browser, Page } from 'puppeteer';
+import { closeBrowser, createPage, launchBrowser } from '../../utils/test/e2e';
+import { withChainsDescribe } from '../../utils/test/testExecutor';
+import { submitRecoveryPhraseE2E } from '../restore-account/test/fillRecoveryPhrase';
+import { travelToRestoreAccountStep } from '../restore-account/test/travelToRestoreAccount';
+import { sleep } from '../../utils/timer';
+
+withChainsDescribe('E2E > Account route', () => {
+  let browser: Browser;
+  let page: Page;
+
+  beforeEach(async () => {
+    browser = await launchBrowser();
+    page = await createPage(browser);
+
+    await travelToRestoreAccountStep(page);
+    const mnemonic = 'degree tackle suggest window test behind mesh extra cover prepare oak script';
+    await submitRecoveryPhraseE2E(page, mnemonic);
+  }, 10000);
+
+  afterEach(async () => {
+    await closeBrowser(browser);
+  });
+
+  it('can create a new account', async () => {
+    await sleep(500);
+
+    // click 2nd item (nothing happens)
+    await page.click(`input[name="SELECT_FIELD_ATTR"]`);
+    await page.click(`div[role="tooltip"] nav > div:nth-child(2)`);
+
+    // click 1st item (create account)
+    await page.click(`input[name="SELECT_FIELD_ATTR"]`);
+    await page.click(`div[role="tooltip"] nav > div:nth-child(1)`);
+
+    await sleep(1000); // wait for account creation do be done
+
+    // click 3rd item (the newly created account)
+    // await page.click(`input[name="SELECT_FIELD_ATTR"]`);
+    // await page.click(`div[role="tooltip"] nav > div:nth-child(3)`);
+  }, 20000);
+});
