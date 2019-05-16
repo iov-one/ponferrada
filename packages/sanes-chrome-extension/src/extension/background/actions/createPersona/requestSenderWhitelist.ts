a4dcecff673dd33d029615ad541681bfec0b386d
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-05-16 11:31:17 +0200

    Remove unnused allowed sender urls logic

--- packages/sanes-chrome-extension/src/extension/background/actions/createPersona/requestSenderWhitelist.ts
+++ packages/sanes-chrome-extension/src/extension/background/actions/createPersona/requestSenderWhitelist.ts
@@ -1,14 +1,12 @@
-const ALLOWED_URLS_KEY = 'auk';
 const BLOCKED_URLS_KEY = 'buk';
 const SEPARATOR = ',';
 
 export class SenderWhitelist {
   private static blocked: ReadonlyArray<string> | undefined = undefined;
-  private static allowed: ReadonlyArray<string> | undefined = undefined;
 
   public static load(): void {
-    SenderWhitelist.allowed = SenderWhitelist.loadAllowed();
-    SenderWhitelist.blocked = SenderWhitelist.loadBlocked();
+    const blocked = localStorage.getItem(BLOCKED_URLS_KEY);
+    SenderWhitelist.blocked = blocked ? blocked.split(SEPARATOR) : [];
   }
 
   public static isBlocked(sender: string): boolean {
@@ -19,24 +17,6 @@ export class SenderWhitelist {
     const elem = SenderWhitelist.blocked.find(url => url === sender);
 
     return !!elem;
-  }
-
-  public static isAllowed(sender: string): boolean {
-    if (!SenderWhitelist.allowed) {
-      throw new Error('SenderWhitelist has not been initialized');
-    }
-
-    const elem = SenderWhitelist.allowed.find(url => url === sender);
-
-    return !!elem;
-  }
-
-  public static isNewSender(sender: string): boolean {
-    return !SenderWhitelist.isBlocked(sender) && !SenderWhitelist.isAllowed(sender);
-  }
-
-  public static loaded(): boolean {
-    return SenderWhitelist.blocked !== undefined && SenderWhitelist.allowed !== undefined;
   }
 
   public static block(sender: string): void {
@@ -50,15 +30,5 @@ export class SenderWhitelist {
     localStorage.setItem(BLOCKED_URLS_KEY, elements.toString());
 
     SenderWhitelist.load();
-  }
-
-  private static loadBlocked(): ReadonlyArray<string> {
-    const blocked = localStorage.getItem(BLOCKED_URLS_KEY);
-    return blocked ? blocked.split(SEPARATOR) : [];
-  }
-
-  private static loadAllowed(): ReadonlyArray<string> {
-    const allowed = localStorage.getItem(ALLOWED_URLS_KEY);
-    return allowed ? allowed.split(SEPARATOR) : [];
   }
 }
