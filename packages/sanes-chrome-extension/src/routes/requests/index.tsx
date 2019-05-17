300c167d94e3a8d209b6c5989aa9a531755d9bf9
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-05-17 17:01:04 +0200

    Dummy request view for showing queued txs

--- packages/sanes-chrome-extension/src/routes/requests/index.tsx
+++ packages/sanes-chrome-extension/src/routes/requests/index.tsx
@@ -3,8 +3,18 @@ import { RequestContext } from '../../context/RequestProvider';
 
 const Requests = (): JSX.Element => {
   const requestContext = React.useContext(RequestContext);
+  const onResolve = (): void => {
+    const firstTx = requestContext.requests[0];
+    console.log(firstTx);
+    firstTx.reject(false);
+  };
 
-  return <div>We have: {requestContext.requests.length}</div>;
+  return (
+    <React.Fragment>
+      <div>We have: {requestContext.requests.length}</div>
+      <button onClick={onResolve}>Resolve the first one</button>
+    </React.Fragment>
+  );
 };
 
 export default Requests;
