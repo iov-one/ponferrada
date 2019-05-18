f6ef7c9eb8728c16d1777069d5eb366da952e0d4
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-05-18 12:10:57 +0200

    Adding ListItemAvatar to be exported from mui

--- packages/medulas-react-components/src/components/List/index.tsx
+++ packages/medulas-react-components/src/components/List/index.tsx
@@ -1,6 +1,7 @@
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 
-export { List, ListItem, ListItemText, ListItemIcon };
+export { List, ListItem, ListItemText, ListItemAvatar, ListItemIcon };
