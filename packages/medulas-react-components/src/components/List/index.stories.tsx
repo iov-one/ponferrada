06e9763829a12757c61ccdf1f355124f4885a904
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-05-18 12:19:00 +0200

    Adding List storybook component

---
+++ packages/medulas-react-components/src/components/List/index.stories.tsx
@@ -0,0 +1,61 @@
+import { makeStyles, Theme } from '@material-ui/core/styles';
+import BeachAccessIcon from '@material-ui/icons/BeachAccess';
+import ImageIcon from '@material-ui/icons/Image';
+import WorkIcon from '@material-ui/icons/Work';
+import { storiesOf } from '@storybook/react';
+import * as React from 'react';
+import { Storybook } from '../../utils/storybook';
+import { Avatar } from '../Avatar';
+import { List, ListItem, ListItemAvatar, ListItemText } from './index';
+
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+function FolderList(): JSX.Element {
+  const classes = useStyles();
+
+  return (
+    <List className={classes.root}>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar>
+            <ImageIcon />
+          </Avatar>
+        </ListItemAvatar>
+        <ListItemText primary="Photos" secondary="Jan 9, 2014" />
+      </ListItem>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar>
+            <WorkIcon />
+          </Avatar>
+        </ListItemAvatar>
+        <ListItemText primary="Work" secondary="Jan 7, 2014" />
+      </ListItem>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar>
+            <BeachAccessIcon />
+          </Avatar>
+        </ListItemAvatar>
+        <ListItemText primary="Vacation" secondary="July 20, 2014" />
+      </ListItem>
+    </List>
+  );
+}
+
+export default FolderList;
+
+storiesOf('Components', module).add(
+  'Lists',
+  (): JSX.Element => (
+    <Storybook>
+      <FolderList />
+    </Storybook>
+  ),
+);
