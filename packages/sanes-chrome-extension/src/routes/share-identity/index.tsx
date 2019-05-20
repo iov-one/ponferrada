11a137b4d4cd616a2999d1d7b0a548baebc9d029
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-05-20 22:40:58 +0200

    Add request logic to share-identity

--- packages/sanes-chrome-extension/src/routes/share-identity/index.tsx
+++ packages/sanes-chrome-extension/src/routes/share-identity/index.tsx
@@ -1,19 +1,47 @@
+import { Location } from 'history';
+import { ToastContext } from 'medulas-react-components/lib/context/ToastProvider';
+import { ToastVariant } from 'medulas-react-components/lib/context/ToastProvider/Toast';
 import * as React from 'react';
+import { RouteComponentProps, withRouter } from 'react-router';
+import { RequestContext } from '../../context/RequestProvider';
+import { history } from '../../store/reducers';
+import { REQUEST_ROUTE } from '../paths';
+import { REQUEST_FIELD } from '../requests/components/RequestList';
 import RejectRequest from './components/RejectRequest';
 import ShowRequest from './components/ShowRequest';
 
-const ShareIdentity = (): JSX.Element => {
+function getIdFrom(location: Location): number | undefined {
+  if (!location || !location.state) {
+    return undefined;
+  }
+
+  return location.state[REQUEST_FIELD];
+}
+
+const ShareIdentity = ({ location }: RouteComponentProps): JSX.Element => {
   const [action, setAction] = React.useState<'show' | 'reject'>('show');
+  const toast = React.useContext(ToastContext);
+  const requestContext = React.useContext(RequestContext);
+
+  const request = requestContext.firstRequest;
+  const expectedId = getIdFrom(location);
+
+  if (!request || typeof expectedId === undefined || expectedId !== request.id) {
+    toast.show('Error: Request not identified', ToastVariant.ERROR);
+    history.push(REQUEST_ROUTE);
+  }
 
   const showRequestView = (): void => setAction('show');
   const showRejectView = (): void => setAction('reject');
 
   const onAcceptRequest = (): void => {
-    console.log('Accept request');
+    request!.accept(); // eslint-disable-line
+    history.push(REQUEST_ROUTE);
   };
 
   const onRejectRequest = (permanent: boolean): void => {
-    console.log(`Reject request. Permanent ${permanent ? 'yes' : 'no'}`);
+    request!.reject(permanent); // eslint-disable-line
+    history.push(REQUEST_ROUTE);
   };
 
   return (
@@ -24,4 +52,4 @@ const ShareIdentity = (): JSX.Element => {
   );
 };
 
-export default ShareIdentity;
+export default withRouter(ShareIdentity);
