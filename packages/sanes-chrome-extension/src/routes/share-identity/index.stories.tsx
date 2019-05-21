f374f1a809a52695a359cb61254a23e11be3d930
Albert Andrejev <albert.andrejev@gmail.com>
2019-05-21 13:59:34 +0300

    Stories update

--- packages/sanes-chrome-extension/src/routes/share-identity/index.stories.tsx
+++ packages/sanes-chrome-extension/src/routes/share-identity/index.stories.tsx
@@ -5,6 +5,7 @@ import ShowRequest from './components/ShowRequest';
 import RejectRequest from './components/RejectRequest';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
+import { Request } from '../../extension/background/actions/createPersona/requestHandler';
 import { CHROME_EXTENSION_ROOT } from '../../utils/storybook';
 import { ACCOUNT_STATUS_PAGE } from '../account/index.stories';
 
@@ -12,12 +13,22 @@ const SHARE_IDENTITY_PATH = `${CHROME_EXTENSION_ROOT}/Share Identity`;
 const SHOW_REQUEST_PAGE = 'Show Request page';
 const REJECT_REQUEST_PAGE = 'Reject Request page';
 
+const request: Request = {
+  id: 0,
+  accept: () => action('accept request'),
+  reject: (permanent: boolean) => action(`reject request. Permanently: ${permanent ? 'yes' : 'no'}`),
+  reason: 'I would like to know who you are on Ethereum"',
+  sender: 'http://localhost/',
+  type: 'getIdentities',
+};
+
 storiesOf(SHARE_IDENTITY_PATH, module)
   .add(
     SHOW_REQUEST_PAGE,
     (): JSX.Element => (
       <Storybook>
         <ShowRequest
+          request={request}
           onAcceptRequest={linkTo(CHROME_EXTENSION_ROOT, ACCOUNT_STATUS_PAGE)}
           showRejectView={linkTo(SHARE_IDENTITY_PATH, REJECT_REQUEST_PAGE)}
         />
@@ -29,6 +40,7 @@ storiesOf(SHARE_IDENTITY_PATH, module)
     (): JSX.Element => (
       <Storybook>
         <RejectRequest
+          request={request}
           onBack={linkTo(SHARE_IDENTITY_PATH, SHOW_REQUEST_PAGE)}
           onRejectRequest={action('onAcceptRequest')}
         />
--- packages/sanes-chrome-extension/src/routes/tx-request/index.stories.tsx
+++ packages/sanes-chrome-extension/src/routes/tx-request/index.stories.tsx
@@ -5,6 +5,7 @@ import ShowRequest from './components/ShowRequest';
 import RejectRequest from './components/RejectRequest';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
+import { Request } from '../../extension/background/actions/createPersona/requestHandler';
 import { CHROME_EXTENSION_ROOT } from '../../utils/storybook';
 import { ACCOUNT_STATUS_PAGE } from '../account/index.stories';
 
@@ -12,12 +13,22 @@ const TX_REQUEST_PATH = `${CHROME_EXTENSION_ROOT}/Transaction Request`;
 const SHOW_REQUEST_PAGE = 'Show Request page';
 const REJECT_REQUEST_PAGE = 'Reject Request page';
 
+const request: Request = {
+  id: 0,
+  accept: () => action('accept request'),
+  reject: (permanent: boolean) => action(`reject request. Permanently: ${permanent ? 'yes' : 'no'}`),
+  reason: 'I would like to know who you are on Ethereum"',
+  sender: 'http://localhost/',
+  type: 'getIdentities',
+};
+
 storiesOf(TX_REQUEST_PATH, module)
   .add(
     SHOW_REQUEST_PAGE,
     (): JSX.Element => (
       <Storybook>
         <ShowRequest
+          request={request}
           onAcceptRequest={linkTo(CHROME_EXTENSION_ROOT, ACCOUNT_STATUS_PAGE)}
           showRejectView={linkTo(TX_REQUEST_PATH, REJECT_REQUEST_PAGE)}
         />
@@ -29,6 +40,7 @@ storiesOf(TX_REQUEST_PATH, module)
     (): JSX.Element => (
       <Storybook>
         <RejectRequest
+          request={request}
           onBack={linkTo(TX_REQUEST_PATH, SHOW_REQUEST_PAGE)}
           onRejectRequest={action('onAcceptRequest')}
         />
