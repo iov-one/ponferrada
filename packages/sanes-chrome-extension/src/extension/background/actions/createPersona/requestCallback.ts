9389a9f65aace9631a2a8cab8c9469b893430ffa
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-05-22 10:29:23 +0200

    Shift element in first place when rejecting a request

--- packages/sanes-chrome-extension/src/extension/background/actions/createPersona/requestCallback.ts
+++ packages/sanes-chrome-extension/src/extension/background/actions/createPersona/requestCallback.ts
@@ -38,6 +38,7 @@ async function requestCallback<T>(
     };
 
     const reject = (permanent: boolean): void => {
+      RequestHandler.solved();
       if (permanent) {
         SenderWhitelist.block(senderUrl);
       }
