ca574918ecef9a204ce8a3a76794d04fee470ad6
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-05-23 17:20:30 +0200

    Fix TX requests TS errors

--- packages/sanes-chrome-extension/src/routes/tx-request/components/ShowRequest.tsx
+++ packages/sanes-chrome-extension/src/routes/tx-request/components/ShowRequest.tsx
@@ -1,27 +1,26 @@
-import * as React from 'react';
-import Button from 'medulas-react-components/lib/components/Button';
-import Typography from 'medulas-react-components/lib/components/Typography';
 import Block from 'medulas-react-components/lib/components/Block';
-import PageLayout from 'medulas-react-components/lib/components/PageLayout';
-import { TX_REQUEST } from '../../paths';
+import Button from 'medulas-react-components/lib/components/Button';
 import Hairline from 'medulas-react-components/lib/components/Hairline';
-import { Request } from '../../../extension/background/actions/createPersona/requestHandler';
+import PageLayout from 'medulas-react-components/lib/components/PageLayout';
+import Typography from 'medulas-react-components/lib/components/Typography';
+import * as React from 'react';
+import { TX_REQUEST } from '../../paths';
 
 export const TX_REQUEST_SHOW = `${TX_REQUEST}_show`;
 
 interface Props {
   readonly onAcceptRequest: () => void;
   readonly showRejectView: () => void;
-  readonly request: Request;
+  readonly sender: string;
 }
 
-const Layout = ({ request, onAcceptRequest, showRejectView }: Props): JSX.Element => (
+const Layout = ({ sender, onAcceptRequest, showRejectView }: Props): JSX.Element => (
   <PageLayout id={TX_REQUEST_SHOW} primaryTitle="Transaction" title="Request">
     <Hairline />
     <Block textAlign="center" marginTop={2} marginBottom={2}>
       <Typography variant="body1">The following site:</Typography>
       <Typography variant="body1" color="primary">
-        {request.sender}
+        {sender}
       </Typography>
       <Typography variant="body1" inline>
         is asking for the following transactions to be made.
--- packages/sanes-chrome-extension/src/routes/tx-request/index.stories.tsx
+++ packages/sanes-chrome-extension/src/routes/tx-request/index.stories.tsx
@@ -1,13 +1,13 @@
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
 import { storiesOf } from '@storybook/react';
 import { Storybook } from 'medulas-react-components/lib/utils/storybook';
 import React from 'react';
-import ShowRequest from './components/ShowRequest';
-import RejectRequest from './components/RejectRequest';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
 import { Request } from '../../extension/background/actions/createPersona/requestHandler';
 import { CHROME_EXTENSION_ROOT } from '../../utils/storybook';
 import { ACCOUNT_STATUS_PAGE } from '../account/index.stories';
+import RejectRequest from './components/RejectRequest';
+import ShowRequest from './components/ShowRequest';
 
 const TX_REQUEST_PATH = `${CHROME_EXTENSION_ROOT}/Transaction Request`;
 const SHOW_REQUEST_PAGE = 'Show Request page';
@@ -17,9 +17,12 @@ const request: Request = {
   id: 0,
   accept: () => action('accept request'),
   reject: (permanent: boolean) => action(`reject request. Permanently: ${permanent ? 'yes' : 'no'}`),
-  reason: 'I would like to know who you are on Ethereum"',
-  sender: 'http://localhost/',
-  type: 'getIdentities',
+  reason: 'I would like you to sign this TX',
+  data: {
+    senderUrl: 'http://localhost/',
+    tx: {},
+  },
+  type: 'signAndPost',
 };
 
 storiesOf(TX_REQUEST_PATH, module)
@@ -28,7 +31,7 @@ storiesOf(TX_REQUEST_PATH, module)
     (): JSX.Element => (
       <Storybook>
         <ShowRequest
-          request={request}
+          sender={request.data.senderUrl}
           onAcceptRequest={linkTo(CHROME_EXTENSION_ROOT, ACCOUNT_STATUS_PAGE)}
           showRejectView={linkTo(TX_REQUEST_PATH, REJECT_REQUEST_PAGE)}
         />
@@ -40,7 +43,7 @@ storiesOf(TX_REQUEST_PATH, module)
     (): JSX.Element => (
       <Storybook>
         <RejectRequest
-          request={request}
+          sender={request.data.senderUrl}
           onBack={linkTo(TX_REQUEST_PATH, SHOW_REQUEST_PAGE)}
           onRejectRequest={action('onAcceptRequest')}
         />
