c0eb093899a2ffe88deb71262b4d19b28b5ab10e
Albert Andrejev <albert.andrejev@gmail.com>
2019-05-23 18:52:49 +0300

    Tests helper methods

--- packages/bierzo-wallet/src/utils/test/e2e.ts
+++ packages/bierzo-wallet/src/utils/test/e2e.ts
@@ -1,4 +1,5 @@
 import puppeteer, { Page, Browser } from 'puppeteer';
+import { extensionId } from '../../communication';
 
 export function launchBrowser(slowMo: number = 0): Promise<Browser> {
   const CRX_PATH = require('path').join(__dirname, '../../../../sanes-chrome-extension/build');
@@ -18,6 +19,30 @@ export async function createPage(browser: Browser): Promise<Page> {
   page.on('console', msg => console.log('PAGE LOG:', msg.text()));
   page.bringToFront();
 
+  return page;
+}
+
+export async function createExtensionPage(browser: Browser): Promise<Page> {
+  const page: Page = await browser.newPage();
+  await page.goto('chrome-extension://dafekhlcpidfaopcimocbcpciholgkkb/index.html', {
+    waitUntil: 'networkidle2',
+  });
+  page.on('console', msg => console.log('EXTENSION PAGE LOG:', msg.text()));
+  page.bringToFront();
+
+  return page;
+}
+
+export async function getBackgroundPage(browser: Browser): Promise<Page> {
+  const targets = await browser.targets();
+  const backgroundPageTarget = targets.find(
+    target => target.type() === 'background_page' && target.url().includes(extensionId),
+  );
+  if (!backgroundPageTarget) {
+    throw new Error('Unable to find extension background page');
+  }
+  const page = await backgroundPageTarget.page();
+  page.on('console', msg => console.log('BACKGROUND PAGE LOG:', msg.text()));
   return page;
 }
 
