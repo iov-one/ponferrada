4cf37e6f1b0f6fd7081796838c8ecdc4d70f1b6e
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-05-30 10:48:20 +0200

    Add tx data to tx-request' ShowRequest component

--- packages/sanes-chrome-extension/src/routes/tx-request/components/ShowRequest.tsx
+++ packages/sanes-chrome-extension/src/routes/tx-request/components/ShowRequest.tsx
@@ -1,9 +1,11 @@
+import { SendTransaction } from '@iov/bcp';
 import Block from 'medulas-react-components/lib/components/Block';
 import Button from 'medulas-react-components/lib/components/Button';
-import Hairline from 'medulas-react-components/lib/components/Hairline';
+import { List, ListItem, ListItemText } from 'medulas-react-components/lib/components/List';
 import PageLayout from 'medulas-react-components/lib/components/PageLayout';
 import Typography from 'medulas-react-components/lib/components/Typography';
 import * as React from 'react';
+import { amountToString } from '../../../utils/balances';
 import { TX_REQUEST } from '../../paths';
 
 export const TX_REQUEST_SHOW = `${TX_REQUEST}_show`;
@@ -12,21 +14,55 @@ interface Props {
   readonly onAcceptRequest: () => void;
   readonly showRejectView: () => void;
   readonly sender: string;
+  readonly tx: SendTransaction;
 }
 
-const Layout = ({ sender, onAcceptRequest, showRejectView }: Props): JSX.Element => (
-  <PageLayout id={TX_REQUEST_SHOW} primaryTitle="Transaction" title="Request">
-    <Hairline />
-    <Block textAlign="center" marginTop={2} marginBottom={2}>
-      <Typography variant="body1">The following site:</Typography>
-      <Typography variant="body1" color="primary">
+const secondaryProps = {
+  noWrap: true,
+};
+
+const Layout = ({ sender, tx, onAcceptRequest, showRejectView }: Props): JSX.Element => (
+  <PageLayout id={TX_REQUEST_SHOW} primaryTitle="Tx" title="Request">
+    <Block textAlign="center" marginBottom={2}>
+      <Typography variant="body1" inline>
+        {'The following site: '}
+      </Typography>
+      <Typography variant="body1" color="primary" inline>
         {sender}
       </Typography>
       <Typography variant="body1" inline>
-        is asking for the following transactions to be made.
+        {' wants you to sign:'}
       </Typography>
     </Block>
-    <Block marginTop={6} />
+    <List>
+      <ListItem>
+        <ListItemText
+          primary="Beneficiary"
+          secondary={tx.recipient}
+          secondaryTypographyProps={secondaryProps}
+        />
+      </ListItem>
+      <ListItem>
+        <ListItemText
+          primary="Amount"
+          secondary={amountToString(tx.amount)}
+          secondaryTypographyProps={secondaryProps}
+        />
+      </ListItem>
+      {tx.fee && tx.fee.tokens && (
+        <ListItem>
+          <ListItemText
+            primary="Fee"
+            secondary={amountToString(tx.fee.tokens)}
+            secondaryTypographyProps={secondaryProps}
+          />
+        </ListItem>
+      )}
+      <ListItem>
+        <ListItemText primary="Notes" secondary={tx.memo || '--'} secondaryTypographyProps={secondaryProps} />
+      </ListItem>
+    </List>
+    <Block marginBottom={3} />
     <Button variant="contained" fullWidth onClick={onAcceptRequest}>
       Approve
     </Button>
@@ -34,6 +70,7 @@ const Layout = ({ sender, onAcceptRequest, showRejectView }: Props): JSX.Element
     <Button variant="contained" fullWidth color="secondary" onClick={showRejectView}>
       Reject
     </Button>
+    <Block marginBottom={2} />
   </PageLayout>
 );
 
--- packages/sanes-chrome-extension/src/routes/tx-request/index.stories.tsx
+++ packages/sanes-chrome-extension/src/routes/tx-request/index.stories.tsx
@@ -1,9 +1,23 @@
+import {
+  Address,
+  Algorithm,
+  Amount,
+  ChainId,
+  PublicIdentity,
+  PublicKeyBytes,
+  SendTransaction,
+  TokenTicker,
+} from '@iov/bcp';
+import { Encoding } from '@iov/encoding';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 import { storiesOf } from '@storybook/react';
 import { Storybook } from 'medulas-react-components/lib/utils/storybook';
 import React from 'react';
-import { Request } from '../../extension/background/actions/createPersona/requestHandler';
+import {
+  Request,
+  SignAndPostRequest,
+} from '../../extension/background/model/signingServer/requestQueueManager';
 import { CHROME_EXTENSION_ROOT } from '../../utils/storybook';
 import { ACCOUNT_STATUS_PAGE } from '../account/index.stories';
 import RejectRequest from './components/RejectRequest';
@@ -13,6 +27,35 @@ const TX_REQUEST_PATH = `${CHROME_EXTENSION_ROOT}/Transaction Request`;
 const SHOW_REQUEST_PAGE = 'Show Request page';
 const REJECT_REQUEST_PAGE = 'Reject Request page';
 
+const defaultCreator: PublicIdentity = {
+  chainId: 'some-chain' as ChainId,
+  pubkey: {
+    algo: Algorithm.Ed25519,
+    // Random 32 bytes pubkey. Derived IOV address:
+    // tiov1dcg3fat5zrvw00xezzjk3jgedm7pg70y222af3 / 6e1114f57410d8e7bcd910a568c9196efc1479e4
+    data: Encoding.fromHex(
+      '7196c465e4c95b3dce425784f51936b95da6bc58b3212648cdca64ee7198df47',
+    ) as PublicKeyBytes,
+  },
+};
+
+const defaultAmount: Amount = {
+  quantity: '1000000001',
+  fractionalDigits: 9,
+  tokenTicker: 'CASH' as TokenTicker,
+};
+
+const transaction: SendTransaction = {
+  kind: 'bcp/send',
+  creator: defaultCreator,
+  amount: defaultAmount,
+  recipient: 'tiov1k898u78hgs36uqw68dg7va5nfkgstu5z0fhz3f' as Address,
+  memo: 'paid transaction',
+  fee: {
+    tokens: defaultAmount,
+  },
+};
+
 const request: Request = {
   id: 0,
   accept: () => action('accept request'),
@@ -20,7 +63,7 @@ const request: Request = {
   reason: 'I would like you to sign this TX',
   data: {
     senderUrl: 'http://localhost/',
-    tx: {},
+    tx: transaction,
   },
   type: 'signAndPost',
 };
@@ -28,15 +71,20 @@ const request: Request = {
 storiesOf(TX_REQUEST_PATH, module)
   .add(
     SHOW_REQUEST_PAGE,
-    (): JSX.Element => (
-      <Storybook>
-        <ShowRequest
-          sender={request.data.senderUrl}
-          onAcceptRequest={linkTo(CHROME_EXTENSION_ROOT, ACCOUNT_STATUS_PAGE)}
-          showRejectView={linkTo(TX_REQUEST_PATH, REJECT_REQUEST_PAGE)}
-        />
-      </Storybook>
-    ),
+    (): JSX.Element => {
+      const { tx } = request.data as SignAndPostRequest;
+
+      return (
+        <Storybook>
+          <ShowRequest
+            tx={tx}
+            sender={request.data.senderUrl}
+            onAcceptRequest={linkTo(CHROME_EXTENSION_ROOT, ACCOUNT_STATUS_PAGE)}
+            showRejectView={linkTo(TX_REQUEST_PATH, REJECT_REQUEST_PAGE)}
+          />
+        </Storybook>
+      );
+    },
   )
   .add(
     REJECT_REQUEST_PAGE,
