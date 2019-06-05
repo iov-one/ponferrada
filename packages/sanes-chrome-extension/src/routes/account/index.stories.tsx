9a8c5a93eff65818a88d6b46268d0d7ec26e0249
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-06-05 17:42:32 +0200

    Adding mock info to generate account status view with transactions in storybook

--- packages/sanes-chrome-extension/src/routes/account/index.stories.tsx
+++ packages/sanes-chrome-extension/src/routes/account/index.stories.tsx
@@ -1,16 +1,38 @@
+import { TokenTicker } from '@iov/bcp';
 import { storiesOf } from '@storybook/react';
-import { Storybook } from 'medulas-react-components/lib/utils/storybook';
 import { ToastProvider } from 'medulas-react-components/lib/context/ToastProvider';
+import { Storybook } from 'medulas-react-components/lib/utils/storybook';
 import React from 'react';
-import Layout from './index';
+import { PersonaProvider } from '../../context/PersonaProvider';
+import { GetPersonaResponse } from '../../extension/background/model/backgroundscript';
 import { CHROME_EXTENSION_ROOT } from '../../utils/storybook';
+import Layout from './index';
 
 export const ACCOUNT_STATUS_PAGE = 'Account Status page';
 
-storiesOf(CHROME_EXTENSION_ROOT, module).add(ACCOUNT_STATUS_PAGE, () => (
-  <Storybook>
-    <ToastProvider>
-      <Layout />
-    </ToastProvider>
-  </Storybook>
-));
+storiesOf(CHROME_EXTENSION_ROOT, module).add(ACCOUNT_STATUS_PAGE, () => {
+  const persona: GetPersonaResponse = {
+    mnemonic: '',
+    accounts: [{ label: 'Account 0' }],
+    txs: [
+      {
+        id: '111',
+        recipient: 'Example Recipient',
+        signer: 'Example Signer',
+        amount: { quantity: '10', fractionalDigits: 3, tokenTicker: 'ETH' as TokenTicker },
+        time: 'Sat May 25 10:10:00 2019 +0200',
+        error: null,
+      },
+    ],
+  };
+
+  return (
+    <PersonaProvider persona={persona}>
+      <Storybook>
+        <ToastProvider>
+          <Layout />
+        </ToastProvider>
+      </Storybook>
+    </PersonaProvider>
+  );
+});
