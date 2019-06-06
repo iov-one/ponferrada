f894716d36a17c660ac282751a91c58c3664d3be
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-06-06 15:37:28 +0200

    Refactor MsgError component

--- packages/sanes-chrome-extension/src/routes/account/components/Tx/MsgError.tsx
+++ packages/sanes-chrome-extension/src/routes/account/components/Tx/MsgError.tsx
@@ -1,31 +1,40 @@
-import * as React from 'react';
+import Block from 'medulas-react-components/lib/components/Block';
 import Typography from 'medulas-react-components/lib/components/Typography';
+import * as React from 'react';
 import { elipsify } from '../../../../utils/strings';
 
 interface MsgErrorProps {
   readonly amount: string;
   readonly recipient: string;
-  readonly onDetailedView: () => void;
 }
 
-const MsgError = ({ amount, recipient, onDetailedView }: MsgErrorProps): JSX.Element => {
+const MsgError = ({ amount, recipient }: MsgErrorProps): JSX.Element => {
   const shortRecipient = elipsify(recipient, 16);
 
   return (
     <React.Fragment>
-      <Typography inline>{'Your '}</Typography>
+      <Typography weight="light" inline>
+        {'Your '}
+      </Typography>
       <Typography weight="semibold" inline>
         {amount}
       </Typography>
-      <Typography inline>{' payment to '}</Typography>
-      <Typography weight="semibold" inline link onClick={onDetailedView}>
+      <Typography weight="semibold" inline>
+        {' payment to '}
+      </Typography>
+      <Typography weight="semibold" inline link>
         {shortRecipient}
       </Typography>
-      <Typography inline>{' was '}</Typography>
+      <Typography weight="light" inline>
+        {' was '}
+      </Typography>
       <Typography weight="semibold" inline>
         {'unsuccessful'}
       </Typography>
-      <Typography inline>{', please try again later'}</Typography>
+      <Typography weight="light" inline>
+        {', please try again later'}
+      </Typography>
+      <Block marginBottom={1.5} />
     </React.Fragment>
   );
 };
