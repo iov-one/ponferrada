de6a24c9cce1e82ce44ee7f70bf6a4d6eefa1355
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-06-06 12:50:49 +0200

    Copy to clipboard recipient address

--- packages/sanes-chrome-extension/src/routes/account/components/Tx/MsgSuccess.tsx
+++ packages/sanes-chrome-extension/src/routes/account/components/Tx/MsgSuccess.tsx
@@ -1,6 +1,12 @@
+import { makeStyles, Theme } from '@material-ui/core';
+import IconButton from '@material-ui/core/IconButton';
+import FileCopy from '@material-ui/icons/FileCopyOutlined';
+import copy from 'clipboard-copy';
 import Block from 'medulas-react-components/lib/components/Block';
 import Link from 'medulas-react-components/lib/components/Link';
 import Typography from 'medulas-react-components/lib/components/Typography';
+import { ToastContext } from 'medulas-react-components/lib/context/ToastProvider';
+import { ToastVariant } from 'medulas-react-components/lib/context/ToastProvider/Toast';
 import * as React from 'react';
 import { elipsify } from '../../../../utils/strings';
 
@@ -10,8 +16,24 @@ interface MsgProps {
   readonly onDetailedView: () => void;
 }
 
+const useStyles = makeStyles((theme: Theme) => ({
+  icon: {
+    padding: `0 ${theme.spacing(1)}px`,
+  },
+}));
+
 const Msg = ({ amount, recipient, onDetailedView }: MsgProps): JSX.Element => {
+  const classes = useStyles();
+  const toast = React.useContext(ToastContext);
+
   const recipientShort = elipsify(recipient, 24);
+  const iconButtonClasses = {
+    root: classes.icon,
+  };
+  const onCopyToClipboard = (): void => {
+    copy(recipient);
+    toast.show('Address copied to clipboard', ToastVariant.INFO);
+  };
 
   return (
     <React.Fragment>
@@ -22,14 +44,21 @@ const Msg = ({ amount, recipient, onDetailedView }: MsgProps): JSX.Element => {
         {` sent ${amount} `}
       </Typography>
       <Typography weight="light" inline>
-        {'to '}
+        {'to:'}
       </Typography>
-      <Block marginBottom={1}>
-        <Link to="https://iov.one">
-          <Typography link onClick={onDetailedView}>
-            {`${recipientShort} `}
-          </Typography>
-        </Link>
+      <Block marginBottom={1.5}>
+        <Typography link onClick={onDetailedView} inline>
+          <Link to="https://iov.one">{`${recipientShort} `}</Link>
+        </Typography>
+        <IconButton
+          color="primary"
+          classes={iconButtonClasses}
+          aria-label="Copy to clipboard address"
+          edge="end"
+          onClick={onCopyToClipboard}
+        >
+          <FileCopy fontSize="small" />
+        </IconButton>
       </Block>
     </React.Fragment>
   );
