8e49af7ca7dfb8ba010a10556dd2ab65d71a3ac3
Albert Andrejev <albert.andrejev@gmail.com>
2019-06-10 22:10:13 +0300

    remove thunk references

--- packages/bierzo-wallet/src/store/index.tsx
+++ packages/bierzo-wallet/src/store/index.tsx
@@ -1,13 +1,12 @@
 import { routerMiddleware, RouterState } from 'connected-react-router';
 import { applyMiddleware, compose, createStore, Store, Middleware } from 'redux';
 import reducer, { history } from './reducers';
-import thunk from 'redux-thunk';
 
 const composeEnhancers =
   (typeof window === 'object' && (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || // eslint-disable-line
   compose;
 
-const middlewares: ReadonlyArray<Middleware> = [thunk, routerMiddleware(history)];
+const middlewares: ReadonlyArray<Middleware> = [routerMiddleware(history)];
 
 export const configureStore = (): Store<{ router: RouterState }> => {
   const store = createStore(reducer, composeEnhancers(applyMiddleware(...middlewares)));
