c784f29748de93a72e50dc111bc980649aa12b31
Albert Andrejev <albert.andrejev@gmail.com>
2019-06-10 22:07:28 +0300

    remove thunk references

--- packages/bierzo-wallet/src/store/types.ts
+++
@@ -1,5 +0,0 @@
-import { ThunkDispatch } from 'redux-thunk';
-
-import { RootActions, RootState } from '../store/reducers';
-
-export type RootThunkDispatch = ThunkDispatch<RootState, any, RootActions>;
