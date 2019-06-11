5bae88bf0ad3ab420963b05cd72e063476f851e1
abefernan <abel.fernandez.nandin@gmail.com>
2019-06-11 02:51:02 +0200

    Missing tests for requests route

---
+++ packages/sanes-chrome-extension/src/routes/requests/test/operateRequests.ts
@@ -0,0 +1,9 @@
+import TestUtils from 'react-dom/test-utils';
+
+export const getRequests = (requestsDom: React.Component): Element[] => {
+  return TestUtils.scryRenderedDOMComponentsWithTag(requestsDom, 'li');
+};
+
+export const getFirstRequest = (requestsDom: React.Component): Element => {
+  return getRequests(requestsDom)[0];
+};
