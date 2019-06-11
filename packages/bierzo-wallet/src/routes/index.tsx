235f6beada3dcb15f72b8abca8f8b6e9886aa0f2
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-06-11 16:39:34 +0200

    Adding react-router-dom's Router to Routes component

--- packages/bierzo-wallet/src/routes/index.tsx
+++ packages/bierzo-wallet/src/routes/index.tsx
@@ -1,15 +1,20 @@
+import { createBrowserHistory } from 'history';
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Router, Switch } from 'react-router-dom';
 import { PAYMENT_ROUTE, WELCOME_ROUTE } from './paths';
 import Payment from './payment';
 import Welcome from './welcome';
 
-const Router = (): JSX.Element => (
-  <Switch>
-    <Route exact path="/" component={Welcome} />
-    <Route exact path={WELCOME_ROUTE} component={Welcome} />
-    <Route exact path={PAYMENT_ROUTE} component={Payment} />
-  </Switch>
+export const history = createBrowserHistory();
+
+const Routes = (): JSX.Element => (
+  <Router history={history}>
+    <Switch>
+      <Route exact path="/" component={Welcome} />
+      <Route exact path={WELCOME_ROUTE} component={Welcome} />
+      <Route exact path={PAYMENT_ROUTE} component={Payment} />
+    </Switch>
+  </Router>
 );
 
-export default Router;
+export default Routes;
