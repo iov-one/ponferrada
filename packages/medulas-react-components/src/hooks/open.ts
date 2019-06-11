22a6577c53e1bf729d67143d1a1330162c94cbca
Albert Andrejev <albert.andrejev@gmail.com>
2019-06-11 15:45:36 +0300

    Open hook refactoring

--- packages/medulas-react-components/src/hooks/open.ts
+++ packages/medulas-react-components/src/hooks/open.ts
@@ -1,11 +1,24 @@
 import * as React from 'react';
 
-type OpenType = [boolean, () => void];
+type OpenType = [boolean, (callBack?: () => void) => void, () => void, boolean];
 
 export function useOpen(): OpenType {
   const [open, setOpen] = React.useState<boolean>(false);
-  const toggle = (): void => setOpen(open => !open);
+  const [visited, setVisited] = React.useState<boolean>(false);
+  const toggle = (callBack?: () => void): void => {
+    if (callBack && typeof callBack === 'function') {
+      callBack();
+    }
+
+    setVisited(true);
+    setOpen(open => !open);
+  };
+
+  const clickAway = (): void => {
+    setVisited(true);
+    setOpen(false);
+  };
 
   const isOpen = open;
-  return [isOpen, toggle];
+  return [isOpen, toggle, clickAway, visited];
 }
