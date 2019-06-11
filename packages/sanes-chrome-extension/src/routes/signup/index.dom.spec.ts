9f6f00e09cadaaf9c4f4763493a4f1429d6ba4c9
abefernan <abel.fernandez.nandin@gmail.com>
2019-06-11 02:59:42 +0200

    Missing tests for signup route

--- packages/sanes-chrome-extension/src/routes/signup/index.dom.spec.ts
+++ packages/sanes-chrome-extension/src/routes/signup/index.dom.spec.ts
@@ -1,33 +1,258 @@
+import TestUtils from 'react-dom/test-utils';
 import { Store } from 'redux';
-import { PersonaData } from '../../extension/background/model/backgroundscript';
 import { aNewStore } from '../../store';
-import { RootState } from '../../store/reducers';
-import * as chromeInternalMsgs from '../../utils/chrome';
-import { whenOnNavigatedToRoute } from '../../utils/test/navigation';
+import { resetHistory, RootState } from '../../store/reducers';
+import { click, input, submit } from '../../utils/test/dom';
+import { travelToSignup, travelToWelcome, whenOnNavigatedToRoute } from '../../utils/test/navigation';
+import { mockCreatePersona, mockPersonaResponse, submitSignup } from '../../utils/test/persona';
+import { randomString } from '../../utils/test/random';
+import { findRenderedDOMComponentWithId } from '../../utils/test/reactElemFinder';
 import { mayTestChains } from '../../utils/test/testExecutor';
-import { ACCOUNT_STATUS_ROUTE } from '../paths';
-import { processSignUp } from './test/travelToSignup';
+import { ACCOUNT_STATUS_ROUTE, SIGNUP_ROUTE, WELCOME_ROUTE } from '../paths';
+import { SECURITY_HINT_STEP_SIGNUP_ROUTE } from './components/SecurityHintForm';
+import { SECOND_STEP_SIGNUP_ROUTE } from './components/ShowPhraseForm';
+import {
+  getAccountNameValidity,
+  getConfirmPasswordValidity,
+  getHintValidity,
+  getNewAccountForm,
+  getNewAccountInputs,
+  getPasswordValidity,
+  isButtonDisabled,
+  submitNewAccount,
+  submitShowPhrase,
+  getConfirmPasswordMismatch,
+} from './test/operateSignup';
 
-describe('DOM > Feature > Signup', (): void => {
+describe('DOM > Feature > Signup', () => {
+  const accountName = randomString(10);
+  const password = randomString(10);
+  const mnemonic = 'badge cattle stool execute involve main mirror envelope brave scrap involve simple';
+  const hint = randomString(10);
+  const personaMock = mockPersonaResponse([], mnemonic, []);
+
   let store: Store<RootState>;
+  let signupDom: React.Component;
 
-  beforeEach(() => {
+  beforeEach(async () => {
+    resetHistory();
     store = aNewStore();
-
-    const response: PersonaData = {
-      accounts: [],
-      mnemonic: 'badge cattle stool execute involve main mirror envelope brave scrap involve simple',
-      txs: [],
-    };
-    jest.spyOn(chromeInternalMsgs, 'createPersona').mockResolvedValueOnce(response);
-  });
+    signupDom = await travelToSignup(store);
+  }, 60000);
 
   mayTestChains(
-    `should finish the signup three steps process`,
-    async (): Promise<void> => {
-      await processSignUp(store);
+    'should finish the signup three steps process',
+    async () => {
+      mockCreatePersona(personaMock);
+      await submitSignup(store);
       await whenOnNavigatedToRoute(store, ACCOUNT_STATUS_ROUTE);
     },
-    55000,
+    60000,
   );
+
+  describe('New Account Step', () => {
+    let newAccountInputs: Element[];
+    let accountNameInput: Element;
+    let passwordInput: Element;
+    let passwordConfirmInput: Element;
+    let newAccountForm: Element;
+    let buttons: Element[];
+    let backButton: Element;
+    let continueButton: Element;
+
+    beforeEach(async () => {
+      newAccountInputs = getNewAccountInputs(signupDom);
+      [accountNameInput, passwordInput, passwordConfirmInput] = newAccountInputs;
+      newAccountForm = getNewAccountForm(signupDom);
+      buttons = TestUtils.scryRenderedDOMComponentsWithTag(signupDom, 'button');
+      [backButton, continueButton] = buttons;
+    });
+
+    it('has three inputs', () => {
+      expect(newAccountInputs.length).toBe(3);
+    }, 60000);
+
+    it('has a valid "Account Name" input', () => {
+      expect(accountNameInput.getAttribute('placeholder')).toBe('Account name');
+
+      input(passwordInput, password);
+      input(passwordConfirmInput, password);
+
+      submit(newAccountForm);
+      expect(getAccountNameValidity(signupDom).textContent).toBe('Required');
+
+      input(accountNameInput, randomString(7));
+      expect(getAccountNameValidity(signupDom).textContent).toBe(
+        'Username should have at least 8 characters',
+      );
+
+      input(accountNameInput, accountName);
+      expect(getAccountNameValidity(signupDom)).toBeUndefined();
+    }, 60000);
+
+    it('has a valid "Password" input', () => {
+      expect(passwordInput.getAttribute('placeholder')).toBe('Password');
+
+      input(accountNameInput, accountName);
+      input(passwordConfirmInput, password);
+
+      submit(newAccountForm);
+      expect(getPasswordValidity(signupDom).textContent).toBe('Required');
+
+      input(passwordInput, randomString(7));
+      expect(getPasswordValidity(signupDom).textContent).toBe('Password should have at least 8 characters');
+
+      input(passwordInput, password);
+      expect(getPasswordValidity(signupDom)).toBeUndefined();
+    }, 60000);
+
+    it('has a valid "Confirm Password" input', () => {
+      expect(passwordConfirmInput.getAttribute('placeholder')).toBe('Confirm Password');
+
+      input(accountNameInput, accountName);
+
+      submit(newAccountForm);
+      expect(getConfirmPasswordValidity(signupDom).textContent).toBe('Required');
+
+      input(passwordInput, password);
+
+      input(passwordConfirmInput, randomString(10));
+      expect(getConfirmPasswordMismatch(signupDom).textContent).toBe('Passwords mismatch');
+
+      input(passwordConfirmInput, password);
+      expect(getConfirmPasswordValidity(signupDom)).toBeUndefined();
+    }, 60000);
+
+    it('has two buttons', () => {
+      expect(buttons.length).toBe(2);
+    }, 60000);
+
+    it('has a "Back" button that redirects to the previous route when clicked', async () => {
+      expect(backButton.textContent).toBe('Back');
+
+      await travelToWelcome(store);
+      await travelToSignup(store);
+      click(backButton);
+      await whenOnNavigatedToRoute(store, WELCOME_ROUTE);
+    }, 60000);
+
+    it('has a valid "Continue" button that redirects to the Show Phrase Form if the form is valid when clicked', async () => {
+      expect(continueButton.textContent).toBe('Continue');
+
+      expect(isButtonDisabled(continueButton)).toBeTruthy();
+
+      input(accountNameInput, accountName);
+      input(passwordInput, password);
+      input(passwordConfirmInput, password);
+      expect(isButtonDisabled(continueButton)).toBeFalsy();
+      mockCreatePersona(personaMock);
+      click(continueButton);
+      await findRenderedDOMComponentWithId(signupDom, SECOND_STEP_SIGNUP_ROUTE);
+    }, 60000);
+
+    it('accepts several UTF-8 alphabets as password fields', async () => {
+      const password = 'abcαβγазб文字漢字한국';
+      input(accountNameInput, accountName);
+      input(passwordInput, password);
+      input(passwordConfirmInput, password);
+      mockCreatePersona(personaMock);
+      submit(newAccountForm);
+      await findRenderedDOMComponentWithId(signupDom, SECOND_STEP_SIGNUP_ROUTE);
+    }, 60000);
+  });
+
+  describe('Show Phrase Step', () => {
+    let questionMark: Element;
+    let checkbox: Element;
+    let buttons: Element[];
+    let backButton: Element;
+    let continueButton: Element;
+
+    beforeEach(async () => {
+      await submitNewAccount(signupDom, accountName, password);
+
+      questionMark = TestUtils.scryRenderedDOMComponentsWithTag(signupDom, 'img')[0];
+      checkbox = TestUtils.findRenderedDOMComponentWithTag(signupDom, 'input');
+      buttons = TestUtils.scryRenderedDOMComponentsWithTag(signupDom, 'button');
+      [backButton, continueButton] = buttons;
+    });
+
+    it('has a question mark button that toggles a tooltip when clicked', async () => {
+      let paragraphs = TestUtils.scryRenderedDOMComponentsWithTag(signupDom, 'p');
+      expect(paragraphs.length).toBe(1);
+      //TODO click throws "document.createRange is not a function" error
+      click(questionMark);
+      paragraphs = TestUtils.scryRenderedDOMComponentsWithTag(signupDom, 'p');
+      expect(paragraphs.length).toBe(2);
+    }, 60000);
+
+    it('has a toggle button that shows the mnemonic when active', async () => {
+      const renderedMnemonic = TestUtils.findRenderedDOMComponentWithTag(signupDom, 'p');
+      expect(renderedMnemonic.textContent).toBe('');
+      click(checkbox);
+      expect(renderedMnemonic.textContent).toBe(mnemonic);
+    }, 60000);
+
+    it('has a "Back" button that redirects to the New Account Form when clicked', async () => {
+      expect(backButton.textContent).toBe('Back');
+
+      click(backButton);
+      await findRenderedDOMComponentWithId(signupDom, SIGNUP_ROUTE);
+    }, 60000);
+
+    it('has a "Continue" button that redirects to the Security Hint Form when clicked', async () => {
+      expect(continueButton.textContent).toBe('Continue');
+
+      click(continueButton);
+      await findRenderedDOMComponentWithId(signupDom, SECURITY_HINT_STEP_SIGNUP_ROUTE);
+    }, 60000);
+  });
+
+  describe('Security Hint Step', () => {
+    let hintInput: Element;
+    let buttons: Element[];
+    let backButton: Element;
+    let createButton: Element;
+
+    beforeEach(async () => {
+      await submitNewAccount(signupDom, accountName, password);
+      await submitShowPhrase(signupDom);
+
+      hintInput = TestUtils.findRenderedDOMComponentWithTag(signupDom, 'input');
+      buttons = TestUtils.scryRenderedDOMComponentsWithTag(signupDom, 'button');
+      [backButton, createButton] = buttons;
+    });
+
+    it('has a valid "Security hint" input', async () => {
+      expect(hintInput.getAttribute('placeholder')).toBe('Security hint');
+
+      input(hintInput, randomString(16));
+      expect(getHintValidity(signupDom).textContent).toBe('15 characters max - Spaces are allowed');
+
+      input(hintInput, hint);
+      expect(getHintValidity(signupDom)).toBeUndefined();
+    }, 60000);
+
+    it('has a "Back" button that redirects to the New Account Form when clicked', async () => {
+      expect(backButton.textContent).toBe('Back');
+
+      click(backButton);
+      await findRenderedDOMComponentWithId(signupDom, SECOND_STEP_SIGNUP_ROUTE);
+    }, 60000);
+
+    it('has a valid "Create" button that redirects to the Account Status view when clicked', async () => {
+      expect(createButton.textContent).toBe('Create');
+
+      expect(isButtonDisabled(createButton)).toBeFalsy();
+
+      input(hintInput, randomString(16));
+      expect(isButtonDisabled(createButton)).toBeTruthy();
+
+      input(hintInput, hint);
+      expect(isButtonDisabled(createButton)).toBeFalsy();
+      mockCreatePersona(personaMock);
+      click(createButton);
+      await whenOnNavigatedToRoute(store, ACCOUNT_STATUS_ROUTE);
+    }, 60000);
+  });
 });
---
+++ packages/sanes-chrome-extension/src/routes/signup/test/operateSignup.ts
@@ -0,0 +1,145 @@
+import { Page } from 'puppeteer';
+import TestUtils from 'react-dom/test-utils';
+import { getHintPhrase } from '../../../utils/localstorage/hint';
+import { click, input, submit } from '../../../utils/test/dom';
+import {
+  findRenderedDOMComponentWithId,
+  findRenderedE2EComponentWithId,
+} from '../../../utils/test/reactElemFinder';
+import { ACCOUNT_STATUS_ROUTE } from '../../paths';
+import {
+  ACCOUNT_NAME_FIELD,
+  FIRST_STEP_SIGNUP_ROUTE,
+  PASSWORD_CONFIRM_FIELD,
+  PASSWORD_FIELD,
+} from '../components/NewAccountForm';
+import { SECURITY_HINT, SECURITY_HINT_STEP_SIGNUP_ROUTE } from '../components/SecurityHintForm';
+import { SECOND_STEP_SIGNUP_ROUTE } from '../components/ShowPhraseForm';
+
+export const getNewAccountInputs = (signupDom: React.Component): Element[] => {
+  return TestUtils.scryRenderedDOMComponentsWithTag(signupDom, 'input');
+};
+
+export const getAccountNameValidity = (signupDom: React.Component): Element => {
+  return TestUtils.findRenderedDOMComponentWithTag(signupDom, 'p');
+};
+
+export const getPasswordValidity = (signupDom: React.Component): Element => {
+  return TestUtils.scryRenderedDOMComponentsWithTag(signupDom, 'p')[0];
+};
+
+export const getConfirmPasswordValidity = (signupDom: React.Component): Element => {
+  return TestUtils.scryRenderedDOMComponentsWithTag(signupDom, 'p')[1];
+};
+
+export const getConfirmPasswordMismatch = (signupDom: React.Component): Element => {
+  return TestUtils.scryRenderedDOMComponentsWithTag(signupDom, 'p')[0];
+};
+
+export const getNewAccountForm = (signupDom: React.Component): Element => {
+  return TestUtils.findRenderedDOMComponentWithTag(signupDom, 'form');
+};
+
+export const getHintValidity = (signupDom: React.Component): Element => {
+  return TestUtils.findRenderedDOMComponentWithTag(signupDom, 'p');
+};
+
+export const getSecurityHintForm = (signupDom: React.Component): Element => {
+  return TestUtils.findRenderedDOMComponentWithTag(signupDom, 'form');
+};
+
+export const isButtonDisabled = (button: Element): boolean => {
+  return button.classList.contains('Mui-disabled');
+};
+
+export const submitNewAccountE2E = async (
+  page: Page,
+  accountName: string,
+  password: string,
+): Promise<void> => {
+  await page.type(`input[name="${ACCOUNT_NAME_FIELD}"]`, accountName);
+  await page.type(`input[name="${PASSWORD_FIELD}`, password);
+  await page.type(`input[name="${PASSWORD_CONFIRM_FIELD}`, password);
+
+  await page.click('button[type="submit"]');
+  await findRenderedE2EComponentWithId(page, SECOND_STEP_SIGNUP_ROUTE);
+};
+
+export const submitNewAccount = async (
+  newAccountDom: React.Component,
+  accountName: string,
+  password: string,
+): Promise<void> => {
+  const inputs = TestUtils.scryRenderedDOMComponentsWithTag(newAccountDom, 'input');
+  expect(inputs.length).toBe(3);
+
+  const [accountNameField, passwordField, passwordConfirmField] = inputs;
+  input(accountNameField, accountName);
+  input(passwordField, password);
+  input(passwordConfirmField, password);
+
+  const newAccountform = TestUtils.findRenderedDOMComponentWithTag(newAccountDom, 'form');
+  submit(newAccountform);
+  await findRenderedDOMComponentWithId(newAccountDom, SECOND_STEP_SIGNUP_ROUTE);
+};
+
+export const submitShowPhraseE2E = async (page: Page): Promise<void> => {
+  const checkbox = await page.$('input[type="checkbox"]');
+  expect(checkbox).not.toBeNull();
+
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  await checkbox!.click();
+
+  const mnemonic = await page.evaluate(
+    (): string | null => {
+      const element = document.querySelector('p');
+      if (!element) {
+        return null;
+      }
+
+      return element.textContent;
+    },
+  );
+
+  expect(mnemonic).not.toBe(null);
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  expect(mnemonic!.split(' ').length).toBe(12);
+
+  const buttons = await page.$$('button');
+  await buttons[1].click();
+  await findRenderedE2EComponentWithId(page, SECURITY_HINT_STEP_SIGNUP_ROUTE);
+};
+
+export const submitShowPhrase = async (showPhraseDom: React.Component): Promise<void> => {
+  const continueButton = TestUtils.scryRenderedDOMComponentsWithTag(showPhraseDom, 'button')[1];
+  click(continueButton);
+  await findRenderedDOMComponentWithId(showPhraseDom, SECURITY_HINT_STEP_SIGNUP_ROUTE);
+};
+
+export const submitSecurityHintE2E = async (page: Page, securityHint: string): Promise<void> => {
+  await page.type(`input[name="${SECURITY_HINT}"]`, securityHint);
+
+  await page.click('button[type="submit"]');
+
+  await findRenderedE2EComponentWithId(page, ACCOUNT_STATUS_ROUTE);
+};
+
+export const submitSecurityHint = async (
+  securityHintDom: React.Component,
+  accountName: string,
+  hint: string,
+): Promise<void> => {
+  const hintInput = TestUtils.findRenderedDOMComponentWithTag(securityHintDom, 'input');
+  input(hintInput, hint);
+
+  const form = TestUtils.findRenderedDOMComponentWithTag(securityHintDom, 'form');
+  submit(form);
+  await findRenderedDOMComponentWithId(securityHintDom, ACCOUNT_STATUS_ROUTE);
+  expect(getHintPhrase(accountName)).toBe(hint);
+};
+
+export const travelToSignupNewAccountStep = async (page: Page): Promise<void> => {
+  await page.click('button:nth-of-type(2)');
+
+  await findRenderedE2EComponentWithId(page, FIRST_STEP_SIGNUP_ROUTE);
+};
