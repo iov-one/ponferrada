48b0e80fe4786a881600970be9074c9d4ae75bbf
Albert Andrejev <albert.andrejev@gmail.com>
2019-06-12 14:06:43 +0300

    Use hooks for media query and refactor

--- packages/medulas-react-components/src/context/MatchMediaContext.tsx
+++ packages/medulas-react-components/src/context/MatchMediaContext.tsx
@@ -1,46 +1,20 @@
-import { throttle } from 'lodash';
-import * as React from 'react';
+import React from 'react';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 const phoneScreen = 425;
 
 type MatchMediaProps = boolean;
 
-const isPhone = (): boolean => (window.matchMedia(`(min-width: ${phoneScreen}px)`).matches ? false : true);
-
 export const MatchMediaContext = React.createContext<MatchMediaProps>(false);
 
-interface State {
-  readonly phone: boolean;
+interface Props {
+  readonly children: React.ReactNode;
 }
 
-class MatchMedia extends React.Component<{}, State> {
-  public readonly state = {
-    phone: isPhone(),
-  };
-  private readonly throttledOnResize: () => void;
+const MatchMedia = ({ children }: Props): JSX.Element => {
+  const phone = useMediaQuery(`(min-width:${phoneScreen}px)`);
 
-  public constructor(props: {}) {
-    super(props);
-    this.throttledOnResize = throttle(this.updateMatchMedia.bind(this), 400);
-  }
-
-  public componentDidMount(): void {
-    window.addEventListener('resize', this.throttledOnResize);
-  }
-
-  public componentWillUnmount(): void {
-    window.removeEventListener('resize', this.throttledOnResize);
-  }
-
-  public updateMatchMedia(): void {
-    this.setState(() => ({ phone: isPhone() }));
-  }
-
-  public render(): JSX.Element {
-    return (
-      <MatchMediaContext.Provider value={this.state.phone}>{this.props.children}</MatchMediaContext.Provider>
-    );
-  }
-}
+  return <MatchMediaContext.Provider value={phone}>{children}</MatchMediaContext.Provider>;
+};
 
 export default MatchMedia;
