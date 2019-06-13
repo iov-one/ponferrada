f04e6f8151c12597d73883905e4c0421e0102454
abefernan <abel.fernandez.nandin@gmail.com>
2019-06-13 19:31:13 +0200

    Adds support for HMR and Redux

--- packages/sil-voting-app/src/index.tsx
+++ packages/sil-voting-app/src/index.tsx
@@ -1,4 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import Routes from './routes';
 
-ReactDOM.render(<div />, document.getElementById('root'));
+const store = createStore(() => {});
+const rootEl = document.getElementById('root');
+
+const render = (Component: React.ComponentType): void => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <Component />
+    </Provider>,
+    rootEl,
+  );
+};
+
+render(Routes);
+
+if (module.hot) {
+  module.hot.accept(
+    './routes',
+    (): void => {
+      const NextApp = require('./routes').default;
+      render(NextApp);
+    },
+  );
+}
---
+++ packages/sil-voting-app/src/routes/login/index.tsx
@@ -0,0 +1,13 @@
+import Block from 'medulas-react-components/lib/components/Block';
+import Typography from 'medulas-react-components/lib/components/Typography';
+import React from 'react';
+
+const Login = (): JSX.Element => {
+  return (
+    <Block width="100vw" height="auto" minHeight="100vh" alignItems="center" justifyContent="center">
+      <Typography variant="h6">Login</Typography>
+    </Block>
+  );
+};
+
+export default Login;
