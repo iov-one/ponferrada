e4b6108f8b26aaba26599ea067966ecf9883a9d9
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-06-17 15:56:01 +0200

    Added specific Mui StyleProps to Grid component

--- packages/medulas-react-components/src/components/Grid/index.tsx
+++ packages/medulas-react-components/src/components/Grid/index.tsx
@@ -1,7 +1,15 @@
+import Box from '@material-ui/core/Box';
+import { ComposedStyleFunction, flexbox, PropsFor, sizing } from '@material-ui/system';
 import * as React from 'react';
-import Box, { BoxProps } from '@material-ui/core/Box';
 
-const Grid = ({ children, ...restProps }: BoxProps): JSX.Element => {
+type GridStyleFunction = ComposedStyleFunction<[typeof flexbox, typeof sizing]>;
+type GridStyleProps = PropsFor<GridStyleFunction>;
+
+interface Props extends GridStyleProps {
+  readonly children?: React.ReactNode;
+}
+
+const Grid = ({ children, ...restProps }: Props): JSX.Element => {
   return (
     <Box display="flex" {...restProps}>
       {children}
