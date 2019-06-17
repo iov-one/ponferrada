2acbf8c6352ef1d4bb6ff87dacbf1f8d7a794314
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-06-17 17:10:47 +0200

    Adding borders and typography def styles to Block

--- packages/medulas-react-components/src/components/Block/index.tsx
+++ packages/medulas-react-components/src/components/Block/index.tsx
@@ -1,6 +1,7 @@
 import Box from '@material-ui/core/Box';
 import {
   border,
+  borders,
   ComposedStyleFunction,
   display,
   flexbox,
@@ -8,16 +9,28 @@ import {
   PropsFor,
   sizing,
   spacing,
+  typography,
 } from '@material-ui/system';
 import * as React from 'react';
 
 type BlockStyleFunction = ComposedStyleFunction<
-  [typeof flexbox, typeof sizing, typeof spacing, typeof display, typeof border, typeof palette]
+  [
+    typeof flexbox,
+    typeof sizing,
+    typeof spacing,
+    typeof display,
+    typeof border,
+    typeof palette,
+    typeof borders,
+    typeof typography
+  ]
 >;
 type BlockStyleProps = PropsFor<BlockStyleFunction>;
 
 interface Props extends BlockStyleProps {
   readonly children?: React.ReactNode;
+  readonly id?: string;
+  readonly className?: string;
 }
 
 const Block = ({ children, display = 'block', ...restProps }: Props): JSX.Element => {
