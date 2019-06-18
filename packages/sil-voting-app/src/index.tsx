70de27cf06036264797a7700802af3a09e3a882c
abefernan <abel.fernandez.nandin@gmail.com>
2019-06-19 00:39:21 +0200

    Change store creation to use new Redux code

--- packages/sil-voting-app/src/index.tsx
+++ packages/sil-voting-app/src/index.tsx
@@ -1,19 +1,22 @@
+import { ToastProvider } from 'medulas-react-components/lib/context/ToastProvider';
 import MedulasThemeProvider from 'medulas-react-components/lib/theme/MedulasThemeProvider';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 import Routes from './routes';
+import { configureStore } from './store';
 import { globalStyles } from './theme/globalStyles';
 
-const store = createStore(() => {});
+const store = configureStore();
 const rootEl = document.getElementById('root');
 
 const render = (Component: React.ComponentType): void => {
   ReactDOM.render(
     <Provider store={store}>
       <MedulasThemeProvider injectFonts injectStyles={globalStyles}>
-        <Component />
+        <ToastProvider>
+          <Component />
+        </ToastProvider>
       </MedulasThemeProvider>
     </Provider>,
     rootEl,
--- packages/sil-voting-app/src/routes/login/index.stories.tsx
+++ packages/sil-voting-app/src/routes/login/index.stories.tsx
@@ -2,11 +2,11 @@ import { storiesOf } from '@storybook/react';
 import MedulasThemeProvider from 'medulas-react-components/lib/theme/MedulasThemeProvider';
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { configureStore } from '../../store';
 import { globalStyles } from '../../theme/globalStyles';
 import Login from './index';
 
-const store = createStore(() => {});
+const store = configureStore();
 
 storiesOf('Voter dashboard', module)
   .addParameters({ viewport: { defaultViewport: 'responsive' } })
