814ba64749a44d58b754d6d2122dbae5840d7ac9
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-06-18 15:29:55 +0200

    Fix ReacRedux import

--- packages/bierzo-wallet/src/routes/login/index.tsx
+++ packages/bierzo-wallet/src/routes/login/index.tsx
@@ -2,19 +2,17 @@ import { ToastContext } from 'medulas-react-components/lib/context/ToastProvider
 import { ToastVariant } from 'medulas-react-components/lib/context/ToastProvider/Toast';
 import PageColumn from 'medulas-react-components/lib/pages/PageColumn';
 import * as React from 'react';
-import { ReactReduxContext } from 'react-redux';
+import * as ReactRedux from 'react-redux';
 import { history } from '..';
 import { generateGetIdentitiesRequest } from '../../communication/identities';
 import { getExtensionStatus } from '../../communication/status';
 import { setExtensionStateAction } from '../../store/reducers/extension';
 import { WELCOME_ROUTE } from '../paths';
 
-const SignupSection = (): JSX.Element => <React.Fragment />;
-
 const Login = (): JSX.Element => {
   const toast = React.useContext(ToastContext);
   //TODO: Fix this as soon as proper react-redux definitions will be available
-  const dispatch = (ReactReduxContext as any).useDispatch();
+  const dispatch = (ReactRedux as any).useDispatch();
 
   const onLogin = async (_: object): Promise<void> => {
     const request = generateGetIdentitiesRequest();
@@ -40,7 +38,6 @@ const Login = (): JSX.Element => {
       primaryTitle="Welcome"
       secondaryTitle="to your IOV wallet"
       subtitle="Continue to access your account"
-      renderHeader={SignupSection}
       nextMsg="Continue"
     />
   );
