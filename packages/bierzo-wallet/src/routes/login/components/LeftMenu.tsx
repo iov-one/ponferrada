72e6e2312bba24f41a5c42586f8a3832c6dd4461
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-06-18 15:30:08 +0200

    Remove unused components

--- packages/bierzo-wallet/src/routes/login/components/LeftMenu.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import people from '../assets/People.svg';
-import Img from 'medulas-react-components/lib/components/Image';
-
-const PeopleImg = (): JSX.Element => <Img src={people} alt="Log in Image" cover />;
-
-export default PeopleImg;
