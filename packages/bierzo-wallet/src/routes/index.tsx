9593a67a45d83695598b7bcb64b9446c74ca38a0
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-06-18 12:44:22 +0200

    Show login page in first place

--- packages/bierzo-wallet/src/routes/index.tsx
+++ packages/bierzo-wallet/src/routes/index.tsx
@@ -1,7 +1,8 @@
 import { createBrowserHistory } from 'history';
 import React from 'react';
 import { Route, Router, Switch } from 'react-router-dom';
-import { PAYMENT_ROUTE, WELCOME_ROUTE } from './paths';
+import Login from './login';
+import { LOGIN_ROUTE, PAYMENT_ROUTE, WELCOME_ROUTE } from './paths';
 import Payment from './payment';
 import Welcome from './welcome';
 
@@ -10,7 +11,8 @@ export const history = createBrowserHistory();
 const Routes = (): JSX.Element => (
   <Router history={history}>
     <Switch>
-      <Route exact path="/" component={Welcome} />
+      <Route exact path="/" component={Login} />
+      <Route exact path={LOGIN_ROUTE} component={Welcome} />
       <Route exact path={WELCOME_ROUTE} component={Welcome} />
       <Route exact path={PAYMENT_ROUTE} component={Payment} />
     </Switch>
--- packages/bierzo-wallet/src/routes/paths.ts
+++ packages/bierzo-wallet/src/routes/paths.ts
@@ -1,2 +1,3 @@
 export const WELCOME_ROUTE = '/welcome';
 export const PAYMENT_ROUTE = '/payment';
+export const LOGIN_ROUTE = '/login';
