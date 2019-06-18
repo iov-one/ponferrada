01b06a40a064f2c285684f35db16b7ddea2f0386
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-06-18 12:18:05 +0200

    Create login route components

---
+++ packages/bierzo-wallet/src/routes/login/index.tsx
@@ -0,0 +1,24 @@
+import PageColumn from 'medulas-react-components/lib/pages/PageColumn';
+import * as React from 'react';
+import WelcomeMessage from './components/WelcomeMessage';
+
+const SignupSection = (): JSX.Element => <React.Fragment />;
+
+interface Props {
+  readonly onSubmit: (values: object) => void;
+}
+
+const Login = ({ onSubmit }: Props): JSX.Element => (
+  <PageColumn
+    icon="white"
+    onSubmit={onSubmit}
+    primaryTitle="Welcome"
+    secondaryTitle="to your IOV wallet"
+    subtitle="Continue to access your account"
+    renderHeader={SignupSection}
+    formRender={WelcomeMessage}
+    nextMsg="Continue"
+  />
+);
+
+export default Login;
