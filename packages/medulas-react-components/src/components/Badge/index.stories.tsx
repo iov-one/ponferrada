c5108c6b4739cd6594704da471eed11aee46c396
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-06-20 16:47:40 +0200

    Add all icon variants

--- packages/medulas-react-components/src/components/Badge/index.stories.tsx
+++ packages/medulas-react-components/src/components/Badge/index.stories.tsx
@@ -2,6 +2,7 @@ import { storiesOf } from '@storybook/react';
 import React from 'react';
 import RecoveryPhraseIcon from '../../theme/assets/badgeIcon/recoveryPhrase.svg';
 import { Storybook } from '../../utils/storybook';
+import Block from '../Block';
 import Img from '../Image';
 import Badge from './index';
 
@@ -10,12 +11,26 @@ storiesOf('Components', module).add(
   (): JSX.Element => (
     <Storybook>
       <div style={{ margin: '36px' }}>
+        <Block marginBottom={3}>This shows the check variant</Block>
         <Badge variant="check">
           <Img src={RecoveryPhraseIcon} alt="Icon" />
         </Badge>
       </div>
       <div style={{ margin: '36px' }}>
+        <Block marginBottom={2}>This shows the badge variant</Block>
         <Badge variant="dot">
+          <Img src={RecoveryPhraseIcon} alt="Icon" />
+        </Badge>
+      </div>
+      <div style={{ margin: '36px' }}>
+        <Block marginBottom={2}>This shows the error badge variant</Block>
+        <Badge variant="dot" color="error">
+          <Img src={RecoveryPhraseIcon} alt="Icon" />
+        </Badge>
+      </div>
+      <div style={{ margin: '36px' }}>
+        <Block marginBottom={2}>This shows the invisible variant</Block>
+        <Badge variant="dot" invisible>
           <Img src={RecoveryPhraseIcon} alt="Icon" />
         </Badge>
       </div>
