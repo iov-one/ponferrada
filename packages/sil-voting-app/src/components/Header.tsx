b83e45ba629b8aa9117c541b10db015419320168
abefernan <abel.fernandez.nandin@gmail.com>
2019-06-24 18:04:48 +0200

    Makes use of minWidth and margin with Block

--- packages/sil-voting-app/src/components/Header.tsx
+++ packages/sil-voting-app/src/components/Header.tsx
@@ -1,31 +1,19 @@
-import { Theme } from '@material-ui/core';
-import { makeStyles } from '@material-ui/styles';
 import Block from 'medulas-react-components/lib/components/Block';
 import CircleImage from 'medulas-react-components/lib/components/Image/CircleImage';
 import Typography from 'medulas-react-components/lib/components/Typography';
 import React from 'react';
 import icon from '../assets/iov-logo.svg';
 
-const useStyles = makeStyles((theme: Theme) => ({
-  logo: {
-    '& > div + h5': {
-      marginLeft: theme.spacing(2),
-    },
-  },
-}));
-
 const Header = (): JSX.Element => {
-  const classes = useStyles();
-
   return (
     <Block width="100%" minHeight="78px" display="flex" alignItems="center">
-      <Block width="20%" display="flex" alignItems="center" justifyContent="center" className={classes.logo}>
+      <Block minWidth="205px" display="flex" alignItems="center" justifyContent="center">
         <CircleImage alt="Logo" icon={icon} dia="48px" circleColor="#fff" />
-        <Typography variant="h5">IOV</Typography>
+        <Block marginLeft={2}>
+          <Typography variant="h5">IOV</Typography>
+        </Block>
       </Block>
-      <Block width="80%">
-        <Typography variant="h5">Voting Dashboard</Typography>
-      </Block>
+      <Typography variant="h5">Voting Dashboard</Typography>
     </Block>
   );
 };
