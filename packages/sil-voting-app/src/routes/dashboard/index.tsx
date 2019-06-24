f0c8611e7692c931a404015a87377930e2f57d9e
abefernan <abel.fernandez.nandin@gmail.com>
2019-06-24 18:46:41 +0200

    Adds main view to Dashboard

--- packages/sil-voting-app/src/routes/dashboard/index.tsx
+++ packages/sil-voting-app/src/routes/dashboard/index.tsx
@@ -1,16 +1,18 @@
 import Block from 'medulas-react-components/lib/components/Block';
 import Hairline from 'medulas-react-components/lib/components/Hairline';
-import Typography from 'medulas-react-components/lib/components/Typography';
 import React from 'react';
 import Header from '../../components/Header';
+import AsideFilter from './components/AsideFilter';
+import ProposalsList from './components/ProposalsList';
 
 const Dashboard = (): JSX.Element => {
   return (
     <Block width="100%" maxWidth="1024px" height="auto" display="flex" flexDirection="column" margin="0 auto">
       <Header />
       <Hairline />
-      <Block minWidth="100%" minHeight="100%">
-        <Typography variant="h6">DASHBOARD VIEW</Typography>
+      <Block minWidth="100%" display="flex">
+        <AsideFilter />
+        <ProposalsList />
       </Block>
     </Block>
   );
