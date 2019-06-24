ebe09cc0c4387d032c2c2985f423f427258a331c
abefernan <abel.fernandez.nandin@gmail.com>
2019-06-24 18:46:56 +0200

    Adds Dashboard's main view components

---
+++ packages/sil-voting-app/src/routes/dashboard/components/ProposalsList.tsx
@@ -0,0 +1,13 @@
+import Block from 'medulas-react-components/lib/components/Block';
+import Typography from 'medulas-react-components/lib/components/Typography';
+import React from 'react';
+
+const ProposalsList = (): JSX.Element => {
+  return (
+    <Block flexGrow={1}>
+      <Typography variant="h6">LIST OF PROPOSALS</Typography>
+    </Block>
+  );
+};
+
+export default ProposalsList;
