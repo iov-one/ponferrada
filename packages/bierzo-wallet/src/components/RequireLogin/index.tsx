4cf0aa3d1a671a68c89c95cb772a40ecfd915825
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-06-26 09:59:44 +0200

    Create RequireLogin container for checking if the user is connecgted

---
+++ packages/bierzo-wallet/src/components/RequireLogin/index.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { useSelector } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import { LOGIN_ROUTE } from '../../routes/paths';
+import { RootState } from '../../store/reducers';
+
+interface Props {
+  readonly children: React.ReactNode;
+}
+
+const RequireLogin = ({ children }: Props): JSX.Element => {
+  const connected = useSelector((state: RootState) => state.extension.connected);
+  console.log('Connected is: ' + connected);
+
+  if (connected) {
+    return <React.Fragment>{children}</React.Fragment>;
+  }
+
+  return <Redirect push to={LOGIN_ROUTE} />;
+};
+
+export default RequireLogin;
