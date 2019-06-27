a5958ea621d0e45a47587a3752566111c654f03d
abefernan <abel.fernandez.nandin@gmail.com>
2019-06-28 00:43:24 +0200

    Adds collapsing capabilities to description

--- packages/sil-voting-app/src/routes/dashboard/components/Proposal.tsx
+++ packages/sil-voting-app/src/routes/dashboard/components/Proposal.tsx
@@ -1,13 +1,16 @@
 import Block from 'medulas-react-components/lib/components/Block';
 import Button from 'medulas-react-components/lib/components/Button';
+import Collapse from '@material-ui/core/Collapse';
 import Typography from 'medulas-react-components/lib/components/Typography';
-import React from 'react';
+import React, { useState } from 'react';
+
+const DESC_MAX_LENGTH = 180;
 
 // Utility methods to generate random data to do layout before consuming API
 
-const smallDesc = 'Lorem ipsum';
+const shortDesc = 'Lorem ipsum';
 
-const bigDesc = `
+const largeDesc = `
 Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
 
 Expetenda tincidunt in sed, ex partem placerat sea, porro commodo ex eam. His putant aeterno interesset at. Usu ea mundi tincidunt, omnium virtute aliquando ius ex. Ea aperiri sententiae duo. Usu nullam dolorum quaestio ei, sit vidit facilisis ea. Per ne impedit iracundia neglegentur. Consetetur neglegentur eum ut, vis animal legimus inimicus id.
@@ -28,8 +31,8 @@ const randomString = (): string => {
 
 const randomDesc = (): string => {
   const random = Math.random();
-  if (random < 0.5) return smallDesc;
-  return bigDesc;
+  if (random < 0.5) return shortDesc;
+  return largeDesc;
 };
 
 const randomDate = (): Date => {
@@ -71,30 +74,76 @@ interface Props {
   readonly status: 'Active' | 'Submitted' | 'Ended';
 }
 
-const Proposal = (props?: Props): JSX.Element => {
-  const quorum = randomQuorum();
-  const creationDate = randomDate();
-  const expiryDate = randomDate();
-  const vote = randomVote();
-  props = {
-    id: 'ID ' + randomString(),
-    title: 'T ' + randomString(),
-    author: 'A ' + randomString(),
-    description: randomDesc(),
-    creationDate: creationDate,
-    expiryDate: expiryDate,
-    quorum: quorum,
-    threshold: quorum / 2 + 1,
-    vote: vote,
-    status: getStatus(vote, creationDate, expiryDate),
+const quorum = randomQuorum();
+const creationDate = randomDate();
+const expiryDate = randomDate();
+const vote = randomVote();
+const props = {
+  id: randomString().substring(0, 3),
+  title: randomString(),
+  author: randomString().substring(0, 5),
+  description: randomDesc(),
+  creationDate: creationDate,
+  expiryDate: expiryDate,
+  quorum: quorum,
+  threshold: quorum / 2 + 1,
+  vote: vote,
+  status: getStatus(vote, creationDate, expiryDate),
+};
+
+const Proposal = (): JSX.Element => {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleExpandClick = (): void => {
+    setExpanded(!expanded);
   };
 
   return (
     <Block key={props.title} width="100%" display="flex" alignItems="center" border="1px solid black">
       <Block width="100%" margin={2}>
-        <Typography variant="h6">{props.title}</Typography>
-        <Typography variant="body1">Proposer: {props.author}</Typography>
-        <Typography variant="body1">{props.description}</Typography>
+        <Block display="flex" alignItems="center">
+          <Typography variant="h6">{props.title}</Typography>
+          <Block marginLeft={2}>
+            <Typography variant="body1">{props.status}</Typography>
+          </Block>
+        </Block>
+        <Block display="flex" alignItems="center">
+          <Typography variant="body1">Author: {props.author}</Typography>
+          <Block marginLeft={2}>
+            <Typography variant="body1">Proposal ID: {props.id}</Typography>
+          </Block>
+        </Block>
+        {props.description.length < DESC_MAX_LENGTH && (
+          <Typography variant="body1">{props.description}</Typography>
+        )}
+
+        {props.description.length >= DESC_MAX_LENGTH && (
+          <React.Fragment>
+            {!expanded && (
+              <Typography variant="body1">
+                {props.description.substring(0, DESC_MAX_LENGTH)}
+                {'... '}
+                <span onClick={handleExpandClick}>Read more</span>
+              </Typography>
+            )}
+            <Collapse in={expanded} timeout="auto" unmountOnExit>
+              <Typography variant="body1">
+                {props.description} <span onClick={handleExpandClick}>Read less</span>
+              </Typography>
+            </Collapse>
+          </React.Fragment>
+        )}
+        <Block display="flex" alignItems="center">
+          <Typography variant="body1">Expires on {props.expiryDate.toLocaleDateString('es-ES')}</Typography>
+          <Block marginLeft={2}>
+            <Typography variant="body1">
+              Created on {props.creationDate.toLocaleDateString('es-ES')}
+            </Typography>
+          </Block>
+          <Block marginLeft={2}>
+            <Typography variant="body1">Delete</Typography>
+          </Block>
+        </Block>
       </Block>
       <Block minWidth="205px" margin={2} display="flex" flexDirection="column">
         <Typography variant="body1">Your Vote:</Typography>
