c52ef037767e0b2379386da3fa96eec494c0ca8c
Albert Andrejev <albert.andrejev@gmail.com>
2019-07-01 23:36:34 +0300

    Drawer test helper methods update

--- packages/sanes-chrome-extension/src/routes/account/test/drawer.ts
+++ packages/sanes-chrome-extension/src/routes/account/test/drawer.ts
@@ -5,6 +5,7 @@ import { findRenderedDOMComponentWithId } from '../../../utils/test/reactElemFin
 
 let recoveryPhraseLink: Element;
 let requestsLink: Element;
+let termsLink: Element;
 
 const initDrawer = async (drawerComponent: React.Component): Promise<void> => {
   const drawerButton = TestUtils.scryRenderedDOMComponentsWithTag(drawerComponent, 'button')[0];
@@ -13,13 +14,16 @@ const initDrawer = async (drawerComponent: React.Component): Promise<void> => {
 
   const drawerList = await findRenderedDOMComponentWithId(drawerComponent, DRAWER_HTML_ID);
   const drawerElements = (drawerList as Element).querySelectorAll('div > div:nth-of-type(2)');
-  expect(drawerElements.length).toBe(2);
+  expect(drawerElements.length).toBe(3);
 
   recoveryPhraseLink = drawerElements[0];
   expect(recoveryPhraseLink.textContent).toBe('Recovery words');
 
   requestsLink = drawerElements[1];
   expect(requestsLink.textContent).toBe('Requests');
+
+  termsLink = drawerElements[2];
+  expect(termsLink.textContent).toBe('Terms & Conditions');
 };
 
 export const clickRecoveryPhrase = async (drawerComponent: React.Component): Promise<void> => {
@@ -30,4 +34,9 @@ export const clickRecoveryPhrase = async (drawerComponent: React.Component): Pro
 export const clickRequests = async (drawerComponent: React.Component): Promise<void> => {
   await initDrawer(drawerComponent);
   click(requestsLink);
+};
+
+export const clickTerms = async (drawerComponent: React.Component): Promise<void> => {
+  await initDrawer(drawerComponent);
+  click(termsLink);
 };
