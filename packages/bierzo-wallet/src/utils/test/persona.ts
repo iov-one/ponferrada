7f8eae52f730bf1756b3612672602d164a3e8ac3
Albert Andrejev <albert.andrejev@gmail.com>
2019-07-02 19:12:38 +0300

    Fix e2e test with extension signup

--- packages/bierzo-wallet/src/utils/test/persona.ts
+++ packages/bierzo-wallet/src/utils/test/persona.ts
@@ -5,6 +5,7 @@ import { whenOnNavigatedToE2eRoute } from './navigation';
 const ACCOUNT_NAME_FIELD = 'accountNameField';
 const PASSWORD_FIELD = 'passwordInputField';
 const PASSWORD_CONFIRM_FIELD = 'passwordConfirmInputField';
+const TERMS_ACCEPT_FIELD = 'termsAcceptCheckboxField';
 
 async function clickCreatePersona(page: Page): Promise<void> {
   await page.click('button:nth-of-type(2)');
@@ -21,6 +22,7 @@ export const submitExtensionSignupForm = async (
   await page.type(`input[name="${ACCOUNT_NAME_FIELD}"]`, accountName);
   await page.type(`input[name="${PASSWORD_FIELD}`, password);
   await page.type(`input[name="${PASSWORD_CONFIRM_FIELD}`, password);
+  await page.click(`input[name="${TERMS_ACCEPT_FIELD}"]`);
   await page.click('button[type="submit"]');
   await sleep(1000);
   const buttons = await page.$$('button');
