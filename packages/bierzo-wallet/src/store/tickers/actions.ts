f4dfac224cccbff5bc459e83b41b6367689264fb
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-07-03 17:44:16 +0200

    Implement initial getTickers logic

--- packages/bierzo-wallet/src/store/tickers/actions.ts
+++ packages/bierzo-wallet/src/store/tickers/actions.ts
@@ -1,7 +1,21 @@
-import { ChainId, Token } from '@iov/bcp';
-import { AddTickerActionType } from './reducer';
+import { EthereumConnection } from '@iov/ethereum';
+import { AddTickerActionType, TickerState } from './reducer';
 
-export const addTickersAction = (chainId: ChainId, token: Token): AddTickerActionType => ({
+export async function getTickers(): Promise<ReadonlyArray<TickerState>> {
+  const tickers = [];
+
+  // TODO for now we only check the ethereum connection. The rest of chains will be
+  // added after it. Stay tuned!
+  const connection = await EthereumConnection.establish('http://localhost:8545', {});
+  const chainId = connection.chainId();
+  const chainTokens = await connection.getAllTokens();
+
+  tickers.push({ chainId, tokens: chainTokens });
+
+  return tickers;
+}
+
+export const addTickersAction = (tokens: ReadonlyArray<TickerState>): AddTickerActionType => ({
   type: '@@bw/ADD_TICKERS',
-  payload: { chainId, token },
+  payload: tokens,
 });
--- packages/bierzo-wallet/src/store/tickers/reducer.ts
+++ packages/bierzo-wallet/src/store/tickers/reducer.ts
@@ -3,10 +3,10 @@ import { Action } from 'redux';
 
 export interface TickerState {
   readonly chainId: ChainId;
-  readonly token: Token;
+  readonly tokens: ReadonlyArray<Token>;
 }
 
 export interface AddTickerActionType extends Action {
   type: '@@bw/ADD_TICKERS';
-  payload: TickerState;
+  payload: ReadonlyArray<TickerState>;
 }
