f83f337b1f5720150c587ff4a7686441b53f42dd
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-07-03 17:59:56 +0200

    Implementing tokens reducer

--- packages/bierzo-wallet/src/store/tickers/reducer.ts
+++ packages/bierzo-wallet/src/store/tickers/reducer.ts
@@ -1,12 +1,32 @@
 import { ChainId, Token } from '@iov/bcp';
 import { Action } from 'redux';
+import { ActionType } from 'typesafe-actions';
+import * as actions from './actions';
 
-export interface TickerState {
+export interface BwToken {
   readonly chainId: ChainId;
   readonly tokens: ReadonlyArray<Token>;
 }
 
 export interface AddTickerActionType extends Action {
-  type: '@@bw/ADD_TICKERS';
-  payload: ReadonlyArray<TickerState>;
+  type: '@@bw/ADD_TOKENS';
+  payload: ReadonlyArray<BwToken>;
+}
+
+export type TokenActions = ActionType<typeof actions>;
+
+interface TokenState {
+  tokens: ReadonlyArray<BwToken>;
+}
+const initState: TokenState = {
+  tokens: [],
+};
+
+export function tokensReducer(state: TokenState = initState, action: TokenActions): TokenState {
+  switch (action.type) {
+    case '@@bw/ADD_TOKENS':
+      return { ...state, tokens: action.payload };
+    default:
+      return state;
+  }
 }
