da292e225e391f534f933234c230b4381e76c5ef
Albert Andrejev <albert.andrejev@gmail.com>
2019-07-04 14:00:27 +0300

    Story for username registration

--- packages/sanes-chrome-extension/src/routes/tx-request/index.stories.tsx
+++ packages/sanes-chrome-extension/src/routes/tx-request/index.stories.tsx
@@ -12,13 +12,14 @@ import { CHROME_EXTENSION_ROOT } from '../../utils/storybook';
 import { ACCOUNT_STATUS_PAGE } from '../account/index.stories';
 import RejectRequest from './components/RejectRequest';
 import ShowRequest from './components/ShowRequest';
-import { getTransaction } from './test';
+import { getTransaction, getUsernameTransaction } from './test';
 
 const TX_REQUEST_PATH = `${CHROME_EXTENSION_ROOT}/Transaction Request`;
-const SHOW_REQUEST_PAGE = 'Show Request page';
+const SHOW_TX_REQUEST_PAGE = 'Show TX Request page';
+const SHOW_USERNAME_REQUEST_PAGE = 'Show USERNAME Request page';
 const REJECT_REQUEST_PAGE = 'Reject Request page';
 
-const request: Request = {
+const txRequest: Request = {
   id: 0,
   accept: () => action('accept request'),
   reject: (permanent: boolean) => action(`reject request. Permanently: ${permanent ? 'yes' : 'no'}`),
@@ -31,18 +32,49 @@ const request: Request = {
   type: 'signAndPost',
 };
 
+const usernameRequest: Request = {
+  id: 0,
+  accept: () => action('accept request'),
+  reject: (permanent: boolean) => action(`reject request. Permanently: ${permanent ? 'yes' : 'no'}`),
+  reason: 'I would like you to sign this TX',
+  data: {
+    senderUrl: 'http://localhost/',
+    creator: '0x873fAA4cdDd5b157e8E5a57e7a5479AFC5aaaaaa' as Address,
+    tx: getUsernameTransaction(),
+  },
+  type: 'signAndPost',
+};
+
 storiesOf(TX_REQUEST_PATH, module)
   .add(
-    SHOW_REQUEST_PAGE,
+    SHOW_TX_REQUEST_PAGE,
     (): JSX.Element => {
-      const { creator, tx } = request.data as SignAndPostRequest;
+      const { creator, tx } = txRequest.data as SignAndPostRequest;
 
       return (
         <Storybook>
           <ShowRequest
             tx={tx}
             creator={creator}
-            sender={request.data.senderUrl}
+            sender={txRequest.data.senderUrl}
+            onAcceptRequest={linkTo(CHROME_EXTENSION_ROOT, ACCOUNT_STATUS_PAGE)}
+            showRejectView={linkTo(TX_REQUEST_PATH, REJECT_REQUEST_PAGE)}
+          />
+        </Storybook>
+      );
+    },
+  )
+  .add(
+    SHOW_USERNAME_REQUEST_PAGE,
+    (): JSX.Element => {
+      const { creator, tx } = usernameRequest.data as SignAndPostRequest;
+
+      return (
+        <Storybook>
+          <ShowRequest
+            tx={tx}
+            creator={creator}
+            sender={usernameRequest.data.senderUrl}
             onAcceptRequest={linkTo(CHROME_EXTENSION_ROOT, ACCOUNT_STATUS_PAGE)}
             showRejectView={linkTo(TX_REQUEST_PATH, REJECT_REQUEST_PAGE)}
           />
@@ -55,8 +87,8 @@ storiesOf(TX_REQUEST_PATH, module)
     (): JSX.Element => (
       <Storybook>
         <RejectRequest
-          sender={request.data.senderUrl}
-          onBack={linkTo(TX_REQUEST_PATH, SHOW_REQUEST_PAGE)}
+          sender={txRequest.data.senderUrl}
+          onBack={linkTo(TX_REQUEST_PATH, SHOW_TX_REQUEST_PAGE)}
           onRejectRequest={action('onAcceptRequest')}
         />
       </Storybook>
