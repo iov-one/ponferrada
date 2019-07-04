e39e62c3a486c67369c9aa0815ef1318106836f4
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-07-04 09:16:31 +0200

    Adding tokens reducer to redux store

--- packages/bierzo-wallet/src/store/reducers.ts
+++ packages/bierzo-wallet/src/store/reducers.ts
@@ -2,16 +2,19 @@ import { combineReducers, Reducer } from 'redux';
 import { StateType } from 'typesafe-actions';
 import { extensionReducer, ExtensionState } from './extension';
 import { notificationReducer, NotificationState } from './notifications';
+import { tokensReducer, TokenState } from './tokens';
 
 export interface RootReducer {
   extension: ExtensionState;
   notifications: NotificationState;
+  tokens: TokenState;
 }
 
 const createRootReducer = (): Reducer<RootReducer> =>
   combineReducers({
     extension: extensionReducer,
     notifications: notificationReducer,
+    tokens: tokensReducer,
   });
 
 export type RootState = StateType<ReturnType<typeof createRootReducer>>;
