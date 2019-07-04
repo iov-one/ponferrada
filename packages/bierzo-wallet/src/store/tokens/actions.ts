2d4ad1e17b674eacf0def9eac3b4269ceae57554
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-07-04 17:31:42 +0200

    Simplify tokens action for fetching connection

--- packages/bierzo-wallet/src/store/tokens/actions.ts
+++ packages/bierzo-wallet/src/store/tokens/actions.ts
@@ -1,27 +1,6 @@
-import { BlockchainConnection } from '@iov/bcp';
-import { BnsConnection } from '@iov/bns';
-import { EthereumConnection } from '@iov/ethereum';
-import { LiskConnection } from '@iov/lisk';
-
-import { ChainSpec, loadConfig } from '../../utils/config';
+import { getConnectionFor } from '../../logic/connection';
+import { loadConfig } from '../../utils/config';
 import { AddTickerActionType, BwToken } from './reducer';
-
-async function createConnectionFor(spec: ChainSpec): Promise<BlockchainConnection | null> {
-  const url = spec.bootstrapNodes[0];
-  if (spec.codecType === 'eth') {
-    return EthereumConnection.establish(url, {});
-  }
-
-  if (spec.codecType === 'bns') {
-    return BnsConnection.establish(url);
-  }
-
-  if (spec.codecType === 'lsk') {
-    return LiskConnection.establish(url);
-  }
-
-  return null;
-}
 
 export async function getTokens(): Promise<{ [ticker: string]: BwToken }> {
   const config = await loadConfig();
@@ -29,11 +8,7 @@ export async function getTokens(): Promise<{ [ticker: string]: BwToken }> {
   const chains = config.chains;
 
   for (const chain of chains) {
-    const connection = await createConnectionFor(chain.chainSpec);
-    if (!connection) {
-      continue;
-    }
-
+    const connection = await getConnectionFor(chain.chainSpec);
     const chainId = connection.chainId();
     const chainTokens = await connection.getAllTokens();
 
