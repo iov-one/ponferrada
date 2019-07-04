bd8bb0f01b5361afaad38f95d0ee29cf418bbd02
abefernan <abel.fernandez.nandin@gmail.com>
2019-07-04 15:52:42 +0200

    Changes Proposal to make use of new extracted components

--- packages/sil-voting-app/src/routes/dashboard/components/Proposal/index.tsx
+++ packages/sil-voting-app/src/routes/dashboard/components/Proposal/index.tsx
@@ -1,12 +1,10 @@
-import Button from '@material-ui/core/Button';
 import Block from 'medulas-react-components/lib/components/Block';
-import Typography from 'medulas-react-components/lib/components/Typography';
 import React from 'react';
-import { elipsify } from '../../../../utils/strings';
+import CreationData from './CreationData';
 import Description from './Description';
 import Status from './Status';
-
-const TITLE_MAX_LENGTH = 30;
+import Title from './Title';
+import VoteActions from './VoteActions';
 
 export interface VoteResult {
   readonly yes: number;
@@ -32,26 +30,9 @@ const Proposal = (props: ProposalProps): JSX.Element => {
   return (
     <Block width="100%" display="flex" alignItems="center">
       <Block flexGrow={1} margin={2}>
-        <Block display="flex" alignItems="center" marginBottom={1}>
-          <Typography variant="h6">{elipsify(props.title, TITLE_MAX_LENGTH)}</Typography>
-          <Block marginLeft={2}>
-            <Typography variant="body1">{props.status}</Typography>
-          </Block>
-        </Block>
-        <Block display="flex" alignItems="center" marginBottom={1}>
-          <Typography variant="body1">Author: {props.author}</Typography>
-          <Block marginLeft={2}>
-            <Typography variant="body1">Proposal ID: {props.id}</Typography>
-          </Block>
-          <Block marginLeft={2}>
-            <Typography variant="body1">
-              Created on {props.creationDate.toLocaleDateString('es-ES')}
-            </Typography>
-          </Block>
-        </Block>
-        <Block marginBottom={1}>
-          <Description description={props.description} />
-        </Block>
+        <Title title={props.title} status={props.status} />
+        <CreationData author={props.author} id={props.id} creationDate={props.creationDate} />
+        <Description description={props.description} />
         <Status
           expiryDate={props.expiryDate}
           quorum={props.quorum}
@@ -59,24 +40,7 @@ const Proposal = (props: ProposalProps): JSX.Element => {
           status={props.status}
         />
       </Block>
-      <Block minWidth="100px" margin={2} display="flex" flexDirection="column">
-        <Typography variant="body1">Your Vote:</Typography>
-        <Block marginTop="4px" marginBottom="4px">
-          <Button fullWidth variant={props.vote === 'Yes' ? 'contained' : 'outlined'}>
-            Yes
-          </Button>
-        </Block>
-        <Block marginTop="4px" marginBottom="4px">
-          <Button fullWidth variant={props.vote === 'No' ? 'contained' : 'outlined'}>
-            No
-          </Button>
-        </Block>
-        <Block marginTop="4px" marginBottom="4px">
-          <Button fullWidth variant={props.vote === 'Abstain' ? 'contained' : 'outlined'}>
-            Abstain
-          </Button>
-        </Block>
-      </Block>
+      <VoteActions vote={props.vote} />
     </Block>
   );
 };
