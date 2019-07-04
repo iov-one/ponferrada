8ae347cd0211245b26b94dbd84d6584fcb246824
abefernan <abel.fernandez.nandin@gmail.com>
2019-07-04 15:51:34 +0200

    Extract CreationData component from Proposal

---
+++ packages/sil-voting-app/src/routes/dashboard/components/Proposal/CreationData.tsx
@@ -0,0 +1,25 @@
+import Block from 'medulas-react-components/lib/components/Block';
+import Typography from 'medulas-react-components/lib/components/Typography';
+import React from 'react';
+
+interface Props {
+  readonly author: string;
+  readonly id: string;
+  readonly creationDate: Date;
+}
+
+const CreationData = (props: Props): JSX.Element => {
+  return (
+    <Block display="flex" alignItems="center" marginBottom={1}>
+      <Typography variant="body1">Author: {props.author}</Typography>
+      <Block marginLeft={2}>
+        <Typography variant="body1">Proposal ID: {props.id}</Typography>
+      </Block>
+      <Block marginLeft={2}>
+        <Typography variant="body1">Created on {props.creationDate.toLocaleDateString('es-ES')}</Typography>
+      </Block>
+    </Block>
+  );
+};
+
+export default CreationData;
