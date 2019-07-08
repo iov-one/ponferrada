26eeb5fd24b762bbad12522978abbd6954c8d77a
Albert Andrejev <albert.andrejev@gmail.com>
2019-07-08 20:42:55 +0300

    add PageMenu component

---
+++ packages/bierzo-wallet/src/components/PageMenu/index.tsx
@@ -0,0 +1,33 @@
+import { Theme } from '@material-ui/core/styles';
+import { useTheme } from '@material-ui/styles';
+import Block from 'medulas-react-components/lib/components/Block';
+import ErrorBoundary from 'medulas-react-components/lib/errors/ErrorBoundary';
+import * as React from 'react';
+import { RouteComponentProps, withRouter } from 'react-router';
+
+import Header from '../Header';
+
+interface Props extends RouteComponentProps {
+  readonly renderProps?: () => React.ReactNode;
+  readonly children?: React.ReactNode;
+  readonly padding?: boolean;
+}
+
+const PageMenu = ({ children, padding = true, renderProps, location }: Props): JSX.Element => {
+  const theme = useTheme<Theme>();
+
+  return (
+    <Block bgcolor={theme.palette.background.default}>
+      <Block display="flex" flexDirection="column" flexGrow={1}>
+        <Header path={location.pathname} />
+        <ErrorBoundary type="inner">
+          <Block display="flex" flexDirection="column" flexGrow={1} padding={padding ? 4 : undefined}>
+            {renderProps !== undefined ? renderProps() : children}
+          </Block>
+        </ErrorBoundary>
+      </Block>
+    </Block>
+  );
+};
+
+export default withRouter(PageMenu);
