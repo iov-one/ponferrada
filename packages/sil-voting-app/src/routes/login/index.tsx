7c75efe60c7b5f3abcb85d564c482099fd7e3534
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-07-08 10:27:41 +0200

    Fix import

--- packages/sil-voting-app/src/routes/login/index.tsx
+++ packages/sil-voting-app/src/routes/login/index.tsx
@@ -35,6 +35,7 @@ const useStyles = makeStyles((theme: Theme) => ({
 const Login = (): JSX.Element => {
   const classes = useStyles();
   const toast = useContext(ToastContext);
+
   //TODO: Fix this as soon as proper react-redux definitions will be available
   const dispatch = (ReactRedux as any).useDispatch();
 
