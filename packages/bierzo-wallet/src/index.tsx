4e867d34a06a8df44e0fe4fab811e72ac2e438ff
Albert Andrejev <albert.andrejev@gmail.com>
2019-07-08 20:42:24 +0300

    Sentry config

--- packages/bierzo-wallet/src/index.tsx
+++ packages/bierzo-wallet/src/index.tsx
@@ -1,3 +1,4 @@
+import * as Sentry from '@sentry/browser';
 import { ToastProvider } from 'medulas-react-components/lib/context/ToastProvider';
 import MedulasThemeProvider from 'medulas-react-components/lib/theme/MedulasThemeProvider';
 import React from 'react';
@@ -7,6 +8,10 @@ import { Provider } from 'react-redux';
 import Routes from './routes';
 import { configureStore } from './store';
 import { globalStyles } from './theme/globalStyles';
+
+Sentry.init({
+  dsn: 'https://6f1aa71313e14f81b9b663d831705ff6@sentry.io/1374813',
+});
 
 const store = configureStore();
 const rootEl = document.getElementById('root');
