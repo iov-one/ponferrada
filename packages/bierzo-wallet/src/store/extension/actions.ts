0afdb2c6a3df28a68afc50bc8a51e788d52748cf
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-07-09 10:43:45 +0200

    Add getExtensionStatus function to extension reducer

--- packages/bierzo-wallet/src/store/extension/actions.ts
+++ packages/bierzo-wallet/src/store/extension/actions.ts
@@ -1,4 +1,34 @@
+import { TransactionEncoder } from '@iov/core';
+
+import { sendGetIdentitiesRequest } from '../../communication/identities';
+import { ExtensionState } from '../../store/extension';
 import { SetExtensionStateActionType } from './reducer';
+
+export async function getExtensionStatus(): Promise<ExtensionState> {
+  const identities = await sendGetIdentitiesRequest();
+
+  if (!identities) {
+    return { installed: false, connected: false, keys: {} };
+  }
+
+  const keys = Object.keys(identities);
+  const hasKeys = keys.length > 0;
+  if (!hasKeys) {
+    return { installed: true, connected: false, keys: {} };
+  }
+
+  let publicKeys: { [chain: string]: string } = {};
+
+  for (const key of keys) {
+    publicKeys[key] = JSON.stringify(TransactionEncoder.toJson(identities[key]));
+  }
+
+  return {
+    installed: true,
+    connected: true,
+    keys: publicKeys,
+  };
+}
 
 export const setExtensionStateAction = (
   connected: boolean,
