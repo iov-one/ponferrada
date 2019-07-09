bef43d5195e630342af63742d3b8881f719edda8
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-07-09 17:38:17 +0200

    Extracted parseGetIdentitiesResponse for mocking value in jest tests

--- packages/bierzo-wallet/src/communication/identities/index.ts
+++ packages/bierzo-wallet/src/communication/identities/index.ts
@@ -1,7 +1,6 @@
 /*global chrome*/
 import { isPublicIdentity, PublicIdentity } from '@iov/bcp';
 import { TransactionEncoder } from '@iov/core';
-import { ethereumCodec } from '@iov/ethereum';
 import { isJsonRpcErrorResponse, JsonRpcRequest, makeJsonRpcId, parseJsonRpcResponse2 } from '@iov/jsonrpc';
 
 import { extensionId } from '..';
@@ -29,6 +28,31 @@ function extensionContext(): boolean {
   return typeof chrome.runtime !== 'undefined' && typeof chrome.runtime.sendMessage !== 'undefined';
 }
 
+// exported for testing purposes
+export const parseGetIdentitiesResponse = (
+  response: any,
+  chains: ReadonlyArray<string>,
+): { [chain: string]: PublicIdentity } => {
+  const parsedResponse = parseJsonRpcResponse2(response);
+  if (isJsonRpcErrorResponse(parsedResponse)) {
+    console.error(parsedResponse.error.message);
+    throw new Error('Received unexpected json rpc response');
+  }
+
+  const parsedResult = TransactionEncoder.fromJson(parsedResponse.result);
+  if (!isArrayOfPublicIdentity(parsedResult)) {
+    throw new Error('Got unexpected type of result');
+  }
+
+  const keys: { [chain: string]: PublicIdentity } = {};
+  for (let i = 0; i < parsedResult.length; i++) {
+    const chain = chains[i];
+    keys[chain] = parsedResult[i];
+  }
+
+  return keys;
+};
+
 export const sendGetIdentitiesRequest = async (): Promise<GetIdentitiesResponse> => {
   const chains = ['ethereum-eip155-5777'];
   const request = generateGetIdentitiesRequest(chains);
@@ -46,30 +70,7 @@ export const sendGetIdentitiesRequest = async (): Promise<GetIdentitiesResponse>
       }
 
       try {
-        const parsedResponse = parseJsonRpcResponse2(response);
-        if (isJsonRpcErrorResponse(parsedResponse)) {
-          console.error(parsedResponse.error.message);
-          resolve({});
-          return;
-        }
-
-        const parsedResult = TransactionEncoder.fromJson(parsedResponse.result);
-        if (!isArrayOfPublicIdentity(parsedResult)) {
-          console.error('Got unexpected type of result');
-          resolve({});
-          return;
-        }
-
-        // TODO remove when the app evolves
-        const addresses = parsedResult.map(ident => ethereumCodec.identityToAddress(ident));
-        console.log(addresses);
-
-        const keys: { [chain: string]: PublicIdentity } = {};
-        for (let i = 0; i < parsedResult.length; i++) {
-          const chain = chains[i];
-          keys[chain] = parsedResult[i];
-        }
-
+        const keys = parseGetIdentitiesResponse(response, chains);
         resolve(keys);
       } catch (error) {
         console.error(error);
