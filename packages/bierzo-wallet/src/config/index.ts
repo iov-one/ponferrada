1cc9077fcee056318ce108fffba6b89963d438b0
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-07-10 11:23:32 +0200

    Using require for taking advantage of webpack's tree shaking

--- packages/bierzo-wallet/src/config/index.ts
+++ packages/bierzo-wallet/src/config/index.ts
@@ -1,10 +1,6 @@
 import { TokenTicker } from '@iov/bcp';
 import { singleton } from 'medulas-react-components/lib/utils/singleton';
 
-import development from './development.json';
-import production from './production.json';
-// import staging from './staging';
-
 export interface Config {
   readonly chains: ReadonlyArray<ChainConfig>;
 }
@@ -31,19 +27,19 @@ export interface FaucetSpec {
 }
 
 const configuration = (): Config => {
-  if (process.env.NODE_ENV === 'test') {
-    return (development as unknown) as Config;
+  if (process.env.REACT_APP_CONFIG === 'development') {
+    return require('./development.json') as Config;
   }
 
-  if (process.env.NODE_ENV === 'production') {
-    return (production as unknown) as Config;
+  if (process.env.REACT_APP_CONFIG === 'staging') {
+    return require('./staging.json') as Config;
   }
 
-  if (process.env.NODE_ENV === 'development') {
-    return (development as unknown) as Config;
+  if (process.env.REACT_APP_CONFIG === 'production') {
+    return require('./production.json') as Config;
   }
 
-  throw new Error('Unexpected NODE_ENV variable for obtaining configuration');
+  throw new Error('Unexpected REACT_APP_CONFIG variable for obtaining configuration');
 };
 
 export const getConfig = singleton<typeof configuration>(configuration);
