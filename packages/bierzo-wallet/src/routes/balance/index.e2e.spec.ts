7db06d4ef2e58879974777fb7a0834d971c718b6
Albert Andrejev <albert.andrejev@gmail.com>
2019-07-15 18:57:42 +0300

    Add e2e test

---
+++ packages/bierzo-wallet/src/routes/balance/index.e2e.spec.ts
@@ -0,0 +1,56 @@
+import express, { Request, Response } from 'express';
+import { Server } from 'http';
+import { Browser, Page } from 'puppeteer';
+
+import { closeBrowser, createExtensionPage, createPage, launchBrowser } from '../../utils/test/e2e';
+import { withChainsDescribe } from '../../utils/test/testExecutor';
+import { travelToBalanceE2E } from './test/travelToBalance';
+
+withChainsDescribe(
+  'E2E > Balance route',
+  (): void => {
+    let browser: Browser;
+    let page: Page;
+    let extensionPage: Page;
+    let server: Server;
+
+    beforeAll(() => {
+      const app = express();
+
+      app.use(express.static(require('path').join(__dirname, '/../../../build')));
+
+      app.get('/*', function(req: Request, res: Response) {
+        res.sendFile(require('path').join(__dirname, 'build', 'index.html'));
+      });
+
+      server = app.listen(9000);
+    });
+
+    beforeEach(async (): Promise<void> => {
+      browser = await launchBrowser();
+      page = await createPage(browser);
+      extensionPage = await createExtensionPage(browser);
+      await travelToBalanceE2E(browser, page, extensionPage);
+    }, 45000);
+
+    afterEach(
+      async (): Promise<void> => {
+        //await closeBrowser(browser);
+      },
+    );
+
+    afterAll(() => {
+      server.close();
+    });
+
+    it('should contain "No funds available" message', async (): Promise<void> => {
+      const message = await page.$('h6.nth-of-type(4)');
+      expect(message).not.toBeNull();
+
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      const text = await (await message!.getProperty('textContent')).jsonValue();
+
+      expect(text).toBe('No funds available');
+    }, 45000);
+  },
+);
