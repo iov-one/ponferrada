b093bc66b156f178388a4f928e414a446a9a854f
abefernan <abel.fernandez.nandin@gmail.com>
2019-07-16 17:12:18 +0200

    Refactor extension state and fix initialState

--- packages/sil-voting-app/src/store/reducers/extension/actions.ts
+++ packages/sil-voting-app/src/store/reducers/extension/actions.ts
@@ -1,9 +1,23 @@
-import { SetExtensionStateActionType } from './reducer';
+import { sendGetIdentitiesRequest } from '../../../communication/identities';
+import { ExtensionState, SetExtensionStateActionType } from './reducer';
+
+export async function getExtensionStatus(): Promise<ExtensionState> {
+  const identities = await sendGetIdentitiesRequest();
+
+  if (!identities) {
+    return { installed: false, connected: false };
+  }
+
+  return {
+    installed: true,
+    connected: true,
+  };
+}
 
 export const setExtensionStateAction = (
   connected: boolean,
   installed: boolean,
 ): SetExtensionStateActionType => ({
-  type: '@@extension/SET_EXTENSION_STATE',
+  type: '@@extension/SET_STATE',
   payload: { connected, installed },
 });
