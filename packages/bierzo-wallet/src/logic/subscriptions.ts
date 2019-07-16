5dc98e09cbf91744065b2f88c5cd4b8680f4804a
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-07-16 15:08:39 +0200

    Add initial pseudo code for subscriptions

---
+++ packages/bierzo-wallet/src/logic/subscriptions.ts
@@ -0,0 +1,6 @@
+export async function subscribe(): Promise<any> {
+  // subscribe to transactions
+  // const transactionsStream = connection.liveTx({ sentFromOrTo: address });
+  // subscribe to balance changes via
+  // const accountStream = connection.watchAccount({ address: address });
+}
