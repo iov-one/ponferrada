b216862fd04526c2c73fb0f953910d220908bce2
Albert Andrejev <albert.andrejev@gmail.com>
2019-07-17 16:13:52 +0300

    Add e2e tests helper methods

--- packages/bierzo-wallet/src/routes/balance/test/operateBalances.ts
+++ packages/bierzo-wallet/src/routes/balance/test/operateBalances.ts
@@ -5,6 +5,14 @@ export const getNoFundsMessage = (h6Elements: Element[]): string => {
   return h6Elements[NO_FUNDS_IDX].textContent || '';
 };
 
-export const getNoFundsMessageE2E = async (h6Elements: ElementHandle<Element>[]): Promise<string> => {
-  return (await (await h6Elements[NO_FUNDS_IDX].getProperty('textContent')).jsonValue()) || '';
+export const getFirstCurrencyBalanceE2E = async (h6Elements: ElementHandle<Element>[]): Promise<string> => {
+  return (await (await h6Elements[5].getProperty('textContent')).jsonValue()) || '';
+};
+
+export const getSecondCurrencyBalanceE2E = async (h6Elements: ElementHandle<Element>[]): Promise<string> => {
+  return (await (await h6Elements[6].getProperty('textContent')).jsonValue()) || '';
+};
+
+export const getThirdCurrencyBalanceE2E = async (h6Elements: ElementHandle<Element>[]): Promise<string> => {
+  return (await (await h6Elements[7].getProperty('textContent')).jsonValue()) || '';
 };
