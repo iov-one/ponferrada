1313e392cc55927057f3afbb2323d3f27dd370d7
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-07-22 09:28:00 +0200

    Include Add Confirmed Tx action types

--- packages/bierzo-wallet/src/store/notifications/actions.ts
+++ packages/bierzo-wallet/src/store/notifications/actions.ts
@@ -1,7 +1,7 @@
 import { Action } from 'redux';
 import { ActionType } from 'typesafe-actions';
 
-import { ProcessedTx } from './reducer';
+import { AnnotatedConfirmedTransaction, ProcessedTx } from './reducer';
 
 export interface AddPendingTransactionActionType extends Action {
   type: '@@notifications/ADD_PENDING_TRANSACTION';
@@ -13,4 +13,18 @@ export const addPendingTransactionAction = (transaction: ProcessedTx): AddPendin
   payload: transaction,
 });
 
-export type NotificationActions = ActionType<typeof addPendingTransactionAction>;
+export interface AddConfirmedTransactionActionType extends Action {
+  type: '@@notifications/ADD_CONFIRMED_TRANSACTION';
+  payload: AnnotatedConfirmedTransaction;
+}
+
+export const addConfirmedTransaction = (
+  transaction: AnnotatedConfirmedTransaction,
+): AddConfirmedTransactionActionType => ({
+  type: '@@notifications/ADD_CONFIRMED_TRANSACTION',
+  payload: transaction,
+});
+
+export type NotificationActions = ActionType<
+  typeof addPendingTransactionAction | typeof addConfirmedTransaction
+>;
