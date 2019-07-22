97b90997f91a8955b0b9e2fd55f70adb084fa63d
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-07-22 12:59:19 +0200

    Parse ConfirmedTransaction on subscription for making more redux friendly

--- packages/bierzo-wallet/src/logic/transactions.ts
+++ packages/bierzo-wallet/src/logic/transactions.ts
@@ -1,14 +1,71 @@
-import { Identity, isConfirmedTransaction } from '@iov/bcp';
+import {
+  BlockchainConnection,
+  ConfirmedTransaction,
+  Identity,
+  isConfirmedTransaction,
+  isSendTransaction,
+  LightTransaction,
+  pubkeyBundleEquals,
+  TxCodec,
+} from '@iov/bcp';
+import { BnsConnection } from '@iov/bns';
 import { TransactionEncoder } from '@iov/core';
 import { Dispatch } from 'redux';
 import { Subscription } from 'xstream';
 
 import { getConfig } from '../config';
-import { addConfirmedTransaction } from '../store/notifications';
+import { addConfirmedTransaction, ProcessedTx } from '../store/notifications';
+import { getUsernameFrom } from '../store/usernames';
 import { getCodec } from './codec';
-import { getConnectionFor } from './connection';
+import { getConnectionFor, isBnsSpec } from './connection';
 
 let txsSubscriptions: Subscription[] = [];
+
+export const parseConfirmedTransaction = async <T extends LightTransaction>(
+  bnsConn: BnsConnection,
+  conn: BlockchainConnection,
+  trans: ConfirmedTransaction<T>,
+  identity: Identity,
+  codec: TxCodec,
+): Promise<ProcessedTx | undefined> => {
+  const payload = trans.transaction;
+  if (!isSendTransaction(payload)) {
+    console.log(`Only handle SendTransaction for now, got ${payload.kind}`);
+    return undefined;
+  }
+
+  const header = await conn.getBlockHeader(trans.height);
+  const time = header.time;
+
+  const received = !pubkeyBundleEquals(trans.primarySignature.pubkey, identity.pubkey);
+
+  // we look up names from the bns chain
+  const chainId = conn.chainId();
+  const recipientAddr = payload.recipient;
+  const recipientName = await getUsernameFrom(bnsConn, chainId, recipientAddr);
+  const recipient = recipientName || recipientAddr;
+
+  const creatorPubkey = trans.primarySignature.pubkey;
+  const creator: Identity = {
+    chainId: chainId,
+    pubkey: creatorPubkey,
+  };
+  const signerAddr = codec.identityToAddress(creator);
+  const signerName = await getUsernameFrom(bnsConn, chainId, signerAddr);
+
+  const signer = signerName || signerAddr;
+
+  return {
+    id: trans.transactionId,
+    memo: payload.memo,
+    amount: payload.amount,
+    time,
+    received,
+    recipient,
+    signer,
+    success: true,
+  };
+};
 
 export async function subscribeTransaction(
   keys: { [chain: string]: string },
@@ -16,6 +73,12 @@ export async function subscribeTransaction(
 ): Promise<void> {
   const config = getConfig();
   const chains = config.chains;
+
+  const bnsSpec = config.chains.find(chain => isBnsSpec(chain.chainSpec));
+  if (!bnsSpec) {
+    throw new Error('For subscribing to transactions we need a BNS connection');
+  }
+  const bnsConnection: BnsConnection = (await getConnectionFor(bnsSpec.chainSpec)) as BnsConnection;
 
   for (const chain of chains) {
     const codec = getCodec(chain.chainSpec);
@@ -36,14 +99,16 @@ export async function subscribeTransaction(
           throw new Error('Confirmed transaction expected');
         }
 
-        await dispatch(addConfirmedTransaction(tx));
+        const proccesedTx = await parseConfirmedTransaction(bnsConnection, connection, tx, identity, codec);
+        if (!proccesedTx) {
+          return;
+        }
+
+        await dispatch(addConfirmedTransaction(proccesedTx));
       },
     });
     txsSubscriptions.push(subscription);
   }
-
-  // subscribe to transactions
-  // const transactionsStream = connection.liveTx({ sentFromOrTo: address });
 }
 
 export function unsubscribeTransactions(): void {
