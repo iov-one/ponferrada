4976fd3314e4909136e23618488fa909211aba0f
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-07-22 09:27:44 +0200

    Added tx subscription logic

--- packages/bierzo-wallet/src/logic/transactions.ts
+++ packages/bierzo-wallet/src/logic/transactions.ts
@@ -1,12 +1,52 @@
-import { Identity } from '@iov/bcp';
+import { Identity, isConfirmedTransaction } from '@iov/bcp';
 import { TransactionEncoder } from '@iov/core';
 import { Dispatch } from 'redux';
+import { Subscription } from 'xstream';
+
+import { getConfig } from '../config';
+import { addConfirmedTransaction } from '../store/notifications';
+import { getCodec } from './codec';
+import { getConnectionFor } from './connection';
+
+let txsSubscriptions: Subscription[] = [];
+
 export async function subscribeTransaction(
   keys: { [chain: string]: string },
   dispatch: Dispatch,
 ): Promise<void> {
+  const config = getConfig();
+  const chains = config.chains;
+
+  for (const chain of chains) {
+    const codec = getCodec(chain.chainSpec);
+    const connection = await getConnectionFor(chain.chainSpec);
+    const chainId = connection.chainId() as string;
+    const plainPubkey = keys[chainId];
+    if (!plainPubkey) {
+      continue;
+    }
+
+    const identity: Identity = TransactionEncoder.fromJson(JSON.parse(plainPubkey));
+    const address = codec.identityToAddress(identity);
+
+    // subscribe to balance changes via
+    const subscription = connection.liveTx({ sentFromOrTo: address }).subscribe({
+      next: async tx => {
+        if (!isConfirmedTransaction(tx)) {
+          throw new Error('Confirmed transaction expected');
+        }
+
+        await dispatch(addConfirmedTransaction(tx));
+      },
+    });
+    txsSubscriptions.push(subscription);
+  }
+
   // subscribe to transactions
   // const transactionsStream = connection.liveTx({ sentFromOrTo: address });
 }
 
-export function unsubscribeTransactions(): void {}
+export function unsubscribeTransactions(): void {
+  txsSubscriptions.forEach(subs => subs.unsubscribe());
+  txsSubscriptions = [];
+}
