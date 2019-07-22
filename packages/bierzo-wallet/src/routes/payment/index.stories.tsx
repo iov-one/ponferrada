f71801131c0a933642860c7cc951747f99531234
Albert Andrejev <albert.andrejev@gmail.com>
2019-07-22 23:35:57 +0300

    Payment route Storybook story

--- packages/bierzo-wallet/src/routes/payment/index.stories.tsx
+++ packages/bierzo-wallet/src/routes/payment/index.stories.tsx
@@ -1,16 +1,44 @@
+import { TokenTicker } from '@iov/bcp';
+import { action } from '@storybook/addon-actions';
 import { storiesOf } from '@storybook/react';
 import React from 'react';
+import { DeepPartial } from 'redux';
 
+import { BalanceState } from '../../store/balances';
+import { RootState } from '../../store/reducers';
 import DecoratedStorybook, { WALLET_ROOT } from '../../utils/storybook';
-import Payment from './index';
+import Layout from './components/index';
+
+const fullStore = (): DeepPartial<RootState> => {
+  return {
+    balances: {
+      BASH: {
+        quantity: '82500',
+        fractionalDigits: 4,
+        tokenTicker: 'BASH' as TokenTicker,
+      },
+      CASH: {
+        quantity: '1226775',
+        fractionalDigits: 5,
+        tokenTicker: 'CASH' as TokenTicker,
+      },
+    },
+  };
+};
+
+async function onSubmit(values: object): Promise<void> {
+  console.log(values);
+  action(JSON.stringify(values, undefined, 2));
+  return;
+}
 
 storiesOf(WALLET_ROOT, module)
   .addParameters({ viewport: { defaultViewport: 'responsive' } })
   .add(
     'Payment page',
     (): JSX.Element => (
-      <DecoratedStorybook>
-        <Payment />
+      <DecoratedStorybook storeProps={fullStore()}>
+        <Layout onCancelPayment={action('onCancelPayment')} onSubmit={onSubmit} />
       </DecoratedStorybook>
     ),
   );
