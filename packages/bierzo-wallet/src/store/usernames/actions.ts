9c81173ba434fee93fda82802a864f7a2bf94361
Simon Warta <simon@warta.it>
2019-07-22 12:44:45 +0200

    Refactor getUsernames
    - Avoid looping over chain connections
    - Avoid connection.getAccount call for address calculation

--- packages/bierzo-wallet/src/store/usernames/actions.ts
+++ packages/bierzo-wallet/src/store/usernames/actions.ts
@@ -1,5 +1,5 @@
 import { Identity } from '@iov/bcp';
-import { BnsConnection } from '@iov/bns';
+import { bnsCodec, BnsConnection } from '@iov/bns';
 import { TransactionEncoder } from '@iov/core';
 
 import { getConfig } from '../../config';
@@ -9,47 +9,33 @@ import { AddUsernamesActionType, BwUsername } from './reducer';
 export async function getUsernames(keys: {
   [chain: string]: string;
 }): Promise<{ [chainId: string]: BwUsername }> {
-  const config = getConfig();
-  const usernames: { [chainId: string]: BwUsername } = {};
-  const chains = config.chains;
+  const bnsChainSpec = getConfig()
+    .chains.map(chain => chain.chainSpec)
+    .find(isBnsSpec);
+  if (!bnsChainSpec) throw new Error('Missing BNS chain spec in config');
 
-  for (const chain of chains) {
-    if (!isBnsSpec(chain.chainSpec)) {
-      continue;
-    }
+  const bnsConnection = (await getConnectionFor(bnsChainSpec)) as BnsConnection;
 
-    const connection = (await getConnectionFor(chain.chainSpec)) as BnsConnection;
-    const chainId = connection.chainId();
+  const plainPubkey = keys[bnsConnection.chainId()];
+  if (!plainPubkey) return {};
 
-    const plainPubkey = keys[chainId];
-    if (!plainPubkey) {
-      break;
-    }
+  const bnsIdentity: Identity = TransactionEncoder.fromJson(JSON.parse(plainPubkey));
+  const bnsAddress = bnsCodec.identityToAddress(bnsIdentity);
 
-    const identity: Identity = TransactionEncoder.fromJson(JSON.parse(plainPubkey));
-    const account = await connection.getAccount({ pubkey: identity.pubkey });
-    if (!account) {
-      break;
-    }
+  const usernames = await bnsConnection.getUsernames({ owner: bnsAddress });
+  const firstUsername = usernames.find(() => true);
 
-    const names = await connection.getUsernames({ owner: account.address });
-    const hasName = names.length > 0;
-    if (!hasName) {
-      break;
-    }
+  if (!firstUsername) return {};
 
-    const name = names[0];
-
-    for (const address of name.addresses) {
-      usernames[address.chainId as string] = {
-        chainId: address.chainId,
-        address: address.address,
-        username: name.id,
-      };
-    }
+  const out: { [chainId: string]: BwUsername } = {};
+  for (const address of firstUsername.addresses) {
+    out[address.chainId as string] = {
+      chainId: address.chainId,
+      address: address.address,
+      username: firstUsername.id,
+    };
   }
-
-  return usernames;
+  return out;
 }
 
 export const addUsernamesAction = (usernames: { [chainId: string]: BwUsername }): AddUsernamesActionType => ({
