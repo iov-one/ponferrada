5678bbc9164b48e13811e42d930b38690758f895
Albert Andrejev <albert.andrejev@gmail.com>
2019-07-23 14:12:33 +0300

    Send payement method

--- packages/bierzo-wallet/src/routes/payment/index.tsx
+++ packages/bierzo-wallet/src/routes/payment/index.tsx
@@ -1,12 +1,23 @@
+import { Address, Identity, TokenTicker } from '@iov/bcp';
+import { TransactionEncoder } from '@iov/core';
+import { FormValues } from 'medulas-react-components/lib/components/forms/Form';
 import { ToastContext } from 'medulas-react-components/lib/context/ToastProvider';
 import { ToastVariant } from 'medulas-react-components/lib/context/ToastProvider/Toast';
 import React from 'react';
+import * as ReactRedux from 'react-redux';
 
 import { history } from '..';
+import { sendSignAndPostRequest } from '../../communication/signAndPost';
 //import { sendSignAndPostRequest } from '../../communication/signAndPost';
 import PageMenu from '../../components/PageMenu';
+import { getAddressByName, isIov } from '../../logic/account';
+import { RootState } from '../../store/reducers';
+import { padAmount, stringToAmount } from '../../utils/balances';
 import { PAYMENT_ROUTE } from '../paths';
 import Layout from './components';
+import { CURRENCY_FIELD, QUANTITY_FIELD } from './components/CurrencyToSend';
+import { ADDRESS_FIELD } from './components/ReceiverAddress';
+import { TEXTNOTE_FIELD } from './components/TextNote';
 
 function onCancelPayment(): void {
   history.push(PAYMENT_ROUTE);
@@ -14,22 +25,44 @@ function onCancelPayment(): void {
 
 const Payment = (): JSX.Element => {
   const toast = React.useContext(ToastContext);
+  const tokens = ReactRedux.useSelector((state: RootState) => state.tokens);
+  const pubKeys = ReactRedux.useSelector((state: RootState) => state.extension.keys);
 
   const onSubmit = async (values: object): Promise<void> => {
-    console.log('onSubmit');
-    console.log(values);
-    /*const formValues = values as FormValues;
+    const formValues = values as FormValues;
 
-    const keys = store.getState().extension.keys;
-    const plainPubkey = keys[chainId];
+    const ticker = formValues[CURRENCY_FIELD] as TokenTicker;
+    const amount = stringToAmount(formValues[QUANTITY_FIELD], ticker);
+    const token = tokens[ticker];
+    const chainId = token.chainId;
+    const tokenAmount = padAmount(amount, token.token.fractionalDigits);
+
+    let recipient: string | undefined = formValues[ADDRESS_FIELD];
+    if (isIov(recipient)) {
+      recipient = await getAddressByName(recipient.replace(/\*iov$/, ''), chainId);
+    }
+
+    if (!recipient) {
+      toast.show('IOV username was not found', ToastVariant.ERROR);
+      return;
+    }
+
+    const plainPubkey = pubKeys[chainId];
     if (!plainPubkey) {
-      continue;
+      toast.show('Public key was not found', ToastVariant.ERROR);
+      return;
     }
 
     const identity: Identity = TransactionEncoder.fromJson(JSON.parse(plainPubkey));
 
     try {
-      const transactionId = await sendSignAndPostRequest(identities[0]);
+      const transactionId = await sendSignAndPostRequest(
+        chainId,
+        identity,
+        formValues[ADDRESS_FIELD] as Address,
+        tokenAmount,
+        formValues[TEXTNOTE_FIELD],
+      );
       if (transactionId === null) {
         toast.show('Request rejected', ToastVariant.ERROR);
       } else {
@@ -39,7 +72,7 @@ const Payment = (): JSX.Element => {
       console.error(error);
       toast.show('An error ocurred', ToastVariant.ERROR);
       return;
-    }*/
+    }
   };
 
   return (
