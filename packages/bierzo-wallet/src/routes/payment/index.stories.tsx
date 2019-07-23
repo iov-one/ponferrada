633ebd55814e01d1683776f98445779cf94de641
Albert Andrejev <albert.andrejev@gmail.com>
2019-07-23 14:11:08 +0300

    Payment route story update

--- packages/bierzo-wallet/src/routes/payment/index.stories.tsx
+++ packages/bierzo-wallet/src/routes/payment/index.stories.tsx
@@ -1,34 +1,40 @@
 import { TokenTicker } from '@iov/bcp';
 import { action } from '@storybook/addon-actions';
 import { storiesOf } from '@storybook/react';
+import { FormValues } from 'medulas-react-components/lib/components/forms/Form';
 import React from 'react';
 import { DeepPartial } from 'redux';
 
 import { BalanceState } from '../../store/balances';
 import { RootState } from '../../store/reducers';
+import { padAmount, stringToAmount } from '../../utils/balances';
 import DecoratedStorybook, { WALLET_ROOT } from '../../utils/storybook';
+import { CURRENCY_FIELD, QUANTITY_FIELD } from './components/CurrencyToSend';
 import Layout from './components/index';
+
+const BALANCES: BalanceState = {
+  BASH: {
+    quantity: '82500',
+    fractionalDigits: 4,
+    tokenTicker: 'BASH' as TokenTicker,
+  },
+  CASH: {
+    quantity: '1226775',
+    fractionalDigits: 5,
+    tokenTicker: 'CASH' as TokenTicker,
+  },
+};
 
 const fullStore = (): DeepPartial<RootState> => {
   return {
-    balances: {
-      BASH: {
-        quantity: '82500',
-        fractionalDigits: 4,
-        tokenTicker: 'BASH' as TokenTicker,
-      },
-      CASH: {
-        quantity: '1226775',
-        fractionalDigits: 5,
-        tokenTicker: 'CASH' as TokenTicker,
-      },
-    },
+    balances: BALANCES,
   };
 };
 
 async function onSubmit(values: object): Promise<void> {
-  console.log(values);
-  action(JSON.stringify(values, undefined, 2));
+  const formValues = values as FormValues;
+  console.log('onSubmit');
+  console.log(formValues);
   return;
 }
 
