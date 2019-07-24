55874ffe1e0d564d8a3a9d7f10beb7dae59a3185
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-07-24 15:43:35 +0200

    Add NoRows component

--- packages/bierzo-wallet/src/routes/transactions/components/index.tsx
+++ packages/bierzo-wallet/src/routes/transactions/components/index.tsx
@@ -2,6 +2,7 @@ import Hairline from 'medulas-react-components/lib/components/Hairline';
 import * as React from 'react';
 
 import DownloadCSV, { DownloadCSVProps } from './DownloadCSV';
+import NoTransactions from './NoTransactions';
 import TxTable from './TxTable';
 import { TxTableProps } from './TxTable/rowTxBuilder';
 
@@ -16,21 +17,29 @@ const Layout = ({
   onSort,
   orderBy,
   order,
-}: Props): JSX.Element => (
-  <React.Fragment>
-    <Hairline />
-    <DownloadCSV onDownloadCSV={onDownloadCSV} />
-    <Hairline />
-    <TxTable
-      txs={txs}
-      onChangeRows={onChangeRows}
-      onPrevPage={onPrevPage}
-      onNextPage={onNextPage}
-      onSort={onSort}
-      orderBy={orderBy}
-      order={order}
-    />
-  </React.Fragment>
-);
+}: Props): JSX.Element => {
+  const hasRows = txs && txs.length > 0;
+
+  return (
+    <React.Fragment>
+      <Hairline />
+      <DownloadCSV onDownloadCSV={onDownloadCSV} />
+      <Hairline />
+      {hasRows ? (
+        <TxTable
+          txs={txs}
+          onChangeRows={onChangeRows}
+          onPrevPage={onPrevPage}
+          onNextPage={onNextPage}
+          onSort={onSort}
+          orderBy={orderBy}
+          order={order}
+        />
+      ) : (
+        <NoTransactions />
+      )}
+    </React.Fragment>
+  );
+};
 
 export default Layout;
--- packages/bierzo-wallet/src/routes/transactions/index.stories.tsx
+++ packages/bierzo-wallet/src/routes/transactions/index.stories.tsx
@@ -1,15 +1,176 @@
+import { TokenTicker } from '@iov/bcp';
 import { storiesOf } from '@storybook/react';
 import React from 'react';
+import { ReadonlyDate } from 'readonly-date';
+import { DeepPartial } from 'redux';
 
+import { ProcessedTx } from '../../store/notifications';
+import { RootState } from '../../store/reducers';
+import { stringToAmount } from '../../utils/balances';
 import DecoratedStorybook, { WALLET_ROOT } from '../../utils/storybook';
 import Payment from './index';
 
-storiesOf(WALLET_ROOT, module)
+const txs: ReadonlyArray<ProcessedTx> = [
+  {
+    received: true,
+    signer: 'george*iov',
+    recipient: 'me',
+    amount: stringToAmount('10.5', 'LSK' as TokenTicker),
+    time: new ReadonlyDate('2018-11-13T05:35:03.763Z'),
+    success: true,
+    id: 'tx1',
+    memo: 'Sample note',
+  },
+  {
+    received: false,
+    signer: 'me',
+    recipient: 'alex*iov',
+    amount: stringToAmount('25.5', 'IOV' as TokenTicker),
+    time: new ReadonlyDate('2018-10-05T16:12:00.763Z'),
+    success: true,
+    id: 'tx2',
+  },
+  {
+    received: false,
+    signer: 'me',
+    recipient: 'alex*iov',
+    amount: stringToAmount('100.5', 'IOV' as TokenTicker),
+    time: new ReadonlyDate('2018-12-24T10:51:33.763Z'),
+    success: false,
+    id: 'tx3',
+    memo: 'Another note',
+  },
+  {
+    received: true,
+    signer: 'Lx9oa7re0894eopiahsdpf98as7y908',
+    recipient: 'me',
+    amount: stringToAmount('10.5', 'LSK' as TokenTicker),
+    time: new ReadonlyDate('2018-11-13T05:35:03.763Z'),
+    success: true,
+    id: 'tx4',
+    memo: 'And again note',
+  },
+  {
+    received: false,
+    signer: 'me',
+    recipient: 'alex*iov',
+    amount: stringToAmount('25.5', 'IOV' as TokenTicker),
+    time: new ReadonlyDate('2018-10-05T16:12:00.763Z'),
+    success: true,
+    id: 'tx5',
+  },
+  {
+    received: false,
+    signer: 'me',
+    recipient: 'alex*iov',
+    amount: stringToAmount('100.5', 'IOV' as TokenTicker),
+    time: new ReadonlyDate('2018-12-24T10:51:33.763Z'),
+    success: false,
+    id: 'tx6',
+  },
+  {
+    received: true,
+    signer: 'george*iov',
+    recipient: 'me',
+    amount: stringToAmount('10.5', 'LSK' as TokenTicker),
+    time: new ReadonlyDate('2018-11-13T05:35:03.763Z'),
+    success: true,
+    id: 'tx7',
+  },
+  {
+    received: false,
+    signer: 'me',
+    recipient: 'alex*iov',
+    amount: stringToAmount('25.5', 'IOV' as TokenTicker),
+    time: new ReadonlyDate('2018-10-05T16:12:00.763Z'),
+    success: true,
+    id: 'tx8',
+  },
+  {
+    received: false,
+    signer: 'me',
+    recipient: 'alex*iov',
+    amount: stringToAmount('100.5', 'IOV' as TokenTicker),
+    time: new ReadonlyDate('2018-12-24T10:51:33.763Z'),
+    success: false,
+    id: 'tx9',
+  },
+  {
+    received: true,
+    signer: 'george*iov',
+    recipient: 'me',
+    amount: stringToAmount('10.5', 'LSK' as TokenTicker),
+    time: new ReadonlyDate('2018-11-13T05:35:03.763Z'),
+    success: true,
+    id: 'tx10',
+  },
+  {
+    received: false,
+    signer: 'me',
+    recipient: 'alex*iov',
+    amount: stringToAmount('25.5', 'IOV' as TokenTicker),
+    time: new ReadonlyDate('2018-10-05T16:12:00.763Z'),
+    success: true,
+    id: 'tx11',
+  },
+  {
+    received: false,
+    signer: 'me',
+    recipient: 'alex*iov',
+    amount: stringToAmount('100.5', 'IOV' as TokenTicker),
+    time: new ReadonlyDate('2018-12-24T10:51:33.763Z'),
+    success: false,
+    id: 'tx12',
+  },
+  {
+    received: true,
+    signer: 'george*iov',
+    recipient: 'me',
+    amount: stringToAmount('10.5', 'LSK' as TokenTicker),
+    time: new ReadonlyDate('2018-11-13T05:35:03.763Z'),
+    success: true,
+    id: 'tx13',
+  },
+  {
+    received: false,
+    signer: 'Lxasdoiu9847ioasdpfuy098q23rui',
+    recipient: 'alex*iov',
+    amount: stringToAmount('25.5', 'IOV' as TokenTicker),
+    time: new ReadonlyDate('2018-10-05T16:12:00.763Z'),
+    success: true,
+    id: 'tx14',
+  },
+  {
+    received: false,
+    signer: 'me',
+    recipient: 'alex*iov',
+    amount: stringToAmount('100.5', 'IOV' as TokenTicker),
+    time: new ReadonlyDate('2018-12-24T10:51:33.763Z'),
+    success: false,
+    id: 'tx15',
+  },
+];
+
+const txStore: DeepPartial<RootState> = {
+  notifications: {
+    transactions: txs,
+  },
+};
+
+storiesOf(`${WALLET_ROOT}/Transactions page`, module)
   .addParameters({ viewport: { defaultViewport: 'responsive' } })
   .add(
-    'Transactions page',
+    'Without transactions',
     (): JSX.Element => (
       <DecoratedStorybook>
+        <Payment />
+      </DecoratedStorybook>
+    ),
+  )
+  .add(
+    'With transactions',
+    (): JSX.Element => (
+      <DecoratedStorybook storeProps={txStore}>
         <Payment />
       </DecoratedStorybook>
     ),
