65be0d8766933c6a5c1e85a81419daad305815f8
Albert Andrejev <albert.andrejev@gmail.com>
2019-07-24 13:48:29 +0300

    Add E2E tests to Payment route

---
+++ packages/bierzo-wallet/src/routes/payment/index.e2e.spec.ts
@@ -0,0 +1,47 @@
+import express, { Request, Response } from 'express';
+import { Server } from 'http';
+import { Browser, Page } from 'puppeteer';
+
+import { closeBrowser, createExtensionPage, createPage, launchBrowser } from '../../utils/test/e2e';
+import { withChainsDescribe } from '../../utils/test/testExecutor';
+import { travelToPaymentE2E } from './test/travelToPayment';
+
+withChainsDescribe('E2E > Payment route', (): void => {
+  let browser: Browser;
+  let page: Page;
+  let extensionPage: Page;
+  let server: Server;
+
+  beforeAll(() => {
+    const app = express();
+
+    app.use(express.static(require('path').join(__dirname, '/../../../build')));
+
+    app.get('/*', function(req: Request, res: Response) {
+      res.sendFile(require('path').join(__dirname, 'build', 'index.html'));
+    });
+
+    server = app.listen(9000);
+  });
+
+  beforeEach(async (): Promise<void> => {
+    browser = await launchBrowser();
+    page = await createPage(browser);
+    extensionPage = await createExtensionPage(browser);
+    await travelToPaymentE2E(browser, page, extensionPage);
+  }, 45000);
+
+  afterEach(
+    async (): Promise<void> => {
+      await closeBrowser(browser);
+    },
+  );
+
+  afterAll(() => {
+    server.close();
+  });
+
+  it('dump test to check tests suite', async (): Promise<void> => {
+    expect(true).toBeTruthy();
+  }, 45000);
+});
