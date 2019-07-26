eed0b74420cdcce3cdeb31da87fbbc5a711153a9
Albert Andrejev <albert.andrejev@gmail.com>
2019-07-27 00:33:04 +0300

    Add config loader to sil-voting-app

---
+++ packages/sil-voting-app/src/config/index.ts
@@ -0,0 +1,27 @@
+import { singleton } from 'medulas-react-components/lib/utils/singleton';
+
+import developmentConfig from './development.json';
+import productionConfig from './production.json';
+import stagingConfig from './staging.json';
+
+export interface Config {
+  readonly extensionId: string;
+}
+
+const configuration = (): Config => {
+  if (process.env.REACT_APP_CONFIG === 'development') {
+    return developmentConfig;
+  }
+
+  if (process.env.REACT_APP_CONFIG === 'staging') {
+    return stagingConfig;
+  }
+
+  if (process.env.REACT_APP_CONFIG === 'production') {
+    return productionConfig;
+  }
+
+  throw new Error('Unexpected REACT_APP_CONFIG variable for obtaining configuration');
+};
+
+export const getConfig = singleton<typeof configuration>(configuration);
