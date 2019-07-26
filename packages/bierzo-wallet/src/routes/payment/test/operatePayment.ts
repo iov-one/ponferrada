70abcd8b909de848ee70be9ef81e9670b4c414a1
Albert Andrejev <albert.andrejev@gmail.com>
2019-07-26 12:12:36 +0300

    Add E2E fill payment form helper

--- packages/bierzo-wallet/src/routes/payment/test/operatePayment.ts
+++ packages/bierzo-wallet/src/routes/payment/test/operatePayment.ts
@@ -1,11 +1,22 @@
+import { Page } from 'puppeteer';
 import TestUtils from 'react-dom/test-utils';
 
-export const getCancelButton = (paymentDom: React.Component): Element => {
+import { QUANTITY_FIELD } from '../components/CurrencyToSend';
+import { ADDRESS_FIELD } from '../components/ReceiverAddress';
+
+export function getCancelButton(paymentDom: React.Component): Element {
   const buttons = TestUtils.scryRenderedDOMComponentsWithTag(paymentDom, 'button');
   return buttons[2];
-};
+}
 
-export const getSelectedCurrency = (inputs: Element[]): string => {
+export function getSelectedCurrency(inputs: Element[]): string {
   const input = inputs[1] as HTMLInputElement;
   return input.value;
-};
+}
+
+export async function fillPaymentForm(page: Page): Promise<void> {
+  await page.bringToFront();
+  await page.type(`input[name="${QUANTITY_FIELD}"]`, '1');
+  await page.type(`input[name="${ADDRESS_FIELD}`, 'tiov1q5lyl7asgr2dcweqrhlfyexqpkgcuzrm4e0cku');
+  await page.click('button[type=submit]');
+}
