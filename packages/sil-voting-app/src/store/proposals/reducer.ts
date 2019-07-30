2de7a217a4cd72a4fa92001f17c5b29055918af8
abefernan <abel.fernandez.nandin@gmail.com>
2019-07-30 23:29:25 +0200

    Corrects some types in proposal reducer

--- packages/sil-voting-app/src/store/proposals/reducer.ts
+++ packages/sil-voting-app/src/store/proposals/reducer.ts
@@ -1,3 +1,4 @@
+import { ChainId } from '@iov/bcp';
 import { Action } from 'redux';
 import { ActionType } from 'typesafe-actions';
 
@@ -5,22 +6,19 @@ import { ProposalProps } from '../../routes/dashboard/components/Proposal';
 import * as actions from './actions';
 
 export interface SilProposal {
-  // Use ChainId type when governance API is integrated
-  readonly chainId: string;
+  readonly chainId: ChainId;
   readonly proposal: ProposalProps;
 }
 
+export type ProposalsState = SilProposal[];
+const initState: ProposalsState = [];
+
 export interface AddProposalsActionType extends Action {
   type: '@@proposals/ADD';
-  payload: { [key: string]: SilProposal };
+  payload: ProposalsState;
 }
 
 export type ProposalsActions = ActionType<typeof actions>;
-
-export interface ProposalsState {
-  [key: string]: SilProposal;
-}
-const initState: ProposalsState = {};
 
 export function proposalsReducer(
   state: ProposalsState = initState,
@@ -28,7 +26,7 @@ export function proposalsReducer(
 ): ProposalsState {
   switch (action.type) {
     case '@@proposals/ADD':
-      return { ...state, ...action.payload };
+      return [...state, ...action.payload];
     default:
       return state;
   }
