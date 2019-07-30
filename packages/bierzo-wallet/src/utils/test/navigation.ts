3e11a519de031fa4e4e3ca3c5062815ee7e8ba3e
Simon Warta <simon@warta.it>
2019-07-30 08:35:24 +0200

    Allow async callbacks in whenTrue

--- packages/bierzo-wallet/src/utils/test/navigation.ts
+++ packages/bierzo-wallet/src/utils/test/navigation.ts
@@ -9,16 +9,16 @@ const retryInterval = 400;
  * @param timeout timeout in milliseconds. Defaults to 10 seconds
  * @returns a promise that resolves when the callback returned true and is rejected when timeout is reached.
  */
-export function whenTrue(callback: () => boolean, timeout: number = 10000): Promise<void> {
+export function whenTrue(callback: () => boolean | Promise<boolean>, timeout: number = 10000): Promise<void> {
   return new Promise((resolve, reject): void => {
     const startTime = Date.now();
-    const interval = setInterval((): void => {
+    const interval = setInterval(async () => {
       const runtime = Date.now() - startTime;
       if (runtime >= timeout) {
         clearInterval(interval);
         reject(`Timeout reached after ${runtime} ms`);
       } else {
-        if (callback()) {
+        if (await callback()) {
           clearInterval(interval);
           resolve();
         }
