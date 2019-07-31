e5d7d0c65e2268107ccacaae1d420ccaf13a2686
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-07-31 09:16:43 +0200

    Add BwTransactionFactory

---
+++ packages/bierzo-wallet/src/logic/transactions/types/BwTransactionFactory.ts
@@ -0,0 +1,22 @@
+import { isSendTransaction } from '@iov/bcp';
+
+import { ParsedTx } from '../BwTransaction';
+import { BwSendTransaction } from './BwSendTransaction';
+
+export class BwTransactionFactory {
+  public static getReactComponent(tx: ParsedTx<any>): JSX.Element {
+    if (isSendTransaction({ kind: tx.kind })) {
+      return new BwSendTransaction().graphicalRepresentation(tx);
+    }
+
+    throw new Error('Not supporting generic components yet');
+  }
+
+  public static getCsvRepresentation(tx: ParsedTx<any>): string {
+    if (isSendTransaction({ kind: tx.kind })) {
+      return new BwSendTransaction().csvRepresentation(tx);
+    }
+
+    throw new Error('Not supporting generic components yet');
+  }
+}
