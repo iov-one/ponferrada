f729be532fa433e053122e23137c22f6c599163f
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-08-01 12:19:43 +0200

    Remove not necessary pending transaction components in Header

--- packages/bierzo-wallet/src/components/Header/index.tsx
+++ packages/bierzo-wallet/src/components/Header/index.tsx
@@ -4,12 +4,10 @@ import Img from 'medulas-react-components/lib/components/Image';
 import * as React from 'react';
 import { useSelector } from 'react-redux';
 
-import { getPendingTransactions } from '../../store/notifications/selectors';
 import logoBlack from './assets/logoBlack.svg';
 import BellMenu from './components/BellMenu';
 import HiMenu from './components/HiMenu';
 import LinksMenu from './components/LinksMenu';
-import TransactionsMenu from './components/TransactionsMenu';
 import { confirmedTxSelector, lastTxSelector } from './selector';
 
 const useStyles = makeStyles({
@@ -28,7 +26,6 @@ interface Props {
 
 const Header = ({ path }: Props): JSX.Element => {
   const classes = useStyles();
-  const pendingTxs = useSelector(getPendingTransactions);
   const txs = useSelector(confirmedTxSelector);
   const lastTx = useSelector(lastTxSelector);
 
@@ -38,7 +35,6 @@ const Header = ({ path }: Props): JSX.Element => {
       <Block flexGrow={1} />
       <LinksMenu path={path} />
       <Block flexGrow={4} />
-      <TransactionsMenu items={pendingTxs} />
       <BellMenu items={txs} lastTx={lastTx} />
       <HiMenu />
     </Block>
