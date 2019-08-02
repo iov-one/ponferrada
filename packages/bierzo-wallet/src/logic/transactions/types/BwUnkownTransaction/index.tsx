2b2e816643bdb34be8432112c8574b98d9e93c14
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-08-02 10:05:59 +0200

    Get rid of received prop

--- packages/bierzo-wallet/src/logic/transactions/types/BwUnkownTransaction/index.tsx
+++ packages/bierzo-wallet/src/logic/transactions/types/BwUnkownTransaction/index.tsx
@@ -7,27 +7,21 @@ import UnkownTransactionRow from './ui/UnknownTxRow';
 
 export interface BwUnknownProps extends ParsedTx {
   readonly id: string;
-  readonly received: boolean;
 }
 
 export class BwUnkownParser extends BwParser<BwUnknownProps> {
   public async parse(
     conn: BlockchainConnection,
     trans: ConfirmedTransaction<SendTransaction>,
-    currentUserAddress: Address,
+    _currentAddress: Address,
   ): Promise<BwUnknownProps> {
-    const payload = trans.transaction;
-
     const header = await conn.getBlockHeader(trans.height);
     const time = header.time;
-
-    const received = payload.recipient === currentUserAddress;
 
     return {
       kind: 'unknown',
       time,
       id: trans.transactionId,
-      received,
     };
   }
 
