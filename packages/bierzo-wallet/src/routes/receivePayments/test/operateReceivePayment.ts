24dadbd0d4490b739ec723618e95b89c5750d37f
Albert Andrejev <albert.andrejev@gmail.com>
2019-08-04 17:55:01 +0300

    Add E2E tests helpers

--- packages/bierzo-wallet/src/routes/receivePayments/test/operateReceivePayment.ts
+++ packages/bierzo-wallet/src/routes/receivePayments/test/operateReceivePayment.ts
@@ -1,10 +1,29 @@
 import { Page } from 'puppeteer';
 
-export async function getAddressRow(page: Page, dataIndex: number): Promise<string> {
-  const element = await page.$(`li:nth-of-type(${dataIndex}) p`);
-  if (element === null) {
-    throw new Error(`LI element with index: ${dataIndex} not found.`);
+export async function getAddressRow(page: Page, dataIndex: number): Promise<ReadonlyArray<string>> {
+  const chainNameEl = await page.$(`tbody tr:nth-of-type(${dataIndex}) td:nth-of-type(1)`);
+  if (chainNameEl === null) {
+    throw new Error(`TD element containig chain name with row index: ${dataIndex} not found.`);
   }
 
-  return await (await element.getProperty('textContent')).jsonValue();
+  const addressEl = await page.$(`tbody tr:nth-of-type(${dataIndex}) td:nth-of-type(2)`);
+  if (addressEl === null) {
+    throw new Error(`TD element containig address with row index: ${dataIndex} not found.`);
+  }
+
+  const chainName = await (await chainNameEl.getProperty('textContent')).jsonValue();
+  const address = await (await addressEl.getProperty('textContent')).jsonValue();
+
+  return [chainName, address];
+}
+
+export async function copyAddress(page: Page, dataIndex: number): Promise<string> {
+  const addressEl = await page.$(`tbody tr:nth-of-type(${dataIndex}) td:nth-of-type(2)`);
+  if (addressEl === null) {
+    throw new Error(`TD element containig address with row index: ${dataIndex} not found.`);
+  }
+
+  await page.click(`tbody tr:nth-of-type(${dataIndex}) td:nth-of-type(3)`);
+
+  return await (await addressEl.getProperty('textContent')).jsonValue();
 }
