9327a758620defb09d3da387e7f748e55d7647e0
Albert Andrejev <albert.andrejev@gmail.com>
2019-08-04 17:55:16 +0300

    Implement E2E tests

--- packages/bierzo-wallet/src/routes/receivePayments/index.e2e.spec.ts
+++ packages/bierzo-wallet/src/routes/receivePayments/index.e2e.spec.ts
@@ -1,12 +1,20 @@
+import clipboardy from 'clipboardy';
 import express, { Request, Response } from 'express';
 import { Server } from 'http';
 import { Browser, Page } from 'puppeteer';
 
-import { createExtensionPage, createPage, launchBrowser } from '../../utils/test/e2e';
+import {
+  closeBrowser,
+  closeToast,
+  createExtensionPage,
+  createPage,
+  getToastMessage,
+  launchBrowser,
+} from '../../utils/test/e2e';
 import { withChainsDescribe } from '../../utils/test/testExecutor';
 import { waitForAllBalances } from '../balance/test/operateBalances';
 import { travelToBalanceE2E } from '../balance/test/travelToBalance';
-import { getAddressRow } from './test/operateReceivePayment';
+import { copyAddress, getAddressRow } from './test/operateReceivePayment';
 import { travelToReceivePaymentE2E } from './test/travelToReceivePayment';
 
 withChainsDescribe('E2E > Receive Payment route', () => {
@@ -34,7 +42,7 @@ withChainsDescribe('E2E > Receive Payment route', () => {
   }, 60000);
 
   afterEach(async () => {
-    //await closeBrowser(browser);
+    await closeBrowser(browser);
   });
 
   afterAll(() => {
@@ -46,8 +54,30 @@ withChainsDescribe('E2E > Receive Payment route', () => {
     await waitForAllBalances(page);
 
     await travelToReceivePaymentE2E(page);
-    const rowText = await getAddressRow(page, 1);
-    console.log(rowText);
-    await page.bringToFront();
+    let [chainName, address] = await getAddressRow(page, 1);
+    expect(chainName).toBe('Ganache');
+    expect(address.length).toBe(42);
+
+    [chainName, address] = await getAddressRow(page, 4);
+    expect(chainName).toBe('IOV Devnet');
+    expect(address.length).toBe(43);
+
+    [chainName, address] = await getAddressRow(page, 5);
+    expect(chainName).toBe('Lisk Devnet');
+    expect(address.length).toBe(20);
+  }, 35000);
+
+  it('should copy address to clipboard and show toast with message', async () => {
+    await travelToBalanceE2E(browser, page, extensionPage);
+    await waitForAllBalances(page);
+
+    await travelToReceivePaymentE2E(page);
+    let address = await copyAddress(page, 1);
+
+    expect(clipboardy.readSync()).toBe(address);
+
+    const toastMessage = await getToastMessage(page);
+    expect(toastMessage).toBe('Address has been copied to clipboard.');
+    await closeToast(page);
   }, 35000);
 });
