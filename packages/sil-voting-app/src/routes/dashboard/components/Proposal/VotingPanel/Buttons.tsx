a592533c129521fa3d0a1f8e034815b2f94e2034
abefernan <abel.fernandez.nandin@gmail.com>
2019-08-05 15:35:19 +0200

    Adds VotingPanel components

---
+++ packages/sil-voting-app/src/routes/dashboard/components/Proposal/VotingPanel/Buttons.tsx
@@ -0,0 +1,36 @@
+import { VoteOption } from '@iov/bns';
+import Button from '@material-ui/core/Button';
+import Block from 'medulas-react-components/lib/components/Block';
+import React from 'react';
+
+interface Props {
+  readonly vote: VoteOption | undefined;
+}
+
+const Buttons = ({ vote }: Props): JSX.Element => {
+  const yesButton = vote === VoteOption.Yes ? 'contained' : 'outlined';
+  const noButton = vote === VoteOption.No ? 'contained' : 'outlined';
+  const abstainButton = vote === VoteOption.Abstain ? 'contained' : 'outlined';
+
+  return (
+    <Block margin={2}>
+      <Block marginTop={0.5} marginBottom={0.5}>
+        <Button fullWidth variant={yesButton}>
+          Yes
+        </Button>
+      </Block>
+      <Block marginTop={0.5} marginBottom={0.5}>
+        <Button fullWidth variant={noButton}>
+          No
+        </Button>
+      </Block>
+      <Block marginTop={0.5} marginBottom={0.5}>
+        <Button fullWidth variant={abstainButton}>
+          Abstain
+        </Button>
+      </Block>
+    </Block>
+  );
+};
+
+export default Buttons;
