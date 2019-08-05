22e6e638baa7d0acfd00c9b54a8ecf3dd54547a3
Simon Warta <simon@warta.it>
2019-08-05 14:01:09 +0200

    Make snapshot generation deterministic

--- packages/valdueza-storybook/.storybook/config.js
+++ packages/valdueza-storybook/.storybook/config.js
@@ -1,4 +1,6 @@
-import { addParameters, configure } from '@storybook/react';
+import { StylesProvider } from '@material-ui/styles';
+import { addDecorator, addParameters, configure } from '@storybook/react';
+import React from 'react';
 //https://github.com/storybooks/storybook/tree/next/addons/storyshots/storyshots-core#configure-jest-to-work-with-webpacks-requirecontext ("Macro" subsection)
 import requireContext from 'require-context.macro';
 
@@ -17,6 +19,18 @@ addParameters({
 });
 
 const req = requireContext('../../../packages', true, /\.stories\.((js|ts)x?)$/);
+
+// This optional decorator removes the material UI style IDs for deterministic snapshots. See:
+// https://github.com/mui-org/material-ui/issues/9492
+// https://github.com/abrcdf1023/egroup-material/commit/a876a4b411fda490a53ee20179acc72fe6514aaa
+addDecorator(story => {
+  const generateClassName = (rule, styleSheet) => `${styleSheet.options.classNamePrefix}-${rule.key}`;
+  return (
+    <StylesProvider generateClassName={generateClassName}>
+      {story()}
+    </StylesProvider>
+  );
+});
 
 function loadStories() {
   req.keys().forEach((filename) => req(filename));
