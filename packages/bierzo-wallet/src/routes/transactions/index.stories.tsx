c1bf25ac9e855639b1280ff2b7fc475513811955
Simon Warta <simon@warta.it>
2019-08-05 12:03:24 +0200

    Add type and fix txs in stories file

--- packages/bierzo-wallet/src/routes/transactions/index.stories.tsx
+++ packages/bierzo-wallet/src/routes/transactions/index.stories.tsx
@@ -1,9 +1,10 @@
-import { Address, Algorithm, ChainId, Identity, PubkeyBytes, TokenTicker } from '@iov/bcp';
-import { Encoding } from '@iov/encoding';
+import { Address, TokenTicker, TransactionId } from '@iov/bcp';
 import { storiesOf } from '@storybook/react';
 import React from 'react';
 import { ReadonlyDate } from 'readonly-date';
 
+import { BwUnknownProps } from '../../logic/transactions/types/BwUnkownTransaction';
+import { ProcessedSendTransaction } from '../../store/notifications';
 import { RootState } from '../../store/reducers';
 import { stringToAmount } from '../../utils/balances';
 import DecoratedStorybook, { WALLET_ROOT } from '../../utils/storybook';
@@ -12,187 +13,188 @@ import Payment from './index';
 export const TRANSACTIONS_STORY_PATH = `${WALLET_ROOT}/Transactions`;
 export const TRANSACTIONS_STORY_SHOW_PATH = 'With transactions';
 
-const defaultCreator: Identity = {
-  chainId: 'registry-chain' as ChainId,
-  pubkey: {
-    algo: Algorithm.Ed25519,
-    // Random 32 bytes pubkey. Derived IOV address:
-    // tiov1dcg3fat5zrvw00xezzjk3jgedm7pg70y222af3 / 6e1114f57410d8e7bcd910a568c9196efc1479e4
-    data: Encoding.fromHex('7196c465e4c95b3dce425784f51936b95da6bc58b3212648cdca64ee7198df47') as PubkeyBytes,
-  },
-};
-
-const txs: ReadonlyArray<any> = [
+const txs: ReadonlyArray<ProcessedSendTransaction | BwUnknownProps> = [
   {
-    kind: 'bns/register_username',
-    creator: defaultCreator,
-    username: 'alice*iov',
-    targets: [
-      {
-        chainId: 'chain1' as ChainId,
-        address: '367X' as Address,
-      },
-      {
-        chainId: 'chain3' as ChainId,
-        address: '0xddffeeffddaa44' as Address,
-      },
-      {
-        chainId: 'chain2' as ChainId,
-        address: '0x00aabbddccffee' as Address,
-      },
-    ],
+    id: 'DA9A61A3CA28C772E468D772D642978180332780ADB6410909E51487C0F61050' as TransactionId,
     time: new ReadonlyDate('2019-12-24T10:51:33.763Z'),
+    original: {
+      kind: 'bns/register_username',
+    },
   },
   {
-    kind: 'bcp/send',
     received: true,
-    sender: 'george*iov',
-    recipient: 'me',
-    amount: stringToAmount('10.5', 'LSK' as TokenTicker),
     time: new ReadonlyDate('2019-12-24T05:35:03.763Z'),
-    id: 'tx1',
-    memo: 'Sample note',
+    id: 'tx1' as TransactionId,
+    original: {
+      kind: 'bcp/send',
+      sender: 'george*iov' as Address,
+      recipient: 'me' as Address,
+      amount: stringToAmount('10.5', 'LSK' as TokenTicker),
+      memo: 'Sample note',
+    },
   },
   {
-    kind: 'bns/register_username',
-    creator: defaultCreator,
-    username: 'bov*iov',
-    targets: [
-      {
-        chainId: 'chain1' as ChainId,
-        address: '367X' as Address,
-      },
-    ],
     time: new ReadonlyDate('2019-12-24T03:35:03.763Z'),
-    id: '0xaaffBdjuhyu8898scchjsg',
+    id: '0xaaffBdjuhyu8898scchjsg' as TransactionId,
+    original: {
+      kind: 'bns/register_username',
+    },
   },
   {
-    kind: 'bcp/send',
     received: false,
-    sender: 'me',
-    recipient: 'alex*iov',
-    amount: stringToAmount('25.5', 'IOV' as TokenTicker),
     time: new ReadonlyDate('2018-10-05T16:12:00.763Z'),
-    id: 'tx2',
+    id: 'tx2' as TransactionId,
+    original: {
+      kind: 'bcp/send',
+      sender: 'me' as Address,
+      recipient: 'alex*iov' as Address,
+      amount: stringToAmount('25.5', 'IOV' as TokenTicker),
+    },
   },
   {
-    kind: 'bcp/send',
     received: false,
-    sender: 'me',
-    recipient: 'alex*iov',
-    amount: stringToAmount('100.5', 'IOV' as TokenTicker),
     time: new ReadonlyDate('2018-12-24T10:51:33.763Z'),
-    id: 'tx3',
-    memo: 'Another note',
+    id: 'tx3' as TransactionId,
+    original: {
+      kind: 'bcp/send',
+      sender: 'me' as Address,
+      recipient: 'alex*iov' as Address,
+      amount: stringToAmount('100.5', 'IOV' as TokenTicker),
+      memo: 'Another note',
+    },
   },
   {
-    kind: 'bcp/send',
     received: true,
-    sender: 'Lx9oa7re0894eopiahsdpf98as7y908',
-    recipient: 'me',
-    amount: stringToAmount('10.5', 'LSK' as TokenTicker),
     time: new ReadonlyDate('2018-11-13T05:35:03.763Z'),
-    id: 'tx4',
-    memo: 'And again note',
+    id: 'tx4' as TransactionId,
+    original: {
+      kind: 'bcp/send',
+      sender: 'Lx9oa7re0894eopiahsdpf98as7y908' as Address,
+      recipient: 'me' as Address,
+      amount: stringToAmount('10.5', 'LSK' as TokenTicker),
+      memo: 'And again note',
+    },
   },
   {
-    kind: 'bcp/send',
     received: false,
-    sender: 'me',
-    recipient: 'alex*iov',
-    amount: stringToAmount('25.5', 'IOV' as TokenTicker),
     time: new ReadonlyDate('2018-10-05T16:12:00.763Z'),
-    id: 'tx5',
+    id: 'tx5' as TransactionId,
+    original: {
+      kind: 'bcp/send',
+      sender: 'me' as Address,
+      recipient: 'alex*iov' as Address,
+      amount: stringToAmount('25.5', 'IOV' as TokenTicker),
+    },
   },
   {
-    kind: 'bcp/send',
     received: false,
-    sender: 'me',
-    recipient: 'alex*iov',
-    amount: stringToAmount('100.5', 'IOV' as TokenTicker),
     time: new ReadonlyDate('2018-12-24T10:51:33.763Z'),
-    id: 'tx6',
+    id: 'tx6' as TransactionId,
+    original: {
+      kind: 'bcp/send',
+      sender: 'me' as Address,
+      recipient: 'alex*iov' as Address,
+      amount: stringToAmount('100.5', 'IOV' as TokenTicker),
+    },
   },
   {
-    kind: 'bcp/send',
     received: true,
-    sender: 'george*iov',
-    recipient: 'me',
-    amount: stringToAmount('10.5', 'LSK' as TokenTicker),
     time: new ReadonlyDate('2018-11-13T05:35:03.763Z'),
-    id: 'tx7',
+    id: 'tx7' as TransactionId,
+    original: {
+      kind: 'bcp/send',
+      sender: 'george*iov' as Address,
+      recipient: 'me' as Address,
+      amount: stringToAmount('10.5', 'LSK' as TokenTicker),
+    },
   },
   {
-    kind: 'bcp/send',
     received: false,
-    sender: 'me',
-    recipient: 'alex*iov',
-    amount: stringToAmount('25.5', 'IOV' as TokenTicker),
     time: new ReadonlyDate('2018-10-05T16:12:00.763Z'),
-    id: 'tx8',
+    id: 'tx8' as TransactionId,
+    original: {
+      kind: 'bcp/send',
+      sender: 'me' as Address,
+      recipient: 'alex*iov' as Address,
+      amount: stringToAmount('25.5', 'IOV' as TokenTicker),
+    },
   },
   {
-    kind: 'bcp/send',
     received: false,
-    sender: 'me',
-    recipient: 'alex*iov',
-    amount: stringToAmount('100.5', 'IOV' as TokenTicker),
     time: new ReadonlyDate('2018-12-24T10:51:33.763Z'),
-    id: 'tx9',
+    id: 'tx9' as TransactionId,
+    original: {
+      kind: 'bcp/send',
+      sender: 'me' as Address,
+      recipient: 'alex*iov' as Address,
+      amount: stringToAmount('100.5', 'IOV' as TokenTicker),
+    },
   },
   {
-    kind: 'bcp/send',
     received: true,
-    sender: 'george*iov',
-    recipient: 'me',
-    amount: stringToAmount('10.5', 'LSK' as TokenTicker),
     time: new ReadonlyDate('2018-11-13T05:35:03.763Z'),
-    id: 'tx10',
+    id: 'tx10' as TransactionId,
+    original: {
+      kind: 'bcp/send',
+      sender: 'george*iov' as Address,
+      recipient: 'me' as Address,
+      amount: stringToAmount('10.5', 'LSK' as TokenTicker),
+    },
   },
   {
-    kind: 'bcp/send',
     received: false,
-    sender: 'me',
-    recipient: 'alex*iov',
-    amount: stringToAmount('25.5', 'IOV' as TokenTicker),
     time: new ReadonlyDate('2018-10-05T16:12:00.763Z'),
-    id: 'tx11',
+    id: 'tx11' as TransactionId,
+    original: {
+      kind: 'bcp/send',
+      sender: 'me' as Address,
+      recipient: 'alex*iov' as Address,
+      amount: stringToAmount('25.5', 'IOV' as TokenTicker),
+    },
   },
   {
-    kind: 'bcp/send',
     received: false,
-    sender: 'me',
-    recipient: 'alex*iov',
-    amount: stringToAmount('100.5', 'IOV' as TokenTicker),
     time: new ReadonlyDate('2018-12-24T10:51:33.763Z'),
-    id: 'tx12',
+    id: 'tx12' as TransactionId,
+    original: {
+      kind: 'bcp/send',
+      sender: 'me' as Address,
+      recipient: 'alex*iov' as Address,
+      amount: stringToAmount('100.5', 'IOV' as TokenTicker),
+    },
   },
   {
-    kind: 'bcp/send',
     received: true,
-    sender: 'george*iov',
-    recipient: 'me',
-    amount: stringToAmount('10.5', 'LSK' as TokenTicker),
     time: new ReadonlyDate('2018-11-13T05:35:03.763Z'),
-    id: 'tx13',
+    id: 'tx13' as TransactionId,
+    original: {
+      kind: 'bcp/send',
+      sender: 'george*iov' as Address,
+      recipient: 'me' as Address,
+      amount: stringToAmount('10.5', 'LSK' as TokenTicker),
+    },
   },
   {
-    kind: 'bcp/send',
     received: false,
-    sender: 'Lxasdoiu9847ioasdpfuy098q23rui',
-    recipient: 'alex*iov',
-    amount: stringToAmount('25.5', 'IOV' as TokenTicker),
     time: new ReadonlyDate('2018-10-05T16:12:00.763Z'),
-    id: 'tx14',
+    id: 'tx14' as TransactionId,
+    original: {
+      kind: 'bcp/send',
+      sender: 'Lxasdoiu9847ioasdpfuy098q23rui' as Address,
+      recipient: 'alex*iov' as Address,
+      amount: stringToAmount('25.5', 'IOV' as TokenTicker),
+    },
   },
   {
-    kind: 'bcp/send',
     received: false,
-    sender: 'me',
-    recipient: 'alex*iov',
-    amount: stringToAmount('100.5', 'IOV' as TokenTicker),
     time: new ReadonlyDate('2018-12-24T10:51:33.763Z'),
-    id: 'tx15',
+    id: 'tx15' as TransactionId,
+    original: {
+      kind: 'bcp/send',
+      sender: 'me' as Address,
+      recipient: 'alex*iov' as Address,
+      amount: stringToAmount('100.5', 'IOV' as TokenTicker),
+    },
   },
 ];
 
