9b50bd441ae1398326369b0175ede79a635467cf
abefernan <abel.fernandez.nandin@gmail.com>
2019-08-05 15:22:29 +0200

    Mends some dummy data generation

--- packages/sil-voting-app/src/store/proposals/actions.ts
+++ packages/sil-voting-app/src/store/proposals/actions.ts
@@ -1,65 +1,43 @@
-import { Proposal, ProposalStatus } from '@iov/bns';
+import { Proposal, ProposalStatus, VoteOption } from '@iov/bns';
 
-import { getDummyElectionRules, getDummyElectorates, getDummyProposals, getDummyVote } from './dummyData';
+import { getDummyElectionRules, getDummyProposals, getDummyVote } from './dummyData';
 import { AddProposalsActionType, ProposalsState } from './reducer';
 
 export async function getProposals(): Promise<ProposalsState> {
-  const electorates = getDummyElectorates();
   const electionRules = getDummyElectionRules();
   const proposals = getDummyProposals();
 
-  const getNumElectorates = (proposal: Proposal): number => {
-    const electorate = electorates.find(electorate => electorate.id === proposal.electorate.id);
-
-    let numElectorates = 0;
-    if (electorate) {
-      numElectorates = Object.keys(electorate.electors).length;
-    }
-
-    return numElectorates;
-  };
-
   const getQuorum = (proposal: Proposal): number => {
-    const numElectorates = getNumElectorates(proposal);
+    const maxVotes = proposal.state.totalElectorateWeight;
     const electionRule = electionRules.find(electionRule => electionRule.id === proposal.electionRule.id);
 
     let quorum = 0;
     if (electionRule && electionRule.quorum) {
-      quorum = (numElectorates * electionRule.quorum.numerator) / electionRule.quorum.denominator;
+      quorum = Math.ceil((maxVotes * electionRule.quorum.numerator) / electionRule.quorum.denominator);
     }
 
     return quorum;
   };
 
   const getThreshold = (proposal: Proposal): number => {
-    const numElectorates = getNumElectorates(proposal);
+    const totalVotes = proposal.state.totalYes + proposal.state.totalNo + proposal.state.totalAbstain;
     const electionRule = electionRules.find(electionRule => electionRule.id === proposal.electionRule.id);
 
     let threshold = 0;
     if (electionRule) {
-      threshold = (numElectorates * electionRule.threshold.numerator) / electionRule.threshold.denominator;
+      threshold = Math.ceil(
+        (totalVotes * electionRule.threshold.numerator) / electionRule.threshold.denominator,
+      );
     }
 
     return threshold;
   };
 
-  const getVote = (proposal: Proposal): 'Invalid' | 'Yes' | 'No' | 'Abstain' => getDummyVote(proposal);
-
-  const getStatus = (proposal: Proposal): 'Active' | 'Submitted' | 'Ended' => {
-    if (proposal.status === ProposalStatus.Submitted) {
-      if (getVote(proposal) === 'Invalid') {
-        return 'Active';
-      }
-
-      return 'Submitted';
-    }
-
-    return 'Ended';
-  };
+  const getVote = (proposal: Proposal): VoteOption | undefined => getDummyVote(proposal);
 
   const proposalsState = proposals.map(proposal => {
     return {
-      id: proposal.id.toString(),
+      id: proposal.id,
       title: proposal.title,
       author: proposal.author,
       description: proposal.description,
@@ -67,13 +45,16 @@ export async function getProposals(): Promise<ProposalsState> {
       expiryDate: new Date(proposal.votingEndTime * 1000),
       quorum: getQuorum(proposal),
       threshold: getThreshold(proposal),
-      result: {
+      tally: {
         yes: proposal.state.totalYes,
         no: proposal.state.totalNo,
         abstain: proposal.state.totalAbstain,
+        totalVotes: proposal.state.totalYes + proposal.state.totalNo + proposal.state.totalAbstain,
+        maxVotes: proposal.state.totalElectorateWeight,
       },
+      result: proposal.result,
       vote: getVote(proposal),
-      status: getStatus(proposal),
+      hasEnded: !(proposal.status === ProposalStatus.Submitted),
     };
   });
 
