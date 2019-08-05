9516be6354801dfd5c5cfe04b0a60b37c528947d
abefernan <abel.fernandez.nandin@gmail.com>
2019-08-05 15:35:32 +0200

    Adapts DOM testing to new Proposal types

--- packages/sil-voting-app/src/routes/dashboard/index.dom.spec.ts
+++ packages/sil-voting-app/src/routes/dashboard/index.dom.spec.ts
@@ -1,12 +1,13 @@
 import { Store } from 'redux';
 
 import { aNewStore } from '../../store';
+import { getProposals as getStoreProposals } from '../../store/proposals/actions';
 import { RootState } from '../../store/reducers';
 import { click } from '../../utils/test/dom';
 import {
-  checkActiveProposal,
-  checkEndedProposal,
-  checkVoteButtons,
+  checkActiveVotingPanel,
+  checkEndedVotingPanel,
+  checkProposal,
   getAsideFilter,
   getAsideFilterOptions,
   getHeader,
@@ -16,50 +17,6 @@ import {
   getTitle,
 } from './test/operateDashboard';
 import { travelToDashboard } from './test/travelToDashboard';
-
-const proposals = [
-  {
-    id: 'qwereqwer',
-    title: 'title1',
-    author: 'author1',
-    description: 'short description',
-    creationDate: new Date('December 10, 1995 01:00:00'),
-    expiryDate: new Date('December 10, 2020 04:00:00'),
-    quorum: 10,
-    threshold: 6,
-    result: { yes: 5, no: 2, abstain: 1 },
-    vote: 'Invalid',
-    status: 'Active',
-  },
-  {
-    id: 'asdfasdf',
-    title: 'title2',
-    author: 'author2',
-    description:
-      'Really Long Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut euismod dolor mauris, a ultricies augue pulvinar eget. Nullam sed aliquam massa, in commodo sapien. Ut elementum urna sed nisl aliquet, eu feugiat magna dignissim. Sed nisi ipsum, egestas lacinia velit at, convallis malesuada elit. Sed placerat malesuada ligula, sed lobortis mauris aliquet eu. Nullam dignissim dui ut tempor imperdiet. Nulla vitae placerat enim.Maecenas volutpat lorem et egestas blandit. Etiam vitae justo eros. Etiam imperdiet ligula eros, a sollicitudin nisi vulputate et. Duis mattis congue sagittis. Donec ornare eros ut turpis sollicitudin, sed porta lectus molestie. Donec in orci dignissim, vestibulum lorem at, porta mi. Donec maximus neque lorem, ut mattis leo dapibus ac. Duis eget ex dolor. Phasellus viverra, nisi id mollis luctus, augue ante efficitur odio, eu sodales nisl lorem vel sapien. Curabitur hendrerit felis enim, ultrices lobortis orci pretium sed. Fusce at massa eu nulla interdum placerat sed nec odio. Donec faucibus orci sit amet arcu varius, id pharetra eros auctor. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec mi dui, ornare non tristique et, pellentesque et leo. Quisque varius eu arcu non congue. Sed pellentesque ligula a elit aliquam hendrerit.Pellentesque metus libero, tincidunt vitae vehicula nec, luctus ac diam. Praesent vel blandit metus. Etiam dignissim ex tellus, pharetra bibendum erat elementum tempor. Proin augue erat, facilisis eget vehicula tempor, feugiat a diam. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Cras at tellus ut erat tincidunt tempor. Quisque faucibus urna ac feugiat finibus. Etiam tristique neque venenatis luctus volutpat.',
-    creationDate: new Date('April 3, 2017 08:00:00'),
-    expiryDate: new Date('May 5, 2023 03:00:00'),
-    quorum: 20,
-    threshold: 14,
-    result: { yes: 4, no: 10, abstain: 3 },
-    vote: 'Yes',
-    status: 'Submitted',
-  },
-
-  {
-    id: 'zxcvzxcv',
-    title: 'title3',
-    author: 'author3',
-    description: 'short description again',
-    creationDate: new Date('January 3, 2010 04:00:00'),
-    expiryDate: new Date('June 5, 2017 03:00:00'),
-    quorum: 30,
-    threshold: 20,
-    result: { yes: 15, no: 4, abstain: 6 },
-    vote: 'Abstain',
-    status: 'Ended',
-  },
-];
 
 const shortenedDescription =
   'Really Long Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut euismod dolor mauris, a ultricies augue pulvinar eget. Nullam sed aliquam massa, in commodo ... ';
@@ -77,7 +34,7 @@ describe('DOM > Feature > Dashboard', () => {
         connected: true,
         installed: true,
       },
-      proposals,
+      proposals: await getStoreProposals(),
     });
 
     dashboardDom = await travelToDashboard(store);
@@ -111,28 +68,31 @@ describe('DOM > Feature > Dashboard', () => {
     expect(proposals.length).toBe(3);
   }, 60000);
 
-  it('has a non-ended proposal with correct fields', async () => {
-    const activeProposal = (await getProposals(dashboardDom))[0];
-    checkActiveProposal(activeProposal);
-    checkVoteButtons(activeProposal);
+  it('has a proposal with correct fields', async () => {
+    const proposal = (await getProposals(dashboardDom))[0];
+    checkProposal(proposal);
   }, 60000);
 
-  it('has an ended proposal with correct fields', async () => {
+  it('has an active proposal with correct voting panel', async () => {
+    const activeProposal = (await getProposals(dashboardDom))[0];
+    checkActiveVotingPanel(activeProposal);
+  }, 60000);
+
+  it('has an ended proposal with correct voting panel', async () => {
     const endedProposal = (await getProposals(dashboardDom))[2];
-    checkEndedProposal(endedProposal);
-    checkVoteButtons(endedProposal);
+    checkEndedVotingPanel(endedProposal);
   }, 60000);
 
   it('has a Read more / Read less toggle when description too long', async () => {
     let longProposal = (await getProposals(dashboardDom))[1].children[0];
-    let readToggle = longProposal.children[2].children[1];
+    let readToggle = longProposal.children[2].children[0].children[1];
 
     click(readToggle);
-    let description = longProposal.children[2].children[0].textContent;
+    let description = longProposal.children[2].children[0].children[0].textContent;
     expect(description).toBe(longDescription);
 
     click(readToggle);
-    description = longProposal.children[2].children[0].textContent;
+    description = longProposal.children[2].children[0].children[0].textContent;
     expect(description).toBe(shortenedDescription);
   }, 60000);
 });
--- packages/sil-voting-app/src/routes/dashboard/test/operateDashboard.ts
+++ packages/sil-voting-app/src/routes/dashboard/test/operateDashboard.ts
@@ -38,45 +38,25 @@ export const getProposals = async (dashboardDom: React.Component): Promise<Eleme
   return Array.from(proposalList.children).filter(isNotHairline);
 };
 
-export const checkVoteButtons = (proposal: Element): void => {
-  const votePanel = proposal.children[1];
-
-  const yourVote = votePanel.children[0].textContent;
-  expect(yourVote).toBe('Your Vote:');
-
-  const yesButton = votePanel.children[1].textContent;
-  expect(yesButton).toBe('Yes');
-
-  const noButton = votePanel.children[2].textContent;
-  expect(noButton).toBe('No');
-
-  const abstainButton = votePanel.children[3].textContent;
-  expect(abstainButton).toBe('Abstain');
-};
-
-export const checkActiveProposal = (proposal: Element): void => {
+export const checkProposal = (proposal: Element): void => {
   const proposalPanel = proposal.children[0];
 
-  const title = proposalPanel.children[0].children[0].textContent;
-  expect(title).toBe('title1');
+  const title = proposalPanel.children[0].textContent;
+  expect(title).toBe('Proposal 1');
 
-  const status = proposalPanel.children[0].children[1].textContent;
-  expect(status).toBe('Active');
-
-  const author = proposalPanel.children[1].children[0].textContent;
-  expect(author).toBe('Author: author1');
-
-  const id = proposalPanel.children[1].children[1].textContent;
-  expect(id).toBe('Proposal ID: qwereqwer');
-
-  const creationDate = proposalPanel.children[1].children[2].textContent;
-  expect(creationDate).toBe('Created on 12/10/1995, 1:00:00 AM');
+  const idAndAuthor = proposalPanel.children[1].textContent;
+  expect(idAndAuthor).toBe('ID:1Author:Author 1');
 
   const description = proposalPanel.children[2].textContent;
   expect(description).toBe('short description');
 
-  const expiryDate = proposalPanel.children[3].children[0].textContent;
+  const dates = proposalPanel.children[3].children[0];
+
+  const expiryDate = dates.children[0].textContent;
   expect(expiryDate).toBe('Expires on 12/10/2020, 4:00:00 AM');
+
+  const creationDate = dates.children[1].textContent;
+  expect(creationDate).toBe('Created on 12/10/1995, 1:00:00 AM');
 
   const deleteIcon = proposalPanel.children[3].children[1].children[0].children[0];
   expect(deleteIcon.getAttribute('alt')).toBe('Delete Icon');
@@ -85,42 +65,51 @@ export const checkActiveProposal = (proposal: Element): void => {
   expect(deleteLabel).toBe('Delete');
 
   const resultsBar = proposalPanel.children[4].textContent;
-  expect(resultsBar).toBe('This poll results will be available until 12/10/2020, 4:00:00 AM');
+  expect(resultsBar).toBe('0% Yes');
 };
 
-export const checkEndedProposal = (proposal: Element): void => {
-  const proposalPanel = proposal.children[0];
+export const checkActiveVotingPanel = (proposal: Element): void => {
+  const votePanel = proposal.children[1];
 
-  const title = proposalPanel.children[0].children[0].textContent;
-  expect(title).toBe('title3');
+  const yourVote = votePanel.children[0].textContent;
+  expect(yourVote).toBe('Your vote: ');
 
-  const status = proposalPanel.children[0].children[1].textContent;
-  expect(status).toBe('Ended');
+  const buttons = votePanel.children[1];
 
-  const author = proposalPanel.children[1].children[0].textContent;
-  expect(author).toBe('Author: author3');
+  const yesButton = buttons.children[0].textContent;
+  expect(yesButton).toBe('Yes');
 
-  const id = proposalPanel.children[1].children[1].textContent;
-  expect(id).toBe('Proposal ID: zxcvzxcv');
+  const noButton = buttons.children[1].textContent;
+  expect(noButton).toBe('No');
 
-  const creationDate = proposalPanel.children[1].children[2].textContent;
-  expect(creationDate).toBe('Created on 1/3/2010, 4:00:00 AM');
+  const abstainButton = buttons.children[2].textContent;
+  expect(abstainButton).toBe('Abstain');
 
-  const description = proposalPanel.children[2].textContent;
-  expect(description).toBe('short description again');
+  const info = votePanel.children[2];
 
-  const expiryDate = proposalPanel.children[3].children[0].textContent;
-  expect(expiryDate).toBe('Expired on 6/5/2017, 3:00:00 AM');
+  const votesNeeded = info.children[0].textContent;
+  expect(votesNeeded && votesNeeded.substring(0, 13)).toBe('Votes needed:');
+  const yesNeeded = info.children[1].textContent;
+  expect(yesNeeded && yesNeeded.substring(0, 13)).toBe('"Yes" needed:');
+  const participation = info.children[2].textContent;
+  expect(participation && participation.substring(0, 14)).toBe('Participation:');
+};
 
-  const quorum = proposalPanel.children[3].children[1].textContent;
-  expect(quorum).toBe('Quorum: 30');
+export const checkEndedVotingPanel = (proposal: Element): void => {
+  const votePanel = proposal.children[1];
 
-  const totalVotes = proposalPanel.children[3].children[2].textContent;
-  expect(totalVotes).toBe('Total votes: 25');
+  const proposalResult = votePanel.children[0].textContent;
+  expect(proposalResult && proposalResult.substring(0, 9)).toBe('PROPOSAL ');
 
-  const result = proposalPanel.children[3].children[3].textContent;
-  expect(result).toBe('Result: Yes');
+  const yourVote = votePanel.children[1].textContent;
+  expect(yourVote && yourVote.substring(0, 10)).toBe('Your vote:');
 
-  const resultsBar = proposalPanel.children[4].textContent;
-  expect(resultsBar).toBe('60% Yes');
+  const info = votePanel.children[2];
+
+  const votesNeeded = info.children[0].textContent;
+  expect(votesNeeded && votesNeeded.substring(0, 13)).toBe('Votes needed:');
+  const yesNeeded = info.children[1].textContent;
+  expect(yesNeeded && yesNeeded.substring(0, 13)).toBe('"Yes" needed:');
+  const participation = info.children[2].textContent;
+  expect(participation && participation.substring(0, 14)).toBe('Participation:');
 };
