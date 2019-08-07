af3b8e6e652cf804583952b398384f73907b198d
Albert Andrejev <albert.andrejev@gmail.com>
2019-08-07 12:53:10 +0300

    Refactor receivePayment component

--- packages/bierzo-wallet/src/routes/receivePayments/index.tsx
+++ packages/bierzo-wallet/src/routes/receivePayments/index.tsx
@@ -1,53 +1,18 @@
-import { Identity } from "@iov/bcp";
-import { TransactionEncoder } from "@iov/encoding";
 import React from "react";
-import * as ReactRedux from "react-redux";
 
 import { history } from "..";
 import PageMenu from "../../components/PageMenu";
-import { getChainName } from "../../config";
-import { getCodecForChainId } from "../../logic/codec";
-import { RootState } from "../../store/reducers";
 import { BALANCE_ROUTE } from "../paths";
-import Layout, { ChainAddress } from "./components";
+import Layout from "./components";
 
 function onReturnToBalance(): void {
   history.push(BALANCE_ROUTE);
 }
 
 const ReceivePayment = (): JSX.Element => {
-  const pubKeys = ReactRedux.useSelector((state: RootState) => state.extension.keys);
-
-  const [chainAddresses, setChainAddresses] = React.useState<readonly ChainAddress[]>([]);
-
-  React.useEffect(() => {
-    async function processAddresses(pubKeys: { [chain: string]: string }): Promise<void> {
-      const identities = Object.values(pubKeys).map(
-        (serializedIdentity): Identity => {
-          return TransactionEncoder.fromJson(JSON.parse(serializedIdentity));
-        },
-      );
-
-      const addresses: ChainAddress[] = [];
-      for (const identity of identities) {
-        addresses.push({
-          chainId: identity.chainId,
-          chainName: await getChainName(identity.chainId),
-          address: (await getCodecForChainId(identity.chainId)).identityToAddress(identity),
-        });
-      }
-      addresses.sort((a: ChainAddress, b: ChainAddress) =>
-        a.chainName.localeCompare(b.chainName, undefined, { sensitivity: "base" }),
-      );
-
-      setChainAddresses(addresses);
-    }
-    processAddresses(pubKeys);
-  }, [pubKeys]);
-
   return (
     <PageMenu>
-      <Layout chainAddresses={chainAddresses} onReturnToBalance={onReturnToBalance} />
+      <Layout onReturnToBalance={onReturnToBalance} />
     </PageMenu>
   );
 };
