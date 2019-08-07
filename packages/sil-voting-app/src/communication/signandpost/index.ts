9d2743134c633ebb577fa8d0713bf10aeeeeace0
willclarktech <willclarktech@users.noreply.github.com>
2019-08-07 17:26:55 +0200

    Add signAndPost communication functions

---
+++ packages/sil-voting-app/src/communication/signandpost/index.ts
@@ -0,0 +1,54 @@
+/* global chrome */
+import { TransactionId, WithCreator } from "@iov/bcp";
+import { BnsConnection, BnsTx } from "@iov/bns";
+import { TransactionEncoder } from "@iov/encoding";
+import { isJsonRpcErrorResponse, JsonRpcRequest, makeJsonRpcId, parseJsonRpcResponse2 } from "@iov/jsonrpc";
+
+import { getConfig } from "../../config";
+
+async function generateSignAndPostRequest(
+  connection: BnsConnection,
+  tx: BnsTx & WithCreator,
+): Promise<JsonRpcRequest> {
+  const txWithFee = connection.withDefaultFee(tx);
+  return {
+    jsonrpc: "2.0",
+    id: makeJsonRpcId(),
+    method: "signAndPost",
+    params: {
+      reason: TransactionEncoder.toJson("I would like you to sign this request"),
+      transaction: TransactionEncoder.toJson(txWithFee),
+    },
+  };
+}
+
+export async function sendSignAndPostRequest(
+  connection: BnsConnection,
+  tx: BnsTx & WithCreator,
+): Promise<TransactionId | null> {
+  const request = await generateSignAndPostRequest(connection, tx);
+  const config = await getConfig();
+
+  return new Promise((resolve, reject) => {
+    chrome.runtime.sendMessage(config.extensionId, request, response => {
+      try {
+        const parsedResponse = parseJsonRpcResponse2(response);
+        if (isJsonRpcErrorResponse(parsedResponse)) {
+          reject(parsedResponse.error.message);
+          return;
+        }
+
+        const parsedResult = TransactionEncoder.fromJson(parsedResponse.result);
+        if (typeof parsedResult === "string") {
+          resolve(parsedResult as TransactionId);
+        } else if (parsedResult === null) {
+          resolve(null);
+        } else {
+          reject("Got unexpected type of result");
+        }
+      } catch (error) {
+        reject(error);
+      }
+    });
+  });
+}
