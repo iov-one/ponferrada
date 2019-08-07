279778865712703a4504ab4ba67e2fac9866a485
abefernan <abel.fernandez.nandin@gmail.com>
2019-08-07 23:14:04 +0200

    Implements voting form

--- packages/sil-voting-app/src/routes/dashboard/components/Proposal/VotingPanel/Buttons.tsx
+++ packages/sil-voting-app/src/routes/dashboard/components/Proposal/VotingPanel/Buttons.tsx
@@ -1,34 +1,59 @@
 import { VoteOption } from "@iov/bns";
 import Button from "@material-ui/core/Button";
-import { Block } from "medulas-react-components";
-import React from "react";
+import Block from "medulas-react-components/lib/components/Block";
+import Form, { useForm } from "medulas-react-components/lib/components/forms/Form";
+import React, { useState } from "react";
+
+import { sendSignAndPostRequest } from "../../../../../communication/signandpost";
+import { getBnsConnection } from "../../../../../logic/connection";
+import { getExtensionStatus } from "../../../../../store/extension";
 
 interface Props {
+  readonly id: number;
   readonly vote: VoteOption | undefined;
 }
 
-const Buttons = ({ vote }: Props): JSX.Element => {
-  const yesButton = vote === VoteOption.Yes ? "contained" : "outlined";
-  const noButton = vote === VoteOption.No ? "contained" : "outlined";
-  const abstainButton = vote === VoteOption.Abstain ? "contained" : "outlined";
+const Buttons = ({ id, vote }: Props): JSX.Element => {
+  const [currentVote, setCurrentVote] = useState(vote);
+
+  const yesButton = currentVote === VoteOption.Yes ? "contained" : "outlined";
+  const noButton = currentVote === VoteOption.No ? "contained" : "outlined";
+  const abstainButton = currentVote === VoteOption.Abstain ? "contained" : "outlined";
+
+  const voteYes = (): void => setCurrentVote(VoteOption.Yes);
+  const voteNo = (): void => setCurrentVote(VoteOption.No);
+  const voteAbstain = (): void => setCurrentVote(VoteOption.Abstain);
+
+  const submitVote = async (): Promise<void> => {
+    const governor = (await getExtensionStatus()).governor;
+    if (governor && currentVote) {
+      const connection = await getBnsConnection();
+      const voteTx = await governor.buildVoteTx(id, currentVote);
+      await sendSignAndPostRequest(connection, voteTx);
+    }
+  };
+
+  const { handleSubmit, submitting } = useForm({ onSubmit: submitVote });
 
   return (
     <Block margin={2}>
-      <Block marginTop={0.5} marginBottom={0.5}>
-        <Button fullWidth variant={yesButton}>
-          Yes
-        </Button>
-      </Block>
-      <Block marginTop={0.5} marginBottom={0.5}>
-        <Button fullWidth variant={noButton}>
-          No
-        </Button>
-      </Block>
-      <Block marginTop={0.5} marginBottom={0.5}>
-        <Button fullWidth variant={abstainButton}>
-          Abstain
-        </Button>
-      </Block>
+      <Form onSubmit={handleSubmit}>
+        <Block marginTop={0.5} marginBottom={0.5}>
+          <Button fullWidth variant={yesButton} onClick={voteYes} type="submit" disabled={submitting}>
+            Yes
+          </Button>
+        </Block>
+        <Block marginTop={0.5} marginBottom={0.5}>
+          <Button fullWidth variant={noButton} onClick={voteNo} type="submit" disabled={submitting}>
+            No
+          </Button>
+        </Block>
+        <Block marginTop={0.5} marginBottom={0.5}>
+          <Button fullWidth variant={abstainButton} onClick={voteAbstain} type="submit" disabled={submitting}>
+            Abstain
+          </Button>
+        </Block>
+      </Form>
     </Block>
   );
 };
