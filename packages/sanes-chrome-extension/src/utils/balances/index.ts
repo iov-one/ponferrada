b51109adf46a4962fca86b032cbc3ad95266cadc
Simon Warta <simon@warta.it>
2019-08-08 11:29:48 +0200

    Remove unused parseFigures

--- packages/sanes-chrome-extension/src/utils/balances/index.ts
+++ packages/sanes-chrome-extension/src/utils/balances/index.ts
@@ -1,24 +1,4 @@
 import { Amount, TokenTicker } from "@iov/bcp";
-import { Omit } from "react-router";
-
-export type Figures = Omit<Amount, "tokenTicker">;
-
-export function parseFigures(amount: string): Figures {
-  // trim off all leading zeros when parsing
-  const trimmed = amount.replace(/^0+/, "");
-  const matched = trimmed.match(/^([0-9]+)?([.,])?([0-9]+)?$/);
-  if (!matched) {
-    throw new Error(`Not a valid number: ${amount}`);
-  }
-  // elements 1 and 3...
-  const wholeString = matched[1] || "";
-  // get fraction part and remove trailing zeros.
-  const fractionString = (matched[3] || "").replace(/0+$/, "");
-  const quantity = `${wholeString}${fractionString}`;
-  const fractionalDigits = fractionString.length;
-
-  return { quantity, fractionalDigits };
-}
 
 export function amountToNumber(amount: Amount): number {
   const { quantity, fractionalDigits } = amount;
--- packages/sanes-chrome-extension/src/utils/balances/index.unit.spec.ts
+++ packages/sanes-chrome-extension/src/utils/balances/index.unit.spec.ts
@@ -1,6 +1,6 @@
 import { TokenTicker } from "@iov/bcp";
 
-import { amountToGwei, amountToString, makeAmount, parseFigures } from "./index";
+import { amountToGwei, amountToString, makeAmount } from "./index";
 
 describe("amountToString", () => {
   const iov = "IOV" as TokenTicker;
@@ -47,42 +47,5 @@ describe("amountToGwei", () => {
     expect(amountToGwei({ ...meta, quantity: "123000000" })).toEqual("0.12 Gwei");
     expect(amountToGwei({ ...meta, quantity: "1333333333" })).toEqual("1.33 Gwei");
     expect(amountToGwei({ ...meta, quantity: "1999999999" })).toEqual("2.00 Gwei");
-  });
-});
-
-describe("parseFigures", () => {
-  it("should handle whole numbers", () => {
-    expect(parseFigures("1200")).toEqual({ quantity: "1200", fractionalDigits: 0 });
-    expect(parseFigures("765")).toEqual({ quantity: "765", fractionalDigits: 0 });
-  });
-
-  it("should handle fractional numbers with or without leading 0", () => {
-    expect(parseFigures("0.1234")).toEqual({ quantity: "1234", fractionalDigits: 4 });
-    expect(parseFigures(".1234")).toEqual({ quantity: "1234", fractionalDigits: 4 });
-    expect(parseFigures("0.023")).toEqual({ quantity: "023", fractionalDigits: 3 });
-  });
-
-  it("should return number even with trailing dot or comma", () => {
-    expect(parseFigures("1.")).toEqual({ quantity: "1", fractionalDigits: 0 });
-    expect(parseFigures("2,")).toEqual({ quantity: "2", fractionalDigits: 0 });
-  });
-
-  it("should support , as separator", () => {
-    expect(parseFigures("0,1234")).toEqual({ quantity: "1234", fractionalDigits: 4 });
-    expect(parseFigures("13,67")).toEqual({ quantity: "1367", fractionalDigits: 2 });
-    expect(parseFigures(",00420")).toEqual({ quantity: "0042", fractionalDigits: 4 });
-  });
-
-  it("should remove trailing zeros from fraction", () => {
-    expect(parseFigures("0,1230")).toEqual({ quantity: "123", fractionalDigits: 3 });
-    expect(parseFigures("13.670")).toEqual({ quantity: "1367", fractionalDigits: 2 });
-    expect(parseFigures("8,675")).toEqual({ quantity: "8675", fractionalDigits: 3 });
-    expect(parseFigures(".00420")).toEqual({ quantity: "0042", fractionalDigits: 4 });
-  });
-
-  it("should error on invalid strings", () => {
-    expect(() => parseFigures("12a")).toThrow(/Not a valid number/);
-    expect(() => parseFigures("0x1234")).toThrow(/Not a valid number/);
-    expect(() => parseFigures("-15.6")).toThrow(/Not a valid number/);
   });
 });
