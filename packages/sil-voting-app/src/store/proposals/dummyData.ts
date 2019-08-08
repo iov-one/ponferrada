1a3a2e9d1d0f5d13a77278bd06fb18971699111e
abefernan <abel.fernandez.nandin@gmail.com>
2019-08-08 12:06:10 +0200

    Fixes testing by using dummy proposals

--- packages/sil-voting-app/src/store/proposals/dummyData.ts
+++ packages/sil-voting-app/src/store/proposals/dummyData.ts
@@ -10,6 +10,8 @@ import {
   VoteOption,
 } from "@iov/bns";
 
+import { ProposalsState } from ".";
+
 const adminAddress = "Admin 1" as Address;
 
 export const getDummyElectorates = (): Electorate[] => [
@@ -180,4 +182,62 @@ export const getDummyVote = (proposal: Proposal): VoteOption | undefined => {
     default:
       return undefined;
   }
+};
+
+export const getDummyProposalsState = (): ProposalsState => {
+  const electionRules = getDummyElectionRules();
+  const proposals = getDummyProposals();
+
+  const getQuorum = (proposal: Proposal): number => {
+    const electionRule = electionRules.find(electionRule => electionRule.id === proposal.electionRule.id);
+    const maxVotes = proposal.state.totalElectorateWeight;
+
+    let quorum = 0;
+    if (electionRule && electionRule.quorum) {
+      quorum = Math.ceil((maxVotes * electionRule.quorum.numerator) / electionRule.quorum.denominator);
+    }
+
+    return quorum;
+  };
+
+  const getThreshold = (proposal: Proposal): number => {
+    const electionRule = electionRules.find(electionRule => electionRule.id === proposal.electionRule.id);
+    const totalVotes = proposal.state.totalYes + proposal.state.totalNo + proposal.state.totalAbstain;
+
+    let threshold = 0;
+    if (electionRule) {
+      threshold = Math.ceil(
+        (totalVotes * electionRule.threshold.numerator) / electionRule.threshold.denominator,
+      );
+    }
+
+    return threshold;
+  };
+
+  const getVote = (proposal: Proposal): VoteOption | undefined => getDummyVote(proposal);
+
+  const proposalsState = proposals.map(proposal => {
+    return {
+      id: proposal.id,
+      title: proposal.title,
+      author: proposal.author,
+      description: proposal.description,
+      creationDate: new Date(proposal.votingStartTime * 1000),
+      expiryDate: new Date(proposal.votingEndTime * 1000),
+      quorum: getQuorum(proposal),
+      threshold: getThreshold(proposal),
+      tally: {
+        yes: proposal.state.totalYes,
+        no: proposal.state.totalNo,
+        abstain: proposal.state.totalAbstain,
+        totalVotes: proposal.state.totalYes + proposal.state.totalNo + proposal.state.totalAbstain,
+        maxVotes: proposal.state.totalElectorateWeight,
+      },
+      result: proposal.result,
+      vote: getVote(proposal),
+      hasEnded: !(proposal.status === ProposalStatus.Submitted),
+    };
+  });
+
+  return proposalsState;
 };
