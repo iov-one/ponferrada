00076510f90d00d46de5d2a33fce7f0a793c8857
Albert Andrejev <albert.andrejev@gmail.com>
2019-08-10 16:33:22 +0300

    Add route to username registration view

--- packages/bierzo-wallet/src/routes/paths.ts
+++ packages/bierzo-wallet/src/routes/paths.ts
@@ -4,3 +4,4 @@ export const TRANSACTIONS_ROUTE = "/transactions";
 export const BALANCE_ROUTE = "/balance";
 export const CONFIRM_TRANSACTION_ROUTE = "/confirm-transaction";
 export const ADDRESSES_ROUTE = "/addresses";
+export const REGISTER_USERNAME_ROUTE = "/register-username";
