177376bb1bfb71631bcca6a378d28e4212e90d68
Simon Warta <simon@warta.it>
2019-08-14 14:11:57 +0200

    Convert singleton to function

--- packages/ui-logic/types/singleton.d.ts
+++ packages/ui-logic/types/singleton.d.ts
@@ -1,3 +1,4 @@
-export declare const singleton: <T extends (...args: any[]) => any>(
+export declare type SingletonInitializerFunction = (...args: any[]) => any;
+export declare function singleton<T extends SingletonInitializerFunction>(
   fn: (...args: Parameters<T>) => ReturnType<T>,
-) => (...args: Parameters<T>) => ReturnType<T>;
+): (...args: Parameters<T>) => ReturnType<T>;
