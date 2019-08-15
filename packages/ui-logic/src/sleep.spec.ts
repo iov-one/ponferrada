f2f3cf808ae96013522413542866ba7944206454
Simon Warta <simon@warta.it>
2019-08-15 15:31:12 +0200

    Avoid testing too small number in sleep()

--- packages/ui-logic/src/sleep.spec.ts
+++ packages/ui-logic/src/sleep.spec.ts
@@ -2,7 +2,7 @@ import { sleep } from "./sleep";
 
 describe("sleep", () => {
   it("resolves after at least x milliseconds", async () => {
-    for (const x of [1, 10, 30, 120, 280]) {
+    for (const x of [10, 30, 120, 280]) {
       const start = Date.now();
       await sleep(x);
       const sleepingTime = Date.now() - start;
@@ -14,7 +14,7 @@ describe("sleep", () => {
     // Don't be too strict as jest will run many tests at the same time and test systems can be slow sometimes.
     const tolerance = 30; // ms
 
-    for (const x of [1, 10, 30, 120, 280]) {
+    for (const x of [10, 30, 120, 280]) {
       const start = Date.now();
       await sleep(x);
       const sleepingTime = Date.now() - start;
