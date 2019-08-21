23efe5127abcbf1bfc7e0f0aab0edefd541fab47
willclarktech <willclarktech@users.noreply.github.com>
2019-08-21 11:48:15 +0200

    sanes: Add skeleton views for processed governance txs

---
+++ packages/sanes-chrome-extension/src/routes/account/components/Tx/MsgCreateProposalTx.tsx
@@ -0,0 +1,51 @@
+import { Typography } from "medulas-react-components";
+import * as React from "react";
+
+interface MsgCreateProposalTxProps {
+  readonly id: string;
+  readonly blockExplorerUrl: string | null;
+  readonly error?: any;
+  readonly title: string;
+}
+
+const MsgCreateProposalTx = ({ title, error }: MsgCreateProposalTxProps): JSX.Element => {
+  if (error) {
+    return (
+      <React.Fragment>
+        <Typography weight="light" inline>
+          Your attempt to create the proposal{" "}
+        </Typography>
+        <Typography weight="semibold" inline link>
+          {title}
+        </Typography>
+        <Typography weight="light" inline>
+          {" "}
+          was{" "}
+        </Typography>
+        <Typography weight="semibold" inline>
+          unsuccessful
+        </Typography>
+        <Typography weight="light" inline>
+          , please try again later.
+        </Typography>
+      </React.Fragment>
+    );
+  } else {
+    return (
+      <React.Fragment>
+        <Typography weight="light" inline>
+          You have created the proposal{" "}
+        </Typography>
+        <Typography weight="semibold" inline link>
+          {title}
+        </Typography>
+        <Typography weight="light" inline>
+          {" "}
+          .
+        </Typography>
+      </React.Fragment>
+    );
+  }
+};
+
+export default MsgCreateProposalTx;
