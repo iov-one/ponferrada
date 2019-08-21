905b43f5b2cf097c772ef5d20f1c705c615ea66a
Albert Andrejev <albert.andrejev@gmail.com>
2019-08-21 14:14:01 +0300

    Add Terms route tests

---
+++ packages/bierzo-wallet/src/routes/terms/index.e2e.spec.ts
@@ -0,0 +1,44 @@
+import express, { Request, Response } from "express";
+import { Server } from "http";
+import { Browser, Page } from "puppeteer";
+
+import { closeBrowser, createExtensionPage, createPage, launchBrowser } from "../../utils/test/e2e";
+import { withChainsDescribe } from "../../utils/test/testExecutor";
+import { travelToTermsE2E } from "./test/travelToTerms";
+
+withChainsDescribe("E2E > Payment route", () => {
+  let browser: Browser;
+  let page: Page;
+  let extensionPage: Page;
+  let server: Server;
+
+  beforeAll(() => {
+    const app = express();
+
+    app.use(express.static(require("path").join(__dirname, "/../../../build")));
+
+    app.get("/*", function(req: Request, res: Response) {
+      res.sendFile(require("path").join(__dirname, "build", "index.html"));
+    });
+
+    server = app.listen(9000);
+  });
+
+  beforeEach(async () => {
+    browser = await launchBrowser();
+    page = await createPage(browser);
+    extensionPage = await createExtensionPage(browser);
+  }, 60000);
+
+  afterEach(async () => {
+    await closeBrowser(browser);
+  });
+
+  afterAll(() => {
+    server.close();
+  });
+
+  it("should travel to terms page", async () => {
+    await travelToTermsE2E(browser, page, extensionPage);
+  }, 35000);
+});
