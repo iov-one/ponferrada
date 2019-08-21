c5ed73b9fca4acd30e8871400c34ebc05ba40ecc
Albert Andrejev <albert.andrejev@gmail.com>
2019-08-21 16:44:54 +0300

    Remove not used story

--- packages/bierzo-wallet/src/components/PageMenuColumn/index.stories.tsx
+++ packages/bierzo-wallet/src/components/PageMenuColumn/index.stories.tsx
@@ -84,31 +84,10 @@ const fullStore = (): DeepPartial<RootState> => {
   };
 };
 
-storiesOf(`${WALLET_ROOT}/Components/PageMenu`, module)
+storiesOf(`${WALLET_ROOT}/Components`, module)
   .addParameters({ viewport: { defaultViewport: "responsive" } })
   .add(
-    "Without padding",
-    (): JSX.Element => (
-      <DecoratedStorybook storeProps={fullStore()}>
-        <PageMenuColumn>
-          <Typography variant="h5" color="primary">
-            What is Lorem Ipsum?
-          </Typography>
-          <Typography variant="body1">
-            <strong>Lorem Ipsum</strong> is simply dummy text of the printing and typesetting industry. Lorem
-            Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer
-            took a galley of type and scrambled it to make a type specimen book. It has survived not only five
-            centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was
-            popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and
-            more recently with desktop publishing software like Aldus PageMaker including versions of Lorem
-            Ipsum.
-          </Typography>
-        </PageMenuColumn>
-      </DecoratedStorybook>
-    ),
-  )
-  .add(
-    "With padding",
+    "PageMenuColumn",
     (): JSX.Element => (
       <DecoratedStorybook storeProps={fullStore()}>
         <PageMenuColumn>
