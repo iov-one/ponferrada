c0824fd39160f9f687ba353c979d2dcc53ccf7b4
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-08-22 16:17:21 +0200

    Add tests for assuring url is calculated properly

---
+++ packages/sanes-chrome-extension/src/routes/paths.unit.spec.ts
@@ -0,0 +1,35 @@
+import { ACCOUNT_STATUS_ROUTE, initialUrl, LOGIN_ROUTE, REQUEST_ROUTE, WELCOME_ROUTE } from "./paths";
+
+describe("Paths", () => {
+  describe("calculates correctly initial route", () => {
+    it("goes to request view", () => {
+      const personaLoaded = true;
+      const hasRequests = true;
+      expect(initialUrl(personaLoaded, true, hasRequests)).toBe(REQUEST_ROUTE);
+      expect(initialUrl(personaLoaded, false, hasRequests)).toBe(REQUEST_ROUTE);
+    });
+
+    it("goes to account status view", () => {
+      const personaLoaded = true;
+      const hasRequests = false;
+      expect(initialUrl(personaLoaded, true, hasRequests)).toBe(ACCOUNT_STATUS_ROUTE);
+      expect(initialUrl(personaLoaded, false, hasRequests)).toBe(ACCOUNT_STATUS_ROUTE);
+    });
+
+    it("goes to login route", () => {
+      const personaLoaded = false;
+      const hasRequests = false;
+      const hasPersonaStored = true;
+      expect(initialUrl(personaLoaded, hasPersonaStored, hasRequests)).toBe(LOGIN_ROUTE);
+      expect(initialUrl(personaLoaded, hasPersonaStored, hasRequests)).toBe(LOGIN_ROUTE);
+    });
+
+    it("goes to welcome route", () => {
+      const personaLoaded = false;
+      const hasRequests = false;
+      const hasPersonaStored = false;
+      expect(initialUrl(personaLoaded, hasPersonaStored, hasRequests)).toBe(WELCOME_ROUTE);
+      expect(initialUrl(personaLoaded, hasPersonaStored, hasRequests)).toBe(WELCOME_ROUTE);
+    });
+  });
+});
