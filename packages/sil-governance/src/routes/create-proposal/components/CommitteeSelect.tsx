425a5a168c57e0113eacdc01aa29775af3e65663
abefernan <abel.fernandez.nandin@gmail.com>
2019-08-26 21:03:51 +0200

    Improves period display

--- packages/sil-governance/src/routes/create-proposal/components/CommitteeSelect.tsx
+++ packages/sil-governance/src/routes/create-proposal/components/CommitteeSelect.tsx
@@ -7,6 +7,23 @@ import React, { Dispatch, SetStateAction, useState } from "react";
 export const COMMITTEE_FIELD = "Committee";
 const COMMITTEE_FIELD_INITIAL = "Select a committee";
 
+const displayPeriod = (seconds: number): string => {
+  const days = Math.floor(seconds / (3600 * 24));
+  seconds -= days * 3600 * 24;
+  const hours = Math.floor(seconds / 3600);
+  seconds -= hours * 3600;
+  const minutes = Math.floor(seconds / 60);
+  seconds -= minutes * 60;
+
+  let period = "";
+  if (days) period += `${days}d `;
+  if (hours) period += `${hours}h `;
+  if (minutes) period += `${minutes}m `;
+  if (seconds) period += `${seconds}s`;
+
+  return period.trim();
+};
+
 interface Props {
   readonly form: FormApi;
   readonly electionRules: readonly ElectionRule[];
@@ -33,8 +50,7 @@ const CommitteeSelect = ({ form, electionRules, changeElectionRuleId }: Props): 
       ? `${committee.quorum.numerator}/${committee.quorum.denominator}`
       : "none";
     const threshold = `${committee.threshold.numerator}/${committee.threshold.denominator}`;
-    //TODO show bigger and smaller timeframes
-    const period = `${Math.floor(committee.votingPeriod / 3600)}h`;
+    const period = displayPeriod(committee.votingPeriod);
 
     setQuorum(quorum);
     setThreshold(threshold);
