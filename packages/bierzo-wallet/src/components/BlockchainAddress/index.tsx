5fffc05fddebe4476ee762653198c434ad501248
Albert Andrejev <albert.andrejev@gmail.com>
2019-08-28 14:36:19 +0300

    Add BlockchainAddress component

---
+++ packages/bierzo-wallet/src/components/BlockchainAddress/index.tsx
@@ -0,0 +1,40 @@
+import { Address } from "@iov/bcp";
+import { makeStyles } from "@material-ui/core";
+import { Badge, Typography } from "medulas-react-components";
+import React from "react";
+
+const useStyles = makeStyles({
+  sectionName: {
+    overflowWrap: "break-word",
+  },
+});
+
+interface Props {
+  readonly addresses: Address[];
+  readonly address: Address;
+}
+
+const BlockchainAddress = ({ addresses, address }: Props): JSX.Element => {
+  const classes = useStyles();
+
+  const addressNode = (
+    <Typography variant="subtitle2" weight="regular" color="textSecondary" className={classes.sectionName}>
+      {address}
+    </Typography>
+  );
+
+  console.log(addresses);
+  console.log(address);
+
+  if (addresses.indexOf(address) > -1) {
+    return (
+      <Badge variant="text" badgeContent="mine">
+        {addressNode}
+      </Badge>
+    );
+  } else {
+    return addressNode;
+  }
+};
+
+export default BlockchainAddress;
