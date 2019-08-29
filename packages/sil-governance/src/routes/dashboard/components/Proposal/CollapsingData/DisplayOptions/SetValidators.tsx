6091b4902e16a5db6c3f19b52c049f6a67901847
abefernan <abel.fernandez.nandin@gmail.com>
2019-08-29 19:14:47 +0200

    Improve validators iteration and label

--- packages/sil-governance/src/routes/dashboard/components/Proposal/CollapsingData/DisplayOptions/SetValidators.tsx
+++ packages/sil-governance/src/routes/dashboard/components/Proposal/CollapsingData/DisplayOptions/SetValidators.tsx
@@ -7,15 +7,17 @@ interface Props {
 }
 
 const SetValidators = ({ action }: Props): JSX.Element => {
-  let validators = [];
+  const validators = Object.entries(action.validatorUpdates).map(([validator, props]) => {
+    const validatorLabel = `Validator ${validator.slice(8, 11)}...${validator.slice(-3)}:`;
 
-  for (const validator in action.validatorUpdates) {
-    validators.push(
-      <Typography variant="body2">
-        {validator} - Power {action.validatorUpdates[validator].power}
-      </Typography>,
+    return (
+      <Block marginTop={0.5} marginBottom={1}>
+        <Typography variant="body2">
+          {validatorLabel} Power {props.power}
+        </Typography>
+      </Block>
     );
-  }
+  });
 
   return (
     <Block marginTop={2} marginBottom={2}>
