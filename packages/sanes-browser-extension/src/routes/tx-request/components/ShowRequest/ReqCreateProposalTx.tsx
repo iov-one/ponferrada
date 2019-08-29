8a2c71e4dfbf36a3c06f02a065c76a0bc56b7be3
Simon Warta <simon@warta.it>
2019-08-29 17:29:26 +0200

    Show formatted JSON for governance transactions

--- packages/sanes-browser-extension/src/routes/tx-request/components/ShowRequest/ReqCreateProposalTx.tsx
+++ packages/sanes-browser-extension/src/routes/tx-request/components/ShowRequest/ReqCreateProposalTx.tsx
@@ -1,18 +1,32 @@
 import { CreateProposalTx } from "@iov/bns";
-import { Typography } from "medulas-react-components";
+import { makeStyles } from "@material-ui/core";
+import { List, ListItem } from "medulas-react-components";
 import * as React from "react";
 
 export const REQ_CREATE_PROPOSAL = "req-create-proposal-tx";
+
+const useStyles = makeStyles({
+  txContent: {
+    margin: "0",
+    fontSize: "1.2rem",
+    overflowX: "scroll",
+  },
+});
 
 interface Props {
   readonly tx: CreateProposalTx;
 }
 
 const ReqCreateProposalTx = ({ tx }: Props): JSX.Element => {
+  const classes = useStyles();
+
+  const content = JSON.stringify(tx, null, 2);
   return (
-    <React.Fragment>
-      <Typography id={REQ_CREATE_PROPOSAL}>{JSON.stringify(tx)}</Typography>
-    </React.Fragment>
+    <List id={REQ_CREATE_PROPOSAL}>
+      <ListItem>
+        <pre className={classes.txContent}>{content}</pre>
+      </ListItem>
+    </List>
   );
 };
 
--- packages/sanes-browser-extension/src/routes/tx-request/components/ShowRequest/ReqVoteTx.tsx
+++ packages/sanes-browser-extension/src/routes/tx-request/components/ShowRequest/ReqVoteTx.tsx
@@ -1,18 +1,32 @@
 import { VoteTx } from "@iov/bns";
-import { Typography } from "medulas-react-components";
+import { makeStyles } from "@material-ui/core";
+import { List, ListItem } from "medulas-react-components";
 import * as React from "react";
 
 export const REQ_VOTE = "req-vote-tx";
+
+const useStyles = makeStyles({
+  txContent: {
+    margin: "0",
+    fontSize: "1.2rem",
+    overflowX: "auto",
+  },
+});
 
 interface Props {
   readonly tx: VoteTx;
 }
 
 const ReqVoteTx = ({ tx }: Props): JSX.Element => {
+  const classes = useStyles();
+
+  const content = JSON.stringify(tx, null, 2);
   return (
-    <React.Fragment>
-      <Typography id={REQ_VOTE}>{JSON.stringify(tx)}</Typography>
-    </React.Fragment>
+    <List id={REQ_VOTE}>
+      <ListItem>
+        <pre className={classes.txContent}>{content}</pre>
+      </ListItem>
+    </List>
   );
 };
 
