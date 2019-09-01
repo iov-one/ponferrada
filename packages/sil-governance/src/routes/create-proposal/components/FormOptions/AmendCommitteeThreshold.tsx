f72cc18e1bf5d3877eaadee0069165b963d7eadb
abefernan <abel.fernandez.nandin@gmail.com>
2019-09-01 17:20:58 +0200

    Implements AmendCommitteeThreshold FormOptions

--- packages/sil-governance/src/routes/create-proposal/components/FormOptions/AmendCommitteeThreshold.tsx
+++ packages/sil-governance/src/routes/create-proposal/components/FormOptions/AmendCommitteeThreshold.tsx
@@ -1,23 +1,73 @@
 import { FormApi } from "final-form";
-import { Block, TextFieldForm, Typography } from "medulas-react-components";
-import React from "react";
+import {
+  Block,
+  SelectFieldForm,
+  SelectFieldFormItem,
+  TextFieldForm,
+  Typography,
+} from "medulas-react-components";
+import React, { useEffect, useState } from "react";
+import * as ReactRedux from "react-redux";
+
+import { RootState } from "../../../../store/reducers";
+import { getElectionRules } from "../ProposalForm";
+
+export const COMMITTEE_THRESHOLD_FIELD = "Committee Rule to amend";
+const COMMITTEE_THRESHOLD_INITIAL = "Select a rule";
+export const THRESHOLD_FIELD = "Threshold";
+const THRESHOLD_PLACEHOLDER = "2/3";
 
 interface Props {
   readonly form: FormApi;
 }
 
 const AmendCommitteeThreshold = ({ form }: Props): JSX.Element => {
+  const governor = ReactRedux.useSelector((state: RootState) => state.extension.governor);
+  const [ruleItems, setRuleItems] = useState<SelectFieldFormItem[]>([]);
+
+  useEffect(() => {
+    const updateCommitteeItems = async (): Promise<void> => {
+      if (!governor) throw new Error("Governor not set in store. This is a bug.");
+      const electionRules = await getElectionRules(governor);
+      const ruleItems = electionRules.map(rule => {
+        return {
+          name: `${rule.id}: ${rule.title}`,
+        };
+      });
+
+      setRuleItems(ruleItems);
+    };
+
+    updateCommitteeItems();
+  }, [governor]);
+
   return (
-    <Block width="100%" display="flex" alignItems="center" marginTop={2} marginBottom={2}>
-      <Typography>Custom Field for AmendCommitteeThreshold </Typography>
-      <TextFieldForm
-        name="AmendCommitteeThreshold"
-        form={form}
-        placeholder="AmendCommitteeThreshold"
-        fullWidth
-        margin="none"
-      />
-    </Block>
+    <React.Fragment>
+      <Block marginTop={2} display="flex" alignItems="center">
+        <Typography>{COMMITTEE_THRESHOLD_FIELD}</Typography>
+        <Block marginLeft={2}>
+          <SelectFieldForm
+            fieldName={COMMITTEE_THRESHOLD_FIELD}
+            fullWidth
+            form={form}
+            items={ruleItems}
+            initial={COMMITTEE_THRESHOLD_INITIAL}
+          />
+        </Block>
+      </Block>
+
+      <Block marginTop={2} display="flex" alignItems="center">
+        <Typography>{THRESHOLD_FIELD}</Typography>
+        <Block marginLeft={2}>
+          <TextFieldForm
+            name={THRESHOLD_FIELD}
+            form={form}
+            placeholder={THRESHOLD_PLACEHOLDER}
+            margin="none"
+          />
+        </Block>
+      </Block>
+    </React.Fragment>
   );
 };
 
