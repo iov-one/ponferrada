2d406b63a079fe5d564809b98e443a81fab979da
Albert Andrejev <albert.andrejev@gmail.com>
2019-09-03 11:18:56 +0300

    Add CsvRepresentaion class

--- packages/bierzo-wallet/src/logic/csvBuilder/index.ts
+++ packages/bierzo-wallet/src/logic/csvBuilder/index.ts
@@ -1,3 +1,5 @@
+import FileSaver from "file-saver";
+
 export interface CsvRow {
   readonly id: string;
   readonly recepient: string;
@@ -11,3 +13,33 @@ export interface CsvRow {
   readonly time: string;
   readonly note: string;
 }
+
+class CsvRepresentation {
+  private readonly fields: readonly string[];
+  private readonly header: string;
+  private readonly rows: CsvRow[] = [];
+
+  public constructor(header: CsvRow) {
+    this.fields = Object.keys(header);
+    this.header = this.parse(header);
+  }
+
+  public addRow(row: CsvRow): void {
+    this.rows.push(row);
+  }
+
+  public get(): Blob {
+    const csvBody = this.rows.map((row: CsvRow) => this.parse(row));
+
+    return new Blob([`${this.header}\n${csvBody.join("\n")}`], { type: "text/plain;charset=utf-8" });
+  }
+
+  private parse(row: CsvRow): string {
+    const parsedRow: string[] = [];
+    this.fields.forEach(field => parsedRow.push(`"${row[field]}"`));
+
+    return parsedRow.join(";");
+  }
+}
+
+export default CsvRepresentation;
