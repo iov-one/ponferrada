fd73708dbdb4ddeb794054765ead74c13fb32736
Albert Andrejev <albert.andrejev@gmail.com>
2019-09-03 15:15:42 +0300

    Refactor CsvRepresentation class

--- packages/bierzo-wallet/src/logic/csvBuilder/index.ts
+++ packages/bierzo-wallet/src/logic/csvBuilder/index.ts
@@ -10,40 +10,49 @@ export interface CsvRow {
   readonly feeTokenTicker: string;
   readonly time: string;
   readonly note: string;
-  [key: string]: string;
 }
 
 class CsvRepresentation {
-  private readonly _fields: readonly string[];
-  private readonly _header: string;
-  private readonly _rows: CsvRow[] = [];
+  private readonly header: CsvRow;
+  private readonly rows: CsvRow[] = [];
 
   public constructor(header: CsvRow) {
-    this._fields = Object.keys(header);
-    this._header = this.parse(header);
+    this.header = header;
   }
 
   public addRow(row: CsvRow): void {
-    this._rows.push(row);
+    this.rows.push(row);
   }
 
   public get csvRows(): CsvRow[] {
-    return this._rows;
+    return this.rows;
   }
 
-  public get csvHeader(): string {
-    return this._header;
+  public get csvHeader(): CsvRow {
+    return this.header;
   }
 
   public blob(): Blob {
-    const csvBody = this._rows.map((row: CsvRow) => this.parse(row));
+    const csvHeader = CsvRepresentation.parse(this.header);
+    const csvBody = this.rows.map((row: CsvRow) => CsvRepresentation.parse(row));
 
-    return new Blob([`${this._header}\n${csvBody.join("\n")}`], { type: "text/plain;charset=utf-8" });
+    return new Blob([`${csvHeader}\n${csvBody.join("\n")}`], { type: "text/csv;charset=utf-8" });
   }
 
-  private parse(row: CsvRow): string {
-    const parsedRow: string[] = [];
-    this._fields.forEach(field => parsedRow.push(`"${row[field]}"`));
+  private static parse(row: CsvRow): string {
+    const parsedRow: string[] = [
+      row.id,
+      row.recepient,
+      row.sender,
+      row.quantity,
+      row.fractionalDigits,
+      row.tokenTicker,
+      row.feeQuantity,
+      row.feeFractionalDigits,
+      row.feeTokenTicker,
+      row.time,
+      row.note,
+    ].map(content => `"${content}"`);
 
     return parsedRow.join(";");
   }
