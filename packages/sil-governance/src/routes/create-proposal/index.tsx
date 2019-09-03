53fad1aca5236b676aa50f48e6181635dad4a5ed
Simon Warta <simon@warta.it>
2019-09-03 23:53:53 +0200

    Simplify and fix routing
    Before, the URL was not changed when switching between different menu
    items

--- packages/sil-governance/src/routes/create-proposal/index.tsx
+++ packages/sil-governance/src/routes/create-proposal/index.tsx
@@ -3,17 +3,12 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import { history } from "..";
-import AsideFilter, { ElectionFilter } from "../../components/AsideFilter";
+import AsideFilter from "../../components/AsideFilter";
 import ConfirmTransaction from "../../components/ConfirmTransaction";
 import Header from "../../components/Header";
 import { RootState } from "../../store/reducers";
 import { setTransactionsStateAction } from "../../store/transactions";
-import {
-  DASHBOARD_ACTIVE_ROUTE,
-  DASHBOARD_ENDED_ROUTE,
-  DASHBOARD_ROUTE,
-  DASHBOARD_SUBMITTED_ROUTE,
-} from "../paths";
+import { DASHBOARD_ROUTE } from "../paths";
 import ProposalForm from "./components/ProposalForm";
 
 const CreateProposal = (): JSX.Element => {
@@ -25,22 +20,6 @@ const CreateProposal = (): JSX.Element => {
     history.push(DASHBOARD_ROUTE);
   };
 
-  const updateFilter = (filter: ElectionFilter): void => {
-    switch (filter) {
-      case ElectionFilter.Active:
-        history.push(DASHBOARD_ACTIVE_ROUTE);
-        break;
-      case ElectionFilter.Submitted:
-        history.push(DASHBOARD_SUBMITTED_ROUTE);
-        break;
-      case ElectionFilter.Ended:
-        history.push(DASHBOARD_ENDED_ROUTE);
-        break;
-      default:
-        history.push(DASHBOARD_ROUTE);
-    }
-  };
-
   return (
     <Block width="100%" maxWidth="1024px" height="auto" display="flex" flexDirection="column" margin="0 auto">
       <Header />
@@ -49,7 +28,7 @@ const CreateProposal = (): JSX.Element => {
         <ConfirmTransaction transactionId={lastSignAndPostResult} onReturnToDashboard={onReturnToDashboard} />
       ) : (
         <Block minWidth="100%" display="flex">
-          <AsideFilter filter={null} onChangeFilter={updateFilter} />
+          <AsideFilter filter={null} />
           <ProposalForm />
         </Block>
       )}
