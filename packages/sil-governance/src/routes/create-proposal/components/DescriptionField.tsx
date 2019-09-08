960e01f559c3825b03efa6fe6cd27f60f4857b7b
abefernan <abel.fernandez.nandin@gmail.com>
2019-09-09 00:06:47 +0200

    DescriptionField validation

--- packages/sil-governance/src/routes/create-proposal/components/DescriptionField.tsx
+++ packages/sil-governance/src/routes/create-proposal/components/DescriptionField.tsx
@@ -1,21 +1,32 @@
 import { FormApi } from "final-form";
-import { Block, TextFieldForm, Typography } from "medulas-react-components";
-import React from "react";
+import {
+  Block,
+  composeValidators,
+  notLongerThan,
+  required,
+  TextFieldForm,
+  Typography,
+} from "medulas-react-components";
+import React, { useMemo } from "react";
 
 export const DESCRIPTION_FIELD = "Description";
 const DESCRIPTION_PLACEHOLDER = "Enter Description of the proposal";
+const DESCRIPTION_MAX_LENGTH = 1000;
 
 interface Props {
   readonly form: FormApi;
 }
 
 const DescriptionField = ({ form }: Props): JSX.Element => {
+  const validator = useMemo(() => composeValidators(required, notLongerThan(DESCRIPTION_MAX_LENGTH)), []);
+
   return (
     <Block marginTop={2}>
       <Typography>{DESCRIPTION_FIELD}</Typography>
       <TextFieldForm
         name={DESCRIPTION_FIELD}
         form={form}
+        validate={validator}
         placeholder={DESCRIPTION_PLACEHOLDER}
         multiline
         rows="2"
