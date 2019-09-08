35580b02fd29f9438e4745f11db859cdb760f532
abefernan <abel.fernandez.nandin@gmail.com>
2019-09-09 00:06:30 +0200

    TitleField validation

--- packages/sil-governance/src/routes/create-proposal/components/TitleField.tsx
+++ packages/sil-governance/src/routes/create-proposal/components/TitleField.tsx
@@ -1,19 +1,36 @@
 import { FormApi } from "final-form";
-import { Block, TextFieldForm, Typography } from "medulas-react-components";
-import React from "react";
+import {
+  Block,
+  composeValidators,
+  notLongerThan,
+  required,
+  TextFieldForm,
+  Typography,
+} from "medulas-react-components";
+import React, { useMemo } from "react";
 
 export const TITLE_FIELD = "Title";
 const TITLE_PLACEHOLDER = "Enter Title";
+const TITLE_MAX_LENGTH = 30;
 
 interface Props {
   readonly form: FormApi;
 }
 
 const TitleField = ({ form }: Props): JSX.Element => {
+  const validator = useMemo(() => composeValidators(required, notLongerThan(TITLE_MAX_LENGTH)), []);
+
   return (
     <Block>
       <Typography>{TITLE_FIELD}</Typography>
-      <TextFieldForm name={TITLE_FIELD} form={form} placeholder={TITLE_PLACEHOLDER} fullWidth margin="none" />
+      <TextFieldForm
+        name={TITLE_FIELD}
+        form={form}
+        validate={validator}
+        placeholder={TITLE_PLACEHOLDER}
+        fullWidth
+        margin="none"
+      />
     </Block>
   );
 };
