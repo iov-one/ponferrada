5257be0088ccdd7976141b85b1593fa80230e514
abefernan <abel.fernandez.nandin@gmail.com>
2019-09-10 00:22:03 +0200

    Adds message for empty proposal list

--- packages/sil-governance/src/routes/dashboard/components/ProposalsList.tsx
+++ packages/sil-governance/src/routes/dashboard/components/ProposalsList.tsx
@@ -1,4 +1,4 @@
-import { Block } from "medulas-react-components";
+import { Block, Typography } from "medulas-react-components";
 import React from "react";
 import * as ReactRedux from "react-redux";
 
@@ -35,11 +35,19 @@ const ProposalsList = ({ filterType }: Props): JSX.Element => {
   const filter = getFilter(filterType);
   const proposals = ReactRedux.useSelector((state: RootState) => state.proposals).filter(filter);
 
+  const hasProposals = proposals.length > 0;
+
   return (
     <Block id={PROPOSALS_HTML_ID} flexGrow={1}>
-      {proposals.map((proposal, index) => (
-        <RenderedProposal key={proposal.id} proposal={proposal} index={index} />
-      ))}
+      {!hasProposals && (
+        <Block margin={2}>
+          <Typography>No proposals available</Typography>
+        </Block>
+      )}
+      {hasProposals &&
+        proposals.map((proposal, index) => (
+          <RenderedProposal key={proposal.id} proposal={proposal} index={index} />
+        ))}
     </Block>
   );
 };
