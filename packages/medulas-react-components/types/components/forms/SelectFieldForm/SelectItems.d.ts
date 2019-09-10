2ba70b082a2efa547024a70bf3a2d83f91537cab
Albert Andrejev <albert.andrejev@gmail.com>
2019-09-10 14:28:05 +0300

    Update definitions

--- packages/medulas-react-components/types/components/forms/SelectFieldForm/SelectItems.d.ts
+++ packages/medulas-react-components/types/components/forms/SelectFieldForm/SelectItems.d.ts
@@ -1,7 +1,7 @@
 import { Item } from "./index";
 interface ListItemProps {
   readonly action: (value: Item) => () => void;
-  readonly selectedItem: string;
+  readonly selectedItem: string | undefined;
   readonly items: readonly Item[];
 }
 declare const ListItems: ({ action, items, selectedItem }: ListItemProps) => JSX.Element;
