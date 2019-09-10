b80c515b25bf3f4ddda4aca954160e109fc69803
Albert Andrejev <albert.andrejev@gmail.com>
2019-09-10 11:44:28 +0300

    Add delete wallet story

---
+++ packages/sanes-browser-extension/src/routes/delete-wallet/index.stories.tsx
@@ -0,0 +1,14 @@
+import { storiesOf } from "@storybook/react";
+import React from "react";
+
+import { CHROME_EXTENSION_ROOT, SanesStorybook } from "../../utils/storybook";
+import Layout from "./index";
+
+storiesOf(CHROME_EXTENSION_ROOT, module).add(
+  "Delete wallet page",
+  (): JSX.Element => (
+    <SanesStorybook>
+      <Layout />
+    </SanesStorybook>
+  ),
+);
