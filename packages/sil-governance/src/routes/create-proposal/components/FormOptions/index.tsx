82e93a7efa005d6d58c44f37a3bfd4cc174b4037
abefernan <abel.fernandez.nandin@gmail.com>
2019-09-12 12:10:39 +0200

    Adds changeRecipients method to FormOptions and state to ProposalForm

--- packages/sil-governance/src/routes/create-proposal/components/FormOptions/index.tsx
+++ packages/sil-governance/src/routes/create-proposal/components/FormOptions/index.tsx
@@ -1,14 +1,14 @@
 import { ProposalType } from "@iov/bns-governance";
 import { FormApi } from "final-form";
 import { Block, FieldInputValue } from "medulas-react-components";
-import React from "react";
+import React, { Dispatch, SetStateAction } from "react";
 
 import AddCommitteeMember from "./AddCommitteeMember";
 import AddValidator from "./AddValidator";
 import AmendCommitteeQuorum from "./AmendCommitteeQuorum";
 import AmendCommitteeThreshold from "./AmendCommitteeThreshold";
 import AmendProtocol from "./AmendProtocol";
-import DistributeFunds from "./DistributeFunds";
+import DistributeFunds, { Recipient } from "./DistributeFunds";
 import ReleaseGuaranteeFunds from "./ReleaseGuaranteeFunds";
 import RemoveCommitteeMember from "./RemoveCommitteeMember";
 import RemoveValidator from "./RemoveValidator";
@@ -43,12 +43,13 @@ export const isFraction = (value: FieldInputValue): string | undefined => {
 interface Props {
   readonly form: FormApi;
   readonly proposalType: ProposalType;
+  readonly changeRecipients: Dispatch<SetStateAction<Recipient[]>>;
 }
 
-const FormOptions = ({ form, proposalType }: Props): JSX.Element => {
+const FormOptions = ({ form, proposalType, changeRecipients }: Props): JSX.Element => {
   const FormComponent = proposalOptions[proposalType];
 
-  return <FormComponent form={form} />;
+  return <FormComponent form={form} changeRecipients={changeRecipients} />;
 };
 
 export default FormOptions;
