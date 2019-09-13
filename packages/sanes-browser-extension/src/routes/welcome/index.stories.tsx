e9b3332d18f90e809865c48ab959f94bfc383a09
Albert Andrejev <albert.andrejev@gmail.com>
2019-09-13 16:13:09 +0300

    Update welcome story

--- packages/sanes-browser-extension/src/routes/welcome/index.stories.tsx
+++ packages/sanes-browser-extension/src/routes/welcome/index.stories.tsx
@@ -2,14 +2,35 @@ import { storiesOf } from "@storybook/react";
 import { Storybook } from "medulas-react-components";
 import React from "react";
 
+import { PersonaProvider } from "../../context/PersonaProvider";
+import { GetPersonaResponse } from "../../extension/background/model/backgroundscript";
 import { CHROME_EXTENSION_ROOT } from "../../utils/storybook";
 import Layout from "./index";
 
-storiesOf(CHROME_EXTENSION_ROOT, module).add(
-  "Welcome page",
-  (): JSX.Element => (
-    <Storybook>
-      <Layout />
-    </Storybook>
-  ),
-);
+const persona: GetPersonaResponse = {
+  mnemonic: "",
+  accounts: [{ label: "Account 0" }],
+  txs: [],
+};
+
+storiesOf(`${CHROME_EXTENSION_ROOT}/Welcome page`, module)
+  .add(
+    "With persona",
+    (): JSX.Element => (
+      <PersonaProvider persona={persona} hasPersona={true}>
+        <Storybook>
+          <Layout />
+        </Storybook>
+      </PersonaProvider>
+    ),
+  )
+  .add(
+    "Without persona",
+    (): JSX.Element => (
+      <PersonaProvider persona={null} hasPersona={false}>
+        <Storybook>
+          <Layout />
+        </Storybook>
+      </PersonaProvider>
+    ),
+  );
