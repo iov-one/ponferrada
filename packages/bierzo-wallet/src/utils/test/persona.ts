4e93c65955cd7c12f6caba93a305473ff2699e0c
Simon Warta <simon@warta.it>
2019-09-16 17:06:07 +0200

    Stabilize submitExtensionCreateWalletForm

--- packages/bierzo-wallet/src/utils/test/persona.ts
+++ packages/bierzo-wallet/src/utils/test/persona.ts
@@ -4,6 +4,9 @@ import { sleep } from "ui-logic";
 import { createExtensionPage } from "./e2e";
 import { whenOnNavigatedToE2eRoute } from "./navigation";
 
+const CREATE_WALLET_ID_STEP_1 = "create-wallet-step1";
+const CREATE_WALLET_ID_STEP_2 = "create-wallet-step2";
+const CREATE_WALLET_ID_STEP_3 = "create-wallet-step3";
 const PASSWORD_FIELD = "passwordInputField";
 const PASSWORD_CONFIRM_FIELD = "passwordConfirmInputField";
 const TERMS_ACCEPT_FIELD = "termsAcceptCheckboxField";
@@ -15,15 +18,20 @@ async function clickCreatePersona(page: Page): Promise<void> {
 export const submitExtensionCreateWalletForm = async (page: Page, password: string): Promise<void> => {
   await clickCreatePersona(page);
 
-  await sleep(20); // wait until redirect and render is done
   // Fill the form
+  await page.waitForSelector(`#${CREATE_WALLET_ID_STEP_1}`);
   await page.type(`input[name="${PASSWORD_FIELD}"]`, password);
   await page.type(`input[name="${PASSWORD_CONFIRM_FIELD}"]`, password);
   await page.click(`input[name="${TERMS_ACCEPT_FIELD}"]`);
   await page.click('button[type="submit"]');
-  await sleep(1000);
+
+  // Show recovery words page
+  await page.waitForSelector(`#${CREATE_WALLET_ID_STEP_2}`);
   const buttons = await page.$$("button");
   await buttons[1].click();
+
+  // Security hint page
+  await page.waitForSelector(`#${CREATE_WALLET_ID_STEP_3}`);
   await page.click('button[type="submit"]');
   await whenOnNavigatedToE2eRoute(page, "/wallet");
 };
