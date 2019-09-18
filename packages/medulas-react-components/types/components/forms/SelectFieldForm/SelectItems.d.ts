88287a80200e36930202f6cd16d089fee911611d
Simon Warta <simon@warta.it>
2019-09-18 09:58:55 +0200

    Remove obsolete medulas types

--- packages/medulas-react-components/types/components/forms/SelectFieldForm/SelectItems.d.ts
+++
@@ -1,8 +0,0 @@
-import { Item } from "./index";
-interface ListItemProps {
-  readonly action: (value: Item) => () => void;
-  readonly selectedItem?: string;
-  readonly items: readonly Item[];
-}
-declare const ListItems: ({ action, items, selectedItem }: ListItemProps) => JSX.Element;
-export default ListItems;
--- packages/medulas-react-components/types/context/Billboard/Billboard.d.ts
+++
@@ -1,8 +0,0 @@
-import React from "react";
-interface Props {
-  readonly show: boolean;
-  readonly message: React.ReactNode;
-  readonly children: React.ReactNode;
-}
-declare function Billboard({ show, children, message }: Props): JSX.Element;
-export default Billboard;
