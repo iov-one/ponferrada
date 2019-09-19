01b39c85a8cc194fc498e478d5ddd235b2143e10
Albert Andrejev <albert.andrejev@gmail.com>
2019-09-19 20:01:19 +0300

    remove not used async/await

--- packages/ui-logic/src/strings.spec.ts
+++ packages/ui-logic/src/strings.spec.ts
@@ -2,36 +2,36 @@ import { ellipsify, ellipsifyMiddle } from "./strings";
 
 describe("string", () => {
   describe("ellipsify", () => {
-    it("should return full string if it is shorter than maxLength", async () => {
+    it("should return full string if it is shorter than maxLength", () => {
       const result = ellipsify("short_string", 12);
-      await expect(result).toBe("short_string");
+      expect(result).toBe("short_string");
     });
 
-    it("should return ellipsified string if it is longer than maxLength", async () => {
+    it("should return ellipsified string if it is longer than maxLength", () => {
       const result = ellipsify("long_string_to_ellipsify", 15);
-      await expect(result).toBe("long_string_...");
+      expect(result).toBe("long_string_...");
     });
 
-    it("should return ellipsis if maxLength less than 5 characters", async () => {
+    it("should return ellipsis if maxLength less than 5 characters", () => {
       const result = ellipsify("short_string", 4);
-      await expect(result).toBe("...");
+      expect(result).toBe("...");
     });
   });
 
   describe("ellipsifyMiddle", () => {
-    it("should return full string if it is shorter than maxLength", async () => {
+    it("should return full string if it is shorter than maxLength", () => {
       const result = ellipsifyMiddle("short_string", 12);
-      await expect(result).toBe("short_string");
+      expect(result).toBe("short_string");
     });
 
-    it("should return ellipsified in the middle string if it is longer than maxLength", async () => {
+    it("should return ellipsified in the middle string if it is longer than maxLength", () => {
       const result = ellipsifyMiddle("long_string_to_ellipsify", 15);
-      await expect(result).toBe("long_...lipsify");
+      expect(result).toBe("long_...lipsify");
     });
 
-    it("should return ellipsis if maxLength less than 6 characters", async () => {
+    it("should return ellipsis if maxLength less than 6 characters", () => {
       const result = ellipsifyMiddle("short_string", 5);
-      await expect(result).toBe("...");
+      expect(result).toBe("...");
     });
   });
 });
