f7989af354e3c0067fe7018e74412ce15314301e
abefernan <abel.fernandez.nandin@gmail.com>
2019-09-24 14:03:46 +0200

    Remove unneeded middlewares

--- packages/sil-governance/src/store/index.ts
+++ packages/sil-governance/src/store/index.ts
@@ -1,59 +1,12 @@
-import {
-  AnyAction,
-  applyMiddleware,
-  compose,
-  createStore,
-  Dispatch,
-  Middleware,
-  MiddlewareAPI,
-  Store,
-} from "redux";
+import { applyMiddleware, compose, createStore, Middleware, Store } from "redux";
 
-import { getProposals, replaceProposalsAction, ReplaceProposalsActionType } from "./proposals";
 import reducer, { RootReducer, RootState } from "./reducers";
 
 const composeEnhancers =
   (typeof window === "object" && (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || // eslint-disable-line
   compose;
 
-const replaceProposals = async (
-  dispatch: Dispatch<ReplaceProposalsActionType>,
-  state: RootState,
-): Promise<void> => {
-  const governor = state.extension.governor;
-
-  if (governor) {
-    const proposals = await getProposals(governor);
-    dispatch(replaceProposalsAction(proposals));
-  }
-};
-
-const refreshProposalsActionMw = ({
-  dispatch,
-  getState,
-}: MiddlewareAPI): ((next: Dispatch<AnyAction>) => (action: AnyAction) => any) => next => {
-  return action => {
-    if (action.type === "@@proposals/REFRESH") replaceProposals(dispatch, getState());
-
-    next(action);
-  };
-};
-
-const refreshProposalsPeriodicallyMw: Middleware<{}, RootState, Dispatch<AnyAction>> = ({
-  dispatch,
-  getState,
-}: MiddlewareAPI): ((next: Dispatch<AnyAction>) => (action: AnyAction) => any) => next => {
-  const refreshProposals = async (): Promise<void> => {
-    await replaceProposals(dispatch, getState());
-    setTimeout(refreshProposals, 3000);
-  };
-
-  refreshProposals();
-
-  return action => next(action);
-};
-
-const middlewares: readonly Middleware[] = [refreshProposalsActionMw, refreshProposalsPeriodicallyMw];
+const middlewares: readonly Middleware[] = [];
 
 export const configureStore = (): Store<RootReducer> => {
   const store = createStore(reducer, composeEnhancers(applyMiddleware(...middlewares)));
