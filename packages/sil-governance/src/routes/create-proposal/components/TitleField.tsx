b01f99136973ddf2f8e668457d51cce0b6045936
abefernan <abel.fernandez.nandin@gmail.com>
2019-09-25 11:15:06 +0200

    Adds validators to title form field

--- packages/sil-governance/src/routes/create-proposal/components/TitleField.tsx
+++ packages/sil-governance/src/routes/create-proposal/components/TitleField.tsx
@@ -2,6 +2,8 @@ import { FormApi } from "final-form";
 import {
   Block,
   composeValidators,
+  FieldInputValue,
+  longerThan,
   notLongerThan,
   required,
   TextField,
@@ -11,14 +13,29 @@ import React from "react";
 
 export const TITLE_FIELD = "Title";
 const TITLE_PLACEHOLDER = "Enter Title";
-const TITLE_MAX_LENGTH = 30;
+const TITLE_MIN_LENGTH = 10;
+const TITLE_MAX_LENGTH = 50;
 
 interface Props {
   readonly form: FormApi;
 }
 
 const TitleField = ({ form }: Props): JSX.Element => {
-  const validator = composeValidators(required, notLongerThan(TITLE_MAX_LENGTH));
+  const charsetValidator = (value: FieldInputValue): string | undefined => {
+    if (typeof value !== "string") throw new Error("Input must be a string");
+
+    const isCharsetValid = !!value.match(/^[a-zA-Z0-9 _.-]*$/);
+
+    if (isCharsetValid) return undefined;
+    return `Symbols supported: ".", "-", "_"`;
+  };
+
+  const validator = composeValidators(
+    required,
+    longerThan(TITLE_MIN_LENGTH),
+    notLongerThan(TITLE_MAX_LENGTH),
+    charsetValidator,
+  );
 
   return (
     <Block>
