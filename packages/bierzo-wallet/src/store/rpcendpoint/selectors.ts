c0265b9443caea0734ca3c8eaf55d013a21d1158
Albert Andrejev <albert.andrejev@gmail.com>
2019-09-25 21:04:19 +0300

    Throw exception in case of undefined endpoint

--- packages/bierzo-wallet/src/store/rpcendpoint/selectors.ts
+++ packages/bierzo-wallet/src/store/rpcendpoint/selectors.ts
@@ -1,7 +1,10 @@
 import { RpcEndpointType } from "../../communication/rpcEndpoint";
 import { RootState } from "../reducers";
 
-export const getRpcEndpointType = (state: RootState): RpcEndpointType | undefined => {
-  const rpcEndpointType = state.rpcEndpoint;
-  return rpcEndpointType ? rpcEndpointType.rpcEndpointType : undefined;
+export const getRpcEndpointType = (state: RootState): RpcEndpointType => {
+  const rpcEndpoint = state.rpcEndpoint;
+  if (!rpcEndpoint) {
+    throw new Error("RPC Endpoint type should be set at this point. Something wrong!");
+  }
+  return rpcEndpoint.type;
 };
