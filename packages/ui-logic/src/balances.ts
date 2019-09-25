ee49d7e9e167d293a58b0a48f937651cc94aefe7
Simon Warta <simon@warta.it>
2019-09-25 09:01:58 +0200

    Use Decimal class to implement amountToNumber and add warning

--- packages/ui-logic/src/balances.ts
+++ packages/ui-logic/src/balances.ts
@@ -17,26 +17,12 @@ export function stringToAmount(
   };
 }
 
+/**
+ * Converts an amount to a JavaScript number. Please note that the resulting value is
+ * an approximation only since not all amounts can accurately be expressed as floats.
+ */
 export function amountToNumber(amount: Amount): number {
-  const { quantity, fractionalDigits } = amount;
-  if (!quantity.match(/^[0-9]+$/)) {
-    throw new Error(`quantity must be a number, got ${quantity}`);
-  }
-  if (fractionalDigits < 0) {
-    throw new Error(`invalid fractional digits: ${fractionalDigits}`);
-  }
-  // let's remove those leading zeros...
-  const temp = quantity.replace(/^0+/, "");
-  // unless we need them to reach a decimal point
-  const pad = fractionalDigits - temp.length;
-  const trimmed = pad > 0 ? "0".repeat(pad) + temp : temp;
-
-  const cut = trimmed.length - fractionalDigits;
-  const whole = cut === 0 ? "0" : trimmed.slice(0, cut);
-  const decimal = fractionalDigits === 0 ? "" : `.${trimmed.slice(cut)}`;
-  const value = `${whole}${decimal}`;
-
-  return Number(value);
+  return Decimal.fromAtomics(amount.quantity, amount.fractionalDigits).toFloatApproximation();
 }
 
 // This produces a human readable format of the amount, value and token ticker
--- packages/ui-logic/types/balances.d.ts
+++ packages/ui-logic/types/balances.d.ts
@@ -7,6 +7,10 @@ export declare function stringToAmount(
   amount: string,
   tokenInfo: Pick<Token, "fractionalDigits" | "tokenTicker">,
 ): Amount;
+/**
+ * Converts an amount to a JavaScript number. Please note that the resulting value is
+ * an approximation only since not all amounts can accurately be expressed as floats.
+ */
 export declare function amountToNumber(amount: Amount): number;
 export declare function amountToString(amount: Amount): string;
 export declare function amountToGwei(amount: Amount): string;
