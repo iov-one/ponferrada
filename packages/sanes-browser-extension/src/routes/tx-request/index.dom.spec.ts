cc6021c99e65a86c6fd65ab33c7cb789a1d67fe3
Albert Andrejev <albert.andrejev@gmail.com>
2019-09-27 13:27:43 +0300

    Test create proposal start date value

--- packages/sanes-browser-extension/src/routes/tx-request/index.dom.spec.ts
+++ packages/sanes-browser-extension/src/routes/tx-request/index.dom.spec.ts
@@ -8,12 +8,13 @@ import { findRenderedDOMComponentWithId } from "../../utils/test/reactElemFinder
 import { REQUEST_ROUTE } from "../paths";
 import { REQ_REGISTER_USERNAME } from "./components/ShowRequest/ReqRegisterUsernameTx";
 import { REQ_SEND_TX } from "./components/ShowRequest/ReqSendTransaction";
-import { getCashTransaction, getUsernameTransaction } from "./test";
+import { getCashTransaction, getCreateTextResolutionActionTransaction, getUsernameTransaction } from "./test";
 import {
   checkPermanentRejection,
   clickOnBackButton,
   clickOnRejectButton,
   confirmRejectButton,
+  getProposalStartDate,
 } from "./test/operateTXRequest";
 
 const sendRequests: readonly Request[] = [
@@ -30,14 +31,14 @@ const sendRequests: readonly Request[] = [
 ];
 
 describe("DOM > Feature > Transaction Request", (): void => {
-  let identityDOM: React.Component;
+  let txRequestDOM: React.Component;
 
   beforeEach(async () => {
-    identityDOM = await travelToTXRequest(sendRequests);
+    txRequestDOM = await travelToTXRequest(sendRequests);
   }, 60000);
 
   it("should accept incoming request and redirect to account status view", async (): Promise<void> => {
-    const inputs = TestUtils.scryRenderedDOMComponentsWithTag(identityDOM, "button");
+    const inputs = TestUtils.scryRenderedDOMComponentsWithTag(txRequestDOM, "button");
 
     expect(inputs.length).toBe(2);
 
@@ -48,35 +49,35 @@ describe("DOM > Feature > Transaction Request", (): void => {
   }, 60000);
 
   it("should reject incoming request and come back", async (): Promise<void> => {
-    await clickOnRejectButton(identityDOM);
-    await confirmRejectButton(identityDOM);
+    await clickOnRejectButton(txRequestDOM);
+    await confirmRejectButton(txRequestDOM);
     // TODO: Check here that share request rejection has been reject successfuly
 
     /**
      * Remove this code if not required in case if there is another redirection
      * in confirmAcceptButton method. And apply this method in separate test method.
      */
-    await clickOnBackButton(identityDOM);
+    await clickOnBackButton(txRequestDOM);
   }, 60000);
 
   it("should reject incoming request permanently and come back", async (): Promise<void> => {
-    await clickOnRejectButton(identityDOM);
-    await checkPermanentRejection(identityDOM);
-    await confirmRejectButton(identityDOM);
+    await clickOnRejectButton(txRequestDOM);
+    await checkPermanentRejection(txRequestDOM);
+    await confirmRejectButton(txRequestDOM);
     await sleep(2000);
     // rejection flag has been set
   }, 60000);
 });
 
 describe("DOM > Feature > Send Transaction Request", (): void => {
-  let identityDOM: React.Component;
+  let txRequestDOM: React.Component;
 
   beforeEach(async () => {
-    identityDOM = await travelToTXRequest(sendRequests);
+    txRequestDOM = await travelToTXRequest(sendRequests);
   }, 60000);
 
   it("should show send transaction request accept view", async (): Promise<void> => {
-    await findRenderedDOMComponentWithId(identityDOM, REQ_SEND_TX);
+    await findRenderedDOMComponentWithId(txRequestDOM, REQ_SEND_TX);
   }, 60000);
 });
 
@@ -94,13 +95,39 @@ describe("DOM > Feature > Username Registration Request", (): void => {
     },
   ];
 
-  let identityDOM: React.Component;
+  let txRequestDOM: React.Component;
 
   beforeEach(async () => {
-    identityDOM = await travelToTXRequest(requests);
+    txRequestDOM = await travelToTXRequest(requests);
   }, 60000);
 
   it("should show register username request accept view", async (): Promise<void> => {
-    await findRenderedDOMComponentWithId(identityDOM, REQ_REGISTER_USERNAME);
+    await findRenderedDOMComponentWithId(txRequestDOM, REQ_REGISTER_USERNAME);
+  }, 60000);
+});
+
+describe("DOM > Feature > Create Proposal Request", (): void => {
+  const requests: readonly Request[] = [
+    {
+      id: 1,
+      senderUrl: "http://finnex.com",
+      reason: "Test Create Proposal Request",
+      responseData: {
+        tx: getCreateTextResolutionActionTransaction(),
+      },
+      accept: jest.fn(),
+      reject: jest.fn(),
+    },
+  ];
+
+  let txRequestDOM: React.Component;
+
+  beforeEach(async () => {
+    txRequestDOM = await travelToTXRequest(requests);
+  }, 60000);
+
+  fit("should show proper propsal start date", async (): Promise<void> => {
+    const proposalStartDate = getProposalStartDate(txRequestDOM);
+    expect(proposalStartDate).toBe("8/21/2019, 10:28:21 AM");
   }, 60000);
 });
