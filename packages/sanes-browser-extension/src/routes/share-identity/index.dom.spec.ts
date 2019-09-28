603c15076699dc289f822ba3382cd0fd6d2a5264
Albert Andrejev <albert.andrejev@gmail.com>
2019-09-28 23:47:00 +0300

    Update Share Identity tests and helpers

--- packages/sanes-browser-extension/src/routes/share-identity/index.dom.spec.ts
+++ packages/sanes-browser-extension/src/routes/share-identity/index.dom.spec.ts
@@ -1,63 +1,71 @@
 import { Address } from "@iov/bcp";
-import TestUtils from "react-dom/test-utils";
 import { sleep } from "ui-logic";
 
 import { Request } from "../../extension/background/model/requestsHandler/requestQueueManager";
-import { click } from "../../utils/test/dom";
-import { travelToShareIdentity } from "../../utils/test/navigation";
+import { travelToShareIdentity, whenOnNavigatedToRoute } from "../../utils/test/navigation";
+import { REQUEST_ROUTE } from "../paths";
 import {
   checkPermanentRejection,
-  clickOnBackButton,
   clickOnRejectButton,
+  confirmAcceptButton,
   confirmRejectButton,
 } from "./test/operateShareIdentity";
 
-describe("DOM > Feature > Share Identity", (): void => {
-  const requests: readonly Request[] = [
-    {
-      id: 1,
-      senderUrl: "http://finnex.com",
-      reason: "Test get Identities",
-      responseData: {
-        requestedIdentities: [
-          {
-            chainName: "Ganache",
-            address: "0x873fAA4cdDd5b157e8E5a57e7a5479AFC5d3aaaa" as Address,
-          },
-        ],
+const request = {
+  id: 1,
+  senderUrl: "http://finnex.com",
+  reason: "Test get Identities",
+  responseData: {
+    requestedIdentities: [
+      {
+        chainName: "Ganache",
+        address: "0x873fAA4cdDd5b157e8E5a57e7a5479AFC5d3aaaa" as Address,
       },
-      accept: jest.fn(),
-      reject: jest.fn(),
-    },
-  ];
+    ],
+  },
+  accept: jest.fn(),
+  reject: jest.fn(),
+};
 
+const requests: readonly Request[] = [request];
+
+describe("DOM > Feature > Share Identity", (): void => {
   let identityDOM: React.Component;
+  let windowCloseCall = false;
 
   beforeEach(async () => {
     identityDOM = await travelToShareIdentity(requests);
+    jest.spyOn(window, "close").mockImplementation(() => {
+      windowCloseCall = true;
+    });
   }, 60000);
 
-  it("should accept incoming request and redirect to account status view", async (): Promise<void> => {
-    const inputs = TestUtils.scryRenderedDOMComponentsWithTag(identityDOM, "button");
-
-    expect(inputs.length).toBe(2);
-
-    const acceptButton = inputs[0];
-    click(acceptButton);
-
-    // TODO: Check here that share request has been accepted successfuly
+  it("should accept incoming request and  close extension popup", async (): Promise<void> => {
+    await confirmAcceptButton(identityDOM);
+    expect(windowCloseCall).toBeTruthy();
   }, 60000);
 
-  it("should reject incoming request and come back", async (): Promise<void> => {
+  it("should accept incoming request and redirect to the list of requests", async () => {
+    const requests = [request, { id: 2, ...request }];
+    identityDOM = await travelToShareIdentity(requests);
+    await confirmAcceptButton(identityDOM);
+    await whenOnNavigatedToRoute(REQUEST_ROUTE);
+  }, 60000);
+
+  it("should reject incoming request and close extension popup", async (): Promise<void> => {
     await clickOnRejectButton(identityDOM);
     await confirmRejectButton(identityDOM);
     // TODO: Check here that share request rejection has been reject successfuly
 
-    /**
-     * Remove this code if not required in case if there is another redirection
-     * in confirmAcceptButton method. And apply this method in separate test method.
-     */
-    await clickOnBackButton(identityDOM);
+    expect(windowCloseCall).toBeTruthy();
+  }, 60000);
+
+  it("should reject incoming request and redirect to the list of requests", async () => {
+    const requests = [request, { id: 2, ...request }];
+    identityDOM = await travelToShareIdentity(requests);
+    await clickOnRejectButton(identityDOM);
+    await confirmRejectButton(identityDOM);
+    await whenOnNavigatedToRoute(REQUEST_ROUTE);
   }, 60000);
 
   it("should reject incoming request permanently and come back", async (): Promise<void> => {
--- packages/sanes-browser-extension/src/routes/share-identity/test/operateShareIdentity.ts
+++ packages/sanes-browser-extension/src/routes/share-identity/test/operateShareIdentity.ts
@@ -1,8 +1,18 @@
 import TestUtils from "react-dom/test-utils";
 
+import { click, submit } from "../../../utils/test/dom";
 import { findRenderedDOMComponentWithId } from "../../../utils/test/reactElemFinder";
 import { SHARE_IDENTITY_REJECT } from "../components/RejectRequest";
 import { SHARE_IDENTITY_SHOW } from "../components/ShowRequest";
+
+export const confirmAcceptButton = async (ShareIdentityDom: React.Component): Promise<void> => {
+  const inputs = TestUtils.scryRenderedDOMComponentsWithTag(ShareIdentityDom, "button");
+
+  expect(inputs.length).toBe(2);
+
+  const acceptButton = inputs[0];
+  await click(acceptButton);
+};
 
 export const clickOnRejectButton = async (ShareIdentityDom: React.Component): Promise<void> => {
   const inputs = TestUtils.scryRenderedDOMComponentsWithTag(ShareIdentityDom, "button");
@@ -19,15 +29,8 @@ export const clickOnRejectButton = async (ShareIdentityDom: React.Component): Pr
 };
 
 export const confirmRejectButton = async (ShareIdentityDom: React.Component): Promise<void> => {
-  const inputs = TestUtils.scryRenderedDOMComponentsWithTag(ShareIdentityDom, "button");
-
-  expect(inputs.length).toBe(2);
-
-  const rejectButton = inputs[0];
-
-  TestUtils.act(() => {
-    TestUtils.Simulate.click(rejectButton);
-  });
+  const form = TestUtils.findRenderedDOMComponentWithTag(ShareIdentityDom, "form");
+  await submit(form);
 };
 
 export const checkPermanentRejection = async (ShareIdentityDom: React.Component): Promise<void> => {
