60414f8517ecf8e549746bc0fb82b3977ebca928
abefernan <abel.fernandez.nandin@gmail.com>
2019-09-30 18:15:05 +0200

    Adds onChanged support to TextField

--- packages/medulas-react-components/types/components/forms/TextField/index.d.ts
+++ packages/medulas-react-components/types/components/forms/TextField/index.d.ts
@@ -1,12 +1,14 @@
 import { TextFieldProps } from "@material-ui/core/TextField";
 import { FieldSubscription, FieldValidator, FormApi } from "final-form";
+import * as React from "react";
 import { FieldInputValue } from "../../../utils/forms/validators";
 interface InnerProps {
-  name: string;
-  form: FormApi;
-  validate?: FieldValidator<FieldInputValue>;
-  subscription?: FieldSubscription;
+  readonly name: string;
+  readonly form: FormApi;
+  readonly validate?: FieldValidator<FieldInputValue>;
+  readonly onChanged?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  readonly subscription?: FieldSubscription;
 }
 declare type Props = InnerProps & TextFieldProps;
-declare const TextField: ({ name, form, validate, ...restProps }: Props) => JSX.Element;
+declare const TextField: ({ name, form, validate, onChanged, ...restProps }: Props) => JSX.Element;
 export default TextField;
