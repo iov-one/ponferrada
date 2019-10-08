3550bd7d976dc8fa2a6db0b9cfc888a740bbf12d
Albert Andrejev <albert.andrejev@gmail.com>
2019-10-08 16:54:42 +0300

    Update story props

--- packages/medulas-react-components/src/pages/PageColumn/index.stories.tsx
+++ packages/medulas-react-components/src/pages/PageColumn/index.stories.tsx
@@ -5,35 +5,14 @@ import React from "react";
 import { medulasRoot, Storybook } from "../../utils/storybook";
 import PageColumn from "./index";
 
-const RenderHeader = (): JSX.Element => <React.Fragment />;
-
-storiesOf(`${medulasRoot}/pages/PageColumn`, module)
+storiesOf(`${medulasRoot}/pages`, module)
   .addParameters({ viewport: { defaultViewport: "responsive" } })
-  .add("default", () => (
+  .add("PageColumn", () => (
     <Storybook>
       <PageColumn
-        icon="white"
-        primaryTitle="Welcome"
-        secondaryTitle="to your IOV wallet"
-        subtitle="To access the wallet please authenticate using Neuma or Ledger Nano S"
-        renderHeader={RenderHeader}
-        primaryNextLabel="Continue"
-        primaryNextClicked={action("Page form submit")}
-      />
-    </Storybook>
-  ))
-  .add("with secondary next button", () => (
-    <Storybook>
-      <PageColumn
-        icon="white"
-        primaryTitle="Page"
-        secondaryTitle="column"
-        subtitle="Storybook PageColumn component example."
-        renderHeader={RenderHeader}
-        primaryNextLabel="Continue"
-        primaryNextClicked={action("Page form submit")}
-        secondaryNextLabel="Password forgotten"
-        secondaryNextClicked={action("Password forgotten clicked")}
+        onLoginWithNeuma={action("onLoginWithNeuma")}
+        onLoginWithLedger={action("onLoginWithLedger")}
+        onGetNeumaExtension={action("onGetNeumaExtension")}
       />
     </Storybook>
   ));
