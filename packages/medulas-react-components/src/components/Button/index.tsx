63d325afe0303666077262edd5615a0080ab927b
Albert Andrejev <albert.andrejev@gmail.com>
2019-10-08 14:27:10 +0300

    Implement inverted colors

--- packages/medulas-react-components/src/components/Button/index.tsx
+++ packages/medulas-react-components/src/components/Button/index.tsx
@@ -1,21 +1,48 @@
+import { PropTypes } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core";
 import MuiButton, { ButtonProps } from "@material-ui/core/Button";
 import CircularProgress from "@material-ui/core/CircularProgress";
+import { Theme } from "@material-ui/core/styles";
 import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
 import React from "react";
 
 import Block from "../Block";
 
-interface Props extends Omit<ButtonProps, "variant"> {
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    backgroundColor: "rgba(49, 230, 201, 0.15)",
+  },
+  label: {
+    color: theme.palette.primary.main,
+  },
+  contained: {
+    "&:hover": {
+      backgroundColor: "rgba(49, 230, 201, 0.15)",
+    },
+  },
+}));
+
+interface Props extends Omit<ButtonProps, "variant" | "color"> {
   readonly variant?: ButtonProps["variant"] | "continue";
   readonly spinner?: boolean;
   readonly children: React.ReactNode;
+  readonly color?: PropTypes.Color | "inverted";
 }
 
-const Button = ({ children, variant, spinner, ...restProps }: Props): JSX.Element => {
+const Button = ({ children, variant, spinner, color, ...restProps }: Props): JSX.Element => {
   const muiVariant: ButtonProps["variant"] = variant && variant === "continue" ? "contained" : variant;
+  let classes: ReturnType<typeof useStyles> | undefined = useStyles();
+
+  let muiColor: PropTypes.Color | undefined;
+  if (color && color === "inverted") {
+    muiColor = "default";
+  } else {
+    muiColor = color;
+    classes = undefined;
+  }
 
   return (
-    <MuiButton variant={muiVariant} {...restProps}>
+    <MuiButton variant={muiVariant} color={muiColor} {...restProps} classes={classes}>
       {spinner && (
         <Block display="flex" marginRight={2}>
           <CircularProgress size={22} color="inherit" />
