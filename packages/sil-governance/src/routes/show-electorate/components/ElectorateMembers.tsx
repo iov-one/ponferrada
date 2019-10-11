6153ac7378154d8f2d6be5a99a28a448a1d729d5
abefernan <abel.fernandez.nandin@gmail.com>
2019-10-11 17:19:54 +0200

    Changes Electorate table to MUI component

--- packages/sil-governance/src/routes/show-electorate/components/ElectorateMembers.tsx
+++ packages/sil-governance/src/routes/show-electorate/components/ElectorateMembers.tsx
@@ -1,37 +1,37 @@
 import { Electorate } from "@iov/bns";
+import { Table, TableBody, TableCell, TableHead, TableRow } from "@material-ui/core";
 import { Block, Typography } from "medulas-react-components";
-import React from "react";
+import React, { useMemo } from "react";
 
 interface Props {
   readonly electorate: Electorate;
 }
 
 const ElectorateMembers = ({ electorate }: Props): JSX.Element => {
-  const MembersTable = (): JSX.Element => (
-    <table>
-      <thead>
-        <tr>
-          <th>
-            <Typography>Address</Typography>
-          </th>
-          <th>
-            <Typography>Weight</Typography>
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        {Object.keys(electorate.electors).map(address => (
-          <tr key={address}>
-            <td>
-              <Typography>{address}</Typography>
-            </td>
-            <td>
-              <Typography>{electorate.electors[address].weight}</Typography>
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+  const MembersTable = useMemo(
+    () => (): JSX.Element => (
+      <Table size="small">
+        <TableHead>
+          <TableRow>
+            <TableCell>Address</TableCell>
+            <TableCell align="right">Weight</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {Object.keys(electorate.electors).map(address => (
+            <TableRow key={address}>
+              <TableCell>
+                <Typography>{address}</Typography>
+              </TableCell>
+              <TableCell align="right">
+                <Typography>{electorate.electors[address].weight}</Typography>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    ),
+    [electorate.electors],
   );
 
   return (
