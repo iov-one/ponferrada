8b41c41f0e1a6ab86a30054cc2a5b1ffb48df3ac
Albert Andrejev <albert.andrejev@gmail.com>
2019-10-14 22:34:20 +0300

    Fix typo

--- packages/bierzo-wallet/src/components/BillboardMessage/index.stories.tsx
+++ packages/bierzo-wallet/src/components/BillboardMessage/index.stories.tsx
@@ -7,7 +7,7 @@ import NeumaBillboardMessage from "./NeumaBillboardMessage";
 
 storiesOf(`${bierzoRoot}/BillboardMessage`, module)
   .addParameters({ viewport: { defaultViewport: "responsive" } })
-  .add("Nauma", () => (
+  .add("Neuma", () => (
     <DecoratedStorybook>
       <Block bgcolor="rgba(26, 26, 26, 0.3)" width="100%" height="100%" padding={4}>
         <NeumaBillboardMessage />
