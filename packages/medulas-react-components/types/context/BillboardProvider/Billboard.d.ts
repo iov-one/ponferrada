dc7cc3e9f670c1c844b260af000957afab025361
Albert Andrejev <albert.andrejev@gmail.com>
2019-10-14 14:01:59 +0300

    Add Billboard positioning

--- packages/medulas-react-components/types/context/BillboardProvider/Billboard.d.ts
+++ packages/medulas-react-components/types/context/BillboardProvider/Billboard.d.ts
@@ -1,8 +1,12 @@
 import React from "react";
+export declare type VPosition = "center" | "start" | "end";
+export declare type HPosition = "center" | "flex-start" | "flex-end";
 interface Props {
   readonly show: boolean;
   readonly message: React.ReactNode;
   readonly children: React.ReactNode;
+  readonly vPosition: VPosition;
+  readonly hPosition: HPosition;
 }
-declare function Billboard({ show, children, message }: Props): JSX.Element;
+declare function Billboard({ show, children, message, hPosition, vPosition }: Props): JSX.Element;
 export default Billboard;
