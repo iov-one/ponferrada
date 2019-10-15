64ab185f9f197ad615ab9fe8853c062c073db3eb
Albert Andrejev <albert.andrejev@gmail.com>
2019-10-15 14:05:22 +0300

    Add Ledger billboard message

---
+++ packages/bierzo-wallet/src/components/BillboardMessage/LedgerBillboardMessage.tsx
@@ -0,0 +1,43 @@
+import { Theme } from "@material-ui/core";
+import { useTheme } from "@material-ui/styles";
+import { Block, Image, makeStyles, Typography } from "medulas-react-components";
+import React from "react";
+
+import ledger from "./assets/ledger.svg";
+
+const useStyles = makeStyles({
+  panel: {
+    border: "1px solid #F3F3F3",
+    boxSizing: "border-box",
+    borderRadius: 5,
+  },
+});
+
+function LedgerBillboardMessage(): JSX.Element {
+  const classes = useStyles();
+  const theme = useTheme<Theme>();
+
+  return (
+    <Block
+      bgcolor={theme.palette.background.paper}
+      marginTop={5}
+      marginRight={15}
+      width={450}
+      height={250}
+      padding={8}
+      paddingTop={9}
+      textAlign="center"
+      className={classes.panel}
+    >
+      <Image src={ledger} alt="ledger" />
+
+      <Block marginTop={4}>
+        <Typography variant="body1" align="center" weight="semibold">
+          Waiting for Ledger device to provide identity...
+        </Typography>
+      </Block>
+    </Block>
+  );
+}
+
+export default LedgerBillboardMessage;
