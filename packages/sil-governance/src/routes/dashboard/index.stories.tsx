64d31eba984ab3b52a3244f6b8c84c3f270ee1bb
abefernan <abel.fernandez.nandin@gmail.com>
2019-10-19 18:03:30 +0200

    Adds Proposals to Dashboard story

--- packages/sil-governance/src/routes/dashboard/index.stories.tsx
+++ packages/sil-governance/src/routes/dashboard/index.stories.tsx
@@ -1,14 +1,34 @@
 import { storiesOf } from "@storybook/react";
 import React from "react";
+import { ReadonlyDate } from "readonly-date";
+import { DeepPartial } from "redux";
 
 import { ElectionFilter } from "../../components/AsideFilter";
+import { adminAddress, getDummyElectorates, getDummyProposalsState } from "../../store/proposals/dummyData";
+import { RootState } from "../../store/reducers";
 import DecoratedStorybook, { silRoot } from "../../utils/storybook";
 import Dashboard from "./index";
 
+const storeProps: DeepPartial<RootState> = {
+  extension: {
+    connected: true,
+    installed: true,
+    governor: {
+      address: adminAddress,
+      getElectorates: getDummyElectorates,
+    },
+  },
+  proposals: getDummyProposalsState(),
+  blockchain: {
+    lastBlockTime: new ReadonlyDate(),
+    lastBlockheight: 44447774,
+  },
+};
+
 storiesOf(silRoot, module)
   .addParameters({ viewport: { defaultViewport: "responsive" } })
-  .add("Dashboard page", () => (
-    <DecoratedStorybook>
+  .add("Dashboard", () => (
+    <DecoratedStorybook storeProps={storeProps}>
       <Dashboard filter={ElectionFilter.All} />
     </DecoratedStorybook>
   ));
