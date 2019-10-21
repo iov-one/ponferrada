6d78d4d4b50f7018fddd760fa9fe36414720006b
Albert Andrejev <albert.andrejev@gmail.com>
2019-10-21 16:31:45 +0300

    Add default typography color

--- packages/medulas-react-components/src/theme/utils/variables.ts
+++ packages/medulas-react-components/src/theme/utils/variables.ts
@@ -1,5 +1,6 @@
 export const white = "#ffffff";
 export const secondaryColor = "#6F749A";
 export const backgroundColor = "#F5F5F5";
+export const defaultColor = "#1C1C1C";
 export const lightFont = 300;
 export const border = "#f3f3f3";
