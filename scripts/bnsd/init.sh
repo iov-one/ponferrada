4f07e45ee31bfeec8a47040f36bd4e7bab3be620
abefernan <abel.fernandez.nandin@gmail.com>
2019-10-23 01:24:44 +0200

    Always preload proposals to test sorting and voting

--- scripts/bnsd/init.sh
+++ scripts/bnsd/init.sh
@@ -5,9 +5,7 @@ command -v shellcheck > /dev/null && shellcheck "$0"
 # get this files directory regardless of pwd when we run it
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-if [ -n "${INIT_PROPOSALS:-}" ]; then
-  sleep 3
-  echo "Deploying proposals..."
-  node "${SCRIPT_DIR}/deploy_proposals.js"
-  echo "Done."
-fi
+sleep 3
+echo "Deploying proposals..."
+node "${SCRIPT_DIR}/deploy_proposals.js"
+echo "Done."
