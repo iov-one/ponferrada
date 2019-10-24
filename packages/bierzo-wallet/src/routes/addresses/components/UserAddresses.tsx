62f08f406b1c8c3707a016bece86ee70c29073a1
Albert Andrejev <albert.andrejev@gmail.com>
2019-10-24 12:55:33 +0300

    Remove not used code

--- packages/bierzo-wallet/src/routes/addresses/components/UserAddresses.tsx
+++ packages/bierzo-wallet/src/routes/addresses/components/UserAddresses.tsx
@@ -1,33 +1,26 @@
-import { faUser } from "@fortawesome/free-regular-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Theme } from "@material-ui/core";
-import Avatar from "@material-ui/core/Avatar";
 import Paper from "@material-ui/core/Paper";
-import { useTheme } from "@material-ui/styles";
-import { Block, Button, makeStyles } from "medulas-react-components";
+import { Block, makeStyles, Typography } from "medulas-react-components";
 import React from "react";
 
 import AddressesTable, { AddressesTableProps } from "../../../components/AddressesTable";
 
 export const PAYMENT_CONFIRMATION_VIEW_ID = "payment-confirmation-view-id";
 
-const useAvatar = makeStyles((theme: Theme) => ({
-  root: {
-    backgroundColor: "#ffe152",
-    fontSize: "27.5px",
-    width: "72px",
-    height: "72px",
-    margin: "-76px 0 40px 0",
+const usePaper = makeStyles({
+  rounded: {
+    borderRadius: "5px",
   },
-}));
+  elevation1: {
+    boxShadow: "none",
+  },
+});
 
 interface Props extends AddressesTableProps {
   readonly onReturnToBalance: () => void;
 }
 
-const UserAddresses = ({ chainAddresses, onReturnToBalance }: Props): JSX.Element => {
-  const avatarClasses = useAvatar();
-  const theme = useTheme<Theme>();
+const UserAddresses = ({ chainAddresses }: Props): JSX.Element => {
+  const paperClasses = usePaper();
 
   return (
     <Block
@@ -36,31 +29,24 @@ const UserAddresses = ({ chainAddresses, onReturnToBalance }: Props): JSX.Elemen
       display="flex"
       alignContent="center"
       justifyContent="center"
-      bgcolor={theme.palette.background.default}
     >
       <Block width={650}>
-        <Paper>
+        <Paper classes={paperClasses}>
           <Block
             display="flex"
             flexDirection="column"
-            alignItems="center"
             width="100%"
             marginTop={4}
-            paddingTop={5}
-            padding={3}
+            padding={5}
+            border="1px solid #F3F3F3"
           >
-            <Avatar classes={avatarClasses}>
-              <FontAwesomeIcon icon={faUser} color="#ffffff" />
-            </Avatar>
+            <Typography variant="subtitle2" weight="semibold">
+              BLOCKCHAIN ADDRESSES
+            </Typography>
+            <Block marginTop={3} />
             <AddressesTable chainAddresses={chainAddresses} />
           </Block>
         </Paper>
-
-        <Block marginTop={4} marginBottom={1}>
-          <Button fullWidth onClick={onReturnToBalance}>
-            Return to Balance
-          </Button>
-        </Block>
       </Block>
     </Block>
   );
