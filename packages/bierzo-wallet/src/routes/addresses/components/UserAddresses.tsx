3065be6f4397dfcacf6a36ccb29b38a223552310
Albert Andrejev <albert.andrejev@gmail.com>
2019-10-24 14:30:08 +0300

    Remove not used Block wrapper

--- packages/bierzo-wallet/src/routes/addresses/components/UserAddresses.tsx
+++ packages/bierzo-wallet/src/routes/addresses/components/UserAddresses.tsx
@@ -19,31 +19,23 @@ const UserAddresses = ({ chainAddresses }: AddressesTableProps): JSX.Element => 
   const paperClasses = usePaper();
 
   return (
-    <Block
-      id={PAYMENT_CONFIRMATION_VIEW_ID}
-      marginTop={4}
-      display="flex"
-      alignContent="center"
-      justifyContent="center"
-    >
-      <Block width={650}>
-        <Paper classes={paperClasses}>
-          <Block
-            display="flex"
-            flexDirection="column"
-            width="100%"
-            marginTop={4}
-            padding={5}
-            border="1px solid #F3F3F3"
-          >
-            <Typography variant="subtitle2" weight="semibold">
-              BLOCKCHAIN ADDRESSES
-            </Typography>
-            <Block marginTop={3} />
-            <AddressesTable chainAddresses={chainAddresses} />
-          </Block>
-        </Paper>
-      </Block>
+    <Block marginTop={1} width={650} id={PAYMENT_CONFIRMATION_VIEW_ID}>
+      <Paper classes={paperClasses}>
+        <Block
+          display="flex"
+          flexDirection="column"
+          width="100%"
+          marginTop={4}
+          padding={5}
+          border="1px solid #F3F3F3"
+        >
+          <Typography variant="subtitle2" weight="semibold">
+            BLOCKCHAIN ADDRESSES
+          </Typography>
+          <Block marginTop={3} />
+          <AddressesTable chainAddresses={chainAddresses} />
+        </Block>
+      </Paper>
     </Block>
   );
 };
