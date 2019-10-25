1d3aec43d1d6321a6b9e2e409d5394a41e663fea
Albert Andrejev <albert.andrejev@gmail.com>
2019-10-25 16:33:26 +0300

    Get addresses associated with username

--- packages/bierzo-wallet/src/routes/addresses/index.tsx
+++ packages/bierzo-wallet/src/routes/addresses/index.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import * as ReactRedux from "react-redux";
 
 import { history } from "..";
+import { ChainAddressPairWithName } from "../../components/AddressesTable";
 import PageMenu from "../../components/PageMenu";
-import { RootState } from "../../store/reducers";
 import { getRpcEndpointType } from "../../store/rpcendpoint/selectors";
+import { BwUsername } from "../../store/usernames";
 import { getFirstUsername } from "../../store/usernames/selectors";
-import { getChainAddressPairWithNames } from "../../utils/tokens";
+import { chainAddressPairSortedMapping } from "../../utils/tokens";
 import { REGISTER_PERSONALIZED_ADDRESS_ROUTE } from "../paths";
 import AddressesTab from "./components/AddressesTab";
 
@@ -15,12 +16,25 @@ function onRegisterUsername(): void {
 }
 
 const Addresses = (): JSX.Element => {
-  const identities = ReactRedux.useSelector((state: RootState) => state.identities);
   const bnsUsername = ReactRedux.useSelector(getFirstUsername);
   const rpcEndpointType = ReactRedux.useSelector(getRpcEndpointType);
   const iovAddress = bnsUsername ? bnsUsername.username : undefined;
+  const [chainAddresses, setChainAddresses] = React.useState<readonly ChainAddressPairWithName[]>([]);
 
-  const chainAddresses = getChainAddressPairWithNames(identities);
+  React.useEffect(() => {
+    let isSubscribed = true;
+    async function getAddressesPairs(bnsUsername: BwUsername | undefined): Promise<void> {
+      if (isSubscribed) {
+        const chainAddresses = bnsUsername ? await chainAddressPairSortedMapping(bnsUsername.addresses) : [];
+        setChainAddresses(chainAddresses);
+      }
+    }
+    getAddressesPairs(bnsUsername);
+
+    return () => {
+      isSubscribed = false;
+    };
+  }, [bnsUsername]);
 
   return (
     <PageMenu>
