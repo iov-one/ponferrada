5a9ef3cb252058ba6a34668a3da5602883be480d
Albert Andrejev <albert.andrejev@gmail.com>
2019-10-25 16:32:51 +0300

    Add travelToStarnamesTabE2E helper

--- packages/bierzo-wallet/src/routes/addresses/test/travelToReceivePayment.ts
+++ packages/bierzo-wallet/src/routes/addresses/test/travelToReceivePayment.ts
@@ -3,7 +3,8 @@ import { Page } from "puppeteer";
 import { ADDRESSES_TEXT } from "../../../components/Header/components/LinksMenu";
 import { whenOnNavigatedToE2eRoute } from "../../../utils/test/navigation";
 import { ADDRESSES_ROUTE } from "../../paths";
-import { yourAddresses } from "../components/AddressesTab";
+import { yourAddresses, yourStarnames } from "../components/AddressesTab";
+import { starnamesViewId } from "../components/Starnames";
 import { yourBlockchainAddressesId } from "../components/UserAddresses";
 
 export async function travelToAddressesE2E(page: Page): Promise<void> {
@@ -16,4 +17,10 @@ export async function travelToBlockchainAddressesTabE2E(page: Page): Promise<voi
   const [addressesLink] = await page.$x(`//h6[contains(., '${yourAddresses}')]`);
   await addressesLink.click();
   await page.waitForSelector(`#${yourBlockchainAddressesId}`);
+}
+
+export async function travelToStarnamesTabE2E(page: Page): Promise<void> {
+  const [addressesLink] = await page.$x(`//h6[contains(., '${yourStarnames}')]`);
+  await addressesLink.click();
+  await page.waitForSelector(`#${starnamesViewId}`);
 }
