a2d45f04706b61c8c329bc212c1b00b7b2fcd48b
abefernan <abel.fernandez.nandin@gmail.com>
2019-11-11 11:31:39 +0100

    Adds Requests view

---
+++ packages/sanes-browser-extension/src/routes/wallet/components/SidePanel/PanelDrawer/Requests/index.tsx
@@ -0,0 +1,24 @@
+import { Block, Typography } from "medulas-react-components";
+import React from "react";
+
+import { RequestContext } from "../../../../../../context/RequestProvider";
+import RequestList from "./RequestList";
+
+const Requests = (): JSX.Element => {
+  const requestContext = React.useContext(RequestContext);
+  const { requests } = requestContext;
+  const hasRequests = requests.length > 0;
+
+  return (
+    <Block marginLeft={3} marginRight={3}>
+      {!hasRequests && (
+        <Typography align="center" weight="semibold">
+          No requests in queue
+        </Typography>
+      )}
+      {hasRequests && <RequestList requests={requests} />}
+    </Block>
+  );
+};
+
+export default Requests;
