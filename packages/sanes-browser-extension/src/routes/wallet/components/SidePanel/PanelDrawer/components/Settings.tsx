315c337ad6f2cdd119450795b329af8209147f2f
abefernan <abel.fernandez.nandin@gmail.com>
2019-11-15 09:47:24 +0100

    Extracts Menu Items as components

--- packages/sanes-browser-extension/src/routes/wallet/components/SidePanel/PanelDrawer/components/Settings.tsx
+++ packages/sanes-browser-extension/src/routes/wallet/components/SidePanel/PanelDrawer/components/Settings.tsx
@@ -1,20 +1,17 @@
-import { Block, Image, List, ListItem, ListItemText, makeStyles } from "medulas-react-components";
+import { Block, List, makeStyles } from "medulas-react-components";
 import React from "react";
 
 import { Views } from "..";
-import greenRightChevron from "../../../../assets/chevronRightGreen.svg";
+import ListNavView from "./ListNavView";
 
 const useStyles = makeStyles({
   listRoot: {
     border: "none",
   },
-  listButton: {
-    borderBottom: "1px solid #e0e0e0",
-  },
 });
 
 interface Props {
-  updateCurrentView: (newView: Views) => void;
+  readonly updateCurrentView: (newView: Views) => void;
 }
 
 const Settings = ({ updateCurrentView }: Props): JSX.Element => {
@@ -22,42 +19,14 @@ const Settings = ({ updateCurrentView }: Props): JSX.Element => {
   const listClasses = {
     root: classes.listRoot,
   };
-  const listItemClasses = {
-    button: classes.listButton,
-  };
-
-  const goToNetworks = (): void => updateCurrentView(Views.Networks);
-  const goToAbout = (): void => updateCurrentView(Views.About);
-  const goToRecoveryWords = (): void => updateCurrentView(Views.RecoveryWords);
-  const goToDeleteWallet = (): void => updateCurrentView(Views.DeleteWallet);
 
   return (
     <Block width="100%">
       <List component="nav" classes={listClasses}>
-        <ListItem button key={Views.Networks} onClick={goToNetworks} classes={listItemClasses}>
-          <Block width="100%" display="flex" alignItems="center" justifyContent="space-between">
-            <ListItemText primary={Views.Networks} />
-            <Image alt="Right Chevron" src={greenRightChevron} />
-          </Block>
-        </ListItem>
-        <ListItem button key={Views.About} onClick={goToAbout} classes={listItemClasses}>
-          <Block width="100%" display="flex" alignItems="center" justifyContent="space-between">
-            <ListItemText primary={Views.About} />
-            <Image alt="Right Chevron" src={greenRightChevron} />
-          </Block>
-        </ListItem>
-        <ListItem button key={Views.RecoveryWords} onClick={goToRecoveryWords} classes={listItemClasses}>
-          <Block width="100%" display="flex" alignItems="center" justifyContent="space-between">
-            <ListItemText primary={Views.RecoveryWords} />
-            <Image alt="Right Chevron" src={greenRightChevron} />
-          </Block>
-        </ListItem>
-        <ListItem button key={Views.DeleteWallet} onClick={goToDeleteWallet} classes={listItemClasses}>
-          <Block width="100%" display="flex" alignItems="center" justifyContent="space-between">
-            <ListItemText primary={Views.DeleteWallet} />
-            <Image alt="Right Chevron" src={greenRightChevron} />
-          </Block>
-        </ListItem>
+        <ListNavView view={Views.Networks} updateCurrentView={updateCurrentView} />
+        <ListNavView view={Views.About} updateCurrentView={updateCurrentView} />
+        <ListNavView view={Views.RecoveryWords} updateCurrentView={updateCurrentView} />
+        <ListNavView view={Views.DeleteWallet} updateCurrentView={updateCurrentView} />
       </List>
     </Block>
   );
