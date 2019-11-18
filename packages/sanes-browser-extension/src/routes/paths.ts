fb2141823b287f31f7d76902e9ba1032e231c9cb
abefernan <abel.fernandez.nandin@gmail.com>
2019-11-18 03:16:13 +0100

    Removes no longer needed routes

--- packages/sanes-browser-extension/src/routes/paths.ts
+++ packages/sanes-browser-extension/src/routes/paths.ts
@@ -1,14 +1,10 @@
 export const WELCOME_ROUTE = "/welcome";
 export const CREATE_WALLET_ROUTE = "/create-wallet";
 export const UNLOCK_ROUTE = "/unlock";
-export const DELETE_WALLET_ROUTE = "/delete-wallet";
-export const RECOVERY_WORDS_ROUTE = "/recovery-words";
 export const RESTORE_WALLET = "/restore-wallet";
-export const SHARE_IDENTITY = "/share-identity";
-export const TX_REQUEST = "/tx-request";
 export const WALLET_STATUS_ROUTE = "/wallet";
-export const REQUEST_ROUTE = "/requests";
 export const TERMS_URL = "https://support.iov.one/hc/en-us";
+export const SUPPORT_CENTER_URL = "https://support.iov.one/hc/en-us/requests/new?ticket_form_id=360000387040";
 
 export function initialUrl(personaLoaded: boolean, hasPersonaStored: boolean): string {
   if (personaLoaded) {
--- packages/sanes-browser-extension/src/utils/test/navigation.ts
+++ packages/sanes-browser-extension/src/utils/test/navigation.ts
@@ -5,17 +5,11 @@ import { GetPersonaResponse } from "../../extension/background/model/backgrounds
 import { Request } from "../../extension/background/model/requestsHandler/requestQueueManager";
 import {
   CREATE_WALLET_ROUTE,
-  DELETE_WALLET_ROUTE,
-  RECOVERY_WORDS_ROUTE,
-  REQUEST_ROUTE,
   RESTORE_WALLET,
-  SHARE_IDENTITY,
-  TX_REQUEST,
   UNLOCK_ROUTE,
   WALLET_STATUS_ROUTE,
   WELCOME_ROUTE,
 } from "../../routes/paths";
-import { REQUEST_FIELD } from "../../routes/requests/components/RequestList";
 import { history } from "../../utils/history";
 import { createDom } from "../../utils/test/dom";
 
@@ -33,12 +27,6 @@ export const travelTo = async (
   TestUtils.act(() => {
     history.push({
       pathname: route,
-      state:
-        requests && requests.length > 0
-          ? {
-              [REQUEST_FIELD]: requests[0].id,
-            }
-          : null,
     });
   });
 
@@ -59,34 +47,10 @@ export const travelToUnlock = async (): Promise<React.Component> => {
   return travelTo(UNLOCK_ROUTE);
 };
 
-export const travelToRecoveryWords = async (persona?: GetPersonaResponse): Promise<React.Component> => {
-  return travelTo(RECOVERY_WORDS_ROUTE, [], persona);
-};
-
 export const travelToRestoreWallet = async (): Promise<React.Component> => {
   return travelTo(RESTORE_WALLET);
 };
 
-export const travelToDeleteWallet = async (persona?: GetPersonaResponse): Promise<React.Component> => {
-  return travelTo(DELETE_WALLET_ROUTE, [], persona);
-};
-
-export const travelToShareIdentity = async (requests: readonly Request[]): Promise<React.Component> => {
-  expect(requests.length).toBeGreaterThanOrEqual(1);
-
-  return travelTo(SHARE_IDENTITY, requests);
-};
-
-export const travelToTXRequest = async (requests: readonly Request[]): Promise<React.Component> => {
-  expect(requests.length).toBeGreaterThanOrEqual(1);
-
-  return travelTo(TX_REQUEST, requests);
-};
-
 export const travelToWallet = async (persona?: GetPersonaResponse): Promise<React.Component> => {
   return travelTo(WALLET_STATUS_ROUTE, [], persona);
-};
-
-export const travelToRequests = async (requests?: readonly Request[]): Promise<React.Component> => {
-  return travelTo(REQUEST_ROUTE, requests);
 };
