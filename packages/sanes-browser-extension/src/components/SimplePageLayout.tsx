88aaa84f33eea54abfe51fb5c6f550a131ee3c75
abefernan <abel.fernandez.nandin@gmail.com>
2019-11-29 10:08:07 +0100

    Adds height and overflow to SimplePageLayout

--- packages/sanes-browser-extension/src/components/SimplePageLayout.tsx
+++ packages/sanes-browser-extension/src/components/SimplePageLayout.tsx
@@ -1,22 +1,32 @@
 import { Block, makeStyles } from "medulas-react-components";
 import * as React from "react";
 
+export const defaultPageHeight = 500;
+
 const useStyles = makeStyles({
   root: {
     backgroundColor: "#f5f7f9",
+    overflow: "auto",
   },
 });
 
 interface Props {
   readonly id?: string;
   readonly children: React.ReactNode;
+  readonly height?: string | number;
 }
 
-const SimplePageLayout = ({ id, children }: Props): JSX.Element => {
+const SimplePageLayout = ({ id, children, height }: Props): JSX.Element => {
   const classes = useStyles();
 
   return (
-    <Block id={id} display="flex" flexDirection="column" minHeight="100%" className={classes.root}>
+    <Block
+      id={id}
+      height={height || defaultPageHeight}
+      display="flex"
+      flexDirection="column"
+      className={classes.root}
+    >
       {children}
     </Block>
   );
