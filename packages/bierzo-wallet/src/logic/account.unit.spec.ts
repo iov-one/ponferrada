b2b2edf2b2d577a79711fbc7434948f0a41985a7
abefernan <abel.fernandez.nandin@gmail.com>
2019-12-04 09:47:49 +0100

    Fix unit test

--- packages/bierzo-wallet/src/logic/account.unit.spec.ts
+++ packages/bierzo-wallet/src/logic/account.unit.spec.ts
@@ -22,7 +22,10 @@ describe("Logic :: account", () => {
     });
 
     it("should return personalized address not found ", async () => {
-      const lookupResult = await lookupRecipientAddressByName("test1*iov", "ethereum-eip155-5777" as ChainId);
+      const lookupResult = await lookupRecipientAddressByName(
+        "example1*iov",
+        "ethereum-eip155-5777" as ChainId,
+      );
 
       expect(lookupResult).toBe("name_not_found");
     });
