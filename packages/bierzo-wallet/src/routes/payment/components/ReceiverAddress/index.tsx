5df4304773d2d98d5044b0b383a8c7fbfe8db2ed
abefernan <abel.fernandez.nandin@gmail.com>
2019-12-05 13:30:19 +0100

    Prepares ReceiverAddress for future cert validation

--- packages/bierzo-wallet/src/routes/payment/components/ReceiverAddress/index.tsx
+++ packages/bierzo-wallet/src/routes/payment/components/ReceiverAddress/index.tsx
@@ -4,28 +4,44 @@ import {
   Block,
   composeValidators,
   FieldInputValue,
+  Image,
   required,
   TextField,
   Typography,
 } from "medulas-react-components";
-import React from "react";
+import React, { useState } from "react";
 
+import tickIcon from "../../../../assets/greenTick.svg";
 import { isIov } from "../../../../logic/account";
 
 export const ADDRESS_FIELD = "addressField";
+
+// NOTE always returns invalid while cert validation not implemented
+const isCertValid = (_address: string): boolean => false;
 
 interface Props {
   readonly form: FormApi;
   readonly selectedChainCodec: TxCodec | null;
   readonly noBalance: boolean;
+  // NOTE prop for storybook testing while cert validation not implemented
+  readonly initialHasValidCert?: boolean;
 }
 
-const ReceiverAddress = ({ form, selectedChainCodec, noBalance }: Props): JSX.Element => {
+const ReceiverAddress = ({
+  form,
+  selectedChainCodec,
+  noBalance,
+  initialHasValidCert,
+}: Props): JSX.Element => {
+  const [addressHasValidCert, setAddressHasValidCert] = useState(initialHasValidCert);
+
   const recipientValidator = React.useMemo(() => {
     const validator: FieldValidator<FieldInputValue> = (value): string | undefined => {
       if (typeof value !== "string") throw new Error("Input must be a string");
 
       if (isIov(value) || (selectedChainCodec && selectedChainCodec.isValidAddress(value))) {
+        setAddressHasValidCert(isCertValid(value));
+
         return undefined;
       } else {
         return "Must be an IOV human readable address or a native address";
@@ -41,9 +57,20 @@ const ReceiverAddress = ({ form, selectedChainCodec, noBalance }: Props): JSX.El
 
   return (
     <Block width="100%" marginTop={4} display="flex" flexDirection="column">
-      <Typography color="textPrimary" variant="subtitle2">
-        To
-      </Typography>
+      <Block display="inline-flex" alignItems="center">
+        <Typography color="textPrimary" variant="subtitle2">
+          To
+        </Typography>
+        {addressHasValidCert && (
+          <Block marginLeft="5px" display="inline-flex" alignItems="center">
+            <Typography color="primary" variant="subtitle2">
+              {" "}
+              verified address
+            </Typography>
+            <Image height={14} alt="Green Tick" src={tickIcon} />
+          </Block>
+        )}
+      </Block>
       <Block width="100%" marginTop={1}>
         <TextField
           name={ADDRESS_FIELD}
