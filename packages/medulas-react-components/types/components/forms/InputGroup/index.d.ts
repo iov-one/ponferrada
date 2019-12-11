0e4aa222c0ff7fbf6392786eb7b49b09cc766300
Albert Andrejev <albert.andrejev@gmail.com>
2019-12-11 22:13:18 +0200

    Add InputGroup definition

---
+++ packages/medulas-react-components/types/components/forms/InputGroup/index.d.ts
@@ -0,0 +1,7 @@
+import * as React from "react";
+interface Props {
+  readonly children?: React.ReactNode;
+  readonly prepend?: React.ReactNode;
+}
+declare const InputGroup: ({ children, prepend }: Props) => JSX.Element;
+export default InputGroup;
