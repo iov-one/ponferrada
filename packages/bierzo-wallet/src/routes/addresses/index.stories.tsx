1774bc2529b0f13bb8a59b09cfc0662ab1cacd3b
Albert Andrejev <albert.andrejev@gmail.com>
2020-02-19 23:35:21 +0200

    Add ManageNames component

--- packages/bierzo-wallet/src/routes/addresses/index.stories.tsx
+++ packages/bierzo-wallet/src/routes/addresses/index.stories.tsx
@@ -12,6 +12,7 @@ import {
 import { BwUsernameWithChainName } from ".";
 import AddressesTab from "./components/AddressesTab";
 import Iovnames from "./components/Iovnames";
+import ManageName from "./components/ManageName";
 import UserAddresses from "./components/UserAddresses";
 
 const chainAddresses: ChainAddressPairWithName[] = [
@@ -99,6 +100,14 @@ storiesOf(`${bierzoRoot}/Addresses`, module)
         usernames={usernames}
         onRegisterUsername={linkTo(REGISTER_USERNAME_STORY_PATH, REGISTER_USERNAME_REGISTRATION_STORY_PATH)}
         rpcEndpointType="extension"
+      />
+    </DecoratedStorybook>
+  ))
+  .add("Manage names", () => (
+    <DecoratedStorybook>
+      <ManageName
+        account={usernames[0]}
+        onRegisterUsername={linkTo(REGISTER_USERNAME_STORY_PATH, REGISTER_USERNAME_REGISTRATION_STORY_PATH)}
       />
     </DecoratedStorybook>
   ));
