49e1c089af4d7bc335947475e97e3532d4c6319b
Albert Andrejev <albert.andrejev@gmail.com>
2020-02-20 18:54:30 +0200

    Refactor transaction assets

--- packages/bierzo-wallet/src/logic/transactions/types/BwSendTransaction/ui/SendTxHeader/index.tsx
+++ packages/bierzo-wallet/src/logic/transactions/types/BwSendTransaction/ui/SendTxHeader/index.tsx
@@ -9,8 +9,8 @@ import { history } from "../../../../../../routes";
 import { PAYMENT_ROUTE } from "../../../../../../routes/paths";
 import { ProcessedSendTransaction } from "../../../../../../store/notifications";
 import { itemBackground } from "../../../../../../theme/css";
+import sendTx from "../../../../assets/transactionSend.svg";
 import receiveTx from "../assets/transactionReceive.svg";
-import sendTx from "../assets/transactionSend.svg";
 import Msg from "./MsgSuccess";
 
 interface ItemProps {
