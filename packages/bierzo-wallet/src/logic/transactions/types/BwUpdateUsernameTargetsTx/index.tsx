995a5f9a707f7643ecc1a72ca52a39a6a5dd663a
Albert Andrejev <albert.andrejev@gmail.com>
2020-02-20 18:56:42 +0200

    Refactor parse method

--- packages/bierzo-wallet/src/logic/transactions/types/BwUpdateUsernameTargetsTx/index.tsx
+++ packages/bierzo-wallet/src/logic/transactions/types/BwUpdateUsernameTargetsTx/index.tsx
@@ -1,4 +1,3 @@
-import { Address, BlockchainConnection, ConfirmedTransaction } from "@iov/bcp";
 import { UpdateTargetsOfUsernameTx } from "@iov/bns";
 import * as React from "react";
 
@@ -7,21 +6,6 @@ import TransactionHeader from "./ui/TransactionHeader";
 import TransactionRow from "./ui/TransactionRow";
 
 export class BwUpdateUsernameTargetParser extends BwParser<UpdateTargetsOfUsernameTx> {
-  public async parse(
-    conn: BlockchainConnection,
-    trans: ConfirmedTransaction<UpdateTargetsOfUsernameTx>,
-    _: Address,
-  ): Promise<ProcessedTx<UpdateTargetsOfUsernameTx>> {
-    const header = await conn.getBlockHeader(trans.height);
-    const time = header.time;
-
-    return {
-      id: trans.transactionId,
-      time,
-      original: trans.transaction,
-    };
-  }
-
   public graphicalRepresentation(tx: ProcessedTx<UpdateTargetsOfUsernameTx>): JSX.Element {
     return <TransactionRow key={tx.id} tx={tx} />;
   }
