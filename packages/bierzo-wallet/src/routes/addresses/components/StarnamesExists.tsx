7f5797727a2534cabfa5cc9497ef83e9dee5eb48
abefernan <abel.fernandez.nandin@gmail.com>
2020-02-26 22:41:10 +0100

    Sorts starnames

--- packages/bierzo-wallet/src/routes/addresses/components/StarnamesExists.tsx
+++ packages/bierzo-wallet/src/routes/addresses/components/StarnamesExists.tsx
@@ -47,47 +47,54 @@ function StarnamesExists({ starnames, onRegisterStarname }: Props): JSX.Element 
           </Typography>
         </Block>
       </Paper>
-      {starnames.map(starname => {
-        const onManage = (): void => {
-          history.push(REGISTER_STARNAME_ROUTE, starname);
-        };
+      {starnames
+        .slice()
+        .sort((a, b) => {
+          if (`${a.name}*${a.domain}` < `${b.name}*${b.domain}`) return -1;
+          if (`${a.name}*${a.domain}` > `${b.name}*${b.domain}`) return 1;
+          return 0;
+        })
+        .map(starname => {
+          const onManage = (): void => {
+            history.push(REGISTER_STARNAME_ROUTE, starname);
+          };
 
-        return (
-          <React.Fragment key={`${starname.name}*${starname.domain}`}>
-            <Block marginTop={3} />
-            <Paper classes={paperClasses}>
-              <Block
-                width={650}
-                padding={5}
-                border="1px solid #F3F3F3"
-                display="flex"
-                alignItems="center"
-                justifyContent="space-between"
-              >
-                <Block display="flex" flexDirection="column">
-                  <Typography variant="h5" weight="semibold">
-                    {`${starname.name}*${starname.domain}`}
-                  </Typography>
-                  <Block marginTop={2} />
-                  <Typography variant="subtitle2" weight="semibold" color="textPrimary">
-                    Expires on {starname.expiryDate.toLocaleDateString()}
+          return (
+            <React.Fragment key={`${starname.name}*${starname.domain}`}>
+              <Block marginTop={3} />
+              <Paper classes={paperClasses}>
+                <Block
+                  width={650}
+                  padding={5}
+                  border="1px solid #F3F3F3"
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="space-between"
+                >
+                  <Block display="flex" flexDirection="column">
+                    <Typography variant="h5" weight="semibold">
+                      {`${starname.name}*${starname.domain}`}
+                    </Typography>
+                    <Block marginTop={2} />
+                    <Typography variant="subtitle2" weight="semibold" color="textPrimary">
+                      Expires on {starname.expiryDate.toLocaleDateString()}
+                    </Typography>
+                  </Block>
+                  <Typography
+                    variant="subtitle2"
+                    weight="semibold"
+                    inline
+                    link
+                    color="primary"
+                    onClick={onManage}
+                  >
+                    Manage
                   </Typography>
                 </Block>
-                <Typography
-                  variant="subtitle2"
-                  weight="semibold"
-                  inline
-                  link
-                  color="primary"
-                  onClick={onManage}
-                >
-                  Manage
-                </Typography>
-              </Block>
-            </Paper>
-          </React.Fragment>
-        );
-      })}
+              </Paper>
+            </React.Fragment>
+          );
+        })}
     </React.Fragment>
   );
 }
