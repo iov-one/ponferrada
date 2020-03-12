ed4cec1a1ebd15799d19851d8c801888c5a0572d
Albert Andrejev <albert.andrejev@gmail.com>
2020-03-13 00:29:25 +0200

    Add associated with starname accounts list

--- packages/bierzo-wallet/src/routes/account/manage/index.stories.tsx
+++ packages/bierzo-wallet/src/routes/account/manage/index.stories.tsx
@@ -15,6 +15,7 @@ import {
   REGISTER_IOVNAME_REGISTRATION_STORY_PATH,
   REGISTER_IOVNAME_STORY_PATH,
 } from "../register/index.stories";
+import AssociatedNamesList from "./components/AssociatedNamesList";
 
 const chainAddresses: ChainAddressPairWithName[] = [
   {
@@ -40,6 +41,27 @@ const account: BwAccountWithChainName = {
   expiryDate: new Date(),
   addresses: [chainAddresses[0], chainAddresses[1]],
 };
+
+const names: BwAccountWithChainName[] = [
+  {
+    name: "test1",
+    domain: "iov",
+    expiryDate: new Date(),
+    addresses: [chainAddresses[0], chainAddresses[1]],
+  },
+  {
+    name: "test2",
+    domain: "iov",
+    expiryDate: new Date(),
+    addresses: [chainAddresses[0], chainAddresses[1]],
+  },
+  {
+    name: "test3",
+    domain: "iov",
+    expiryDate: new Date(),
+    addresses: [chainAddresses[0], chainAddresses[1]],
+  },
+];
 
 const username: BwUsernameWithChainName = {
   username: "test2*iov",
@@ -86,6 +108,16 @@ storiesOf(`${bierzoRoot}/Account Manage`, module)
         onEditAccount={linkTo(REGISTER_IOVNAME_STORY_PATH, REGISTER_IOVNAME_REGISTRATION_STORY_PATH)}
         account={username}
         menuItems={menuItems}
+      />
+    </DecoratedStorybook>
+  ))
+  .add("Associated names list", () => (
+    <DecoratedStorybook>
+      <AssociatedNamesList
+        names={names}
+        onRegisterName={() => {
+          action("Register Name")();
+        }}
       />
     </DecoratedStorybook>
   ));
