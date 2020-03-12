6c55773e9bd95ad8478e92e9597443bb8566ad59
Albert Andrejev <albert.andrejev@gmail.com>
2020-03-13 00:30:38 +0200

    Add starname manage form

---
+++ packages/bierzo-wallet/src/routes/account/manage/components/StarnameForm.tsx
@@ -0,0 +1,46 @@
+import { ActionMenuItem } from "medulas-react-components";
+import * as React from "react";
+
+import { history } from "../../..";
+import AccountManage, { BwUsernameWithChainName } from "../../../../components/AccountManage";
+import { REGISTER_IOVNAME_ROUTE, REGISTER_STARNAME_ROUTE } from "../../../paths";
+import AssociatedNamesList from "./AssociatedNamesList";
+
+function onRegisterUsername(): void {
+  history.push(REGISTER_IOVNAME_ROUTE);
+}
+
+function onRegisterStarname(): void {
+  history.push(REGISTER_STARNAME_ROUTE);
+}
+
+const menuItems: readonly ActionMenuItem[] = [
+  { title: "Renew", action: () => console.log("Renew") },
+  { title: "Transfer iovname", action: () => console.log("Transfer starname") },
+  { title: "Delete iovname", action: () => console.log("Delete starname") },
+];
+
+const StarnameAccountManage = (): JSX.Element => {
+  const addressToManage: BwUsernameWithChainName | undefined = history.location.state;
+
+  if (!addressToManage) {
+    throw new Error("No address to manage provided, something wrong.");
+  }
+
+  const onEditAccount = (): void => {
+    history.push(REGISTER_IOVNAME_ROUTE, addressToManage);
+  };
+
+  const onRegisterName = (): void => {
+    history.push(REGISTER_IOVNAME_ROUTE, addressToManage);
+  };
+
+  return (
+    <React.Fragment>
+      <AccountManage menuItems={menuItems} onEditAccount={onEditAccount} account={addressToManage} />
+      <AssociatedNamesList names={[]} onRegisterName={onRegisterName} />
+    </React.Fragment>
+  );
+};
+
+export default StarnameAccountManage;
