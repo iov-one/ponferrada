f610f109f2f436bc580a07a6f79c5669f231261f
Albert Andrejev <albert.andrejev@gmail.com>
2020-03-13 19:36:24 +0200

    Manage starname account

--- packages/bierzo-wallet/src/routes/account/manage/components/StarnameForm.tsx
+++ packages/bierzo-wallet/src/routes/account/manage/components/StarnameForm.tsx
@@ -2,43 +2,71 @@ import { ActionMenuItem } from "medulas-react-components";
 import * as React from "react";
 
 import { history } from "../../..";
-import AccountManage, { BwUsernameWithChainName } from "../../../../components/AccountManage";
-import { IOVNAME_REGISTER_ROUTE, STARNAME_REGISTER_ROUTE } from "../../../paths";
+import AccountManage, { BwAccountWithChainName } from "../../../../components/AccountManage";
+import { getChainName } from "../../../../config";
+import { getConnectionForBns } from "../../../../logic/connection";
+import { NAME_EDIT_ROUTE, NAME_REGISTER_ROUTE } from "../../../paths";
 import AssociatedNamesList from "./AssociatedNamesList";
-
-function onRegisterUsername(): void {
-  history.push(IOVNAME_REGISTER_ROUTE);
-}
-
-function onRegisterStarname(): void {
-  history.push(STARNAME_REGISTER_ROUTE);
-}
 
 const menuItems: readonly ActionMenuItem[] = [
   { title: "Renew", action: () => console.log("Renew") },
-  { title: "Transfer iovname", action: () => console.log("Transfer starname") },
-  { title: "Delete iovname", action: () => console.log("Delete starname") },
+  { title: "Transfer starname", action: () => console.log("Transfer starname") },
+  { title: "Delete starname", action: () => console.log("Delete starname") },
 ];
 
 const StarnameAccountManage = (): JSX.Element => {
-  const addressToManage: BwUsernameWithChainName | undefined = history.location.state;
+  const [domainAccounts, setDomainAccounts] = React.useState<BwAccountWithChainName[]>([]);
+  const account: BwAccountWithChainName = history.location.state;
 
-  if (!addressToManage) {
-    throw new Error("No address to manage provided, something wrong.");
-  }
+  React.useEffect(() => {
+    let isSubscribed = true;
+    async function getDomainAccounts(): Promise<void> {
+      const connection = await getConnectionForBns();
+      const accountsNft = await connection.getAccounts({ domain: account.domain });
+      if (isSubscribed) {
+        const bwAccountsWithChain: BwAccountWithChainName[] = await Promise.all(
+          accountsNft
+            .filter(nft => nft.name)
+            .map(async nft => {
+              return {
+                name: nft.name ? nft.name : "",
+                domain: nft.domain,
+                expiryDate: new Date(nft.validUntil * 1000),
+                addresses: await Promise.all(
+                  nft.targets.map(async address => {
+                    return {
+                      chainId: address.chainId,
+                      address: address.address,
+                      chainName: await getChainName(address.chainId),
+                    };
+                  }),
+                ),
+              };
+            }),
+        );
 
-  const onEditAccount = (): void => {
-    history.push(IOVNAME_REGISTER_ROUTE, addressToManage);
+        setDomainAccounts(bwAccountsWithChain);
+      }
+    }
+    getDomainAccounts();
+
+    return () => {
+      isSubscribed = false;
+    };
+  }, [account]);
+
+  const onEdit = (): void => {
+    history.push(NAME_EDIT_ROUTE, account);
   };
 
   const onRegisterName = (): void => {
-    history.push(IOVNAME_REGISTER_ROUTE, addressToManage);
+    history.push(NAME_REGISTER_ROUTE);
   };
 
   return (
     <React.Fragment>
-      <AccountManage menuItems={menuItems} onEditAccount={onEditAccount} account={addressToManage} />
-      <AssociatedNamesList names={[]} onRegisterName={onRegisterName} />
+      <AccountManage menuItems={menuItems} onEdit={onEdit} account={account} />
+      <AssociatedNamesList names={domainAccounts} onRegisterName={onRegisterName} />
     </React.Fragment>
   );
 };
