0c164c4d803ae42d24ff02bddb216856cd119a8f
Albert Andrejev <albert.andrejev@gmail.com>
2020-03-18 16:46:18 +0200

    Add iovname transfer action

--- packages/bierzo-wallet/src/routes/account/manage/components/IovnameForm.tsx
+++ packages/bierzo-wallet/src/routes/account/manage/components/IovnameForm.tsx
@@ -3,19 +3,19 @@ import * as React from "react";
 
 import { history } from "../../..";
 import AccountManage, { BwUsernameWithChainName } from "../../../../components/AccountManage";
-import { IOVNAME_EDIT_ROUTE } from "../../../paths";
-
-const menuItems: readonly ActionMenuItem[] = [
-  // eslint-disable-next-line no-console
-  { title: "Renew", action: () => console.log("Renew") },
-  // eslint-disable-next-line no-console
-  { title: "Transfer iovname", action: () => console.log("Transfer iovname") },
-  // eslint-disable-next-line no-console
-  { title: "Delete iovname", action: () => console.log("Delete iovname") },
-];
+import { IOVNAME_EDIT_ROUTE, IOVNAME_TRANSFER_ROUTE } from "../../../paths";
 
 const IovnameAccountManage = (): JSX.Element => {
   const account: BwUsernameWithChainName = history.location.state;
+
+  const menuItems: readonly ActionMenuItem[] = [
+    // eslint-disable-next-line no-console
+    { title: "Renew", action: () => console.log("Renew") },
+    // eslint-disable-next-line no-console
+    { title: "Transfer iovname", action: () => history.push(IOVNAME_TRANSFER_ROUTE, account) },
+    // eslint-disable-next-line no-console
+    { title: "Delete iovname", action: () => console.log("Delete iovname") },
+  ];
 
   const onEdit = (): void => {
     history.push(IOVNAME_EDIT_ROUTE, account);
