09b48c5d3e485c8724a5da5ec761b399dce9e250
Albert Andrejev <albert.andrejev@gmail.com>
2020-03-19 20:35:32 +0200

    Adopt stories to new component

--- packages/bierzo-wallet/src/components/AccountTransfer/index.stories.tsx
+++ packages/bierzo-wallet/src/components/AccountTransfer/index.stories.tsx
@@ -1,9 +1,13 @@
-import { Address, ChainId, Token, TokenTicker } from "@iov/bcp";
+import { Address, Algorithm, ChainId, Identity, PubkeyBytes, Token, TokenTicker } from "@iov/bcp";
+import { JsonRpcRequest } from "@iov/jsonrpc";
 import { action } from "@storybook/addon-actions";
 import { storiesOf } from "@storybook/react";
+import { Typography } from "medulas-react-components";
 import React from "react";
 import { stringToAmount } from "ui-logic";
 
+import { extensionRpcEndpoint } from "../../communication/extensionRpcEndpoint";
+import { generateTransferDomainTxRequest } from "../../communication/requestgenerators";
 import { ChainAddressPairWithName } from "../../components/AddressesTable";
 import DecoratedStorybook, { bierzoRoot } from "../../utils/storybook";
 import { BwAccountWithChainName } from "../AccountManage";
@@ -42,6 +46,14 @@ const iov: Pick<Token, "tokenTicker" | "fractionalDigits"> = {
   tokenTicker: "IOV" as TokenTicker,
 };
 
+const bnsIdentity: Identity = {
+  chainId: "local-iov-devnet" as ChainId,
+  pubkey: {
+    algo: Algorithm.Ed25519,
+    data: new Uint8Array([]) as PubkeyBytes,
+  },
+};
+
 storiesOf(ACCOUNT_TRANSFER_STORY_PATH, module)
   .addParameters({ viewport: { defaultViewport: "responsive" } })
   .add(ACCOUNT_TRANSFER_SAMPLE_STORY_PATH, () => (
@@ -49,14 +61,22 @@ storiesOf(ACCOUNT_TRANSFER_STORY_PATH, module)
       <AccountTransfer
         id="account-transfer-id"
         account={account}
-        onTransfer={async (values: object): Promise<void> => {
-          action("Transfer")(values);
+        getRequest={async (newOwner: Address): Promise<JsonRpcRequest> => {
+          action("getRequest")(newOwner);
+          return await generateTransferDomainTxRequest(bnsIdentity, account.domain, newOwner);
         }}
         onCancel={action("Transfer cancel")}
         getFee={async newOwner => {
           action("get fee")(newOwner);
           return { tokens: stringToAmount("5", iov) };
         }}
-      />
+        bnsChainId={"local-iov-devnet" as ChainId}
+        rpcEndpoint={extensionRpcEndpoint}
+        setTransactionId={value => {
+          action("setTransactionId")(value);
+        }}
+      >
+        <Typography>Some additional description</Typography>
+      </AccountTransfer>
     </DecoratedStorybook>
   ));
--- packages/bierzo-wallet/src/routes/account/transfer/index.stories.tsx
+++ packages/bierzo-wallet/src/routes/account/transfer/index.stories.tsx
@@ -1,10 +1,14 @@
-import { Address, ChainId, Token, TokenTicker } from "@iov/bcp";
+import { Address, Algorithm, ChainId, Identity, PubkeyBytes, Token, TokenTicker } from "@iov/bcp";
+import { JsonRpcRequest } from "@iov/jsonrpc";
 import { action } from "@storybook/addon-actions";
 import { linkTo } from "@storybook/addon-links";
 import { storiesOf } from "@storybook/react";
+import { Typography } from "medulas-react-components";
 import React from "react";
 import { stringToAmount } from "ui-logic";
 
+import { extensionRpcEndpoint } from "../../../communication/extensionRpcEndpoint";
+import { generateTransferUsernameTxRequest } from "../../../communication/requestgenerators";
 import { BwUsernameWithChainName } from "../../../components/AccountManage";
 import { ACCOUNT_MANAGE_STORY_PATH } from "../../../components/AccountManage/index.stories";
 import AccountTransfer from "../../../components/AccountTransfer";
@@ -41,6 +45,14 @@ const iov: Pick<Token, "tokenTicker" | "fractionalDigits"> = {
   tokenTicker: "IOV" as TokenTicker,
 };
 
+const bnsIdentity: Identity = {
+  chainId: "local-iov-devnet" as ChainId,
+  pubkey: {
+    algo: Algorithm.Ed25519,
+    data: new Uint8Array([]) as PubkeyBytes,
+  },
+};
+
 export const ACCOUNT_TRANSFER_IOVNAMES_STORY_PATH = "Transfer iovname";
 
 storiesOf(ACCOUNT_TRANSFER_STORY_PATH, module)
@@ -50,14 +62,22 @@ storiesOf(ACCOUNT_TRANSFER_STORY_PATH, module)
       <AccountTransfer
         id="account-transfer-id"
         account={account}
-        onTransfer={async (values: object): Promise<void> => {
-          action("Transfer")(values);
+        getRequest={async (newOwner: Address): Promise<JsonRpcRequest> => {
+          action("getRequest")(newOwner);
+          return await generateTransferUsernameTxRequest(bnsIdentity, account.username, newOwner);
         }}
         onCancel={linkTo(ACCOUNT_MANAGE_STORY_PATH, ACCOUNT_MANAGE_IOVNAMES_STORY_PATH)}
         getFee={async newOwner => {
           action("get fee")(newOwner);
           return { tokens: stringToAmount("5", iov) };
         }}
-      />
+        bnsChainId={"local-iov-devnet" as ChainId}
+        rpcEndpoint={extensionRpcEndpoint}
+        setTransactionId={value => {
+          action("setTransactionId")(value);
+        }}
+      >
+        <Typography>Some additional description</Typography>
+      </AccountTransfer>
     </DecoratedStorybook>
   ));
