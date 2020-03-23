4ca1a6c6707b1961ec3d8002ae80dbe982b13f76
Albert Andrejev <albert.andrejev@gmail.com>
2020-03-23 23:15:54 +0200

    Add AccountOperation common component

---
+++ packages/bierzo-wallet/src/components/AccountOperation/index.stories.tsx
@@ -0,0 +1,102 @@
+import { Address, Algorithm, ChainId, Identity, PubkeyBytes, Token, TokenTicker } from "@iov/bcp";
+import { JsonRpcRequest } from "@iov/jsonrpc";
+import { action } from "@storybook/addon-actions";
+import { storiesOf } from "@storybook/react";
+import { FormValues, Typography } from "medulas-react-components";
+import React from "react";
+import { stringToAmount } from "ui-logic";
+
+import { extensionRpcEndpoint } from "../../communication/extensionRpcEndpoint";
+import { generateTransferDomainTxRequest } from "../../communication/requestgenerators";
+import { ChainAddressPairWithName } from "../../components/AddressesTable";
+import DecoratedStorybook, { bierzoRoot } from "../../utils/storybook";
+import { BwAccountWithChainName } from "../AccountManage";
+import AccountOperation, { RECEPIENT_ADDRESS } from ".";
+
+const chainAddresses: ChainAddressPairWithName[] = [
+  {
+    chainId: "local-iov-devnet" as ChainId,
+    address: "tiov1dcg3fat5zrvw00xezzjk3jgedm7pg70y222af3" as Address,
+    chainName: "IOV Devnet",
+  },
+  {
+    chainId: "lisk-198f2b61a8" as ChainId,
+    address: "1349293588603668134L" as Address,
+    chainName: "Lisk Devnet",
+  },
+  {
+    chainId: "ethereum-eip155-5777" as ChainId,
+    address: "0xD383382350F9f190Bd2608D6381B15b4e1cec0f3" as Address,
+    chainName: "Ganache",
+  },
+];
+
+const account: BwAccountWithChainName = {
+  name: "albert",
+  domain: "iov",
+  expiryDate: new Date("June 5, 2120 03:00:00"),
+  owner: "tiov1dcg3fat5zrvw00xezzjk3jgedm7pg70y222af3" as Address,
+  addresses: chainAddresses,
+};
+
+export const ACCOUNT_TRANSFER_STORY_PATH = `${bierzoRoot}/Account Transfer`;
+export const ACCOUNT_TRANSFER_SAMPLE_STORY_PATH = "Transfer sample";
+
+const iov: Pick<Token, "tokenTicker" | "fractionalDigits"> = {
+  fractionalDigits: 9,
+  tokenTicker: "IOV" as TokenTicker,
+};
+
+const bnsIdentity: Identity = {
+  chainId: "local-iov-devnet" as ChainId,
+  pubkey: {
+    algo: Algorithm.Ed25519,
+    data: new Uint8Array([]) as PubkeyBytes,
+  },
+};
+
+const Header: React.FunctionComponent = (): JSX.Element => (
+  <React.Fragment>
+    <Typography color="default" variant="h5" inline>
+      You are makeing operation with{" "}
+    </Typography>
+    <Typography color="primary" variant="h5" inline>
+      albert*iov
+    </Typography>
+    <Typography color="default" variant="h5" inline>
+      {" "}
+      account
+    </Typography>
+  </React.Fragment>
+);
+
+storiesOf(`${bierzoRoot}/Account Operation`, module)
+  .addParameters({ viewport: { defaultViewport: "responsive" } })
+  .add("Sample", () => (
+    <DecoratedStorybook>
+      <AccountOperation
+        id="account-operation-id"
+        getRequest={async (formValues: FormValues): Promise<JsonRpcRequest> => {
+          action("getRequest")(formValues);
+          return await generateTransferDomainTxRequest(
+            bnsIdentity,
+            account.domain,
+            formValues[RECEPIENT_ADDRESS] as Address,
+          );
+        }}
+        onCancel={action("Transfer cancel")}
+        getFee={async newOwner => {
+          action("get fee")(newOwner);
+          return { tokens: stringToAmount("5", iov) };
+        }}
+        bnsChainId={"local-iov-devnet" as ChainId}
+        rpcEndpoint={extensionRpcEndpoint}
+        setTransactionId={value => {
+          action("setTransactionId")(value);
+        }}
+        header={<Header />}
+      >
+        <Typography>Some additional description</Typography>
+      </AccountOperation>
+    </DecoratedStorybook>
+  ));
---
+++ packages/bierzo-wallet/src/components/AccountOperation/index.tsx
@@ -0,0 +1,175 @@
+import { Address, ChainId, Fee, TransactionId } from "@iov/bcp";
+import { bnsCodec } from "@iov/bns";
+import { JsonRpcRequest } from "@iov/jsonrpc";
+import { Paper, Theme } from "@material-ui/core";
+import { useTheme } from "@material-ui/styles";
+import { FormApi } from "final-form";
+import {
+  Back,
+  BillboardContext,
+  Block,
+  Button,
+  Form,
+  FormValues,
+  makeStyles,
+  ToastContext,
+  useForm,
+} from "medulas-react-components";
+import React from "react";
+import { amountToString } from "ui-logic";
+
+import { RpcEndpoint } from "../../communication/rpcEndpoint";
+import { submitTransaction } from "../../utils/transaction";
+import LedgerBillboardMessage from "../BillboardMessage/LedgerBillboardMessage";
+import NeumaBillboardMessage from "../BillboardMessage/NeumaBillboardMessage";
+
+export const RECEPIENT_ADDRESS = "account-recepient-address";
+
+const useMessagePaper = makeStyles({
+  rounded: {
+    borderRadius: "5px",
+    border: "1px solid #F3F3F3",
+  },
+  elevation1: {
+    boxShadow: "none",
+  },
+});
+
+function getTransferButtonCaption(fee: Fee | undefined): string {
+  if (fee && fee.tokens) {
+    return `Transfer for ${amountToString(fee.tokens)}`;
+  }
+
+  return "Transfer";
+}
+
+interface Props {
+  readonly id: string;
+  readonly children: React.ReactNode;
+  readonly subSection?: (form: FormApi) => React.FunctionComponent;
+  readonly header?: React.ReactNode;
+  readonly bnsChainId: ChainId;
+  readonly onCancel: () => void;
+  readonly getFee: (values: FormValues) => Promise<Fee | undefined>;
+  readonly getRequest: (values: FormValues) => Promise<JsonRpcRequest>;
+  readonly rpcEndpoint: RpcEndpoint;
+  readonly setTransactionId: React.Dispatch<React.SetStateAction<TransactionId | null>>;
+}
+
+const AccountOperation = ({
+  id,
+  onCancel,
+  getFee,
+  getRequest,
+  children,
+  rpcEndpoint,
+  setTransactionId,
+  subSection,
+  header,
+}: Props): JSX.Element => {
+  const [transferFee, setTransferFee] = React.useState<Fee | undefined>();
+  const messagePaperClasses = useMessagePaper();
+  const theme = useTheme<Theme>();
+  const billboard = React.useContext(BillboardContext);
+  const toast = React.useContext(ToastContext);
+
+  const onSubmit = async (values: object): Promise<void> => {
+    const formValues = values as FormValues;
+
+    const request = await getRequest(formValues);
+    await submitTransaction(
+      request,
+      billboard,
+      toast,
+      rpcEndpoint,
+      <NeumaBillboardMessage text={rpcEndpoint.authorizeSignAndPostMessage} />,
+      <LedgerBillboardMessage text={rpcEndpoint.authorizeSignAndPostMessage} />,
+      (transactionId: TransactionId) => setTransactionId(transactionId),
+    );
+  };
+
+  const { form, handleSubmit, invalid, pristine, submitting, values } = useForm({
+    onSubmit,
+  });
+
+  const SubSection = React.useMemo(() => {
+    if (subSection) {
+      return subSection(form);
+    }
+
+    return undefined;
+  }, [subSection, form]);
+
+  React.useEffect(() => {
+    let isSubscribed = true;
+
+    async function setFee(): Promise<void> {
+      const fee = await getFee(values as FormValues);
+      if (isSubscribed) {
+        setTransferFee(fee);
+      }
+    }
+
+    if (!invalid && values[RECEPIENT_ADDRESS]) {
+      setFee();
+    }
+
+    return () => {
+      isSubscribed = false;
+    };
+  }, [getFee, invalid, values]);
+
+  return (
+    <Block
+      id={id}
+      marginTop={5}
+      display="flex"
+      flexDirection="column"
+      alignContent="center"
+      alignItems="center"
+      bgcolor={theme.palette.background.default}
+    >
+      <Block width={650}>
+        <Paper classes={messagePaperClasses}>
+          <Block padding={5}>
+            {header}
+            <Block
+              marginTop={3}
+              padding={3}
+              bgcolor={theme.palette.background.default}
+              borderLeft="4px solid rgba(255, 189, 2, .5)"
+            >
+              {children}
+            </Block>
+          </Block>
+        </Paper>
+
+        <Block marginTop={3} />
+
+        <Form onSubmit={handleSubmit}>
+          {SubSection && <SubSection />}
+
+          <Block
+            marginTop={4}
+            marginBottom={1}
+            justifyContent="center"
+            display="flex"
+            alignItems="center"
+            flexDirection="column"
+          >
+            <Block width="75%">
+              <Button fullWidth type="submit" disabled={invalid || pristine || submitting}>
+                {getTransferButtonCaption(transferFee)}
+              </Button>
+              <Back fullWidth disabled={submitting} onClick={onCancel}>
+                Cancel
+              </Back>
+            </Block>
+          </Block>
+        </Form>
+      </Block>
+    </Block>
+  );
+};
+
+export default AccountOperation;
