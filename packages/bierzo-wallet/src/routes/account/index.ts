e1ed45e27106fb972185fad1ef267e3e00549d3e
abefernan <abel.fernandez.nandin@gmail.com>
2020-03-25 21:27:52 +0100

    Remove not shared entity type

--- packages/bierzo-wallet/src/routes/account/index.ts
+++ packages/bierzo-wallet/src/routes/account/index.ts
@@ -3,7 +3,7 @@ import AccountManage from "./manage";
 import AccountRegister from "./register";
 import AccountUpdate from "./update";
 
-export type AccountEntity = "iovname" | "starname" | "name" | "name-back";
+export type AccountEntity = "iovname" | "starname" | "name";
 
 export interface AccountProps {
   entity: AccountEntity;
