09a5bffbfe69456b8db68e899833ffbc85d51509
Albert Andrejev <albert.andrejev@gmail.com>
2020-04-08 13:04:51 +0300

    Add iovname tx support

---
+++ packages/bierzo-wallet/src/logic/transactions/types/BwTransferUsernameTx/ui/TransactionHeader/MsgSuccess.tsx
@@ -0,0 +1,22 @@
+import { Typography } from "medulas-react-components";
+import * as React from "react";
+
+interface MsgProps {
+  readonly username: string;
+}
+
+const Msg = ({ username }: MsgProps): JSX.Element => {
+  return (
+    <React.Fragment>
+      <Typography variant="body2" weight="semibold" inline>
+        {username}
+      </Typography>
+      <Typography variant="body2" inline>
+        {" "}
+        account has been transferred
+      </Typography>
+    </React.Fragment>
+  );
+};
+
+export default Msg;
