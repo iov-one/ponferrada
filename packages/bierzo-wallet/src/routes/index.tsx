60076dd65bc5caa6e81ed45a738621d0dc3f4107
abefernan <abel.fernandez.nandin@gmail.com>
2020-04-23 13:15:37 +0200

    Adds routes for starnames & iovnames and reorders

--- packages/bierzo-wallet/src/routes/index.tsx
+++ packages/bierzo-wallet/src/routes/index.tsx
@@ -7,6 +7,7 @@ import { AccountDelete, AccountManage, AccountRegister, AccountRenew, AccountUpd
 import AccountTransfer from "./account/transfer";
 import Addresses from "./addresses";
 import Balance from "./balance";
+import Iovnames from "./iovnames";
 import Login from "./login";
 import {
   ADDRESSES_ROUTE,
@@ -14,6 +15,7 @@ import {
   IOVNAME_EDIT_ROUTE,
   IOVNAME_MANAGE_ROUTE,
   IOVNAME_REGISTER_ROUTE,
+  IOVNAME_ROUTE,
   IOVNAME_TRANSFER_ROUTE,
   LOGIN_ROUTE,
   NAME_DELETE_ROUTE,
@@ -28,12 +30,14 @@ import {
   STARNAME_MANAGE_ROUTE,
   STARNAME_REGISTER_ROUTE,
   STARNAME_RENEW_ROUTE,
+  STARNAME_ROUTE,
   STARNAME_TRANSFER_ROUTE,
   TERMS_ROUTE,
   TRANSACTIONS_ROUTE,
 } from "./paths";
 import Payment from "./payment";
 import Policy from "./policy";
+import Starnames from "./starnames";
 import Terms from "./terms";
 import Transactions from "./transactions";
 
@@ -45,8 +49,15 @@ const Routes = (): JSX.Element => (
       <Route exact path="/" component={Login} />
       <Route exact path={LOGIN_ROUTE} component={Login} />
       <RequireLogin>
-        <Route exact path={PAYMENT_ROUTE} component={Payment} />
+        <Route exact path={BALANCE_ROUTE} component={Balance} />
         <Route exact path={ADDRESSES_ROUTE} component={Addresses} />
+        <Route exact path={TRANSACTIONS_ROUTE} component={Transactions} />
+        <Route exact path={PAYMENT_ROUTE} component={Payment} />
+        <Route exact path={TERMS_ROUTE} component={Terms} />
+        <Route exact path={POLICY_ROUTE} component={Policy} />
+
+        <Route exact path={STARNAME_ROUTE} component={Starnames} />
+        <Route exact path={IOVNAME_ROUTE} component={Iovnames} />
 
         <Route exact path={IOVNAME_MANAGE_ROUTE} render={() => <AccountManage entity="iovname" />} />
         <Route exact path={STARNAME_MANAGE_ROUTE} render={() => <AccountManage entity="starname" />} />
@@ -64,15 +75,10 @@ const Routes = (): JSX.Element => (
         <Route exact path={NAME_TRANSFER_ROUTE} render={() => <AccountTransfer entity="name" />} />
         <Route exact path={NAME_TRANSFER_BACK_ROUTE} render={() => <AccountTransfer entity="name-back" />} />
 
-        <Route exact path={STARNAME_DELETE_ROUTE} render={() => <AccountDelete entity="starname" />} />
-        <Route exact path={NAME_DELETE_ROUTE} render={() => <AccountDelete entity="name" />} />
-
         <Route exact path={STARNAME_RENEW_ROUTE} render={() => <AccountRenew entity="starname" />} />
 
-        <Route exact path={TRANSACTIONS_ROUTE} component={Transactions} />
-        <Route exact path={BALANCE_ROUTE} component={Balance} />
-        <Route exact path={TERMS_ROUTE} component={Terms} />
-        <Route exact path={POLICY_ROUTE} component={Policy} />
+        <Route exact path={STARNAME_DELETE_ROUTE} render={() => <AccountDelete entity="starname" />} />
+        <Route exact path={NAME_DELETE_ROUTE} render={() => <AccountDelete entity="name" />} />
       </RequireLogin>
     </Switch>
   </Router>
