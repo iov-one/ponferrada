ff23ca230761528dbfb1423fb3d4fe13cc5d2bec
abefernan <abel.fernandez.nandin@gmail.com>
2020-04-30 18:57:35 +0200

    Fix DOM tests

--- packages/bierzo-wallet/src/routes/balance/test/operateBalances.ts
+++ packages/bierzo-wallet/src/routes/balance/test/operateBalances.ts
@@ -12,12 +12,16 @@ const mainMenuH6Elements = 3;
 const numberOfTokensFromFaucet = 4;
 
 export const getNoFundsMessage = (h6Elements: Element[]): string => {
-  const index = mainMenuH6Elements + 6;
+  // NOTE disabled "starnames" tab
+  // const index = mainMenuH6Elements + 6;
+  const index = mainMenuH6Elements + 5;
   return h6Elements[index].textContent || "";
 };
 
 export const getIovUsername = (h6Elements: Element[]): string => {
-  const index = mainMenuH6Elements + 4;
+  // NOTE disabled "starnames" tab
+  // const index = mainMenuH6Elements + 4;
+  const index = mainMenuH6Elements + 3;
   return h6Elements[index].textContent || "";
 };
 
