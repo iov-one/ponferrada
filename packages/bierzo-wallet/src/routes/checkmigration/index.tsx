ae52c5c8130c708b737617a2bf00695e35849375
Antoine Herzog <antoine@iov.one>
2020-05-18 21:21:39 +0200

    fix lint

--- packages/bierzo-wallet/src/routes/checkmigration/index.tsx
+++ packages/bierzo-wallet/src/routes/checkmigration/index.tsx
@@ -1,17 +1,12 @@
 import { ChainId, Identity } from "@iov/bcp";
 import { BnsConnection } from "@iov/bns";
-import { BillboardContext, ToastContext, ToastVariant } from "medulas-react-components";
 import React from "react";
 import * as ReactRedux from "react-redux";
 
 import { history } from "..";
-import { generateUpdateUsernameTxRequest } from "../../communication/requestgenerators";
-import LedgerBillboardMessage from "../../components/BillboardMessage/LedgerBillboardMessage";
-import NeumaBillboardMessage from "../../components/BillboardMessage/NeumaBillboardMessage";
 import { getConnectionForChainId } from "../../logic/connection";
 import { ExtendedIdentity } from "../../store/identities";
-import { RootState } from "../../store/reducers";
-import { getFirstUsername, getFirstUsernameMigrated } from "../../store/usernames/selectors";
+import { getFirstUsernameMigrated } from "../../store/usernames/selectors";
 import { BALANCE_ROUTE, UPGRADE_ROUTE } from "../paths";
 
 export function getBnsIdentity(identities: ReadonlyMap<ChainId, ExtendedIdentity>): Identity | undefined {
@@ -24,10 +19,7 @@ export function getBnsIdentity(identities: ReadonlyMap<ChainId, ExtendedIdentity
 }
 
 const CheckMigration = (): JSX.Element => {
-  const tokens = ReactRedux.useSelector((state: RootState) => state.balances);
-  const bnsUsername = ReactRedux.useSelector(getFirstUsername);
   const bnsUsernameMigrated = ReactRedux.useSelector(getFirstUsernameMigrated);
-  const iovAddress = bnsUsername ? bnsUsername.username : undefined;
   const iovAddressWithNewChain = bnsUsernameMigrated ? bnsUsernameMigrated.username : undefined;
 
   if (iovAddressWithNewChain) {
