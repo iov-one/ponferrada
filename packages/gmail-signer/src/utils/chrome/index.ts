87491b9fe4519c9cf46aa64d823e667a57df932e
Antoine Herzog <antoine@iov.one>
2020-05-30 20:19:53 +0200

    fix windows of utils

--- packages/gmail-signer/src/utils/chrome/index.ts
+++ packages/gmail-signer/src/utils/chrome/index.ts
@@ -15,60 +15,60 @@ export function extensionContext(): boolean {
 }
 
 export async function getPersonaData(): Promise<GetPersonaResponse> {
-  const extensionWindow = chrome.extension.getBackgroundPage() as IovWindowExtension;
+  const extensionWindow = (window as Window) as IovWindowExtension;
   const response = await extensionWindow.getPersonaData();
 
   return response;
 }
 
 export async function createPersona(password: string, mnemonic?: string): Promise<PersonaData> {
-  const extensionWindow = chrome.extension.getBackgroundPage() as IovWindowExtension;
+  const extensionWindow = (window as Window) as IovWindowExtension;
   const response = await extensionWindow.createPersona(password, mnemonic);
 
   return response;
 }
 
 export async function loadPersona(password: string): Promise<PersonaData> {
-  const extensionWindow = chrome.extension.getBackgroundPage() as IovWindowExtension;
+  const extensionWindow = (window as Window) as IovWindowExtension;
   const response = await extensionWindow.loadPersona(password);
 
   return response;
 }
 
 export async function checkPassword(password: string): Promise<boolean> {
-  const extensionWindow = chrome.extension.getBackgroundPage() as IovWindowExtension;
+  const extensionWindow = (window as Window) as IovWindowExtension;
   const response = await extensionWindow.checkPassword(password);
 
   return response;
 }
 
 export async function createAccount(): Promise<readonly PersonaAcccount[]> {
-  const extensionWindow = chrome.extension.getBackgroundPage() as IovWindowExtension;
+  const extensionWindow = (window as Window) as IovWindowExtension;
   const response = await extensionWindow.createAccount();
 
   return response;
 }
 
 export function getQueuedRequests(): readonly Request[] {
-  const extensionWindow = chrome.extension.getBackgroundPage() as IovWindowExtension;
+  const extensionWindow = (window as Window) as IovWindowExtension;
   const response = extensionWindow.getQueuedRequests();
 
   return response;
 }
 
 export async function getHasStoredPersona(): Promise<boolean> {
-  const extensionWindow = chrome.extension.getBackgroundPage() as IovWindowExtension;
+  const extensionWindow = (window as Window) as IovWindowExtension;
   const response = await extensionWindow.hasStoredPersona();
 
   return response;
 }
 
 export async function clearPersona(): Promise<void> {
-  const extensionWindow = chrome.extension.getBackgroundPage() as IovWindowExtension;
+  const extensionWindow = (window as Window) as IovWindowExtension;
   extensionWindow.clearPersona();
 }
 
 export async function clearDatabase(): Promise<void> {
-  const extensionWindow = chrome.extension.getBackgroundPage() as IovWindowExtension;
+  const extensionWindow = (window as Window) as IovWindowExtension;
   extensionWindow.clearDatabase();
 }
