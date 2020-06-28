d3b9667f9aece5c07a8ba579904afac17e237990
Antoine Herzog <antoine@iov.one>
2020-06-28 17:03:53 +0200

    HARCCODE HACK cosmos connection

--- packages/starname-wallet/src/logic/connection.ts
+++ packages/starname-wallet/src/logic/connection.ts
@@ -1,6 +1,7 @@
 import { BlockchainConnection, ChainId } from "@iov/bcp";
 import { BnsConnection, createBnsConnector } from "@iov/bns";
 import { createEthereumConnector, EthereumConnectionOptions } from "@iov/ethereum";
+import { createCosmosConnector } from "@iov/cosmos-sdk";
 
 import { ChainSpec, CodecType, getConfig } from "../config";
 import { getErc20TokensConfig } from "../utils/tokens";
@@ -25,8 +26,28 @@ async function establishBnsConnection(url: string, chainId: ChainId): Promise<vo
   }
 }
 
+async function establishCosmosConnection(url: string, chainId: ChainId): Promise<void> {
+  const defaultConfig = {
+    bankTokens: [
+      {
+        fractionalDigits: 6,
+        name: "IOV Token",
+        ticker: "IOV",
+        denom: "uiov",
+      },
+    ],
+  };
+
+  const defaultAddressPrefix = "star";
+
+  const connector = createCosmosConnector(url, defaultAddressPrefix, defaultConfig);
+  connections.set(chainId, await connector.establishConnection());
+}
+
 export async function establishConnection(spec: ChainSpec): Promise<void> {
   switch (spec.codecType) {
+    case CodecType.Cosmos:
+      return await establishCosmosConnection(spec.node, spec.chainId as ChainId);
     case CodecType.Bns:
       return await establishBnsConnection(spec.node, spec.chainId as ChainId);
     case CodecType.Ethereum:
