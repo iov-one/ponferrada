890ff4953e866c07e9c29b6ffb2eaef046a0e179
Dave Puchyr <dave@slim.avaritia.com>
2020-06-29 19:41:57 +0200

    Convert from Cosmos to IOV

--- packages/starname-wallet/src/communication/ledger.ts
+++ packages/starname-wallet/src/communication/ledger.ts
@@ -1,42 +1,41 @@
-import { default as CosmosLedgerApp } from "ledger-cosmos-js";
-
-import { signatureImport } from "secp256k1";
-const semver = require("semver");
-import * as crypto from "crypto";
-import * as Ripemd160 from "ripemd160";
 import * as bech32 from "bech32";
+import * as crypto from "crypto";
+import { default as IovLedgerApp } from "ledger-cosmos-js";
+import RIPEMD160 from "ripemd160";
+import { signatureImport } from "secp256k1";
+import semver from "semver";
 
 const INTERACTION_TIMEOUT = 120; // seconds to wait for user action on Ledger, currently is always limited to 60
-const REQUIRED_COSMOS_APP_VERSION = "1.5.3";
+const REQUIRED_IOV_APP_VERSION = "2.16.1";
 
 declare global {
   interface Window {
-    chrome: any;
+    chrome: typeof chrome;
     opr: any;
     google: any;
   }
   interface Navigator {
-    hid: Object;
+    hid: Record<string, any>;
   }
 }
 
 /*
 HD wallet derivation path (BIP44)
-DerivationPath{44, 118, account, 0, index}
+DerivationPath{44, 234, account, 0, index}
 */
-const HDPATH = [44, 118, 0, 0, 0];
-const BECH32PREFIX = `cosmos`;
+const HDPATH = [44, 234, 0, 0, 0];
+const BECH32PREFIX = "star";
 
 export default class Ledger {
-  private readonly testModeAllowed: Boolean;
-  private cosmosApp: any;
+  private readonly testModeAllowed: boolean;
+  private iovApp: any;
   private hdPath: Array<number>;
   private hrp: string;
   public platform: string;
   public userAgent: string;
 
   constructor(
-    { testModeAllowed = false }: { testModeAllowed: Boolean } = { testModeAllowed: false },
+    { testModeAllowed = false }: { testModeAllowed: boolean } = { testModeAllowed: false },
     hdPath: Array<number> = HDPATH,
     hrp: string = BECH32PREFIX,
   ) {
@@ -48,35 +47,35 @@ export default class Ledger {
   }
 
   // quickly test connection and compatibility with the Ledger device throwing away the connection
-  async testDevice() {
+  async testDevice(): Promise<Ledger> {
     // poll device with low timeout to check if the device is connected
     const secondsTimeout = 3; // a lower value always timeouts
     await this.connect(secondsTimeout);
-    this.cosmosApp = null;
+    this.iovApp = null;
 
     return this;
   }
 
   // check if the Ledger device is ready to receive signing requests
-  private async isReady() {
+  private async isReady(): Promise<void> {
     // check if the version is supported
-    const version = await this.getCosmosAppVersion();
+    const version = await this.getIovAppVersion();
 
-    if (!semver.gte(version, REQUIRED_COSMOS_APP_VERSION)) {
-      const msg = `Outdated version: Please update Ledger Cosmos App to the latest version.`;
+    if (!semver.gte(version, REQUIRED_IOV_APP_VERSION)) {
+      const msg = "Outdated version: Please update Ledger IOV App to the latest version.";
       throw new Error(msg);
     }
 
     // throws if not open
-    await this.isCosmosAppOpen();
+    await this.isIovAppOpen();
   }
 
   // connects to the device and checks for compatibility
   // the timeout is the time the user has to react to requests on the Ledger device
   // set a low timeout to only check the connection without preparing the connection for user input
-  async connect(timeout = INTERACTION_TIMEOUT) {
+  async connect(timeout = INTERACTION_TIMEOUT): Promise<Ledger> {
     // assume well connection if connected once
-    if (this.cosmosApp) return this;
+    if (this.iovApp) return this;
 
     // check if browser is supported
     getBrowser(this.userAgent);
@@ -85,7 +84,7 @@ export default class Ledger {
     if (isWindows(this.platform)) {
       if (!navigator.hid) {
         throw new Error(
-          `Your browser doesn't have HID enabled. Please enable this feature by visiting: chrome://flags/#enable-experimental-web-platform-features`,
+          "Your browser doesn't have HID enabled. Please enable this feature by visiting: chrome://flags/#enable-experimental-web-platform-features",
         );
       }
 
@@ -133,8 +132,7 @@ export default class Ledger {
       }
     }
 
-    const cosmosLedgerApp = new CosmosLedgerApp(transport);
-    this.cosmosApp = cosmosLedgerApp;
+    this.iovApp = new IovLedgerApp(transport);
 
     // checks if the Ledger is connected and the app is open
     await this.isReady();
@@ -142,36 +140,36 @@ export default class Ledger {
     return this;
   }
 
-  // returns the cosmos app version as a string like "1.1.0"
-  async getCosmosAppVersion() {
+  // returns the IOV app version as a string like "1.1.0"
+  async getIovAppVersion(): Promise<string> {
     await this.connect();
 
-    const response = await this.cosmosApp.getVersion();
+    const response = await this.iovApp.getVersion();
     this.checkLedgerErrors(response);
-    const { major, minor, patch, test_mode } = response;
-    checkAppMode(this.testModeAllowed, test_mode);
+    const { major, minor, patch, testMode } = response;
+    checkAppMode(this.testModeAllowed, testMode);
     const version = versionString({ major, minor, patch });
 
     return version;
   }
 
-  // checks if the cosmos app is open
+  // checks if the IOV app is open
   // to be used for a nicer UX
-  async isCosmosAppOpen() {
+  async isIovAppOpen(): Promise<void> {
     const appName = await this.getOpenApp();
 
-    if (appName.toLowerCase() === `dashboard`) {
-      throw new Error(`Please open the Cosmos Ledger app on your Ledger device.`);
+    if (appName.toLowerCase() === "dashboard") {
+      throw new Error("Please open the IOV Ledger app on your Ledger device.");
     }
-    if (appName.toLowerCase() !== `cosmos`) {
-      throw new Error(`Please close ${appName} and open the Cosmos Ledger app on your Ledger device.`);
+    if (appName !== "IOV" && appName !== "IOVTEST") {
+      throw new Error(`Please close ${appName} and open the IOV Ledger app on your Ledger device.`);
     }
   }
 
-  async getOpenApp() {
+  async getOpenApp(): Promise<string> {
     await this.connect();
 
-    const response = await this.cosmosApp.appInfo();
+    const response = await this.iovApp.appInfo();
     this.checkLedgerErrors(response);
     const { appName } = response;
 
@@ -179,16 +177,16 @@ export default class Ledger {
   }
 
   // returns the public key from the Ledger device as a Buffer
-  async getPubKey() {
+  async getPubKey(): Promise<Buffer> {
     await this.connect();
 
-    const response = await this.cosmosApp.publicKey(this.hdPath);
+    const response = await this.iovApp.publicKey(this.hdPath);
     this.checkLedgerErrors(response);
     return response.compressed_pk;
   }
 
-  // returns the cosmos address from the Ledger as a string
-  async getCosmosAddress() {
+  // returns the star1 address from the Ledger as a string
+  async getIovAddress(): Promise<string> {
     await this.connect();
 
     const pubKey = await this.getPubKey();
@@ -196,29 +194,29 @@ export default class Ledger {
     return res;
   }
 
-  // triggers a confirmation request of the cosmos address on the Ledger device
-  async confirmLedgerAddress() {
+  // triggers a confirmation request of the IOV address on the Ledger device
+  async confirmLedgerAddress(): Promise<void> {
     await this.connect();
-    const cosmosAppVersion = await this.getCosmosAppVersion();
+    const iovAppVersion = await this.getIovAppVersion();
 
-    if (semver.lt(cosmosAppVersion, REQUIRED_COSMOS_APP_VERSION)) {
-      // we can't check the address on an old cosmos app
+    if (semver.lt(iovAppVersion, REQUIRED_IOV_APP_VERSION)) {
+      // we can't check the address on an old IOV app
       return;
     }
 
-    const response = await this.cosmosApp.showAddressAndPubKey(this.hdPath, this.hrp);
+    const response = await this.iovApp.showAddressAndPubKey(this.hdPath, this.hrp);
     this.checkLedgerErrors(response, {
       rejectionMessage: "Displayed address was rejected",
     });
   }
 
   // create a signature for any message
-  // in Cosmos this should be a serialized StdSignMsg
+  // in IOV this should be a serialized StdSignMsg
   // this is ideally generated by the @lunie/cosmos-js library
-  async sign(signMessage: string) {
+  async sign(signMessage: string): Promise<Uint8Array> {
     await this.connect();
 
-    const response = await this.cosmosApp.sign(this.hdPath, signMessage);
+    const response = await this.iovApp.sign(this.hdPath, signMessage);
     this.checkLedgerErrors(response);
     // we have to parse the signature from Ledger as it's in DER format
     const parsedSignature = signatureImport(response.signature);
@@ -228,54 +226,57 @@ export default class Ledger {
   // parse Ledger errors in a more user friendly format
   /* istanbul ignore next: maps a bunch of errors */
   private checkLedgerErrors(
-    { error_message, device_locked }: { error_message: string; device_locked: Boolean },
+    // eslint-disable-next-line @typescript-eslint/camelcase
+    { error_message, device_locked }: { error_message: string; device_locked: boolean },
     {
-      timeoutMessag = "Connection timed out. Please try again.",
+      timeoutMessage = "Connection timed out. Please try again.",
       rejectionMessage = "User rejected the transaction",
     } = {},
-  ) {
+  ): Error | void {
+    // eslint-disable-next-line @typescript-eslint/camelcase
     if (device_locked) {
-      throw new Error(`Ledger's screensaver mode is on`);
+      throw new Error("Ledger's screensaver mode is on");
     }
+    // eslint-disable-next-line @typescript-eslint/camelcase
     switch (error_message) {
-      case `U2F: Timeout`:
-        throw new Error(timeoutMessag);
-      case `Cosmos app does not seem to be open`:
-        throw new Error(`Cosmos app is not open`);
-      case `Command not allowed`:
-        throw new Error(`Transaction rejected`);
-      case `Transaction rejected`:
+      case "U2F: Timeout":
+        throw new Error(timeoutMessage);
+      case "IOV app does not seem to be open":
+        throw new Error("IOV app is not open");
+      case "Command not allowed":
+        throw new Error("Transaction rejected");
+      case "Transaction rejected":
         throw new Error(rejectionMessage);
-      case `Unknown Status Code: 26628`:
-        throw new Error(`Ledger's screensaver mode is on`);
-      case `Instruction not supported`:
+      case "Unknown Status Code: 26628":
+        throw new Error("Ledger's screensaver mode is on");
+      case "Instruction not supported":
         throw new Error(
-          `Your Cosmos Ledger App is not up to date. ` +
-            `Please update to version ${REQUIRED_COSMOS_APP_VERSION}.`,
+          `Your IOV Ledger App is not up to date. Please update to version ${REQUIRED_IOV_APP_VERSION}.`,
         );
-      case `No errors`:
+      case "No errors":
         // do nothing
         break;
       default:
+        // eslint-disable-next-line @typescript-eslint/camelcase
         throw new Error(`Ledger Native Error: ${error_message}`);
     }
   }
 }
 
 // stiched version string from Ledger app version object
-function versionString({ major, minor, patch }: { major: Number; minor: Number; patch: Number }) {
+function versionString({ major, minor, patch }: { major: number; minor: number; patch: number }): string {
   return `${major}.${minor}.${patch}`;
 }
 
 // wrapper to throw if app is in testmode but it is not allowed to be in testmode
-export const checkAppMode = (testModeAllowed: Boolean, testMode: Boolean) => {
+export const checkAppMode = (testModeAllowed: boolean, testMode: boolean): void => {
   if (testMode && !testModeAllowed) {
-    throw new Error(`DANGER: The Cosmos Ledger app is in test mode and shouldn't be used on mainnet!`);
+    throw new Error("DANGER: The IOV Ledger app is in test mode and shouldn't be used on mainnet!");
   }
 };
 
 // doesn't properly work in ledger-cosmos-js
-function getBech32FromPK(hrp, pk) {
+function getBech32FromPK(hrp: string, pk: Buffer): string {
   if (pk.length !== 33) {
     throw new Error("expected compressed public key [31 bytes]");
   }
@@ -283,23 +284,21 @@ function getBech32FromPK(hrp, pk) {
     .createHash("sha256")
     .update(pk)
     .digest();
-  const hashRip = new Ripemd160().update(hashSha256).digest();
+  const hashRip = new RIPEMD160().update(hashSha256).digest();
   return bech32.encode(hrp, bech32.toWords(hashRip));
 }
 
-function isWindows(platform) {
+function isWindows(platform: string): boolean {
   return platform.indexOf("Win") > -1;
 }
 
-function getBrowser(userAgent) {
+function getBrowser(userAgent: string): string {
   const ua = userAgent.toLowerCase();
   const isChrome = /chrome|crios/.test(ua) && !/edge|opr\//.test(ua);
   const isBrave = isChrome && !window.google;
 
-  if (!isChrome && !isBrave) {
-    throw new Error("Your browser doesn't support Ledger devices.");
-  }
-
   if (isBrave) return "brave";
   if (isChrome) return "chrome";
+
+  throw new Error("Your browser doesn't support Ledger devices.");
 }
