e1f3c4cccb4a72d7c9a3c32747bbc159ed5be494
Dave Puchyr <dave@slim.avaritia.com>
2020-06-30 19:37:18 +0200

    Revert inihilation of CurrencyToSend

--- packages/starname-wallet/src/routes/payment/components/index.tsx
+++ packages/starname-wallet/src/routes/payment/components/index.tsx
@@ -3,7 +3,9 @@ import { Back, Block, Button, Form, useForm } from "medulas-react-components";
 import React from "react";
 import * as ReactRedux from "react-redux";
 
+import { CodecType, ConfigErc20Options, getConfig } from "../../../config";
 import { RootState } from "../../../store/reducers";
+import CurrencyToSend from "./CurrencyToSend";
 import ReceiverAddress from "./ReceiverAddress";
 import TextNote from "./TextNote";
 
@@ -14,20 +16,43 @@ interface Props {
   readonly selectedChainCodec: TxCodec | null;
 }
 
-const Layout = ({ onSubmit, onCancelPayment, selectedChainCodec }: Props): JSX.Element => {
-  const [memoDisabled] = React.useState(false);
+const Layout = ({
+  onSubmit,
+  onCancelPayment,
+  onTokenSelectionChanged,
+  selectedChainCodec,
+}: Props): JSX.Element => {
+  const [memoDisabled, setMemoDisabled] = React.useState(false);
 
   const balances = ReactRedux.useSelector((state: RootState) => state.balances);
   const noBalance = Object.keys(balances).length === 0;
   const { form, handleSubmit, invalid, pristine, submitting } = useForm({
     onSubmit,
   });
+  const onTokenSelectionControl = async (ticker: TokenTicker): Promise<void> => {
+    const chains = (await getConfig()).chains;
+    const ethChain = chains.find(chain => chain.chainSpec.codecType === CodecType.Ethereum);
+    const erc20s = ethChain?.chainSpec?.ethereumOptions?.erc20s;
+    if (erc20s) {
+      const erc20Tokens = erc20s as ConfigErc20Options[];
+      setMemoDisabled(erc20Tokens.some(token => token.symbol.toUpperCase() === ticker.toUpperCase()));
+    }
+    onTokenSelectionChanged(ticker);
+  };
 
   return (
     <Form onSubmit={handleSubmit}>
       <Block marginTop={5} display="flex" justifyContent="center">
         <Block width="450px" display="flex" flexDirection="column" alignItems="center">
           <Block width="450px" bgcolor="white" padding={5}>
+            {false && (
+              <CurrencyToSend
+                form={form}
+                balances={balances}
+                noBalance={noBalance}
+                onTokenSelectionControl={onTokenSelectionControl}
+              />
+            )}
             <ReceiverAddress form={form} noBalance={noBalance} selectedChainCodec={selectedChainCodec} />
             <TextNote form={form} noMemo={noBalance || memoDisabled} />
           </Block>
